06f9a904c72a92c35a297ce6bd4e64b43f277228
James Vodanovich <james@jvodan.com>
2014-08-04 12:20:12 +1000

    Refactoring so Container > ManagedContainer  > ManagedEngine and ManagesService

--- app/controllers/engines_controller.rb
+++ app/controllers/engines_controller.rb
@@ -1,10 +1,11 @@
 require "/opt/mpas/lib/ruby/ManagedContainer.rb"
 require "/opt/mpas/lib/ruby/SysConfig.rb"
+require "/opt/mpas/lib/ruby/ManagedEngine.rb"
 
 class EnginesController < ApplicationController
   
   def index
-    @engines = ManagedContainer.getManagedContainers("container")
+    @engines = ManagedEngine.getManagedEngines()
       if @engines == nil
         @result =false
       else
@@ -13,7 +14,7 @@ class EnginesController < ApplicationController
   end
   
   def show
-    @engine = ManagedContainer.load("container",params[:id])
+    @engine = ManagedEngine.load(params[:id])
       if @engine == nil
         @result =false
       else
@@ -22,37 +23,37 @@ class EnginesController < ApplicationController
   end
   
   def stop
-    @engine = ManagedContainer.load("container",params[:id])
+    @engine = ManagedEngine.load(params[:id])
     @result = @engine.stop_container
     redirect_to engine_path(@engine.containerName)
   end
   
   def start
-    @engine = ManagedContainer.load("container",params[:id])
+    @engine = ManagedEngine.load(params[:id])
     @result =  @engine.start_container
     redirect_to engine_path(@engine.containerName)
   end
   
   def pause
-    @engine = ManagedContainer.load("container",params[:id])
+    @engine = ManagedEngine.load(params[:id])
     @result = @engine.pause_container
     redirect_to engine_path(@engine.containerName)
   end
   
   def unpause
-     @engine = ManagedContainer.load("container",params[:id])
+    @engine = ManagedEngine.load(params[:id])
      @result = @engine.unpause_container
     redirect_to engine_path(@engine.containerName)
    end
    
   def destroy_engine
-    @engine = ManagedContainer.load("container",params[:id])
+    @engine = ManagedEngine.load(params[:id])
     @result =@engine.destroy_container
     redirect_to engine_path(@engine.containerName)
   end 
   
   def deleteimage
-    @engine = ManagedContainer.load("container",params[:id])
+    @engine = ManagedEngine.load(params[:id])
       if (@engine)      
         @result =@engine.delete_image        
         redirect_to engine_path(@engine.containerName)
@@ -60,43 +61,43 @@ class EnginesController < ApplicationController
   end 
   
   def restart
-    @engine = ManagedContainer.load("container",params[:id])
+    @engine = ManagedEngine.load(params[:id])
     @result = @engine.restart_container
     redirect_to engine_path(@engine.containerName)
   end
   
   def create_engine
-    @engine = ManagedContainer.load("container",params[:id])
+    @engine = ManagedEngine.load(params[:id])
         @result = @engine.create_container
         redirect_to engine_path(@engine.containerName)
   end
 
   def monitor
-    @engine = ManagedContainer.load("container",params[:id])
+    @engine = ManagedEngine.load(params[:id])
             @result = @engine.monitor_site
             redirect_to engine_path(@engine.containerName)
   end
   
   def demonitor
-    @engine = ManagedContainer.load("container",params[:id])
+    @engine = ManagedEngine.load(params[:id])
             @result = @engine.demonitor_site
             redirect_to engine_path(@engine.containerName)
   end
   
   def register_site
-    @engine = ManagedContainer.load("container",params[:id])
+       @engine = ManagedEngine.load(params[:id])
                 @result = @engine.register_site
                 redirect_to engine_path(@engine.containerName)
   end
   
   def deregister_site
-    @engine = ManagedContainer.load("container",params[:id])
+       @engine = ManagedEngine.load(params[:id])
                 @result = @engine.deregister_site
                 redirect_to engine_path(@engine.containerName)
   end
   
   def edit
-    @engine = ManagedContainer.load("container",params[:id])
+       @engine = ManagedEngine.load(params[:id])
       #only on nocontainer but with image 
       #will trigger a create
   end
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,4 +1,19 @@
+
 class ServicesController < ApplicationController
   def index
+    @services = ManagedService.getManagedServices()
+           if @services == nil
+             @result =false
+           else
+             @result = true
+           end
   end
+  def show
+      @service = ManagedService.load(params[:id])
+        if @service == nil
+          @result =false
+        else
+          @result =true
+        end
+    end
 end
