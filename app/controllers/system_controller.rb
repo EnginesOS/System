e4c9ebbc89cd21b4cf51c703876579ff46cbc460
James Vodanovich <james@jvodan.com>
2014-08-05 14:08:26 +1000

    removed debugibg

--- app/controllers/system_controller.rb
+++ app/controllers/system_controller.rb
@@ -1,7 +1,7 @@
 class SystemController < ApplicationController
   def index
     @snapshop = Vmstat.snapshot
-    sleep(1)
+    sleep(1) #FIXME this is a kludge need to use time stamps and do a wait for or something better then just sleep?
     @vm2 = Vmstat.memory
   end
 end
