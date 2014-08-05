f140e2b50634ab657186261414502009ed1f0627
James Vodanovich <james@jvodan.com>
2014-08-06 09:42:56 +1000

    added nagios service

--- app/controllers/engines_controller.rb
+++ app/controllers/engines_controller.rb
@@ -3,6 +3,7 @@ require "/opt/mpas/lib/ruby/SysConfig.rb"
 require "/opt/mpas/lib/ruby/ManagedEngine.rb"
 require "/opt/mpas/lib/ruby/ManagedService.rb"
 require "/opt/mpas/lib/ruby/NginxService.rb"
+require "/opt/mpas/lib/ruby/NagioService.rb"
 
 class EnginesController < ApplicationController
   
