bd5f6efaa087d7bcb9cf4d4ae0cacc28ab8208e8
James Vodanovich <james@jvodan.com>
2014-08-08 10:01:18 +1000

    welcome page routes

--- app/controllers/system_controller.rb
+++ app/controllers/system_controller.rb
@@ -6,4 +6,6 @@ class SystemController < ApplicationController
     sleep(1) #FIXME this is a kludge need to use time stamps and do a wait for or something better then just sleep?
     @vm2 = Vmstat.memory
   end
+  
+  
 end
