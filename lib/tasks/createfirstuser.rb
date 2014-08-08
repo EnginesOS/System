406f862d4f3aa32b77d7a8152e5897574c11a0e5
James Vodanovich <james@jvodan.com>
2014-08-08 14:31:05 +1000

    email to login

--- lib/tasks/createfirstuser.rb
+++ lib/tasks/createfirstuser.rb
@@ -1,9 +1,9 @@
 namespace :db do
   desc "Create default admin"
   task populate: :environment do
-    admin = User.create!(name: "Test User",
-                 email: "admin",
-                 password: "test1967",
+    admin = User.create!(name: "Admin User",
+                 login: "admin",
+                 password: "EngOS",
                  password_confirmation: "test1967")
     admin.toggle!(:admin)
   end
