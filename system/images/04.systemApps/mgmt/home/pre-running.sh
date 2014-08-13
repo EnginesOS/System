3339a078106f5ed79866303fdf9194cbea6dba11
James Vodanovich <james@jvodan.com>
2014-08-14 09:48:22 +1000

    tidy source

--- system/images/04.systemApps/mgmt/home/pre-running.sh
+++ system/images/04.systemApps/mgmt/home/pre-running.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-cd /home/app
+
+
 gem  install rake
+
 gem install vmstat
+
 /etc/init.d/ssh start
 
-#git pull
-#bundle install
 if test -z $ContUser
         then
                 $ContUser=www-data
 fi
 
-chown  -R   $ContUser .
+
+chown  -R   $ContUser /home/app
+
 
