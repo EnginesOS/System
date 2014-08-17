7b1a9607f5e12ffb04ced4305f40d825a3b236d2
James Vodanovich <james@jvodan.com>
2014-08-17 13:52:21 +1000

    pass container as argument

--- lib/Docker.rb
+++ lib/Docker.rb
@@ -19,38 +19,38 @@ class Docker
       return ret_val
    end
    
-  def delete_image
-           commandargs= " rmi " +   @image
+  def delete_image container
+           commandargs= " rmi " +   container.image
            return  run_docker(commandargs)                
    end
    
-   def destroy_container
-     commandargs= " rm " +   @containerName
+   def destroy_container container
+     commandargs= " rm " +   container.containerName
      
      return  run_docker(commandargs)      
    end
    
-  def create_container              
+  def create_container container             
      e_option =String.new
       volume_option = String.new 
       eportoption = String.new
-       if(@environments)
-          @environments.each do |environment|
+       if(container.environments)
+         container.environments.each do |environment|
             if environment != nil                                                       
                    e_option = e_option + " -e " + environment.name + "=" + environment.value
             end
          end
        end
-       if(@volumes)
-         @volumes.each do |volume|
+       if(container.volumes)
+         container.volumes.each do |volume|
              if volume.name !=nil
                volume_option = volume_option + " -v " + volume.localpath + "/"  + volume.name + ":" + volume.remotepath + "/" + volume.name
              end
          end
        end
        
-       if(@eports )
-           @eports.each do |eport|
+       if(container.eports )
+         container.eports.each do |eport|
              eportoption = eportoption +  " -p " + eport.port.to_s
                if eport.external >0
                  eportoption = eportoption + ":" + eport.external.to_s + " "
@@ -59,54 +59,54 @@ class Docker
        end
       
  
-      commandargs =  " run -h " + @hostName + e_option + " --memory=" + @memory.to_s + "m " + volume_option + eportoption + " --cidfile " + SysConfig.CidDir + "/" + @containerName + ".cid --name " + @containerName + " -d  -t " + @image       + " /bin/bash /home/init.sh"                 
+      commandargs =  " run -h " + container.hostName + e_option + " --memory=" + container.memory.to_s + "m " + volume_option + eportoption + " --cidfile " + SysConfig.CidDir + "/" + container.containerName + ".cid --name " + container.containerName + " -d  -t " + container.image       + " /bin/bash /home/init.sh"                 
      puts commandargs
-     cidfile = SysConfig.CidDir + "/"  + @containerName + ".cid"
+     cidfile = SysConfig.CidDir + "/"  + container.containerName + ".cid"
      if File.exists? cidfile
        File.delete cidfile
      end
      return  run_docker(commandargs)
    end
    
-   def start_container        
-     commandargs =" start " + @containerName
+   def start_container   container      
+     commandargs =" start " + container.containerName
  
      return  run_docker(commandargs)
      
    end
    
-   def stop_container
-     commandargs=" stop " + @containerName
+   def stop_container container
+     commandargs=" stop " + container.containerName
 
      return  run_docker(commandargs)
    end
    
-   def pause_container
-     commandargs = " pause " + @containerName
+   def pause_container container
+     commandargs = " pause " + container.containerName
 
      return  run_docker(commandargs)    
    end
    
-   def unpause_container
-      commandargs=" unpause " + @containerName
+   def unpause_container container
+      commandargs=" unpause " + container.containerName
 
      return  run_docker(commandargs)      
     end
     
-   def ps_container
-     commandargs=" top " + @containerName + " axl"
+   def ps_container container 
+     commandargs=" top " + container.containerName + " axl"
 
      return  run_docker(commandargs)   
    end
    
-   def logs_container
-       commandargs=" logs " + @containerName
+   def logs_container container 
+       commandargs=" logs " + container.containerName
 
        return  run_docker(commandargs)
      end
   
-    def inspect_container
-       commandargs=" inspect " + @containerName
+    def inspect_container container
+       commandargs=" inspect " + container.containerName
 
       return  run_docker(commandargs)
     end
