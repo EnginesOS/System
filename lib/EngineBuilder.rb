5a382fd019df5bd2b96bfff8d164683dfe198720
James Vodanovich <james@jvodan.com>
2014-08-23 14:31:01 +1000

    change init script name removed -i option in docker cmd

--- lib/EngineBuilder.rb
+++ lib/EngineBuilder.rb
@@ -323,7 +323,7 @@ class EngineBuilder
       puts cmd
        res= %x<#{cmd}>
        
-       cmd = "cd " + SysConfig.DeploymentDir + "/" +  @buildname + "; docker run -i  -v /opt/dl_cache/:/opt/dl_cache/ --name setup -t " + @hostName +  "/init /bin/bash /home/presetup.sh "
+       cmd = "cd " + SysConfig.DeploymentDir + "/" +  @buildname + "; docker run   -v /opt/dl_cache/:/opt/dl_cache/ --name setup -t " + @hostName +  "/init /bin/bash /home/presetup.sh "
          puts cmd
        res= %x<#{cmd}>
               if $? == false
@@ -342,7 +342,7 @@ class EngineBuilder
           @vols.each do |vol|
             volumes = volumes + " -v " + vol.localpath + "/" + vol.name + ":" + vol.remotepath + "/" + vol.name
           end
-       cmd= "cd " + SysConfig.DeploymentDir + "/" +  @buildname + "; docker run -i  --name deploy " + volumes + " -t " +   @hostName + "/setup /bin/bash /home/_init.sh " # su -s /bin/bash www-data /home/configcontainer.sh"
+       cmd= "cd " + SysConfig.DeploymentDir + "/" +  @buildname + "; docker run  --name deploy " + volumes + " -t " +   @hostName + "/setup /bin/bash /home/c_init.sh " # su -s /bin/bash www-data /home/configcontainer.sh"
          puts(cmd) 
        res= %x<#{cmd}>
        puts res
