74a0d07094ab920fd1e9062da1e24bdde30556f4
James Vodanovich <james@jvodan.com>
2014-09-04 09:01:20 +1000

    correct syntax

--- system/images/03.serviceImages/mysql/home/firstrun.sh
+++ system/images/03.serviceImages/mysql/home/firstrun.sh
@@ -7,8 +7,8 @@ pass="pass"
 	mv /etc/mysql/my.cnf.sed /etc/mysql/my.cnf
 	/etc/init.d/mysql start
 	echo "CREATE USER 'rma'@'localhost';  grant all ON *.* TO  'rma'@'localhost'  WITH GRANT OPTION; " |mysql -u root
-	echo "CREATE USER 'root'@'localhost' identified by '$pass';  grant all ON *.* TO  'root'@'%'  WITH GRANT OPTION; "
-	echo "CREATE USER 'root'@'localhost' identified by '$pass';  grant all ON *.* TO  'root'@'%'  WITH GRANT OPTION; " |mysql -u root
-	 /usr/bin/mysqladmin -u root -h password '$pass'
+	echo "CREATE USER 'root'@'%' identified by '$pass';  grant all ON *.* TO  'root'@'%'  WITH GRANT OPTION; "
+	echo "CREATE USER 'root'@'%' identified by '$pass';  grant all ON *.* TO  'root'@'%'  WITH GRANT OPTION; " |mysql -u root
+	 /usr/bin/mysqladmin -u root  password '$pass'
  fi
 	 
