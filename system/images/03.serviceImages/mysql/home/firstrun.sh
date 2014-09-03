45c5afca0e7a2932437d56475ef3e8d02c321d00
James Vodanovich <james@jvodan.com>
2014-09-04 08:20:48 +1000

    start mysql_server prior to attempting to create user

--- system/images/03.serviceImages/mysql/home/firstrun.sh
+++ system/images/03.serviceImages/mysql/home/firstrun.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 /usr/bin/mysql_install_db
+cat /etc/mysql/my.cnf |sed "/127.0.0.1/s//0.0.0.0/" >/etc/mysql/my.cnf.sed
+mv /etc/mysql/my.cnf.sed /etc/mysql/my.cnf
+/etc/init.d/mysql start
 #echo "CREATE USER 'rma'@'localhost';  grant SELECT,INSERT,UPdate,create,SHOW DATABASES,CREATE USER ON *.* TO  'rma'@'localhost'  WITH GRANT OPTION; " |mysql -u root
 echo "CREATE USER 'rma'@'localhost';  grant all ON *.* TO  'rma'@'localhost'  WITH GRANT OPTION; " |mysql -u root
