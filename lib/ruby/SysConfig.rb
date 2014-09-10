6bd5da626111974e53469648be03b2f982f9dc43
James Vodanovich <james@jvodan.com>
2014-09-10 15:31:19 +1000

    move internal domain from being docker to engines.local

--- lib/ruby/SysConfig.rb
+++ lib/ruby/SysConfig.rb
@@ -1,12 +1,12 @@
 
 class SysConfig
 
-  @@addSiteCmd="ssh -i   /opt/engos/etc/keys/nginx -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"   rma@nginx.docker sudo sh /home/addsite.sh"
-  @@rmSiteCmd="ssh -i  /opt/engos/etc/keys/nginx -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"   rma@nginx.docker sudo sh /home/rmsite.sh"
-  @@addSiteMonitorCmd="ssh -i  /opt/engos/etc/keys/nagios  -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"   rma@monit.docker sudo sh /home/addsite.sh"
-  @@rmSiteMonitorCmd="ssh -i  /opt/engos/etc/keys/nagios   -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"   rma@monit.docker sudo sh /home/rmsite.sh"               
-  @@addDBServiceCmd="ssh  -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"  -i /opt/engos/etc/keys/mysql rma@mysql.docker  /home/createdb.sh "
-  @@DBHost="mysql.docker"
+  @@addSiteCmd="ssh -i   /opt/engos/etc/keys/nginx -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"   rma@nginx.engines.local sudo sh /home/addsite.sh"
+  @@rmSiteCmd="ssh -i  /opt/engos/etc/keys/nginx -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"   rma@nginx.engines.local sudo sh /home/rmsite.sh"
+  @@addSiteMonitorCmd="ssh -i  /opt/engos/etc/keys/nagios  -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"   rma@monit.engines.local sudo sh /home/addsite.sh"
+  @@rmSiteMonitorCmd="ssh -i  /opt/engos/etc/keys/nagios   -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"   rma@monit.engines.local sudo sh /home/rmsite.sh"               
+  @@addDBServiceCmd="ssh  -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"  -i /opt/engos/etc/keys/mysql rma@mysql.engines.local  /home/createdb.sh "
+  @@DBHost="mysql.engines.local"
   @@CidDir="/opt/engos/run"
   @@ContainersDir="/opt/engos/run/containers/"
   @@DeploymentDir="/home/dockuser/droplets/deployment/deployed"
@@ -15,7 +15,7 @@ class SysConfig
   @@LocalFSVolHome = "/var/engos/fs"
   @@galleriesDir = "/opt/engos/etc/galleries"
   @@ddnsKey = "/opt/engos/etc/keys/ddns.key"
-  @@internalDomain = "docker"
+  @@internalDomain = "engines.local"
   @@defaultDNS ="172.17.42.1"
   
   def SysConfig.defaultDNS
