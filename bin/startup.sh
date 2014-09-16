93f7eed920122acf79c16eac00b28bc7b1fd2bab
James Vodanovich <james@jvodan.com>
2014-09-16 11:29:56 +1000

    correct order and mark executable

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-/opt/engos/bin/engines.rb service dns start
+/opt/engos/bin/engines.rb service start dns 
 sleep 10
-/opt/engos/bin/engines.rb service nginx start
+/opt/engos/bin/engines.rb service start nginx 
 sleep 10
 /opt/engos/bin/engines.rb service check_and_act all
 sleep 10
