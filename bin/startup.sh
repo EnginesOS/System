c16aceabbedd951745bc275f9654a6c00707468b
James Vodanovich <james@jvodan.com>
2014-09-16 11:23:32 +1000

    typo

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-/opt/engsos/bin/engines.rb service dns start
+/opt/engos/bin/engines.rb service dns start
 sleep 10
-/opt/engsos/bin/engines.rb service nginx start
+/opt/engos/bin/engines.rb service nginx start
 sleep 10
-/opt/engsos/bin/engines.rb service check_and_act all
+/opt/engos/bin/engines.rb service check_and_act all
 sleep 10
-/opt/engsos/bin/engines.rb engine check_and_act all
+/opt/engos/bin/engines.rb engine check_and_act all
