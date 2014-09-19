e8cb89af9a567ecc329678e1e4705af218bc5b5e
James Vodanovich <james@jvodan.com>
2014-09-20 09:50:45 +1000

    All services now present a common 'interface'

--- lib/ruby/NagiosService.rb
+++ lib/ruby/NagiosService.rb
@@ -4,40 +4,11 @@ require "/opt/engos/lib/ruby/ManagedService.rb"
 
 class NagiosService < ManagedService
   
-  def add_consumer(engine)
-    site_string=engine.containerName + ":" + engine.fqdn + ":" + engine.port.to_s    
-    result = @docker_api.add_monitor(site_string)
-     if result != true
-       return result
+  def add_consumer_to_service(site_string)
+      return  @docker_api.add_monitor(site_string)
      end
+  def rm_consumer_from_service (site_string)
+       return  @docker_api.rm_monitor(site_string)
+    end 
  
-      if @consumers == nil
-        @consumers = Array.new
-      end
-      
-      if @consumers.include?(site_string) == false     # only add if doesnt exists but allow register
-         @consumers.push(site_string)
-      end
-    save_state
-     return result
-   end
-   
-   def remove_consumer engine
-     site_string=engine.containerName + ":" + engine.fqdn + ":" + engine.port.to_s   
-     result = @docker_api.rm_monitor(site_string)
-    
-     if(@consumers !=  nil || @consumes.length>0)
-            @consumers.delete(site_string)
-         end    
-     save_state
-     return result
-   end
-   
-     
-  def reregister_consumers
-     
-     @consumers.each do |site|
-       @docker_api.add_monitor(engine)    
-     end
-  end
 end 
