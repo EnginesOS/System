f10836e5c7abef22589f2d1ee768654fea59bd87
James Vodanovich <james@jvodan.com>
2014-09-19 10:36:09 +1000

    formatting

--- system/images/04.systemApps/mgmt/home/app/Procfile.sh
+++ system/images/04.systemApps/mgmt/home/app/Procfile.sh
@@ -2,15 +2,11 @@
 
 
 cd /home/app
- ls -la
-echo +++++++++++
-whoami
 
 git pull
+
 bundle install --path vendor/bundle
 bundle exec rake db:migrate
 bundle exec rake db:populate
-
-
 
 bundle exec thin -p 8000 start
