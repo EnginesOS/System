f88acaf9a4179ff38d621cdb8a5375ae2acaba59
James Vodanovich <james@jvodan.com>
2014-09-20 11:42:55 +1000

    not doen here at th  moment

--- lib/ruby/DBManagedService.rb
+++ lib/ruby/DBManagedService.rb
@@ -1,15 +1,17 @@
 require "/opt/engos/lib/ruby/ManagedContainer.rb"
 require "/opt/engos/lib/ruby/DatabaseService.rb"
 class DBManagedService < ManagedService
-  
+
+    
+#overloaded for the moment
   def add_consumer_to_service(site_string)
-      return  @docker_api.add_monitor(site_string)
+      return true
      end
   def rm_consumer_from_service (site_string)
-       return  @docker_api.rm_monitor(site_string)
+       return  true
     end 
     
-#overload
+
   def get_site_string(database)
     return database.flavor + ":" + database.dbUser + ":" + database.dbPass + "@" + database.dbHost + "/" + database.name
   end
