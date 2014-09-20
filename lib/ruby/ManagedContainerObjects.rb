53ad4a55a0b6d4d97d089c7d5ec5f92ac195e6b6
James Vodanovich <james@jvodan.com>
2014-09-20 11:38:09 +1000

    missed filename change

--- lib/ruby/ManagedContainerObjects.rb
+++ lib/ruby/ManagedContainerObjects.rb
@@ -1,7 +1,7 @@
 require_relative 'WorkPort.rb'
 require_relative 'EnvironmentVariable.rb'
 require_relative 'VolumeService.rb'
-require_relative 'Service.rb'
+require_relative 'StaticService.rb'
 require_relative 'DatabaseService.rb'
 
 
