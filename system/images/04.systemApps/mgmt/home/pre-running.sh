7e83089b429a12c0d047a8fbd4b9aaf65e2160d2
James Vodanovich <james@jvodan.com>
2014-09-21 11:53:33 +1000

    improve ruby en

--- system/images/04.systemApps/mgmt/home/pre-running.sh
+++ system/images/04.systemApps/mgmt/home/pre-running.sh
@@ -8,8 +8,9 @@ rvm gemset create git
 su -l $ContUser /opt/engos/bin/containers_startup.sh 
 
 gem  install rake
-
+rvm gemset create rake
 gem install vmstat
+rvm gemset create  vmstat
 
 /etc/init.d/ssh start
 
