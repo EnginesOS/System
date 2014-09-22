3fcd6823c4660d1a8352d2e95f859fb977b44e77
James Vodanovich <james@jvodan.com>
2014-09-22 11:26:51 +1000

    absolute dir now included in localpath

--- lib/ruby/EngineBuilder.rb
+++ lib/ruby/EngineBuilder.rb
@@ -151,8 +151,8 @@ class EngineBuilder
      end
      
      def create_file_service vol
-       if Dir.exists?(SysConfig.LocalFSVolHome + "/" + vol.localpath ) ==false
-                Dir.mkdir(SysConfig.LocalFSVolHome + "/" + vol.localpath)
+       if Dir.exists?( vol.localpath ) ==false
+                Dir.mkdir(vol.localpath)
               end
      end
  
