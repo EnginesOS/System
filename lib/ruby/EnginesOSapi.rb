9968ea7119f0e7a2652eddf2a0f08638ab3ede3a
James Vodanovich <james@jvodan.com>
2014-09-23 17:18:26 +1000

    removed debug

--- lib/ruby/EnginesOSapi.rb
+++ lib/ruby/EnginesOSapi.rb
@@ -39,12 +39,10 @@ class EnginesOSapi
         managed_engine = loadManagedEngine(contdir)
         if managed_engine  != false
           ret_val.push(managed_engine)
-          puts "pushed " + managed_engine.to_s
         else
           puts "failed to load " + yfn 
           p     managed_engine     
         end
-        return 
       end
     end
     return ret_val
