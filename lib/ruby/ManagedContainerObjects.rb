6a6a0336549866b4ada2a507c7371b980c76ae0e
James Vodanovich <james@jvodan.com>
2014-09-24 08:45:29 +1000

    fixed nme chnge

--- lib/ruby/ManagedContainerObjects.rb
+++ lib/ruby/ManagedContainerObjects.rb
@@ -1,6 +1,6 @@
 require_relative 'WorkPort.rb'
 require_relative 'EnvironmentVariable.rb'
-require_relative 'VolumeService.rb'
+require_relative 'Volume.rb'
 require_relative 'StaticService.rb'
 require_relative 'DatabaseService.rb'
 
