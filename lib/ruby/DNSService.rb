451ec515e6c3d4e000a5361af3a89574fe66f2d8
James Vodanovich <james@jvodan.com>
2014-09-25 11:58:03 +1000

    string so no to throw error

--- lib/ruby/DNSService.rb
+++ lib/ruby/DNSService.rb
@@ -4,7 +4,7 @@ class DNSService < ManagedService
   
   def get_site_string(engine)
    
-    return engine.hostName + ":" + engine.fqdn + ":" +     engine.get_ip_str 
+    return engine.hostName + ":" + engine.fqdn + ":" +     engine.get_ip_str.to_s
   end
   
   def add_consumer_to_service(site_string)
