942432bb7c3ae2f43ce67135ba98e8318148b14e
James Vodanovich <james@jvodan.com>
2014-09-27 00:30:53 +1000

    correct params

--- system/images/04.systemApps/mgmt/home/app/Procfile.sh
+++ system/images/04.systemApps/mgmt/home/app/Procfile.sh
@@ -13,4 +13,4 @@ bundle install --path vendor/bundle
 bundle exec rake db:migrate
 bundle exec rake db:populate
 
-bundle exec thin -p 8000   --ssl --ssl-key-file /opt/engos/etc/ssl/certs/engines.key --ssl-cert-file /opt/engos/etc/ssl/keys/engines.crt start
+bundle exec thin -p 8000   --ssl --ssl-key-file /opt/engos/etc/ssl/keys/engines.key --ssl-cert-file /opt/engos/etc/ssl/certs/engines.crt start
