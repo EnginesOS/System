a7f9d0c56f617e8a728be89655e321c40e4ef8d3
James Vodanovich <james@jvodan.com>
2014-09-26 19:50:53 +1000

    admin is now ssl

--- system/images/04.systemApps/mgmt/home/app/Procfile.sh
+++ system/images/04.systemApps/mgmt/home/app/Procfile.sh
@@ -13,4 +13,4 @@ bundle install --path vendor/bundle
 bundle exec rake db:migrate
 bundle exec rake db:populate
 
-bundle exec thin -p 8000 start
+bundle exec thin -p 8000  start --ssl --ssl-verify --ssl-key-file /opt/engos/etc/ssl/certs/engines.key --ssl-cert-file /opt/engos/etc/ssl/keys/engines.crt
