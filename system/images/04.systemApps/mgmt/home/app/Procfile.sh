a503ec80b48fadcae49961f46329286b93a28962
James Vodanovich <james@jvodan.com>
2014-09-28 09:37:28 +1000

    no ssl if no cert ideal for install situation

--- system/images/04.systemApps/mgmt/home/app/Procfile.sh
+++ system/images/04.systemApps/mgmt/home/app/Procfile.sh
@@ -13,4 +13,9 @@ bundle install --path vendor/bundle
 bundle exec rake db:migrate
 bundle exec rake db:populate
 
-bundle exec thin -p 8000   --ssl --ssl-key-file /opt/engos/etc/ssl/keys/engines.key --ssl-cert-file /opt/engos/etc/ssl/certs/engines.crt start
+	if test -f /opt/engos/etc/ssl/keys/engines.key -a test -f /opt/engos/etc/ssl/certs/engines.crt 
+	then
+		bundle exec thin -p 8000   --ssl --ssl-key-file /opt/engos/etc/ssl/keys/engines.key --ssl-cert-file /opt/engos/etc/ssl/certs/engines.crt start
+	else
+		bundle exec thin -p 8000   
+	fi
