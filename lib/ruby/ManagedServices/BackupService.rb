f27972850755911236bdcad220bda36149d76aa0
James Vodanovich <james@jvodan.com>
2014-09-28 09:36:34 +1000

    site_has is supplied and nit creted unlike other services

--- lib/ruby/ManagedServices/BackupService.rb
+++ lib/ruby/ManagedServices/BackupService.rb
@@ -4,25 +4,29 @@ class BackupService < ManagedService
   
   
   def add_consumer_to_service(site_hash)
-    return  @docker_api.create_database(site_hash) 
+    return  @docker_api.create_backup(site_hash) 
      end
      
   def rm_consumer_from_service (site_hash)
        return  true
     end 
     
-  def get_site_hash(object)
-    #object is nil Volume | Database
-    site_hash = Hash.new()
-     
-    site_hash[:name]=engine.containerName
-    site_hash[:source_type]=engine.ctype #fs|db
-    site_hash[:dest_proto]=engine.hostName
-    site_hash[:dest_port]=engine.hostName
-    site_hash[:dest_address]=engine.get_ip_str.to_s
-    site_hash[:dest_folder]=engine.get_ip_str.to_s
-    site_hash[:dest_user]=engine.get_ip_str.to_s
-    site_hash[:dest_pass]=engine.get_ip_str.to_s
+  def get_site_hash(site_hash)
+ # called with a hash no need to do anything accept return with arg1
+#     
+#    site_hash[:name]
+#    site_hash[:source_type] #vol|mysql|pgsql|nosql|sys
+#    site_hash[:source_name]
+#    site_hash[:source_host]
+#    site_hash[:source_user]
+#    site_hash[:source_pass]
+#      
+#    site_hash[:dest_proto]
+#    site_hash[:dest_port]
+#    site_hash[:dest_address]
+#    site_hash[:dest_folder]
+#    site_hash[:dest_user]
+#    site_hash[:dest_pass]
              
     return site_hash       
    end
