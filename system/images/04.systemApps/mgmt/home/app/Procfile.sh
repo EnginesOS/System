9ec973570d6882a50b01c5448c4f8972065da731
James Vodanovich <james@jvodan.com>
2014-09-28 10:02:55 +1000

    duh

--- system/images/04.systemApps/mgmt/home/app/Procfile.sh
+++ system/images/04.systemApps/mgmt/home/app/Procfile.sh
@@ -13,7 +13,7 @@ bundle install --path vendor/bundle
 bundle exec rake db:migrate
 bundle exec rake db:populate
 
-	if test -f /opt/engos/etc/ssl/keys/engines.key -a test -f /opt/engos/etc/ssl/certs/engines.crt 
+	if test -f /opt/engos/etc/ssl/keys/engines.key -a  -f /opt/engos/etc/ssl/certs/engines.crt 
 	then
 		bundle exec thin -p 8000   --ssl --ssl-key-file /opt/engos/etc/ssl/keys/engines.key --ssl-cert-file /opt/engos/etc/ssl/certs/engines.crt start
 	else
