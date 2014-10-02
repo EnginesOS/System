4debfae49f05e4f0c33ec483036dbd8e69876562
James Vodanovich <james@jvodan.com>
2014-10-02 10:47:10 +1000

    logging unfied to single fs

--- installers/install_enginesOS_fromScratch.sh
+++ installers/install_enginesOS_fromScratch.sh
@@ -238,11 +238,13 @@ set_os_flavor
 setup_mgmt_git
 
 mkdir -p  /var/lib/engos/backup_paths
-mkdir -p /var/lib/engos/fs
-mkdir -p /home/dockuser/droplets/deployment/deployed/
-mkdir -p /var/lib/engos/pgsql
-mkdir -p  /var/log/engos/services/nginx
+mkdir -p  /var/lib/engos/fs
+mkdir -p  /home/dockuser/droplets/deployment/deployed/
+mkdir -p  /var/lib/engos/pgsql
+mkdir -p  /var/log/engos/services/nginx/nginx
 mkdir -p  /var/log/engos/services/backup
+mkdir -p  /var/log/engos/services/mgmt
+
 
 set_permissions
 
