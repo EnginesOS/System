8df2427817d984e1ea3688c2a83424131449f749
James Vodanovich <james@jvodan.com>
2014-10-03 12:58:52 +1000

    correct postgresql version number

--- system/images/03.serviceImages/pgsql/home/firstrun.sh
+++ system/images/03.serviceImages/pgsql/home/firstrun.sh
@@ -3,8 +3,8 @@ pass="pass"
  if ! test -f /var/lib/postgresql
  then
  echo "ALTER ROLE postgres WITH ENCRYPTED PASSWORD 'pass'; " |su -l postgres psql
- echo listen_addresses = '*' >> /etc/postgresql/8.1/postgresql.conf
- echo "host all all 172.17.42.0/16  md5" >> /etc/postgresql/8.1/main/pg_hba.conf
+ echo listen_addresses = '*' >> /etc/postgresql/9.1/postgresql.conf
+ echo "host all all 172.17.42.0/16  md5" >> /etc/postgresql/9.1/main/pg_hba.conf
  
 	
  fi
