0ae2652308b3af7da262b0103819531028573ef3
James Vodanovich <james@jvodan.com>
2014-10-08 19:36:59 +1100

    correct version

--- system/images/03.serviceImages/pgsql/home/firstrun.sh
+++ system/images/03.serviceImages/pgsql/home/firstrun.sh
@@ -3,8 +3,8 @@ pass="pass"
  if ! test -f /var/lib/postgresql/conf
  then
  echo "ALTER ROLE postgres WITH ENCRYPTED PASSWORD 'pass'; "| su -l postgres  /usr/bin/perl   /usr/bin/psql -l
- echo listen_addresses = '*' >> /etc/postgresql/9.1/postgresql.conf
- echo "host all all 172.17.42.0/16  md5" >> /etc/postgresql/9.1/main/pg_hba.conf
+ echo listen_addresses = '*' >> /etc/postgresql/9.3/postgresql.conf
+ echo "host all all 172.17.42.0/16  md5" >> /etc/postgresql/9.3/main/pg_hba.conf
  touch /var/lib/postgresql/conf
 	
  fi
