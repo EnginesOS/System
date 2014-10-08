63a9dc6643b64919014ae2bafa27c28ab302c9dd
James Vodanovich <james@jvodan.com>
2014-10-08 19:27:25 +1100

    correct version

--- system/images/03.serviceImages/pgsql/home/pre-running.sh
+++ system/images/03.serviceImages/pgsql/home/pre-running.sh
@@ -3,7 +3,7 @@
 chown -R postgres /var/lib/postgresql
 
 
- echo listen_addresses = '*' >> /etc/postgresql/9.1/postgresql.conf
- echo "host all all 172.17.42.0/16  md5" >> /etc/postgresql/9.1/main/pg_hba.conf
+ echo listen_addresses = '*' >> /etc/postgresql/9.3/postgresql.conf
+ echo "host all all 172.17.42.0/16  md5" >> /etc/postgresql/9.3/main/pg_hba.conf
  
  service postgresql restart
