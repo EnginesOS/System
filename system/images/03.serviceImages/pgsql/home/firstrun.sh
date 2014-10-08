08d88ab773f7990b4976f6face49f897a7e2bb40
James Vodanovich <james@jvodan.com>
2014-10-08 19:41:49 +1100

    no need for -l

--- system/images/03.serviceImages/pgsql/home/firstrun.sh
+++ system/images/03.serviceImages/pgsql/home/firstrun.sh
@@ -2,7 +2,7 @@
 pass="pass"
  if ! test -f /var/lib/postgresql/conf
  then
- echo "ALTER ROLE postgres WITH ENCRYPTED PASSWORD 'pass'; "| su -l postgres  /usr/bin/perl   /usr/bin/psql -l
+ echo "ALTER ROLE postgres WITH ENCRYPTED PASSWORD 'pass'; "| su  postgres  /usr/bin/perl   /usr/bin/psql -l
  echo listen_addresses = '*' >> /etc/postgresql/9.3/postgresql.conf
  echo "host all all 172.17.42.0/16  md5" >> /etc/postgresql/9.3/main/pg_hba.conf
  touch /var/lib/postgresql/conf
