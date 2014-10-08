d2d17d0e7970eb09da2a71013944b94e271532cf
James Vodanovich <james@jvodan.com>
2014-10-08 20:31:40 +1100

    first working

--- system/images/03.serviceImages/pgsql/home/firstrun.sh
+++ system/images/03.serviceImages/pgsql/home/firstrun.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 pass="pass"
+	echo "listen_addresses = '*'" >> /etc/postgresql/9.3/main/postgresql.conf
+ 	echo "host all all 172.17.42.0/16  md5" >> /etc/postgresql/9.3/main/pg_hba.conf
+ 	
  if ! test -f /var/lib/postgresql/conf
  then
- 	echo listen_addresses = '*' >> /etc/postgresql/9.3/main/postgresql.conf
- 	echo "host all all 172.17.42.0/16  md5" >> /etc/postgresql/9.3/main/pg_hba.conf
+ chown -R postgres /var/lib/postgresql
+chown postgres -R /var/log/postgresql/
+
+
+
+ service postgresql start
+ 
  	touch /var/lib/postgresql/conf
 	 echo "ALTER ROLE postgres WITH ENCRYPTED PASSWORD 'pass'; " > /tmp/t.sql
 	 echo /usr/bin/perl   /usr/bin/psql -l > /tmp/t.sh
--- system/images/03.serviceImages/pgsql/home/pre-running.sh
+++ system/images/03.serviceImages/pgsql/home/pre-running.sh
@@ -4,7 +4,5 @@ chown -R postgres /var/lib/postgresql
 chown postgres -R /var/log/postgresql/
 
 
- echo listen_addresses = '*' >> /etc/postgresql/9.3/postgresql.conf
- echo "host all all 172.17.42.0/16  md5" >> /etc/postgresql/9.3/main/pg_hba.conf
- 
+
  service postgresql restart
