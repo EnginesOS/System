e669d0f7d30de8c1dc5e08430141ce1d75f96ad6
James Vodanovich <james@jvodan.com>
2014-10-08 23:21:25 +1100

    use flag file for creation status

--- system/images/03.serviceImages/pgsql/home/firstrun.sh
+++ system/images/03.serviceImages/pgsql/home/firstrun.sh
@@ -3,13 +3,14 @@ pass="pass"
 	echo "listen_addresses = '*'" >> /etc/postgresql/9.3/main/postgresql.conf
  	echo "host all all 172.17.42.0/16  md5" >> /etc/postgresql/9.3/main/pg_hba.conf
  	
- if ! test -f /var/lib/postgresql/9.3
+ if ! test -f /var/lib/postgresql/conf
  then
 
 
 cp -rp /var/lib/postgresql_firstrun/* /var/lib/postgresql/ 
 
 chown -R postgres /var/lib/postgresql
+mkdir -p /var/log/postgresql
 chown postgres -R /var/log/postgresql
 
  service postgresql start
