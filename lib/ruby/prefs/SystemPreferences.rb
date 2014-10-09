ef4179ec95f9d61c9d891bc96f108edb3d1c573d
James Vodanovich <james@jvodan.com>
2014-10-09 19:44:43 +1100

    beginings

---
+++ lib/ruby/prefs/SystemPreferences.rb
@@ -0,0 +1,29 @@
+def SystemPreferences
+  
+#  def SMTPSettings
+#    @host
+#    @user
+#    @pass
+#    @auth_mech
+#  end
+# 
+  #  @smtp_settings
+ 
+
+   
+#  @primary_hostname
+#  
+#  @domains array
+#  
+#  @primary_domain  
+#  @services_enabled  
+#  @services_disabled  
+
+
+    
+  #def get_domain_list
+  #end
+  
+
+  
+end
