cf4dd34e3abb84487e5349fc39ef0b85b4b5d611
James Vodanovich <james@jvodan.com>
2014-10-11 18:42:48 +1100

    templated configs

--- system/templates/deployment/global/home/config_functions.sh
+++ system/templates/deployment/global/home/config_functions.sh
@@ -6,14 +6,15 @@ function copy_substituted_templates {
 export dbname dbport dbuser dbpass dbhost FRAMEWORK cont_user cont_grp FSCONTFSVolHome SAR TZ
 echo  $dbname $dbport $dbuser $dbpass $dbhost $FRAMEWORK $cont_user $cont_grp $FSCONTFSVolHome $SAR $TZ
 
-templates=`find /tmp/home/engines/templates/ -type f`
+templates=`find /home/engines/templates/ -type f`
         for file in $templates
         	do     
-                dest_file=`echo $file | sed "/^.*templates\//s///"`
-                dest_dir=app/`echo $dest_file | sed "/\/[a-z,A-Z,0-9,_,.]*$/s///"`
+                dest_file=app/`echo $file | sed "/^.*templates\//s///"`
+                dest_dir=`basename $dest_file`
                 
                 mkdir -p $dest_dir
-
+				rm app/$dest_file
+				
                  while read line
                     do
                          eval echo "$line" >> $dest_file
