ae110888035fa23bb9f12e3cf39237d5b85568c6
James Vodanovich <james@jvodan.com>
2014-10-17 16:44:53 +1100

    only try and delete config if exists. if not file http or https return FAILED otherwise OK. only reload nginx (not restart) in addition only reload is file was deleted

--- system/images/03.serviceImages/nginx/home/rmsite.sh
+++ system/images/03.serviceImages/nginx/home/rmsite.sh
@@ -4,6 +4,24 @@ fqdn=`echo $1 |cut -f2 -d:`
 port=`echo $1 |cut -f3 -d:`
 host=`echo $fqdn |cut -f1 -d.`
 
-rm /etc/nginx/sites-enabled/*_$fqdn.site
-service nginx restart
+result="FAIL"
+
+	if test  -f /etc/nginx/sites-enabled/http_$fqdn.site
+		then
+			rm /etc/nginx/sites-enabled/http_$fqdn.site
+			result="OK"
+	fi
+	
+	if test -f /etc/nginx/sites-enabled/https_$fqdn.site
+		then
+			rm /etc/nginx/sites-enabled/https_$fqdn.site
+			result="OK"
+	fi
+
+echo $result
+
+	if test $result == "OK"
+		then
+			service nginx reload
+	fi
 
