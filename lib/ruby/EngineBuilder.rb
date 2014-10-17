e1a3b42ff659cbc9d6a74856d6ad8ccbd58abd10
James Vodanovich <james@jvodan.com>
2014-10-17 16:42:56 +1100

    log volume map done elsewhere transparently in container create

--- lib/ruby/EngineBuilder.rb
+++ lib/ruby/EngineBuilder.rb
@@ -444,20 +444,20 @@ class EngineBuilder
   end
 
     def setup_framework_logging
-      rmt_log_dir_var_fname=get_basedir + "/home/LOG_DIR" 
-      if File.exists?(rmt_log_dir_var_fname)
-        rmt_log_dir_varfile = File.open(rmt_log_dir_var_fname)
-        rmt_log_dir = rmt_log_dir_varfile.read
-      else
-        rmt_log_dir="/var/log"
-      end
-      local_log_dir = SysConfig.SystemLogRoot + "/" + @hostName 
-      cmd = "mkdir " +  local_log_dir
-      system  cmd
-      
-      log_vol = Volume.new("",local_log_dir,rmt_log_dir,"rw",PermissionRights.new("system","","")) #(name,localpath,remotepath,mapping_permissions,vol_permissions)
-      
-      @vols.push(log_vol)
+#      rmt_log_dir_var_fname=get_basedir + "/home/LOG_DIR" 
+#      if File.exists?(rmt_log_dir_var_fname)
+#        rmt_log_dir_varfile = File.open(rmt_log_dir_var_fname)
+#        rmt_log_dir = rmt_log_dir_varfile.read
+#      else
+#        rmt_log_dir="/var/log"
+#      end
+#      local_log_dir = SysConfig.SystemLogRoot + "/" + @hostName 
+#      cmd = "mkdir " +  local_log_dir
+#      system  cmd
+#      
+#      log_vol = Volume.new("",local_log_dir,rmt_log_dir,"rw",PermissionRights.new("system","","")) #(name,localpath,remotepath,mapping_permissions,vol_permissions)
+#      
+#      @vols.push(log_vol)
 
     end
     
