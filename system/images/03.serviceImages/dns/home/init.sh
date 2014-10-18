576f2aab9f49c9f8b70825e1846f94e2ef47e4f1
James Vodanovich <james@jvodan.com>
2014-10-18 18:16:54 +1100

    short time in test loop from 200

--- system/images/03.serviceImages/dns/home/init.sh
+++ system/images/03.serviceImages/dns/home/init.sh
@@ -7,6 +7,7 @@ mkdir -p /var/log/named
 chown bind /var/log/named
 
 /usr/sbin/named -c /etc/bind/named.conf -u bind 
+
 mkdir -p /engines/var/run/
 touch  /engines/var/run/startup_complete
 chown 21000 /engines/var/run/startup_complete
@@ -15,7 +16,7 @@ sleep 30
 
 while test -f /var/run/named/named.pid
 	do
-	  sleep 200
+	  sleep 120
 	done
 
 rm /engines/var/run/startup_complete
