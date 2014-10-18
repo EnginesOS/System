fa8f4c9d23eb2044468e1e0c3d74e6fb61319c14
James Vodanovich <james@jvodan.com>
2014-10-18 20:03:06 +1100

    the rbenv way

--- system/images/04.systemApps/mgmt/home/app/Procfile.sh
+++ system/images/04.systemApps/mgmt/home/app/Procfile.sh
@@ -13,15 +13,15 @@ git pull
 #cp /tmp/t /home/app/config/environments/production.rb
 
 
-bundle install
+~/.rbenv/shims/bundle install
 
-bundle exec rake db:migrate 
+~/.rbenv/shims/bundle exec rake db:migrate 
 # RAILS_ENV=production 
-bundle exec rake db:populate 
+~/.rbenv/shims/bundle exec rake db:populate 
 # RAILS_ENV=production 
-bundle exec rake assets:precompile 
+~/.rbenv/shims/bundle exec rake assets:precompile 
 # RAILS_ENV=production 
-bundle exec rake generate_secret_token 
+~/.rbenv/shims/bundle exec rake generate_secret_token 
 # RAILS_ENV=production 
 
 mkdir -p /engines/var/run/
@@ -30,9 +30,9 @@ chown 21000 /engines/var/run/startup_complete
 
 	if test -f /opt/engos/etc/ssl/keys/engines.key -a  -f /opt/engos/etc/ssl/certs/engines.crt 
 	then
-	 env SECRET_KEY_BASE=`bundle exec rake secret` 	 bundle exec thin -p 8000   --ssl --ssl-key-file /opt/engos/etc/ssl/keys/engines.key --ssl-cert-file /opt/engos/etc/ssl/certs/engines.crt start
+	 env SECRET_KEY_BASE=`bundle exec rake secret` 	~/.rbenv/shims/bundle exec thin -p 8000   --ssl --ssl-key-file /opt/engos/etc/ssl/keys/engines.key --ssl-cert-file /opt/engos/etc/ssl/certs/engines.crt start
 	else
-	 env SECRET_KEY_BASE=`bundle exec rake secret` 	bundle exec thin -p 8000   start
+	 env SECRET_KEY_BASE=`bundle exec rake secret` 	~/.rbenv/shims/bundle exec thin -p 8000   start
 	fi
 	
 rm /engines/var/run/startup_complete
