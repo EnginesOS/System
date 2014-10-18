11e996395c523ba5382d836b834dd5a1dac395b1
James Vodanovich <james@jvodan.com>
2014-10-18 13:50:45 +1100

    broke base image into three images. base servicebase and framework base

---
+++ system/images/03.serviceImages/fs/home/init.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+mkdir -p /var/log/volmgr
+chown $ContUser.$CountGrp /var/log/volmgr
+
+service ssh start
+
+mkdir -p /engines/var/run/
+touch  /engines/var/run/startup_complete
+chown 21000 /engines/var/run/startup_complete
+
+sleep 30
+
+while test -f /var/run/sshd.pid
+	do
+	  sleep 200
+	done
+
+
