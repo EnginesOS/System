dc79b5e805349234ac800d782c479c291829b451
James Vodanovich <james@jvodan.com>
2014-10-20 16:22:53 +1100

    add mongo service

---
+++ system/images/03.serviceImages/mongo/home/startworker.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+service ssh start
+
+service mongodb start
+
+
+mkdir -p /engines/var/run/
+touch  /engines/var/run/startup_complete
+chown 21000 /engines/var/run/startup_complete
+
+sleep 30
+
+while test -f /var/run/mongodb/mongodb.pid
+do
+	  sleep 200
+done
+
+
+rm /engines/var/run/startup_complete
