df911a71d95386a53c2fde2d5b5dda833b088a3f
James Vodanovich <james@jvodan.com>
2014-10-20 11:50:04 +1100

    fixed initial draught into a working? first run

--- system/images/03.serviceImages/pgsql/home/firstrun.sh
+++ system/images/03.serviceImages/pgsql/home/firstrun.sh
@@ -16,9 +16,13 @@ pass="pass"
  
  	touch /var/lib/postgresql/conf
 	 echo "ALTER ROLE postgres WITH ENCRYPTED PASSWORD 'pass'; " > /tmp/t.sql
-	 echo "/usr/bin/perl   /usr/bin/psql -l" > /tmp/t.sh
+	  echo "Add ROLE rma WITH ENCRYPTED PASSWORD 'pass'; " >> /tmp/t.sql
+	  echo "Alter ROLE rma WITH superuser;" >> /tmp/t.sql
+	   echo "Alter ROLE rma WITH login;" >> /tmp/t.sql
+	 echo "  /usr/bin/psql " > /tmp/t.sh
 	 chmod +x /tmp/t.sh
-	  su postgres  /tmp/t.sh
+	  su postgres -c  /tmp/t.sh </tmp/t.sql
+	  su postgres creatdb rma
 	  
 	  rm /tmp/t.sh
 	  rm /tmp/t.sql
