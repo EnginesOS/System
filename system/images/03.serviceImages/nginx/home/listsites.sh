cef31e4dc59e87e962b21c793210abb680a3cffb
James Vodanovich <james@jvodan.com>
2014-10-22 23:06:25 +1100

    no longer used and no longer relevant

--- system/images/03.serviceImages/nginx/home/listsites.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sites=`ls /etc/nginx/sites-enabled/ |grep -v default`
-
-	for site in $sites
-		do
-			grep server_name /etc/nginx/sites-enabled/$site  |sed "/;/s///" |  sed "/.*server_name/s///"
-		done
-
