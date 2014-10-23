331de506e3cec9b8cbf74f0f77556c9148d2cf16
James Vodanovich <james@jvodan.com>
2014-10-23 21:11:12 +1100

    write ot start command

--- system/templates/deployment/global/home/configcontainer.sh
+++ system/templates/deployment/global/home/configcontainer.sh
@@ -301,6 +301,9 @@ echo run state $run
 	/usr/local/rbenv/shims/bundle exec rake secret >/tmp/.sc
 	a=`cat /tmp/.sc`
 	rm /tmp/.sc
+	echo "Starting with"
+	echo "env DATABASE_URL=$DATABASE_URL SECRET_KEY_BASE=$a /usr/local/rbenv/shims/bundle exec thin -e $rails_env_prod_or_devel -p $PORT  start"
+	
 	env DATABASE_URL=$DATABASE_URL SECRET_KEY_BASE=$a /usr/local/rbenv/shims/bundle exec thin -e $rails_env_prod_or_devel -p $PORT  start
 
 	" > Procfile.sh
