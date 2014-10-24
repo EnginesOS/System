45ab29488a80517740de23b6eda664d99ae8f48f
James Vodanovich <james@jvodan.com>
2014-10-24 14:23:53 +1100

    added debugging and full path to nginx

--- system/images/03.serviceImages/nginx/home/startworker.sh
+++ system/images/03.serviceImages/nginx/home/startworker.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 chown -R www-data /var/log/nginx/ 
-
-nginx
-
+echo "chowned"
+/usr/sbin/nginx
+echo "started Nginx"
 mkdir -p /engines/var/run/
 touch  /engines/var/run/startup_complete
 chown 21000 /engines/var/run/startup_complete
-
+echo 
 sleep 30
 
 while test -f /var/run/nginx.pid
