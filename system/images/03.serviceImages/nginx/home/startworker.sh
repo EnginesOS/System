ecd56a48f3f6a9aa891e804cd08dbcadb4d3be27
James Vodanovich <james@jvodan.com>
2014-10-24 13:49:35 +1100

    revvert back to useing script but setting that as the default cmd

--- system/images/03.serviceImages/nginx/home/startworker.sh
+++ system/images/03.serviceImages/nginx/home/startworker.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
+chown -R www-data /var/log/nginx/ 
+
 service nginx start
+
 mkdir -p /engines/var/run/
 touch  /engines/var/run/startup_complete
 chown 21000 /engines/var/run/startup_complete
 
 sleep 30
+
 while test -f /var/run/nginx.pid
 do
 
 	  sleep 200
 done
-
 
 rm /engines/var/run/startup_complete
