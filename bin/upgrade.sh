c86f116abf6980da6e6edfcda6fc107c7133ff37
James Vodanovich <james@jvodan.com>
2014-10-27 15:02:31 +1100

    complete update script

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -7,4 +7,12 @@ cat ~dockuser/.profile |sed  "s/engos/engines/" |grep engines >/tmp/t
 cp /tmp/t ~dockuser/.profile
 chown dockuser  ~dockuser/.profile
 
+#su -l dockuser /opt/engines/bin/eservices stop
+#su -l dockuser /opt/engines/bin/engines stop
+docker stop `docker ps -a |awk '{print $1}' `
+docker rm `docker ps -a |awk '{print $1}' `
 
+ 
+
+ su -l dockuser  /opt/engines/bin/containers_startup.sh 
+ 
