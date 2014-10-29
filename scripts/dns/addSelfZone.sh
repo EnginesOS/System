e701ce46f2476efad882cd7a9a026123a4b54c57
James Vodanovich <james@jvodan.com>
2014-10-29 12:43:53 +1100

    typo in zonefile_tmpl path

--- scripts/dns/addSelfZone.sh
+++ scripts/dns/addSelfZone.sh
@@ -4,7 +4,7 @@
 		then
 			cat /opt/engines/etc/dns/tmpls/selfzone.tmpl | sed "s/DOMAIN/$1/" |sed  "s/DOMAIN/$2/" > /opt/engines/etc/dns/config/zones/named.conf.$1
 			#echo "include \"/etc/bind/engines/zones/named.conf.$1\";" >> /opt/engines/etc/dns/config/named.conf.engines
-			cat /opt/engines/etc/dns/tmpls/ config_file_zone_entry.tmpl  | sed "s/DOMAIN/$1/" >> /opt/engines/etc/dns/config/named.conf.engines
+			cat /opt/engines/etc/dns/tmpls/config_file_zone_entry.tmpl  | sed "s/DOMAIN/$1/" >> /opt/engines/etc/dns/config/named.conf.engines
 		fi
 		
 #service name reload		
