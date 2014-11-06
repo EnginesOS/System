74392ff036feb0bb2fd1f9be81c8bc7536410841
James Vodanovich <james@jvodan.com>
2014-11-06 13:19:29 +1100

    no loner runs as root so not useful and ssh with go else where

--- system/images/04.systemApps/mgmt/home/pre-running.sh
+++ system/images/04.systemApps/mgmt/home/pre-running.sh
@@ -2,14 +2,14 @@
 
 
 
-/etc/init.d/ssh start
+#/etc/init.d/ssh start
 
-if test -z $ContUser
-        then
+#if test -z $ContUser
+ #       then
                 $ContUser=www-data
-fi
-mkdir -p /engines/var/run/
-chown  -R   $ContUser /engines/var/run/
-chown  -R   $ContUser /home/app
+#fi
+#mkdir -p /engines/var/run/
+#chown  -R   $ContUser /engines/var/run/
+#chown  -R   $ContUser /home/app
 
 
