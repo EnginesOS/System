70909f94215273f1acc94b0903f1c1fbfc005fba
James Vodanovich <james@jvodan.com>
2014-11-07 21:18:50 +1100

    updated to ruby 2.1.3

--- system/images/04.systemApps/pass-mgmt/home/app/Procfile.sh
+++ system/images/04.systemApps/pass-mgmt/home/app/Procfile.sh
@@ -2,33 +2,24 @@
 PATH="/usr/local/rbenv/bin:$PATH"
 
 cd /home/app
-
-
-/usr/local/rbenv/shims/gem install git bundle oink
-/usr/local/rbenv/shims/gem install vmstat
-
+cp /home/gitconfig /home/app/.git/config
 git pull
 
-#cat /home/app/config/environments/production.rb |sed "/config.serve_static_assets = false/s//config.serve_static_assets = true/" >/tmp/t
-#cp /tmp/t /home/app/config/environments/production.rb
+RAILS_ENV=prodution
 
+SECRET_KEY_BASE=`/usr/local/rbenv/shims/bundle exec rake secret`
+export SECRET_KEY_BASE RAILS_ENV
 
 /usr/local/rbenv/shims/bundle install
 
 /usr/local/rbenv/shims/bundle exec rake db:migrate
-# RAILS_ENV=production
+
 /usr/local/rbenv/shims/bundle exec rake db:populate
-# RAILS_ENV=production
+
 /usr/local/rbenv/shims/bundle exec rake assets:precompile
-# RAILS_ENV=production
-#/usr/local/rbenv/shims/bundle exec rake generate_secret_token
-# RAILS_ENV=production
-RAILS_ENV=development
+
 
 touch  /engines/var/run/startup_complete
-
-SECRET_KEY_BASE=`/usr/local/rbenv/shims/bundle exec rake secret`
-export SECRET_KEY_BASE RAILS_ENV
 
 /usr/sbin/apache2ctl -D FOREGROUND
 rm /engines/var/run/startup_complete
