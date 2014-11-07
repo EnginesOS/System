0e6ab057d2e10183358f54e539e0f422727f0c17
James Vodanovich <james@jvodan.com>
2014-11-07 19:47:13 +1100

    prep for mgmt run in phuson

--- system/images/04.systemApps/pass-mgmt/home/app/Procfile.sh
+++ system/images/04.systemApps/pass-mgmt/home/app/Procfile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PATH="/usr/local/rbenv/bin:$PATH" 
+PATH="/usr/local/rbenv/bin:$PATH"
 
 cd /home/app
 
@@ -15,25 +15,20 @@ git pull
 
 /usr/local/rbenv/shims/bundle install
 
-/usr/local/rbenv/shims/bundle exec rake db:migrate 
-# RAILS_ENV=production 
-/usr/local/rbenv/shims/bundle exec rake db:populate 
-# RAILS_ENV=production 
-/usr/local/rbenv/shims/bundle exec rake assets:precompile 
-# RAILS_ENV=production 
-#/usr/local/rbenv/shims/bundle exec rake generate_secret_token 
-# RAILS_ENV=production 
+/usr/local/rbenv/shims/bundle exec rake db:migrate
+# RAILS_ENV=production
+/usr/local/rbenv/shims/bundle exec rake db:populate
+# RAILS_ENV=production
+/usr/local/rbenv/shims/bundle exec rake assets:precompile
+# RAILS_ENV=production
+#/usr/local/rbenv/shims/bundle exec rake generate_secret_token
+# RAILS_ENV=production
 RAILS_ENV=development
 
 touch  /engines/var/run/startup_complete
-chown 21000 /engines/var/run/startup_complete
+
 SECRET_KEY_BASE=`/usr/local/rbenv/shims/bundle exec rake secret`
 export SECRET_KEY_BASE RAILS_ENV
-#	if test -f /opt/engines/etc/ssl/keys/engines.key -a  -f /opt/engines/etc/ssl/certs/engines.crt 
-#	then
-#	 	/usr/local/rbenv/shims/bundle exec thin -p 8000   --ssl --ssl-key-file /opt/engines/etc/ssl/keys/engines.key --ssl-cert-file /opt/engines/etc/ssl/certs/engines.crt start
-#	else
-#	 	/usr/local/rbenv/shims/bundle exec thin -p 8000   start
-#	fi
+
 /usr/sbin/apache2ctl -D FOREGROUND
 rm /engines/var/run/startup_complete
