3c107108923c5a6d12b0e8b572dd086dc4cbab09
James Vodanovich <james@jvodan.com>
2014-11-10 13:33:15 +1100

    add ed @data_uid=data_uid     @data_gid=data_gid

--- lib/ruby/ManagedEngine.rb
+++ lib/ruby/ManagedEngine.rb
@@ -3,7 +3,7 @@
 class ManagedEngine < ManagedContainer
 
   
-  def initialize(name,memory,hostname,domain_name,image,volumes,port,eports,repo,dbs,environments,framework,runtime,docker_api)
+  def initialize(name,memory,hostname,domain_name,image,volumes,port,eports,repo,dbs,environments,framework,runtime,docker_api,data_uid,data_gid)
                             
                  @last_error="None"                 
                  @containerName=name
@@ -24,12 +24,15 @@ class ManagedEngine < ManagedContainer
                  @framework=framework
                  @runtime=runtime
                  @docker_api= docker_api
+    
                  
     @ctype ="container"
     @conf_self_start=false
     @conf_register_dns=true
     @conf_register_site=true
     @conf_monitor_site=false
+    @data_uid=data_uid
+    @data_gid=data_gid
     
     save_state # no config.yaml throws a no such container so save so others can use
     
