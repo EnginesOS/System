b670b0ad74dfdea2646429f2e7cfba6bfaad899f
James Vodanovich <james@jvodan.com>
2014-11-11 10:06:43 +1100

    chmod and cp -rp

--- system/images/04.systemApps/fsconfigerator/setup_vols.sh
+++ system/images/04.systemApps/fsconfigerator/setup_vols.sh
@@ -1,12 +1,13 @@
 #/bin/bash
+#VOLUME /client/var/log
+#VOLUME /client/log
+#VOLUME /client/state
+#VOLUME /client/fs
+#VOLUME /dest/fs
+chown $fw_user -R /client/log/
+chown $fw_user -R /client/var/log
+chown $fw_user -R /client/state/
 
-chown $fw_user -R /cont/log/
-chown $fw_user -R /cont/run/
-chown $fw_user -R /cont/state/
+cp -rp  client/fs* /dest/fs/
 
-chown $fw_user -R /cont/persistant
 
-if test -n $vols
-	then
-		for vol in $vols
-			do
