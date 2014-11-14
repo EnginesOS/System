db2f7a74a6efe5feb0eaf18889fc5aa43272294f
James Vodanovich <james@jvodan.com>
2014-11-14 17:03:06 +1100

    c n p  error in persistant file

--- lib/ruby/EngineBuilder.rb
+++ lib/ruby/EngineBuilder.rb
@@ -388,7 +388,7 @@ p env
       path =  arc_dir=clean_path(file["path"])
       pcf=path
       docker_file.puts("RUN mkdir -p /home/" + File.dirname(path))
-      docker_file.puts("RUN  if [ ! -d /home/" + path + " ]; then touch -p /home/" + path +" ; fi")
+      docker_file.puts("RUN  if [ ! -f /home/" + path + " ]; then touch  /home/" + path +" ; fi")
       docker_file.puts("RUN mkdir -p $CONTFSVolHome/" + File.dirname(path))
         
       link_src = path.sub(/app/,"")
