903b26b2b0ab9d598e86114a8e4f48a96532c09e
James Vodanovich <james@jvodan.com>
2014-11-15 15:01:06 +1100

    make apachce2 log file

--- system/templates/deployment/rails4/Rack.sh
+++ system/templates/deployment/rails4/Rack.sh
@@ -33,7 +33,7 @@ SECRET_KEY_BASE=`/usr/local/rbenv/shims/bundle exec rake secret`
 export SECRET_KEY_BASE 
 
 touch  /engines/var/run/startup_complete
-
+mkdir /var/log/apache2/
 /usr/sbin/apache2ctl -D FOREGROUND
 
 rm /var/run/apache2/apache2.pid
