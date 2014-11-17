57af5924e816300fe2cdd1c4464ba59d9989776e
James Vodanovich <james@jvodan.com>
2014-11-17 12:09:59 +1100

    stypo

--- lib/ruby/EngineBuilder.rb
+++ lib/ruby/EngineBuilder.rb
@@ -328,7 +328,7 @@ class EngineBuilder
          file = clean_path(sed["file"])
          dest = clean_path(sed["dest"])
         tmp_file = "/tmp/" + File.basename(file) + "." + n.to_s
-         template_file = file.gsuub(/^_TEMPLATES/,"")
+         template_file = file.gsub(/^_TEMPLATES/,"")
          if  template_file != nil
           src_file = "/home/engines/templates/" +  template_file[0]
         else    
