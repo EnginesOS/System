2a86a27811b881e08b81bc7ed05d439d724381fa
James Vodanovich <james@jvodan.com>
2014-11-17 12:02:37 +1100

    correct changing template

--- lib/ruby/EngineBuilder.rb
+++ lib/ruby/EngineBuilder.rb
@@ -328,7 +328,7 @@ class EngineBuilder
          file = clean_path(sed["file"])
          dest = clean_path(sed["dest"])
         tmp_file = "/tmp/" + File.basename(file) + "." + n.to_s
-         template_file = file.match(/^_TEMPLATES.*/)
+         template_file = file.gsuub(/^_TEMPLATES/,"")
          if  template_file != nil
           src_file = "/home/engines/templates/" +  template_file[0]
         else    
