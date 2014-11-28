ef52599213fe14e12706f1e0b8c596873bd0c5ef
James Vodanovich <james@jvodan.com>
2014-11-29 09:03:39 +1100

    remove old accessors

--- lib/ruby/WorkPort.rb
+++ lib/ruby/WorkPort.rb
@@ -8,26 +8,5 @@ class WorkPort
   end
 
   attr_reader :proto_type,:name,:port,:external,:publicFacing
-#  def proto_type
-#      @proto_type
-#  end
-#    
-#  def set_proto_type newtype
-#    @proto_type = newtype
-#  end
-#  def name
-#    return @name
-#  end
-#  
-#  def port
-#    return @port
-#  end
-#  
-#  def external
-#    @external    
-#  end
-#  
-#  def publicFacing
-#    return @publicFacing
-#  end  
+
 end
