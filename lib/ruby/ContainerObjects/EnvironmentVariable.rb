aebe0aad1019b149a98e8905651c0ae4fa7125c3
James Vodanovich <james@jvodan.com>
2014-12-09 12:11:56 +1100

    @setatrun = @set_at_build_time #Kludge so as noto break Guo should be removed when gui fixed

--- lib/ruby/ContainerObjects/EnvironmentVariable.rb
+++ lib/ruby/ContainerObjects/EnvironmentVariable.rb
@@ -5,9 +5,9 @@ class EnvironmentVariable
     @set_at_build_time=setatrun
     @build_time_only = build_time_only
     @mandatory = mandatory
-    
+    @setatrun = @set_at_build_time #Kludge so as noto break Guo should be removed when gui fixed
   end
   
-  attr_reader :set_at_build_time,:name,:value,:build_time_only,:mandatory,:label
+  attr_reader :set_at_build_time,:name,:value,:build_time_only,:mandatory,:label,:setatrun
   
 end
