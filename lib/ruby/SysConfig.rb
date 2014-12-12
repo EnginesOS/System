bdf18e8ec96120e89b8f5543d4ca4a153061e3d7
James Vodanovich <james@jvodan.com>
2014-12-12 13:56:11 +1100

    removed  unused and grouped others

--- lib/ruby/SysConfig.rb
+++ lib/ruby/SysConfig.rb
@@ -1,11 +1,13 @@
 
 class SysConfig
 
-  @@addSiteCmd="ssh -i   /opt/engines/etc/keys/nginx -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"   rma@nginx.engines.internal sudo sh /home/addsite.sh"
-  @@rmSiteCmd="ssh -i  /opt/engines/etc/keys/nginx -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"   rma@nginx.engines.internal sudo sh /home/rmsite.sh"
-  @@addSiteMonitorCmd="ssh -i  /opt/engines/etc/keys/nagios  -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"   rma@monit.engines.internal sudo sh /home/addsite.sh"
-  @@rmSiteMonitorCmd="ssh -i  /opt/engines/etc/keys/nagios   -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"   rma@monit.engines.internal sudo sh /home/rmsite.sh"               
-  @@addDBServiceCmd="ssh  -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"  -i /opt/engines/etc/keys/mysql rma@"  
+#  @@addSiteCmd="ssh -i   /opt/engines/etc/keys/nginx -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"   rma@nginx.engines.internal sudo sh /home/addsite.sh"
+#  @@rmSiteCmd="ssh -i  /opt/engines/etc/keys/nginx -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"   rma@nginx.engines.internal sudo sh /home/rmsite.sh"
+#  @@addSiteMonitorCmd="ssh -i  /opt/engines/etc/keys/nagios  -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"   rma@monit.engines.internal sudo sh /home/addsite.sh"
+#  @@rmSiteMonitorCmd="ssh -i  /opt/engines/etc/keys/nagios   -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"   rma@monit.engines.internal sudo sh /home/rmsite.sh"               
+#  @@addDBServiceCmd="ssh  -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"  -i /opt/engines/etc/keys/mysql rma@"  
+#  
+  
   @@DBHost="mysql.engines.internal"
   @@CidDir="/opt/engines/run"
   @@ContainersDir="/opt/engines/run/containers/"
@@ -14,24 +16,32 @@ class SysConfig
   @@CONTFSVolHome = "/home/app/fs"
   @@LocalFSVolHome = "/var/lib/engines/fs"
   @@galleriesDir = "/opt/engines/etc/galleries"
-  @@ddnsKey = "/opt/engines/etc/keys/ddns.key"
-  @@internalDomain = "engines.internal"
-  @@defaultDNS ="172.17.42.1"
+ 
   @@timeZone_fileMapping=" -v /etc/localtime:/etc/localtime:ro "
+  
   @@addBackupCmd = "ssh -i  /opt/engines/etc/keys/backup   -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"   rma@backup.engines.internal sudo sh /home/add_backup.sh "
   @@rmBackupCmd = "ssh -i  /opt/engines/etc/keys/backup   -o UserKnownHostsFile=/dev/null -o \"StrictHostKeyChecking no\"   rma@backup.engines.internal sudo sh /home/rm_backup.sh "
+  
   @@SystemLogRoot ="/var/log/engines/"
-  @@HostedDomainsFile="/opt/engines/etc/hosted_domains"
+ 
+ #NGINX
   @@HttpNginxTemplate="/opt/engines/etc/nginx/tmpls/http_site.tmpl"
   @@HttpsNginxTemplate="/opt/engines/etc/nginx/tmpls/https_site.tmpl"
   @@HttpHttpsNginxTemplate="/opt/engines/etc/nginx/tmpls/http_to_https_site.tmpl"
   @@NginxSiteDir="/opt/engines/etc/nginx/sites-enabled/"
   @@NginxCertDir="/opt/engines/etc/nginx/ssl/certs"
   @@NginxDefaultCert="engines"
+  
+ #DNS
+  @@ddnsKey = "/opt/engines/etc/keys/ddns.key"
+   @@internalDomain = "engines.internal"
+   @@defaultDNS ="172.17.42.1"
+#Named
+  @@NamedPIDFile="/opt/engines/run/services/nginx/run/nginx/nginx.pid" 
   @@SelfHostedDNStemplate="/opt/engines/etc/dns/tmpls/selfhosted.tmpl"
   @@DNSZoneDir="/opt/engines/etc/dns/config/zones/"
   @@DNSConfDir="/opt/engines/etc/dns/config/conf/"
-  @@NamedPIDFile="/opt/engines/run/services/nginx/run/nginx/nginx.pid"
+  @@HostedDomainsFile="/opt/engines/etc/hosted_domains"
   @@DNSHostedList="/opt/engines/etc/dns/config/conf/domains.hosted"
   
   def SysConfig.DNSHostedList
