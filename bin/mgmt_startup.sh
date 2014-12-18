3ecd9f3153b460ddc22cbc48cd2a194e44d071b4
James Vodanovich <james@jvodan.com>
2014-12-18 14:13:37 +1100

    save ip of th0

--- bin/mgmt_startup.sh
+++ bin/mgmt_startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ifconfig eth0 |grep \"inet addr\"  |  cut -f 2 -d: |cut -f 1 -d" " > /opt/engines/.ip 
+
 /opt/engines/bin/containers_startup.sh &
 
 if test ` docker ps -a |grep mgmt |wc -c` -eq 0
