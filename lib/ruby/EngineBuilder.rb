a1d6d6a5cd018567f2408e9ae265f7c9d9d0ac25
James Vodanovich <james@jvodan.com>
2014-12-19 20:24:51 +1100

    added missing then statements in shell

--- lib/ruby/EngineBuilder.rb
+++ lib/ruby/EngineBuilder.rb
@@ -385,7 +385,8 @@ count_layer
         end
         @blueprint_reader.single_chmods.each do |path|
           if path !=nil           
-            @docker_file.puts("RUN if [ ! -f /home/app/" + path + "];\\" )
+            @docker_file.puts("RUN if [ ! -f /home/app/" + path + "]\\" )
+            @docker_file.puts(" then ;\\")
                   @docker_file.puts("touch  /home/app/" + path + ";\\")
                   @docker_file.puts("fi;\\")
                   @docker_file.puts( "chmod  770 /home/app/" + path )
@@ -408,7 +409,8 @@ count_layer
         end
         @blueprint_reader.recursive_chmods.each do |directory|          
           if directory !=nil
-            @docker_file.puts("RUN if [ ! -f /home/app/" + directory + "];\\" )
+            @docker_file.puts("RUN if [ ! -f /home/app/" + directory + "] \\" )
+            @docker_file.puts(" then ;\\")
             @docker_file.puts("mkdir  /home/app/" + directory + ";\\")
             @docker_file.puts("fi;\\")
             @docker_file.puts( "chmod -R 770 /home/app/" + directory )
