fe174c2a45a21fd3cab21a355b0df2df1287c14a
James Vodanovich <james@jvodan.com>
2015-01-05 08:58:35 +1100

    fix filename

--- system/images/01.baseImages/001.ubuntu/setup.sh
+++ system/images/01.baseImages/001.ubuntu/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-wget http://cloud-images.ubuntu.com/releases/14.04/release/ubuntu-14.04-server-cloudimg-amd64.tar.gz
+#should check md5
+if ! test -f ubuntu-14.04-server-cloudimg-amd64.tar.gz
+	then
+		wget http://cloud-images.ubuntu.com/releases/14.04/release/ubuntu-14.04-server-cloudimg-amd64.tar.gz
+	end
