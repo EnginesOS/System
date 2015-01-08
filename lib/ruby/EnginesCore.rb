7fe49422c0a3f6fefa70aef1e2f2d39686563b4e
James Vodanovich <james@jvodan.com>
2015-01-08 15:05:13 +1100

    lace / after containerid so explicit it's a dir

--- lib/ruby/EnginesCore.rb
+++ lib/ruby/EnginesCore.rb
@@ -677,7 +677,7 @@ class EnginesCore
           container.container_id=(container_id)
         end
         if container && container.container_id != nil && container.container_id != '-1'
-          path = "/sys/fs/cgroup/memory/docker/" + container.container_id
+          path = "/sys/fs/cgroup/memory/docker/" + container.container_id + "/"
           if Dir.exists?(path)
             ret_val.store(:maximum , File.read(path + "/memory.max_usage_in_bytes"))
             ret_val.store(:current , File.read(path + "/memory.usage_in_bytes"))
