aec2ab2cfa0b7b485a18b3b5c16fdd661162e613
James Vodanovich <james@jvodan.com>
2015-01-14 12:52:00 +1100

    use class instance and not static loadmanagedservices from loading cron service

--- lib/ruby/EnginesCore.rb
+++ lib/ruby/EnginesCore.rb
@@ -123,7 +123,7 @@ class EnginesCore
     end
     def add_cron(cron_hash)
        begin
-         cron_service =  EnginesOSapi.loadManagedService("cron", @core_api)          
+         cron_service = @engines_api.loadManagedService("cron")          
          cron_service.add_consumer(cron_hash)           
              
                   cron_line = format_cron_line(cron_hash)  
