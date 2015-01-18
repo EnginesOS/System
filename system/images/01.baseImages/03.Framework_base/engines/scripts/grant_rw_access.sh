06b4a989d7368ca18a52eab43d7c57331724e458
James Vodanovich <james@jvodan.com>
2015-01-19 10:15:14 +1100

    correct data-user name

--- system/images/01.baseImages/03.Framework_base/engines/scripts/grant_rw_access.sh
+++ system/images/01.baseImages/03.Framework_base/engines/scripts/grant_rw_access.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 path=`echo $1 |sed '/[ ;\\\"\`]/s///g ' | sed '/\.\./s///g'`
-sudo -u data-owner /home/engines/scripts/_grant_rw_access.sh $path
+sudo -u data-user /home/engines/scripts/_grant_rw_access.sh $path
--- system/images/01.baseImages/03.Framework_base/engines/scripts/revoke_rw_access.sh
+++ system/images/01.baseImages/03.Framework_base/engines/scripts/revoke_rw_access.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 path=`echo $1 |sed '/[ ;\\\"\`]/s///g ' | sed '/\.\./s///g'`
-sudo -u data-owner /home/engines/scripts/_revoke_rw_access.sh $path
+sudo -u data-user /home/engines/scripts/_revoke_rw_access.sh $path
