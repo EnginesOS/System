54dc2ddf93ac709cde24a1af12f2d87979da7c3d
James Vodanovich <james@jvodan.com>
2015-01-18 18:44:44 +1100

    corect sed for all

--- system/images/01.baseImages/03.Framework_base/engines/scripts/revoke_rw_access.sh
+++ system/images/01.baseImages/03.Framework_base/engines/scripts/revoke_rw_access.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-path=`echo $1 |sed "/[ ;\\\.\'\"\`]/s///g"`
+path=`echo $1 |sed '/[ ;\\\"\`]/s///g ' | sed '/\.\./s///g'`
 sudo -u data-owner /home/engines/scripts/_revoke_rw_access.sh $path
