774a936f9d561e46b2064d9168c7ec40e07c9169
James Vodanovich <james@jvodan.com>
2015-01-20 08:49:00 +1100

    lauch install scipts in Docker file

--- system/templates/deployment/global/home/setup.sh
+++ system/templates/deployment/global/home/setup.sh
@@ -4,12 +4,12 @@ cd /home/
 		source /home/config_functions.sh
 		copy_substituted_templates		
 		cd /home/app			
-	fi
 	
-	if test -f /home/engines/scripts/post_install.sh
-				then 				
-				echo "Running Post Install"
-				/bin/bash /home/engines/scripts/post_install.sh 
-	fi
+	
+	if test -f /home/engines/scripts/install.sh 
+	then
+		echo running custom install
+	    bash /home/engines/scripts/install.sh
+	fi 
 	
 	
