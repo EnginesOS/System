e2eaaddf4ba5490bd507ce0c60c52aa1566aeebe
James Vodanovich <james@jvodan.com>
2015-02-05 20:49:00 +1100

    move database create back in to db services

--- lib/ruby/EnginesCore.rb
+++ lib/ruby/EnginesCore.rb
@@ -1821,38 +1821,6 @@ class EnginesCore
     end
   end
 
-  def create_database  site_hash
-    clear_error
-    begin
-      p site_hash
-      container_name =  site_hash[:flavor] + "_server"
-      cmd = "docker exec " +  container_name + " /home/createdb.sh " + site_hash[:name] + " " + site_hash[:user] + " " + site_hash[:pass]
-
-      #save details with some manager
-      SystemUtils.debug_output(cmd)
-
-      return run_system(cmd)
-    rescue  Exception=>e
-      log_exception(e)
-      return false
-    end
-  end
-
-  def drop_database  site_hash
-    clear_error
-    begin
-      container_name =  site_hash[:flavor] + "_server"
-      cmd = "docker exec " +  container_name + " /home/dropdb.sh " + site_hash[:name] + " " + site_hash[:user] + " " + site_hash[:pass]
-
-      #save details with some manager
-      SystemUtils.debug_output(cmd)
-
-      return run_system(cmd)
-    rescue  Exception=>e
-      log_exception(e)
-      return false
-    end
-  end
 
   def delete_image(container)
     begin
--- lib/ruby/ManagedServices/DBManagedService.rb
+++ lib/ruby/ManagedServices/DBManagedService.rb
@@ -11,13 +11,46 @@ class DBManagedService < ManagedService
     if site_hash.has_key?(:name) == false || site_hash[:name] = nil    
           site_hash[:name] = site_hash[:database_name]
       end
-    return  @core_api.create_database(site_hash) 
+    return  create_database(site_hash) 
      end
      
   def rm_consumer_from_service (site_hash)
        return  true
     end 
     
+
+  def create_database  site_hash
+    clear_error
+    begin
+      p site_hash
+      container_name =  site_hash[:flavor] + "_server"
+      cmd = "docker exec " +  container_name + " /home/createdb.sh " + site_hash[:name] + " " + site_hash[:user] + " " + site_hash[:pass]
+
+      #save details with some manager
+      SystemUtils.debug_output(cmd)
+
+      return SystemUtils.run_system(cmd)
+    rescue  Exception=>e
+      SystemUtils.log_exception(e)
+      return false
+    end
+  end
+
+  def drop_database  site_hash
+    clear_error
+    begin
+      container_name =  site_hash[:flavor] + "_server"
+      cmd = "docker exec " +  container_name + " /home/dropdb.sh " + site_hash[:name] + " " + site_hash[:user] + " " + site_hash[:pass]
+
+      #save details with some manager
+      SystemUtils.debug_output(cmd)
+
+      return run_system(cmd)
+    rescue  Exception=>e
+      SystemUtils.log_exception(e)
+      return false
+    end
+  end
 
   def get_site_hash(database)
     site_hash = Hash.new()
