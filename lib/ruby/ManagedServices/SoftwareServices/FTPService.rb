3530f74b818a0caaf33a43cbf9ca076446040c12
James Vodanovich <james@jvodan.com>
2015-02-06 08:50:57 +1100

    move services defs

--- lib/ruby/ManagedServices/SoftwareServices/FTPService.rb
+++ lib/ruby/ManagedServices/SoftwareServices/FTPService.rb
@@ -6,15 +6,20 @@ require_relative "SoftwareService.rb"
 class FTPService < SoftwareService
   def add_consumer_to_service(site_hash)
 
-    return  @core_api.add_ftp_service(site_hash)
+    return  add_ftp_service(site_hash)
   end
 
   def rm_consumer_from_service (site_hash)
-    return  @core_api.rm_ftp_service(site_hash)
+    return  rm_ftp_service(site_hash)
   end
   
+  def add_ftp_service(site_hash)
+    
+  end
   
-  
+  def rm_ftp_service(site_hash)
+    
+  end
 
   def get_site_hash(site_hash)
     site_hash[:service_type]='ftp'
