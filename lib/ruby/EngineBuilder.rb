a633559c82fcb3aa8c1567caba10653a78142201
James Vodanovich <james@jvodan.com>
2015-02-05 12:58:33 +1100

    add parent_engine to serach hash

--- lib/ruby/EngineBuilder.rb
+++ lib/ruby/EngineBuilder.rb
@@ -1737,6 +1737,7 @@ class EngineBuilder
               service[:username] = @set_environments["ftpuser"]
               service[:password] = @set_environments["password"]
               service[:rw_access] =true
+              service[:parent_engine]=mc.containerName
               service_def[:service_hash]=service
               service_def[:parent_engine]=mc.containerName
               service_def[:service_type]=service["servicetype_name"]
