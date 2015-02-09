226190651112d3ec18e510c54594a56191d3003c
James Vodanovich <james@jvodan.com>
2015-02-09 11:37:52 +1100

    create engines group and add engines to  it

--- installers/routines.sh
+++ installers/routines.sh
@@ -78,7 +78,9 @@ echo "Installing required  packages"
 echo "Setting up engines system user"
 		 #Kludge should not be a static but a specified or atleaqst checked id
 		 adduser -q --uid 21000 --ingroup docker   -gecos "Engines OS User"  --home /home/engines --disabled-password engines
-		 
+		 addgroup engines
+		 usermod  -G engines engines
+		  
 		echo "PATH=\"/opt/engines/bin:$PATH\"" >>~engines/.profile 
 		
 echo "Installing ruby"
