59b114b1fcd866ba43037f0ca42930207b4f9f64
James Vodanovich <james@jvodan.com>
2015-02-09 15:14:33 +1100

    include full path to ifconfig

--- bin/mgmt_startup.sh
+++ bin/mgmt_startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ifconfig eth0 |grep "inet addr"  |  cut -f 2 -d: |cut -f 1 -d" " > /opt/engines/.ip 
+/sbin/ifconfig eth0 |grep "inet addr"  |  cut -f 2 -d: |cut -f 1 -d" " > /opt/engines/.ip 
 
 /opt/engines/bin/containers_startup.sh &
 
