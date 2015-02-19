64ab84cdf576122603bdb3e1304c5f8806de7faf
James Vodanovich <james@jvodan.com>
2015-02-19 14:16:37 +1100

    email in to use posfix and postfix admin

--- system/images/03.serviceImages/couriermail/custom_start.sh
+++ system/images/03.serviceImages/couriermail/custom_start.sh
@@ -10,6 +10,8 @@ rm -f /run/apache2/apache2.pid
 cp /usr/lib/courier/etc/webadmin/password  /etc/courier/webadmin/
  touch  /usr/lib/courier/etc/webadmin/unsecureok /etc/courier/webadmin/unsecureok
 cp /usr/lib/courier/courier/webmail/webadmin /usr/lib/cgi-bin/
+ln -s  /etc/apache2/mods-available/ssl.{conf,load}   /etc/apache2/mods-enabled/
+ln -s /etc/apache2/mods-available/socache_shmcb.load /etc/apache2/mods-enabled/
 
 /usr/sbin/apache2ctl -D FOREGROUND 
 rm -f /run/apache2/apache2.pid 
