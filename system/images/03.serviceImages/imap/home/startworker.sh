72ef4098241d3e3678ac324b2c57ca652a4a1950
James Vodanovich <james@jvodan.com>
2015-02-20 11:39:15 +1100

    startup script

---
+++ system/images/03.serviceImages/imap/home/startworker.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+syslogd -R syslog.engines.internal
+
+mkdir -p /engines/var/run/
+touch  /engines/var/run/startup_complete
+chown 21000 /engines/var/run/startup_complete
+
+sleep 30
+while test -d /
+do
+	  sleep 200
+done
+
+
+rm /engines/var/run/startup_complete
