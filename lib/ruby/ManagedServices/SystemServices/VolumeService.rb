f8228fe0280931077f5d39697585536fd9a8ebe4
James Vodanovich <james@jvodan.com>
2015-02-24 10:56:49 +1100

    mkdir filesystem in the correct dir

--- lib/ruby/ManagedServices/SystemServices/VolumeService.rb
+++ lib/ruby/ManagedServices/SystemServices/VolumeService.rb
@@ -15,7 +15,7 @@ class VolumeService < ManagedService
   def add_volume(site_hash)
     
     begin
-      dest = SysConfig.CONTFSVolHome() + "/" + site_hash[:parent_engine] + "/" + site_hash[:dest]
+      dest = SysConfig.LocalFSVolHome() + "/" + site_hash[:parent_engine] + "/" + site_hash[:dest]
       if Dir.exists?( dest) == false
         p dest
         FileUtils.mkdir_p(dest)
