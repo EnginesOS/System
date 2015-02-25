01b1c33b73cab67a34ddd9187bea438c486c2b1c
James Vodanovich <james@jvodan.com>
2015-02-25 17:14:41 +1100

    nvp into shell and so to use hash keys as variable names

--- system/images/03.serviceImages/smtp/home/setup_params.sh
+++ system/images/03.serviceImages/smtp/home/setup_params.sh
@@ -1,31 +1,52 @@
 #/bin/bash
 
+n=1
 
-host=`echo $1 |cut -f1`
-port=`echo $1 |cut -f2`
-	if test -z port
+echo $1 |grep = >/dev/null
+        if test $? -ne 0
+        then
+                exit
+        fi
+
+res="${1//[^:]}"
+echo $res
+fcnt=${#res}
+fcnt=`expr $fcnt + 1`
+
+        while test $fcnt -ge $n
+        do
+                nvp="`echo $1 |cut -f$n -d:`"
+                n=`expr $n + 1`
+                name=`echo $nvp |cut -f1 -d=`
+                export $name=`echo $nvp |cut -f2 -d=`
+        done
+
+	if test -z $smarthost_port
 	then
-		port=25
+		smarthost_port=25
 	fi
 	
-echo "*	$host:$port" > > /etc/postfix/transport
-chown root.root /etc/postfix/transport
-chmod 600 /etc/postfix/transport
+	 #smarthost_hostname"=>"203.14.203.141", "smarthost_username"=>"", "smarthost_password"=>"", "smarthost_authtype"=>"", "smarthost_port"=>"", 
+	if test -n $smarthost_hostname
+	then 
+		echo "*	$smarthost_hostname:$smarthost_port" > > /etc/postfix/transport
+		chown root.root /etc/postfix/transport
+		chmod 600 /etc/postfix/transport
+fi
 
-user=`echo $1 |cut -f3`
-pass=`echo $1 |cut -f4`
-if test -z $user
+
+if test -z $smarthost_username
  then 
- exit
+ 	exit
  fi
  
  
- if test -z $pass
+ if test -z $smarthost_password
  then 
- exit
+ 	exit
  fi
 
-echo "$host $user:$pass" > /etc/postfix/smarthost_passwd
+echo "$smarthost_hostname $smarthost_username:$smarthost_password" > /etc/postfix/smarthost_passwd
 chown root.root /etc/postfix/smarthost_passwd
 chmod 600 /etc/postfix/smarthost_passwd
-postmap      /etc/postfix/smarthost_passwd
+postmap   /etc/postfix/smarthost_passwd
