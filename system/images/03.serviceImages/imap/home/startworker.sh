12dc09823ea41bab9a7e12e3bb2649410d5dd15f
James Vodanovich <james@jvodan.com>
2015-02-27 10:20:15 +1100

    start dovecot block on syslog

--- system/images/03.serviceImages/imap/home/startworker.sh
+++ system/images/03.serviceImages/imap/home/startworker.sh
@@ -6,12 +6,8 @@ syslogd -R syslog.engines.internal:5140
 mkdir -p /engines/var/run/
 touch  /engines/var/run/startup_complete
 chown 21000 /engines/var/run/startup_complete
-
-sleep 30
-while test -d /
-do
-	  sleep 200
-done
+/usr/sbin/dovecot
+syslogd -n -R syslog.engines.internal:5140
 
 
 rm /engines/var/run/startup_complete
