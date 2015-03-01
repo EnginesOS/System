578f4327ec5b36c439af95b8906dcee9c5889298
James Vodanovich <james@jvodan.com>
2015-03-01 11:08:21 +1100

    error_checling

--- lib/ruby/ManagedServices/ServiceManager.rb
+++ lib/ruby/ManagedServices/ServiceManager.rb
@@ -19,7 +19,7 @@ class ServiceManager
 
   def get_software_service_container_name(params)
    server_service =  software_service_definition(params)
-   if server_service == nil
+   if server_service == nil || server_service == false
      p :no_luck_getint_service_container_for
      p params
      return nil
