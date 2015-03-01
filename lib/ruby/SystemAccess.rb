80ef10796dfe9ff1ea75822754a1c1242922f07f
James Vodanovich <james@jvodan.com>
2015-03-01 20:51:23 +1100

    now System is SystemAccess

--- lib/ruby/SystemAccess.rb
+++ lib/ruby/SystemAccess.rb
@@ -1,16 +1,16 @@
-class System
+class SystemAccess
   
   #This Class is the public face of the system
   #release etc
   
-  def System.release
+  def SystemAccess.release
     if File.exists?(SysConfig.ReleaseFile) == false
       return "current"
     end
     return File.read(SysConfig.ReleaseFile)        
   end
   
-  def System.mysql_host
+  def SystemAccess.mysql_host
     return "mysql.engines.internal"
   end
   
