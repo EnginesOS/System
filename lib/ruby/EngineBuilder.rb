251f48fd3a2961518558eeea6823387fe47f7bfb
James Vodanovich <james@jvodan.com>
2015-03-04 15:28:45 +1100

    added create_apache_config

--- lib/ruby/EngineBuilder.rb
+++ lib/ruby/EngineBuilder.rb
@@ -1961,7 +1961,7 @@ class EngineBuilder
     end
   end
     
-    def create_apache_conf
+    def create_apache_config
       FileUtils.mkdir_p(get_basedir() + File.dirname(SysConfig.CustomApacheConfFiles))
       if @blueprint[:software].has_key?(:custom_apache_conf) && @blueprint[:software][:custom_apache_conf]  != nil            
         write_software_file(SysConfig.CustomApacheConfFile,@blueprint[:software][:custom_apache_conf])               
