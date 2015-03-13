1f119375d0996683abe4133398287ef0095befc3
James Vodanovich <james@jvodan.com>
2015-03-13 15:07:46 +1100

    missed a "

--- system/templates/deployment/global/home/config_functions.sh
+++ system/templates/deployment/global/home/config_functions.sh
@@ -24,7 +24,7 @@ templates=`find /home/engines/templates/ -type f |grep -v keep_me`
                if test $write -eq 0
                 then
                    $_dest_file=`echo $dest_file | sed "/home/s///"`
-                   echo "echo $dest_file | sed "/home/s///"
+                   echo "echo $dest_file | sed "/home/s///""
                    
                   /home/engines/scripts/grant_rw_access.sh `dirname $_dest_file`
                   /home/engines/scripts/grant_rw_access.sh $_dest_file
