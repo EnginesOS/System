77889e1460a77cabb99e08cdeee1041089feae09
James Vodanovich <james@jvodan.com>
2015-03-22 20:50:17 +1100

    save build report to buildreport.txt"

--- lib/ruby/api/system/SystemApi.rb
+++ lib/ruby/api/system/SystemApi.rb
@@ -328,7 +328,7 @@ class SystemApi
    def save_build_report(container,build_report)
       clear_error
       stateDir=SysConfig.CidDir + "/"  + container.ctype + "s/" + container.containerName
-      f = File.new(stateDir/buildreport.txt,File::CREAT|File::TRUNC|File::RDWR, 0644)
+      f = File.new(stateDir  + "/buildreport.txt",File::CREAT|File::TRUNC|File::RDWR, 0644)
       f.puts(build_report)
       f.close           
        return true
