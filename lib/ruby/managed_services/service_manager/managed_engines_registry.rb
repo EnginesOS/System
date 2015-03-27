25f96e7103b4c89bed15409aeb1e4e419f29cabe
James Vodanovich <james@jvodan.com>
2015-03-27 16:53:14 +1100

    catch failure to find engines services

--- lib/ruby/managed_services/service_manager/managed_engines_registry.rb
+++ lib/ruby/managed_services/service_manager/managed_engines_registry.rb
@@ -29,7 +29,11 @@ def find_engine_services_hashes(params)
 
   def get_engine_persistant_services(params) #params is :engine_name
     services = find_engine_services(params)
-
+    if services == nil
+      p :failed_to_find_engine_in_persistant_service
+      p params
+      return nil
+    end
     leafs = Array.new
 
     services.children.each do |service|
@@ -41,4 +45,45 @@ def find_engine_services_hashes(params)
     return leafs
 
   end
+  
+  
+def add_to_managed_engines_tree(service_hash)
+
+   if service_hash[:variables].has_key?(:parent_engine) == false || service_hash[:variables][:parent_engine] != nil
+     p :no_parent_engine_key
+     return false
+   end
+   if managed_engine_tree[service_hash[:variables][:parent_engine] ] != nil
+     engine_node = managed_engine_tree[ service_hash[:variables][:parent_engine] ]
+   else
+     engine_node = Tree::TreeNode.new(service_hash[:variables][:parent_engine],service_hash[:variables][:parent_engine] + " Engine Service Tree")
+     managed_engine_tree << engine_node
+   end
+
+   service_type_node = create_type_path_node(engine_node,service_hash[:type_path])
+
+   service_label = get_service_label(service_hash)
+
+   if service_type_node == nil
+     p service_hash
+     p :error_service_type_node
+     return false
+   end
+   if service_label == nil
+     p service_hash
+     p :error_service_hash_has_nil_name
+     return false
+   end
+
+   service_node = service_type_node[service_label]
+
+   if  service_node == nil
+     service_node = Tree::TreeNode.new(service_label,service_hash)
+     service_type_node << service_node
+   else
+     p :Node_existed
+     p service_label
+   end
+
+ end
 end
