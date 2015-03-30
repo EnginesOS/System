05879245b4a326f2e5becacae04b01de3ad9e542
James Vodanovich <james@jvodan.com>
2015-03-30 19:12:15 +1100

    added .so_s to debug output

--- lib/ruby/api/public/EnginesOSapi.rb
+++ lib/ruby/api/public/EnginesOSapi.rb
@@ -134,7 +134,7 @@ class EnginesOSapi
     end
     if engine != nil
       if engine.is_active == false
-        return failed(params[:engine_name],"Failed to start  " + @engine_builder.last_error ,"build_engine")
+        return failed(params[:engine_name],"Failed to start  " + @engine_builder.last_error.to_s ,"build_engine")
       end
       return engine
     end
