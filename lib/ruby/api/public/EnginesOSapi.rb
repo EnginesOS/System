e5d72a12f28dbb369ff786b7e5fc7fc545bab985
James Vodanovich <james@jvodan.com>
2015-03-31 11:59:55 +1100

    breakup api into sensible modules

--- lib/ruby/api/public/EnginesOSapi.rb
+++ lib/ruby/api/public/EnginesOSapi.rb
@@ -9,6 +9,8 @@ require "/opt/engines/lib/ruby/prefs/SystemPreferences.rb"
 require 'objspace'
 
 require_relative "EnginesOSapiResult.rb"
+require_relative "services_api.rb"
+include ServicesApi
 
 class EnginesOSapi
   def initialize()
@@ -160,9 +162,7 @@ class EnginesOSapi
   rescue Exception=>e
     return log_exception_and_fail("last_api_error",e)
   end
-  def list_avail_services_for(object)
-    return @core_api.list_avail_services_for(object)
-  end
+
   def list_apps()
     p :list_apps
     return @core_api.list_managed_engines
@@ -170,11 +170,7 @@ class EnginesOSapi
     return log_exception_and_fail("list_apps",e)
   end
 
-  def list_services()
-    return @core_api.list_managed_services
-  rescue Exception=>e
-    return log_exception_and_fail("list_services",e)
-  end
+
 
   def getManagedEngines()
     return  @core_api.getManagedEngines()
@@ -182,21 +178,8 @@ class EnginesOSapi
     return log_exception_and_fail("getManagedEngines",e)
   end
 
-  def getManagedServices()
-    return @core_api.getManagedServices()
-  rescue Exception=>e
-    return log_exception_and_fail("getManagedServices",e)
-  end
 
-  def self.loadManagedService(service_name,core_api)
-    service = core_api.loadManagedService(service_name)
-    if service == false
-      return self.failed(service_name,core_api.last_error ,"Load Service")
-    end
-    return service
-  rescue Exception=>e
-    return self.log_exception_and_fail("LoadMangedService",e)
-  end
+
 
   def loadManagedEngine(engine_name)
     engine = @core_api.loadManagedEngine(engine_name)   
@@ -208,16 +191,7 @@ class EnginesOSapi
     return log_exception_and_fail("loadManagedEngine",e)
   end
 
-  def getManagedService(service_name)
-
-    managed_service = EnginesOSapi.loadManagedService(service_name,@core_api)
-    #  if managed_service == nil
-    #   return failed(service_name,"Fail to Load configuration:","Load Service")
-    #end
-    return managed_service
-  rescue Exception=>e
-    return log_exception_and_fail("getManagedService",e)
-  end
+ 
 
   def backup_volume(params)
     
@@ -404,61 +378,6 @@ class EnginesOSapi
     return log_exception_and_fail("startEngine",e)
   end
 
-#  def enable_https_for_engine engine_name
-#    engine = loadManagedEngine engine_name
-#    if  engine.is_a?(EnginesOSapiResult)
-#      return failed(engine_name,"no Engine","enable_https")
-#    end
-#    retval =  engine.enable_https()
-#    if retval == false
-#      return failed(engine_name,engine.last_error,"enable_https")
-#    end
-#    return success(engine_name,"enable_https")
-#  rescue Exception=>e
-#    return log_exception_and_fail("enable_https",e)
-#  end
-#
-#  def enable_httpsonly_for_engine engine_name
-#    engine = loadManagedEngine engine_name
-#    if  engine.is_a?(EnginesOSapiResult)
-#      return failed(engine_name,"no Engine","enable_httpsonly")
-#    end
-#    retval =  engine.enable_httpsonly()
-#    if retval == false
-#      return failed(engine_name,engine.last_error,"enable_httpsonly")
-#    end
-#    return success(engine_name,"enable_httpsonly")
-#  rescue Exception=>e
-#    return log_exception_and_fail("enable_httpsonly",e)
-#  end
-#
-#  def disable_httpsonly_for_engine engine_name
-#    engine = loadManagedEngine engine_name
-#    if  engine.is_a?(EnginesOSapiResult)
-#      return failed(engine_name,"no Engine","disable_httpsonly")
-#    end
-#    retval =  engine.disable_httpsonly()
-#    if retval == false
-#      return failed(engine_name,engine.last_error,"disable_httpsonly")
-#    end
-#    return success(engine_name,"disable_httpsonly")
-#  rescue Exception=>e
-#    return log_exception_and_fail("disable_httpsonly",e)
-#  end
-#
-#  def disable_https_for_engine engine_name
-#    engine = loadManagedEngine engine_name
-#    if  engine.is_a?(EnginesOSapiResult)
-#      return failed(engine_name,"no Engine","disable_https")
-#    end
-#    retval =  engine.disable_https()
-#    if retval == false
-#      return failed(engine_name,engine.last_error,"disable_https")
-#    end
-#    return success(engine_name,"disable_https")
-#  rescue Exception=>e
-#    return log_exception_and_fail("disable_https",e)
-#  end
 
   def unpauseEngine engine_name
     engine = loadManagedEngine engine_name
@@ -853,82 +772,7 @@ class EnginesOSapi
     return log_exception_and_fail("DeRegister Service Web",e)
   end
 
-  def registerServiceDNS service_name
-    service =getManagedService(service_name)
-    if service == nil
-      return  failed(service_name,service.last_error,"Register Service DNS")
-    end
-
-    if service.is_a?(EnginesOSapiResult)
-      return service
-    end
-
-    retval =   service.register_dns()
-
-    if retval != true
-      return failed(service_name,retval,"Register Service DNS")
-    end
-    return success(service_name,"Register Service DNS")
-  rescue Exception=>e
-    return log_exception_and_fail("Register Service DNS",e)
-  end
-
-  def deregisterServiceDNS service_name
-    service =getManagedService(service_name)
-    if service == nil
-      return  failed(service_name,service.last_error,"Deregister Service DNS")
-    end
-
-    if service.is_a?(EnginesOSapiResult)
-      return service
-    end
-
-    retval =   service.deregister_dns()
-    if retval != true
-      return failed(service_name,retval,"Deregister Service DNS")
-    end
-    return success(service_name,"Deregister Service DNS")
-  rescue Exception=>e
-    return log_exception_and_fail("DeRegister Service DNS",e)
-  end
-
-  def createService service_name
-    service =getManagedService(service_name)
-    if service == nil
-      return  failed(service_name,service.last_error,"Create Service")
-    end
-
-    if service.is_a?(EnginesOSapiResult)
-      return service
-    end
-
-    retval =   service.create_service()
-    if retval == false
-      return failed(service_name,service.last_error,"Create Service")
-    end
-    return success(service_name,"Create Service")
-  rescue Exception=>e
-    return log_exception_and_fail("Create Service",e)
-  end
-
-  def recreateService service_name
-    service =getManagedService(service_name)
-    if service == nil
-      return failed(service_name,"No Such Service","Recreate Service")
-    end
-
-    if service.is_a?(EnginesOSapiResult)
-      return service
-    end
-
-    retval =   service.recreate()
-    if retval == false
-      return failed(service_name,service.last_error,"Recreate Service")
-    end
-    return success(service_name,"Recreate Service")
-  rescue Exception=>e
-    return log_exception_and_fail("Recreate Service",e)
-  end
+ 
 
   def get_volumes
 
@@ -1107,86 +951,8 @@ class EnginesOSapi
   def list_service_providers_in_use
      return @core_api.list_providers_in_use
   end
-  
-  def find_service_consumers(params)
-    p params
-    return @core_api.find_service_consumers(params)
-  end
-  
-  def get_engine_persistant_services(params)
-    return @core_api.get_engine_persistant_services(params)
-  end
-  
-  def attach_service(params)
-    if  @core_api.attach_service(params) == true
-      success(params[:parent_engine],"attach service")
-    else
-      return failed(params[:parent_engine],core_api.last_error ,params[:parent_engine])
-    end
-  end
 
-  def get_service_definition(service_type,service_provider)
-    #Fixme ignoring service_provider
-    
-    return SoftwareServiceDefinition.find(service_type,service_provider)
-  end
-  
-  def detach_service(params)
-    if   @core_api.dettach_service(params)== true
-      success(params[:parent_engine],"detach service")
-    else
-      return failed(params[:parent_engine],core_api.last_error ,params[:parent_engine])
-    end
-    end
-    
-  def get_managed_engine_tree
-    return @core_api.get_managed_engine_tree  
-  end
-  
-  def managed_service_tree
-    return @core_api.managed_service_tree
-  end
 
-  def get_orphaned_services_tree
-  return @core_api.get_orphaned_services_tree
-  end
-  
-  def software_service_definition (params)
-    retval = @core_api.software_service_definition(params)
-    if retval != nil
-      return retval
-    end 
-    p :error_software_service_definition 
-    p params
-     return failed(params[:type_path] + ":" + params[:publisher_namespace] ,@core_api.last_error,"get software_service_definition")
-  end
-  
-  #protected if protected static cant call
-  def success(item_name ,cmd)
-    return EnginesOSapiResult.success(item_name ,cmd)
-  end
-
-  def list_services_for(object)
-    return @core_api.list_services_for(object)
-  end
-  
-  def attach_subservice(params)
-    #service params and component objectname / and component name and parent name    
-  end
-  
-  def detach_subservice(params)
-  end
-  def load_avail_services_for_type(typename)
-    return  @core_api.load_avail_services_for_type(typename)
-    
-  end
-  def list_attached_services_for(object_name,identifier)
-    SystemUtils.debug_output("list_attached_services_for",object_name + " " + identifier)
-    attached = @core_api.list_attached_services_for(object_name,identifier)
-    p :found_attached
-   # p attached
-     return @core_api.list_attached_services_for(object_name,identifier)
-   end
   
   def failed(item_name,mesg ,cmd)
     p :engines_os_api_fail_on
