27de6bd6b31fbd4756e0060c21c2ae8d3a923772
James Vodanovich <james@jvodan.com>
2015-04-03 13:38:45 +1100

    add checking to rm service script

--- system/images/03.serviceImages/dns/home/rm_service.sh
+++ system/images/03.serviceImages/dns/home/rm_service.sh
@@ -6,16 +6,34 @@ service_hash=$1
 
 load_service_hash_to_environment
 
+
+
+	if test -z ${hostname}
+	then
+		echo Error:Missing hostname
+        exit -1
+    fi
+
+    
 #FIXME make engines.internal settable
 
 	fqdn_str=${hostname}.engines.internal
 	echo server 127.0.0.1 > /tmp/.dns_cmd
-	echo update delete $fqdn >> /tmp/.dns_cmd
-	echo send >> /tmp/.dns_cmd	
-	nsupdate -k /etc/dns/keysddns.private /tmp/.dns_cmd
+	echo update delete $fqdn_str >> /tmp/.dns_cmd
+	echo send >> /tmp/.dns_cmd
+	
+	nsupdate -k /etc/bind/keys/ddns.private /tmp/.dns_cmd
+	
 	if test $? -ge 0
 	then
 		echo Success
 	else
-		echo Error
+	file=`cat /tmp/.dns_cmd`
+		echo Error:With nsupdate $file
+		exit -1
 	fi
+	
+
+
+
+
