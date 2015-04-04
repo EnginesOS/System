3a0d219ea715b25de128336e98124dde747ed737
James Vodanovich <james@jvodan.com>
2015-04-04 12:33:32 +1100

    mysql to generic service

--- system/images/03.serviceImages/mysql/home/add_service.sh
+++ system/images/03.serviceImages/mysql/home/add_service.sh
@@ -11,8 +11,6 @@ BTICK='`'
 E_BADARGS=65
 MYSQL=`which mysql`
 
-
-
 if test -z $database_name
 	then
 		echo Error:No database_name value
@@ -24,17 +22,29 @@ if test -z $db_username
 		echo Error:No db_username value
 		exit -1
 	fi
+		
+if test -z $db_password
+	then
+		echo Error:No db_password value
+		exit -1
+	fi
+	
+if test -z $collation
+	then
+		echo Error:No collation value
+		exit -1
+	fi
+	
 
-
-Q1="Drop DATABASE  ${BTICK}$1${BTICK}   ;"
-Q2="DELETE FROM user where user='$2'@'%';"
+Q1="CREATE DATABASE IF NOT EXISTS ${BTICK}$database_name${BTICK}   DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE $collation ;"
+Q2="GRANT ALL ON ${BTICK}$database_name${BTICK}.* TO '$db_username'@'%' IDENTIFIED BY '$db_password';"
 Q3="FLUSH PRIVILEGES;"
 SQL="${Q1}${Q2}${Q3}"
 
 #echo "$SQL"
 
 $MYSQL   -urma  -e "$SQL"
-
 
 if $? -ge 0
 	then 
@@ -44,3 +54,4 @@ if $? -ge 0
 	
 	echo "Error:"
 	exit -1
+
