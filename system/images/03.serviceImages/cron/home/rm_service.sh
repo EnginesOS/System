ef6747bb9b441fd7031ee581f7f52e2c3814ccf5
James Vodanovich <james@jvodan.com>
2015-04-09 09:53:17 +1000

    removed stray \n in script

--- system/images/03.serviceImages/cron/home/rm_service.sh
+++ system/images/03.serviceImages/cron/home/rm_service.sh
@@ -19,8 +19,7 @@ load_service_hash_to_environment
         exit -1
     fi  
 
-if test -f     
- /home/entries/${parent_engine}/$name
+if test -f /home/entries/${parent_engine}/$name
  then
 	rm /home/cron/entries/${parent_engine}/$name
 	/home/rebuild_crontab.sh
