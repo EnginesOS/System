b9b55a598955776ba4b792e267b10d559c6f743a
James Vodanovich <james@jvodan.com>
2015-04-08 10:13:28 +1000

    handle * in cron line

--- system/images/03.serviceImages/cron/home/add_service.sh
+++ system/images/03.serviceImages/cron/home/add_service.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-service_hash=$1
+
+service_hash=`echo  "$*" | sed "/\*/s//STAR/g"`
+
+
 
 . /home/engines/scripts/functions.sh
 
@@ -18,10 +21,15 @@ load_service_hash_to_environment
 		echo Error:missing name
         exit -1
     fi  
+    	if test -z ${parent_engine}
+	then
+		echo Error:missing parent_engine
+        exit -1
+    fi  
     
 mkdir -p /home/entries/${parent_engine}/
 
-echo $cron_line > /home/cron/entries/${parent_engine}/$name
+echo $cron_line  | sed "/STAR/s//\*/g" > /home/cron/entries/${parent_engine}/$name
 
 /home/rebuild_crontab.sh
 
