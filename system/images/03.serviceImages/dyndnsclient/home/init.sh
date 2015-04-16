12385478ffa731a07287773f91d54ad12d69d749
James Vodanovich <james@jvodan.com>
2015-04-16 15:27:06 +1000

    work on dyndns config

--- system/images/03.serviceImages/dyndnsclient/home/init.sh
+++ system/images/03.serviceImages/dyndnsclient/home/init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+mkdir -p  /home/dyndns/cache
+
 while test 0 -ne 1
 do
-	ddclient
+	ddclient -web -ssl -syslog -f /home/dyndns/dyndns.conf -cache /home/dyndns/cache  -pid /home/dyndns/dyndns.pid
 	sleep 300
 done
