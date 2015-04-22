23fb6736429c167604bacc55fa6f0749dd23754a
James Vodanovich <james@jvodan.com>
2015-04-22 16:01:49 +1000

    moved my.cnf settings changes to Docker file

--- system/images/03.serviceImages/mysql/home/startworker.sh
+++ system/images/03.serviceImages/mysql/home/startworker.sh
@@ -2,8 +2,7 @@
 
 
 
- cat /etc/mysql/my.cnf |sed "/127.0.0.1/s//0.0.0.0/" >/etc/mysql/my.cnf.sed
-mv /etc/mysql/my.cnf.sed /etc/mysql/my.cnf
+
 /etc/init.d/mysql start
 
 mkdir -p /engines/var/run/
