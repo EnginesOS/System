d26b0a92d220cb229e5af40fb04cccab0c5eca72
James Vodanovich <james@jvodan.com>
2015-04-24 17:18:14 +1000

    tidy script

--- system/templates/deployment/global/home/setup.sh
+++ system/templates/deployment/global/home/setup.sh
@@ -3,13 +3,13 @@
 cd /home/
 		source /home/config_functions.sh
 		copy_substituted_templates		
-		cd /home/app			
+					
 	
 	
-	if test -f /home/engines/scripts/custom_install.sh 
+	if test -f engines/scripts/custom_install.sh 
 	then
 		echo running custom install
-	    bash /home/engines/scripts/custom_install.sh
+	     engines/scripts/custom_install.sh
 	fi 
 	
 	
