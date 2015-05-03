94314d14efcd3d0f2f6badd8b6025d0c57078008
James Vodanovich <james@jvodan.com>
2015-05-03 15:07:18 +1000

    access to auth via ssh

--- system/images/03.serviceImages/auth/home/init.sh
+++ system/images/03.serviceImages/auth/home/init.sh
@@ -5,9 +5,15 @@
 #source /home/trap.sh
 
 
-
+if test -f /home/auth/first_run.sh
+	then
+		/home/auth/first_run.sh
+		mv /home/auth/first_run.sh /home/auth/first_run.done
+	fi
 
 touch /var/run/startup_complete
+
+sshd -f /home/ssh/sshd_config
 exec syslogd -n -R syslog.engines.internal:5140
 
 
