e6679259cf867cabd751219147b3416871302af4
James Vodanovich <james@jvodan.com>
2015-05-04 17:28:59 +1000

    advance in templating to support completing service_hashs loaded from disk (boot strapping services)

---
+++ lib/ruby/managed_services/service_manager/process_templates.rb
@@ -0,0 +1,10 @@
+module ProcessTemplates
+  @builder_public = nil
+  @system_access = nil
+  #@builder_public.blueprint
+  @engine_public = nil
+  def engine_environment
+    return nil
+  end
+  
+end
