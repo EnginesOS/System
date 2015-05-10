8ec9dc1af52fcbc9402eb3bde22247ce89b8b1c2
James Vodanovich <james@jvodan.com>
2015-05-10 10:18:07 +1000

    needs work

--- system/images/03.serviceImages/mysql/home/firstrun.sh
+++ system/images/03.serviceImages/mysql/home/firstrun.sh
@@ -9,7 +9,7 @@ chown mysql /var/log/mysql
 	/usr/bin/mysql_install_db
 	cat /etc/mysql/my.cnf |sed "/127.0.0.1/s//0.0.0.0/" >/etc/mysql/my.cnf.sed
 	mv /etc/mysql/my.cnf.sed /etc/mysql/my.cnf
-	/etc/init.d/mysql start
+	#/etc/init.d/mysql start
 	echo "CREATE USER 'rma'@'localhost';  grant all ON *.* TO  'rma'@'localhost'  WITH GRANT OPTION; " |mysql -u root
 	echo "CREATE USER 'root'@'%' identified by '$pass';  grant all ON *.* TO  'root'@'%'  WITH GRANT OPTION; "
 	echo "CREATE USER 'root'@'%' identified by '$pass';  grant all ON *.* TO  'root'@'%'  WITH GRANT OPTION; " |mysql -u root
