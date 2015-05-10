064b8fea1ecb6a66d83ee8b21761fe1d5506fed8
James Vodanovich <james@jvodan.com>
2015-05-10 11:41:36 +1000

    container user now postfix

--- system/images/03.serviceImages/smtp/home/init.sh
+++ system/images/03.serviceImages/smtp/home/init.sh
@@ -3,20 +3,6 @@
 
 sudo /sbin/syslogd -R syslog.engines.internal:5140
 
-
-exec sudo /usr/lib/postfix/master
-#service postfix start 
 postmap /etc/postfix/transport 
+exec sudo /usr/lib/postfix/master
 
-mkdir -p /engines/var/run/
-touch  /engines/var/run/startup_complete
-chgrp containers /engines/var/run/startup_complete
-
-sleep 30
-while test -f /var/spool/postfix/pid/master.pid
-do
-	  sleep 200
-done
-
-
-rm /engines/var/run/startup_complete
