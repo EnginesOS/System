cbca8f7a4ed527d8366159859af5f6ad000e5442
James Vodanovich <james@jvodan.com>
2015-05-12 17:38:54 +1000

    log additions and removals (will grow quickly)

--- etc/auth/scripts/ftp/add_service.sh
+++ etc/auth/scripts/ftp/add_service.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 echo add_ftp_service.sh
-
-echo $SSH_ORIGINAL_COMMAND
+TS=` date +%Y%m%d%H%M%S`
+echo $TS: $SSH_ORIGINAL_COMMAND >> add.log
+#FIXME should be /var/log/engines/services/auth/ftp/rm.log
--- etc/auth/scripts/ftp/rm_service.sh
+++ etc/auth/scripts/ftp/rm_service.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 echo rm_ftp_service.sh
-
-echo $SSH_ORIGINAL_COMMAND
+TS=` date +%Y%m%d%H%M%S`
+echo $TS: $SSH_ORIGINAL_COMMAND >> rm.log
+#FIXME should be /var/log/engines/services/auth/ftp/rm.log
