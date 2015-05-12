094d4f72d9ec8668955f467e1ae3577600b46b29
James Vodanovich <james@jvodan.com>
2015-05-12 17:44:13 +1000

    logs to /var/log/engines/services/auth/ftp

--- etc/auth/scripts/ftp/rm_service.sh
+++ etc/auth/scripts/ftp/rm_service.sh
@@ -2,5 +2,5 @@
 
 echo rm_ftp_service.sh
 TS=` date +%Y%m%d%H%M%S`
-echo $TS: $SSH_ORIGINAL_COMMAND >> rm.log
-#FIXME should be /var/log/engines/services/auth/ftp/rm.log
+echo $TS: $SSH_ORIGINAL_COMMAND >> /var/log/engines/services/auth/ftp/rm.log
+
