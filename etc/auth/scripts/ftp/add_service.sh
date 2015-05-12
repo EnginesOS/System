0a67088ebe18fba645f0c6a1eb2a1eb934b99d5a
James Vodanovich <james@jvodan.com>
2015-05-12 17:45:56 +1000

    use container local path

--- etc/auth/scripts/ftp/add_service.sh
+++ etc/auth/scripts/ftp/add_service.sh
@@ -2,5 +2,5 @@
 
 echo add_ftp_service.sh
 TS=` date +%Y%m%d%H%M%S`
-echo $TS: $SSH_ORIGINAL_COMMAND >> /var/log/engines/services/auth/ftp/add.log
+echo $TS: $SSH_ORIGINAL_COMMAND >> /var/log/ftp/add.log
 
