970660972c242d5dce09ae77b22e78414a7f5d8c
James Vodanovich <james@jvodan.com>
2015-05-28 18:44:33 +1000

    container_name

--- lib/ruby/managed_services/ManagedService.rb
+++ lib/ruby/managed_services/ManagedService.rb
@@ -216,8 +216,7 @@ class ManagedService < ManagedContainer
   def create_service()
     if Dir.exists?("/opt/engines/ssh/keys/services/" + container_name) == false    
       FileUtils.mkdir_p("/opt/engines/ssh/keys/services/" + container_name)
-      SystemUtils.run_command("/opt/engines/scripts/setup_service_key_dir.sh " +container_name)
-      
+      SystemUtils.run_command("/opt/engines/scripts/setup_service_key_dir.sh " +container_name)      
     end
     envs = @core_api.load_and_attach_persistant_services(self)
     if envs !=nil    
@@ -234,7 +233,7 @@ class ManagedService < ManagedContainer
       
       @core_api.load_and_attach_nonpersistant_services(self)       
 
-      @core_api.register_non_persistant_services(container_name)
+      @core_api.register_non_persistant_services(self)
           
       reregister_consumers()
       save_state()
