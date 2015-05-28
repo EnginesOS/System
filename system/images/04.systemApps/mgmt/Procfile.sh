9a9ff17b9d09dd33891dd621934e213f870bc2c1
James Vodanovich <james@jvodan.com>
2015-05-28 13:14:50 +1000

    handle prexisting icons in persistant storage

--- system/images/04.systemApps/mgmt/Procfile.sh
+++ system/images/04.systemApps/mgmt/Procfile.sh
@@ -3,6 +3,8 @@ PATH="/usr/local/rbenv/bin:$PATH"
 mkdir /var/log/apache2
 cd /home/app/
 rm /home/app/app/config/newrelic.yml
+git fetch origin master
+git reset --hard FETCH_HEAD
 git pull --depth 1
 cp /home/newrelic.yml /home/app/
 
