4b72cc06137b5dbee23a2b0ba2a55b4260f18da3
James Vodanovich <james@jvodan.com>
2015-06-02 14:30:12 +1000

    logs_container

--- lib/ruby/api/system/DockerApi.rb
+++ lib/ruby/api/system/DockerApi.rb
@@ -123,14 +123,16 @@ class DockerApi
      clear_error
   
        commandargs="docker logs " + container.container_name
-    if  execute_docker_cmd(commandargs,container) == false
-                return container.last_result
-              else
-                return container.last_error 
-              end
-            rescue  Exception=>e
-            SystemUtils.log_exception(e)
-            return "error"        
+     result = SystemUtils.execute_command(cmdline)
+     if result[:result] == 0
+       return result[:stdout]
+     end
+     
+    return    false
+    
+     rescue  Exception=>e
+         SystemUtils.log_exception(e)
+         return "error"        
    end
 
    def inspect_container container
