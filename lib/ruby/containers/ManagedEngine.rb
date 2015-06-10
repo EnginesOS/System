9ea3605efd1bd79588a996e8846cc23dd110a0dd
James Vodanovich <james@jvodan.com>
2015-06-10 18:21:04 +1000

    return service hash as string

--- lib/ruby/containers/ManagedEngine.rb
+++ lib/ruby/containers/ManagedEngine.rb
@@ -46,7 +46,18 @@ class ManagedEngine < ManagedContainer
   end
 
   def engine_persistant_services
-    return @core_api.engine_persistant_services(@container_name)
+    services = @core_api.engine_persistant_services(@container_name)
+    retval = String.new
+    
+    if services.is_a?(Array)
+      services.each do |service|
+        retval += SystemUtils.service_hash_variables_as_str(service)
+      end
+    elsif services.is_a?(Hash)
+      retval = SystemUtils.service_hash_variables_as_str(services)
+    end
+    
+    return retval
   end
   
     
--- lib/ruby/system/SystemUtils.rb
+++ lib/ruby/system/SystemUtils.rb
@@ -288,4 +288,20 @@ def SystemUtils.execute_command(cmd)
     return service_hash
 
   end
+  
+  
+def SystemUtils.service_hash_variables_as_str(service_hash)
+   argument = String.new
+     
+   service_variables =  service_hash[:variables]
+     if service_variables == nil
+       return argument
+     end
+   service_variables.each_pair do |key,value|
+     argument+= key.to_s + "=" + value.to_s + ":"      
+   end
+   
+   return argument
+ end
+ 
 end
