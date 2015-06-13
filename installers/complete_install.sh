0bf4b61c0f4edca77f66c7916aca67250a5d0de0
James Vodanovich <james@jvodan.com>
2015-06-13 13:38:57 +1000

    update ruby version

--- installers/complete_install.sh
+++ installers/complete_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-RUBY_VER=2.1.3
+RUBY_VER=2.2.2
 export RUBY_VER
 . /tmp/203.14.203.141/EnginesInstaller/routines.sh
 
-rbenv install 2.1.3
-rbenv  local 2.1.3
+rbenv install 2.2.2
+rbenv  local 2.2.2
  	~/.rbenv/shims/gem install multi_json rspec rubytree git 
 #gem install multi_json rspec rubytree git 
 
