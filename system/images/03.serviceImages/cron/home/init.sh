e6dc3f79a5adc05b99d6ff66588976d497a58f3a
James Vodanovich <james@jvodan.com>
2015-06-15 08:50:01 +1000

    correct shell syntax

--- system/images/03.serviceImages/cron/home/init.sh
+++ system/images/03.serviceImages/cron/home/init.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 
-PID+FILE=/home/cron/fcron.pid
+PID_FILE=/home/cron/fcron.pid
 export PID_FILE
-source /home/trap.sh
+. /home/trap.sh
 
 /home/cron/sbin/fcron -p  /home/cron/log/cron.log
 
