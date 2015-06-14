a5a7523d28b108529caf9a313801942058eea192
James Vodanovich <james@jvodan.com>
2015-06-14 19:32:01 +1000

    dns not root user

--- system/images/03.serviceImages/dns/home/init.sh
+++ system/images/03.serviceImages/dns/home/init.sh
@@ -6,22 +6,10 @@ PIDFILE=/var/run/named/named.pid
 export PIDFILE
 source /home/trap.sh
 
-mkdir -p /var/run/named
-chown -R bind /var/run/named
-mkdir -p /var/log/named
-chown -R bind /var/log/named
-
-cd /tmp
-/usr/sbin/dnssec-keygen -a HMAC-MD5 -b 128 -n HOST  -r /dev/urandom -n HOST DDNS_UPDATE >/dev/null
-mv *private /etc/bind/keys/ddns.private
-mv *key /etc/bind/keys/ddns.key
-key=`cat /etc/bind/keys/ddns.private |grep Key | cut -f2 -d" "`
-cp /etc/bind/templates/named.conf.default-zones.start /etc/bind/named.conf.default-zones;\
-echo "secret \"$key\";" >> /etc/bind/named.conf.default-zones;\
-cat /etc/bind/templates/named.conf.default-zones.end >> /etc/bind/named.conf.default-zones
+sudo /home/setup.sh
 
 sudo syslogd  -R syslog.engines.internal:5140
- sudo /usr/sbin/named  -c /etc/bind/named.conf -u bind 
- wait $! 
+sudo /usr/sbin/named  -c /etc/bind/named.conf -u bind 
+wait $! 
 
 
