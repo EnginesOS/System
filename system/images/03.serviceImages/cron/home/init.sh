6c762fc71871461b30c9341edaf4015cefe5e92a
James Vodanovich <james@jvodan.com>
2015-06-15 10:00:01 +1000

    fixes

--- system/images/03.serviceImages/cron/home/init.sh
+++ system/images/03.serviceImages/cron/home/init.sh
@@ -5,12 +5,12 @@ PID_FILE=/home/cron/fcron.pid
 export PID_FILE
 . /home/trap.sh
 
-/home/cron/sbin/fcron -p  /home/cron/log/cron.log
+
 
 touch /engines/var/run/startup_complete
 
 sudo syslogd  -R syslog.engines.internal:5140
-/home/cron/sbin/fcron -f -p  /home/cron/log/cron.log  
+/home/cron/sbin/fcron -f -p  /home/cron/log/cron.log &  
 wait 
 
 rm -f /engines/var/run/startup_complete
