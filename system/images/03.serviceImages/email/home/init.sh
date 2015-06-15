39203e45ff2f9c2cf48f8cc5b61a1d2c50f1a253
James Vodanovich <james@jvodan.com>
2015-06-16 09:41:34 +1000

    tweak startup to wait

--- system/images/03.serviceImages/email/home/init.sh
+++ system/images/03.serviceImages/email/home/init.sh
@@ -6,10 +6,11 @@ export PID_FILE
 . /home/trap.sh
 mkdir -p /engines/var/run/flags/
 sudo /sbin/syslogd -R syslog.engines.internal:5140
-sudo /usr/sbin/apache2ctl start
+
 postmap /etc/postfix/transport 
 postmap /etc/postfix/smarthost_passwd
 /usr/lib/postfix/master &
+sudo /usr/sbin/apache2ctl  -DFOREGROUND & 
 touch /engines/var/run/flags/startup_complete  
 wait 
 
