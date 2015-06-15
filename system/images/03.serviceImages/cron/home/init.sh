a232465f7829bb32201f0fb02d43b8e519e3c28d
James Vodanovich <james@jvodan.com>
2015-06-15 10:27:33 +1000

    mkdir flags dir

--- system/images/03.serviceImages/cron/home/init.sh
+++ system/images/03.serviceImages/cron/home/init.sh
@@ -6,11 +6,11 @@ export PID_FILE
 . /home/trap.sh
 
 
-
-touch /engines/var/run/startup_complete
+mkdir -p /engines/var/run/flags/
+touch /engines/var/run/flags/startup_complete
 
 sudo syslogd  -R syslog.engines.internal:5140
 /home/cron/sbin/fcron -f -p  /home/cron/log/cron.log &  
 wait 
 
-rm -f /engines/var/run/startup_complete
+rm -f /engines/var/run/flags/startup_complete
