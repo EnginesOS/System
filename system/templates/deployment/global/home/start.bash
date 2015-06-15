7634d1eed023b3aa828ca1c35436f3de9a5a7791
James Vodanovich <james@jvodan.com>
2015-06-15 12:53:48 +1000

    trap argu

--- system/templates/deployment/global/home/start.bash
+++ system/templates/deployment/global/home/start.bash
@@ -54,9 +54,9 @@ if test -f /home/engines/scripts/startwebapp.sh
 	
 #Apache based below here
 
-PIDFILE=/run/apache2/apache2.pid
-export PIDFILE
-source /home/trap.sh
+PID_FILE=/run/apache2/apache2.pid
+export PID_FILE
+. /home/trap.sh
 
   
 if test -f /home/app/Rack.sh
@@ -65,7 +65,7 @@ if test -f /home/app/Rack.sh
 		. /home/app/Rack.sh  
 	fi
 
-touch /engines/var/run/startup_complete
+
 mkdir -p /var/log/apache2/ >/dev/null
 
 	if test -f /home/blocking.sh
@@ -76,9 +76,6 @@ mkdir -p /var/log/apache2/ >/dev/null
 		/usr/sbin/apache2ctl start
 	
 	fi	
- 
- wait $!
-
- 
- 
- rm /engines/var/run/startup_complete
+ touch /engines/var/run/startup_complete
+ wait 
+ /engines/var/run/startup_complete
