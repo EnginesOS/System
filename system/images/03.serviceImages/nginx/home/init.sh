0d4cb3799dd79bd0f3792239886aaa6b1cf16302
James Vodanovich <james@jvodan.com>
2015-06-19 11:41:19 +1000

    change to init.sh start

--- system/images/03.serviceImages/nginx/home/init.sh
+++ system/images/03.serviceImages/nginx/home/init.sh
@@ -3,20 +3,20 @@
 rm /etc/nginx/sites-enabled/http*
 PID_FILE=/var/run/nginx.pid
 
- source /home/trap.sh
+. /home/trap.sh
 
+
+mkdir -p /engines/var/run/
 
 /usr/sbin/nginx
-echo "started Nginx"
-mkdir -p /engines/var/run/
-touch  /engines/var/run/startup_complete
-chmod oug+rw /engines/var/run/startup_complete
-echo 
-sleep 30
 
-while test -f /var/run/nginx.pid
-do
-	  sleep 200
-done
+
+touch  /engines/var/run/startup_complete
+
+while test -f $PID_FILE
+	do
+		sleep 600 &
+		wait
+	done
 
 rm /engines/var/run/startup_complete
