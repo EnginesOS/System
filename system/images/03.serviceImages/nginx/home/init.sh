4d2eac6f850b18b28a0926d190d45a8af6a73c0d
James Vodanovich <james@jvodan.com>
2015-06-19 12:56:49 +1000

    export pid file and set flags dir

--- system/images/03.serviceImages/nginx/home/init.sh
+++ system/images/03.serviceImages/nginx/home/init.sh
@@ -2,16 +2,16 @@
 
 rm /etc/nginx/sites-enabled/http*
 PID_FILE=/var/run/nginx.pid
-
+export PID_FILE
 . /home/trap.sh
 
 
-mkdir -p /engines/var/run/
+mkdir -p /engines/var/run/flags/
 
 /usr/sbin/nginx
 
 
-touch  /engines/var/run/startup_complete
+touch  /engines/var/run/flags/startup_complete
 
 while test -f $PID_FILE
 	do
@@ -19,4 +19,4 @@ while test -f $PID_FILE
 		wait
 	done
 
-rm /engines/var/run/startup_complete
+rm /engines/var/run/flags/startup_complete
