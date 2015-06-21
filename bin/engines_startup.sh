25f42ecfadfa40084742dd808aae2e72f27f442e
James Vodanovich <james@jvodan.com>
2015-06-21 13:13:57 +1000

    support two stage installer

--- bin/engines_startup.sh
+++ bin/engines_startup.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-/sbin/ifconfig eth0 |grep "inet addr"  |  cut -f 2 -d: |cut -f 1 -d" " > /opt/engines/.ip 
-
-/opt/engines/bin/containers_startup.sh &
-
-if test ` docker ps -a |grep mgmt |wc -c` -eq 0
+if test -f  /opt/engines/.complete_install
 then
-
-eservice create mgmt
-
+   /opt/engines/installers/complete_install.sh
 else
- docker start mgmt
+	/opt/engines/bin/containers_startup.sh &
+
+	if test ` docker ps -a |grep mgmt |wc -c` -eq 0
+		then
+
+			eservice create mgmt
+
+	else
+ 			eservice start mgmt
+	fi
 fi 
 
 
