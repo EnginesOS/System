d850b2a62286873bf23bd95cc228e0114c39a5b7
James Vodanovich <james@jvodan.com>
2015-06-30 11:53:45 +1000

    fix sed syntax

--- system/images/03.serviceImages/email/home/init.sh
+++ system/images/03.serviceImages/email/home/init.sh
@@ -13,17 +13,17 @@ sudo -n postmap /etc/postfix/smarthost_passwd
 sudo -n /usr/lib/postfix/master &
 
 	
- echo dbflavor=$dbflavor >/home/auth/.dbenv
+ echo dbflavor=$dbflavor >/home/.dbenv
  echo dbhost=$dbhost >>/home/auth/.dbenv
- echo dbname=$dbname >>/home/auth/.dbenv
- echo dbpasswd=$dbpasswd >>/home/auth/.dbenv
- echo dbuser=$dbuser >>/home/auth/.dbenv
+ echo dbname=$dbname >>/home/.dbenv
+ echo dbpasswd=$dbpasswd >>/home/.dbenv
+ echo dbuser=$dbuser >>/home/.dbenv
 
-cat /home/app/_config.inc.php |\
- sed "/DBHOST/s/$dbhost//"\
-	 "/DBNAME/s/$dbname//"\
-	 "/DBUSER/s/$dbuser//"\
-	  "/DBPASSWD/s/$dbpasswd//" > /home/app/config.inc.php
+cat /home/app/_config.inc.php \
+ | sed "/DBHOST/s//$dbhost/"\
+	| sed  "/DBNAME/s//$dbname/"\
+	| sed  "/DBUSER/s//$dbuser/"\
+	| sed   "/DBPASSWD/s//$dbpasswd/" > /home/app/config.inc.php
 
 sudo -n  /usr/sbin/apache2ctl  -DFOREGROUND & 
 touch /engines/var/run/flags/startup_complete  
