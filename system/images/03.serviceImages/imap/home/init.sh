ef3464aefbd39af552c1a5baf61b9c45b3d5940b
James Vodanovich <james@jvodan.com>
2015-06-30 16:01:25 +1000

    start imap as root

--- system/images/03.serviceImages/imap/home/init.sh
+++ system/images/03.serviceImages/imap/home/init.sh
@@ -15,7 +15,7 @@ cat /home/_dovecot-sql.conf.ext \
 
 sudo syslogd  -R syslog.engines.internal:5140
 
-/usr/sbin/dovecot -F &
+sudo /usr/sbin/dovecot -F &
 touch  /engines/var/run/flags/startup_complete
 wait
 sudo /home/engines/scripts/_kill_syslog.sh
