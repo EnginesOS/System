ff581a7b50a5a87ca5565a8c50db3c1004bb5f6d
James Vodanovich <james@jvodan.com>
2015-07-01 10:24:55 +1000

    start of sql backup

--- system/images/03.serviceImages/backup/home/configurators/set_system_backup.sh
+++ system/images/03.serviceImages/backup/home/configurators/set_system_backup.sh
@@ -12,6 +12,7 @@ load_service_hash_to_environment
  echo "$*" >>/var/log/backup/addbackup.log
 
 Backup_ConfigDir=/home/backup/.duply/
+export
 
 service_hash=`cat /home/configurators/saved/default_destination`
 load_service_hash_to_environment
@@ -40,7 +41,7 @@ echo "TARGET_PASS='$pass'"  >>$Backup_ConfigDir/system/conf
 						src=/home/sql_dumps
 						mkdir -p $Backup_ConfigDir/system_databases
 						cat /home/tmpl/dumpall >>  $Backup_ConfigDir/system_databases/pre
-                		cp /home/tmpl/duply_sql_post  $Backup_ConfigDir/system_databases/post
+                		cp /home/tmpl/dumpall_post  $Backup_ConfigDir/system_databases/post
                 		chmod u+x $Backup_ConfigDir/system/pre
                 		chmod u+x $Backup_ConfigDir/system/post
                 		cp /home/tmpl/duply_conf $Backup_ConfigDir/system_databases/conf
