64ff620f8c657155210945435ba83a93fea495e5
James Vodanovich <james@jvodan.com>
2015-07-11 16:54:42 +1000

    error check

--- system/images/03.serviceImages/auth/home/get_access.sh
+++ system/images/03.serviceImages/auth/home/get_access.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 service=`echo $SSH_ORIGINAL_COMMAND |  awk -F/ '{print $6}'`
-cat /home/auth/static/access/$service/access
-#echo $SSH_ORIGINAL_COMMAND
+	if test -f /home/auth/static/access/$service/access
+		then
+			cat /home/auth/static/access/$service/access
+		
+else
+	echo err with $SSH_ORIGINAL_COMMAND
+fi
