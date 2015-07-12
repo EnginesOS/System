c0c099c52b63020551e9acddaeed932fa61aacc4
James Vodanovich <james@jvodan.com>
2015-07-12 11:55:56 +1000

    change key location to ftpd home dir

--- system/images/03.serviceImages/ftp/home/get_pubkey.sh
+++ system/images/03.serviceImages/ftp/home/get_pubkey.sh
@@ -2,12 +2,13 @@
 
 if test $1 = 'add' -o $1 = 'rm' -o $1 = 'access'
  then
-	if test -f /home/.ssh/${1}_rsa.pub
+	if test -f /home/ftpd/.ssh/${1}_rsa.pub
 		then
-	 		cat /home/.ssh/${1}_rsa.pub | awk '{print $2}'	
+	 		cat /home/ftpd/.ssh/${1}_rsa.pub | awk '{print $2}'	
  		else
- 	 		ssh-keygen  -f /home/.ssh/${1}_rsa -P "" > /dev/null
- 	 		cat /home/.ssh/${1}_rsa.pub | awk '{print $2}' 	 	
+ 			mkdir /home/ftpd/.ssh
+ 	 		ssh-keygen  -f /home/ftpd/.ssh/${1}_rsa -P "" > /dev/null
+ 	 		cat /home/ftpd/.ssh/${1}_rsa.pub | awk '{print $2}' 	 	
  	fi
  fi
  
