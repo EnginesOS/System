827d8324ee550e1716d0b913c5363d65b4dc1bdb
James Vodanovich <james@jvodan.com>
2015-07-17 18:16:44 +1000

    provide facility to disable gui reboot

--- bin/restart_system.sh
+++ bin/restart_system.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-sudo /opt/engines/scripts/_restart_system.sh &
-echo restarting
-exit
+ if ! test -f /opt/engines/etc/reboot_disabled
+ 	then
+		sudo /opt/engines/scripts/_restart_system.sh &
+		echo restarting
+		exit
+	fi
+	
+exit 127
