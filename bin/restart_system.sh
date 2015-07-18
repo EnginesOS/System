a6625d2da1389c83396d2c1030d18e23a797b191
James Vodanovich <james@jvodan.com>
2015-07-18 10:24:09 +1000

    just use system to restart (no io handling)

--- bin/restart_system.sh
+++ bin/restart_system.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
  if ! test -f /opt/engines/etc/reboot_disabled
  	then
-		sudo /opt/engines/scripts/_restart_system.sh &
+		sudo /opt/engines/scripts/_restart_system.sh  &
 		echo restarting
 		exit
 	fi
