b83be51463a4edba99952b63ef2326843745ec67
James Vodanovich <james@jvodan.com>
2015-07-22 11:15:32 +1000

    add follow_start

---
+++ bin/follow_start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+docker logs -f mgmt &
+pid=$!
+
+while ! test -f /opt/engines/run/services/mgmt/run/flags/startup_complete 
+ do
+    sleep 5
+ done
+ 
+ kill $pid
+ 
+ gw_ifac=`netstat -nr |grep ^0.0.0.0 | awk '{print $8}'`
+
+lan_ip=`/sbin/ifconfig $gw_ifac |grep "inet addr"  |  cut -f 2 -d: |cut -f 1 -d" "`
+ext_ip=`curl http://ipecho.net/plain`
+echo please visit https://$lan_ip:10443/ or https://$ext_ip:10443/
