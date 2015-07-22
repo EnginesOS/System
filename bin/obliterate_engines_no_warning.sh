d34a39add6cd03c91912a20070bb9cda3c1ab951
James Vodanovich <james@jvodan.com>
2015-07-22 14:09:04 +1000

    obliterate

---
+++ bin/obliterate_engines_no_warning.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "15 seconds until destruction with no visual countdown starting now"
+sleep 20
+if test -d EnginesInstaller
+	then
+		service docker stop
+		rm -r /var/lib/engines
+		rm -r /var/log/engines
+		rm -r /opt/engines
+		 rm -r /var/spool/cron/crontabs/engines
+		apt-get -y remove lxc-docker
+		apt-get -y autoremove
+		userdel -r  engines
+		groupdel engines
+		cat /etc/rc.local |grep -v engines >/tmp/.local
+		cp /tmp/.local   /etc/rc.local
+		 rm -r EnginesInstaller
+		groupdel containers
+		rm -rf /usr/local/rbenv
+		 rm /etc/network/if-up.d/set_ip.sh 
+    else
+      echo Script must be run as root from the dir that contains EnginesInstaller
+fi
