9f82d94c37bf134532da5f7cf676f4e5ddfd542b
James Vodanovich <james@jvodan.com>
2015-07-23 16:27:47 +1000

    set persistant

--- lib/ruby/api/public/FirstRunWizard.rb
+++ lib/ruby/api/public/FirstRunWizard.rb
@@ -128,7 +128,7 @@ class FirstRunWizard
     service_param[:type_path] = "cert_auth"
     service_param[:service_container_name] = "cert_auth"
      service_param[:container_type] = "system"
-
+    service_param[:persistant]=true
        service_param[:publisher_namespace] = "EnginesSystem"
        service_param[:service_handle] ="default_ssl_cert"
        service_param[:variables] = Hash.new
