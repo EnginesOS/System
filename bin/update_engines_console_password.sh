b8909c39a80eb941b212631044791a753a85776d
James Vodanovich <james@jvodan.com>
2015-07-25 10:27:47 +1000

    duh arg not a hash

--- bin/update_engines_console_password.sh
+++ bin/update_engines_console_password.sh
@@ -2,26 +2,9 @@
 
 args=$SSH_ORIGINAL_COMMAND
 
-config_params=`echo  $SSH_ORIGINAL_COMMAND | awk '{print $2}'`
+console_password=`echo  $SSH_ORIGINAL_COMMAND | awk '{print $2}'`
 
-n=1
 
-fcnt=`echo $config_params| grep -o : |wc -l`
-
-fcnt=`expr $fcnt + 1`
-
-        while test $fcnt -ge $n
-        do
-                nvp="`echo $config_params |cut -f$n -d:`"
-                n=`expr $n + 1`
-                name=`echo $nvp |cut -f1 -d=`
-                value=`echo $nvp |cut -f2 -d=`
-                if test ${#name} -gt 0
-                	then
-                		export $name="$value"
-                	fi
-        done
-        
 if test -z $console_password
  then
  	echo Error:no Console Password
