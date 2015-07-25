a07544393422f47d2eb0de742af4daf21a6b8fb5
James Vodanovich <james@jvodan.com>
2015-07-25 10:33:35 +1000

    dbg

--- bin/update_engines_console_password.sh
+++ bin/update_engines_console_password.sh
@@ -2,8 +2,9 @@
 
 args=$SSH_ORIGINAL_COMMAND
 
-console_password=`echo  $SSH_ORIGINAL_COMMAND | awk '{print $2}'`
+console_password=`echo  '$SSH_ORIGINAL_COMMAND' | awk '{print $2}'`
 
+echo $console_password
 
 if test -z $console_password
  then
@@ -11,5 +12,5 @@ if test -z $console_password
  	exit -1
  fi
  
- sudo /opt/engines/bin/scripts/_update_engines_console_password.sh $console_password
+ sudo /opt/engines/bin/scripts/_update_engines_console_password.sh '$console_password'
  
