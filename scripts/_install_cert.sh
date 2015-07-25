da077cb5ee3750b058c5bc49c2f72f8c31a2523f
James Vodanovich <james@jvodan.com>
2015-07-25 19:42:17 +1000

    move location of service certs to /opt/engines/etc/{service_name}/ssl

--- scripts/_install_cert.sh
+++ scripts/_install_cert.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 name=$1
-file_prefix=/opt/engines/etc/certs/engines/${name}
+file_prefix=/opt/engines/etc/ssl/certs/engines/${name}
 chmod og-rw ${file_prefix}.key
 chmod og-w ${file_prefix}.crt
 
@@ -12,42 +12,42 @@ chmod og-w ${file_prefix}.crt
 		exit
 	fi
 	
-	
+	#AS DOMAIN CERT for NGINX
 		cp -p $file_prefix.crt   /opt/engines/etc/nginx/ssl/certs/$name.crt 
 		chown 22005 /opt/engines/etc/nginx/ssl/certs/$name.crt 
-
 		cp -p $file_prefix.key   /opt/engines/etc/nginx/ssl/keys/$name.key
 		chown 22005 /opt/engines/etc/nginx/ssl/keys/$name.key
 		
 #Below here install default cert FIXME to only do so if asked and extend so individual certs can be installed per service ie imap. smtp.
 		cp -p $file_prefix.crt /opt/engines/etc/ssl/certs/engines.crt
 		chown 21000 /opt/engines/etc/ssl/certs/engines.crt
-
 		cp -p $file_prefix.key /opt/engines/etc/ssl/keys/engines.key 
 		chown 21000 /opt/engines/etc/ssl/engines.key 
 
-		cp -p $file_prefix.crt /opt/engines/etc/ssl/smtp/engines.crt 
-		chown 22003 /opt/engines/etc/ssl/smtp/engines.crt 
+		cp -p $file_prefix.crt /opt/engines/etc/ftp/ssl/engines.crt 
+		chown 22003 /opt/engines/etc/ftp/ssl/engines.crt 
+		cp -p $file_prefix.key /opt/engines/etc/ftp/ssl/keys/engines.key
+		chown 22003 /opt/engines/etc/ftp/ssl/keys/engines.key
 
-		cp -p $file_prefix.key /opt/engines/etc/ssl/smtp/keys/engines.key
-		chown 22003 /opt/engines/etc/ssl/smtp/keys/engines.key
+		cp -p $file_prefix.crt /opt/engines/etc/smtp/ssl/engines.crt 
+		chown 22003 /opt/engines/etc/smtp/ssl/engines.crt 
+		cp -p $file_prefix.key /opt/engines/etc/smtp/ssl/keys/engines.key
+		chown 22003 /opt/engines/etc/smtp/ssl/keys/engines.key
+
 	
-		cp -p $file_prefix.crt  /opt/engines/etc/ssl/imap/certs/engines.crt 
-		chown 22013 /opt/engines/etc/ssl/imap/certs/engines.crt 
+		cp -p $file_prefix.crt  /opt/engines/etc/imap/ssl/certs/engines.crt 
+		chown 22013 /opt/engines/etc/imap/ssl/certs/engines.crt 
+		cp -p $file_prefix.key /opt/engines/etc/imap/ssl/keys/engines.key 
+		chown 22013 /opt/engines/etc/imap/ssl/keys/engines.key 
 
-		cp -p $file_prefix.key /opt/engines/etc/ssl/imap/keys/engines.key 
-		chown 22013 /opt/engines/etc/ssl/imap/keys/engines.key 
-
-		cp -p $file_prefix.crt /opt/engines/etc/ssl/pgsql/certs/engines.crt
-		chown 22002 /opt/engines/etc/ssl/pgsql/certs/engines.crt
-
-		cp -p $file_prefix.key /opt/engines/etc/ssl/pgsql/private/engines.key
-		chown 22002 /opt/engines/etc/ssl/pgsql/private/engines.key
+		cp -p $file_prefix.crt /opt/engines/etc/pgsql/ssl/certs/engines.crt
+		chown 22002 /opt/engines/etc/pgsql/ssl/certs/engines.crt
+		cp -p $file_prefix.key /opt/engines/etc/pgsql/ssl/private/engines.key
+		chown 22002 /opt/engines/etc/pgsql/ssl/private/engines.key
   
-
+#AS Default Cert Nginx
 		cp -p $file_prefix.crt   /opt/engines/etc/nginx/ssl/certs/engines.crt
 		chown 22005 /opt/engines/etc/nginx/ssl/certs/engines.crt
-
 		cp -p $file_prefix.key   /opt/engines/etc/nginx/ssl/keys/engines.key	
 		chown 22005 /opt/engines/etc/nginx/ssl/keys/engines.key			
 
