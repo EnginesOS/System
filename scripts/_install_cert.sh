a9c869b3d40e7c10b63e92cdf3642864353440a4
James Vodanovich <james@jvodan.com>
2015-07-25 22:13:56 +1000

    add email

--- scripts/_install_cert.sh
+++ scripts/_install_cert.sh
@@ -3,8 +3,7 @@
 name=$1
 cert=/opt/engines/etc/ssl/certs/${name}
 key=/opt/engines/etc/ssl/keys/${name}
-chmod og-rw ${key}.key
-chmod og-w ${cert}.crt
+
 
 	file $cert.crt | grep PEM
 	   if test $? -ne 0
@@ -22,11 +21,7 @@ chmod og-w ${cert}.crt
 		chmod og-rw /opt/engines/etc/nginx/ssl/keys/$name.key
 		
 #Below here install default cert FIXME to only do so if asked and extend so individual certs can be installed per service ie imap. smtp.
-#		cp -p $cert.crt /opt/engines/etc/ssl/certs/engines.crt
-#		chown 21000 /opt/engines/etc/ssl/certs/engines.crt
-		chmod og-rw 
-#		cp -p $key.key /opt/engines/etc/ssl/keys/engines.key 
-#		chown 21000 /opt/engines/etc/ssl/engines.key 
+
 
 		cp -p $cert.crt /opt/engines/etc/ftp/ssl/engines.crt 
 		chown 22003 /opt/engines/etc/ftp/ssl/engines.crt 
@@ -54,7 +49,14 @@ chmod og-w ${cert}.crt
 		chmod og-rw /opt/engines/etc/pgsql/ssl/certs/engines.crt
 		cp -p $key.key /opt/engines/etc/pgsql/ssl/private/engines.key
 		chown 22002 /opt/engines/etc/pgsql/ssl/private/engines.key
-  		chmod og-rw /opt/engines/etc/pgsql/ssl/private/engines.key
+  		chmod og-rw /opt/engines/etc/pgsql/ssl/private/engines.key	
+	
+		cp -p $cert.crt   /opt/engines/etc/email/ssl/certs/engines.crt
+		chown 22005 /opt/engines/etc/email/ssl/certs/engines.crt
+		chmod og-rw /opt/engines/etc/email/ssl/certs/engines.crt
+		cp -p $key.key   /opt/engines/etc/email/ssl/keys/engines.key	
+		chown 22005 /opt/engines/etc/email/ssl/keys/engines.key			
+		chmod og-rw /opt/engines/etc/email/ssl/keys/engines.key	
   		
 #AS Default Cert Nginx
 		cp -p $cert.crt   /opt/engines/etc/nginx/ssl/certs/engines.crt
@@ -62,8 +64,6 @@ chmod og-w ${cert}.crt
 		chmod og-rw /opt/engines/etc/nginx/ssl/certs/engines.crt
 		cp -p $key.key   /opt/engines/etc/nginx/ssl/keys/engines.key	
 		chown 22005 /opt/engines/etc/nginx/ssl/keys/engines.key			
-		chmod og-rw /opt/engines/etc/nginx/ssl/keys/engines.key	
-	
-
+		chmod og-rw /opt/engines/etc/nginx/ssl/keys/engines.key
 
  
