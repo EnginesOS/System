e7d1e15cf230ef0e82e8f7921be2cac89178d846
James Vodanovich <james@jvodan.com>
2015-07-26 12:40:04 +1000

    setup log dir perms

--- scripts/_setup_service_dir.sh
+++ scripts/_setup_service_dir.sh
@@ -4,3 +4,6 @@
  mkdir -p /opt/engines/run/services/$1/run
 chgrp containers  /opt/engines/run/services/$1/run
 chmod g+w  /opt/engines/run/services/$1/run
+
+mkdir /var/log/engines/services/$1
+chown $uid /var/log/engines/services/$1
