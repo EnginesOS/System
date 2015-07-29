a67a9fbdbc91dcf375cac4887bd101be67e2dbfe
James Vodanovich <james@jvodan.com>
2015-07-29 13:19:54 +1000

    public face of _ophan tree

--- lib/ruby/managed_services/service_manager/ServiceManager.rb
+++ lib/ruby/managed_services/service_manager/ServiceManager.rb
@@ -472,7 +472,10 @@ def  get_managed_engine_tree
   return _get_managed_engine_tree
 end
 
-
+def orphaned_services_tree
+  
+  return _orphaned_services_tree
+end
 
   #Calls on service on the service_container to add the service associated by the hash
   #@return result boolean
