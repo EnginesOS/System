d1369f5ffa1f7d985013f36ce6f25040ac959f2b
James Vodanovich <james@jvodan.com>
2015-08-13 09:34:31 +1000

    time stamp seperate update log  files stop and start registry to update it while mgmt is down

--- bin/update_engines_system_software.sh
+++ bin/update_engines_system_software.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-date >> /var/log/engines/engines_system_updates.log
 
-sudo /opt/engines/scripts/_update_engines_system_software.sh >> /var/log/engines/engines_system_updates.log
+ts=`date +%d-%m-%Y-%H:%M`
+sudo /opt/engines/scripts/_update_engines_system_software.sh >> /var/log/engines/engines_system_update_$ts.log
+
+echo "Restarting"
+sleep 5
+
 
 /opt/engines/bin/eservice stop mgmt
+
+docker stop registry
+docker start registry
+sleep 15
 
 /opt/engines/eservice start mgmt
 
