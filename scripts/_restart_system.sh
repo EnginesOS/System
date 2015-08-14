188ee805a046bb12d7ac71784fa0e99c573fa071
James Vodanovich <james@jvodan.com>
2015-08-14 15:20:58 +1000

    add a safty to restart function to allow the function to be disabled

--- scripts/_restart_system.sh
+++ scripts/_restart_system.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+if test -f /opt/engines/run/system/flags/restart_disabled
+	then
+		echo "Restart From Gui Disabled"
+		exit
+	fi
+	
 sleep 30
+
 docker stop `docker ps |awk '{print $1}'` 
 shutdown -r now
