f07dea61a51ac3062f9617cd20239f6d77713891
James Vodanovich <james@jvodan.com>
2015-08-15 09:29:27 +1000

    correct key name in setting first wizard

--- lib/ruby/api/public/FirstRunWizard.rb
+++ lib/ruby/api/public/FirstRunWizard.rb
@@ -27,7 +27,7 @@ class FirstRunWizard
       return false
     end
    domain_hash = Hash.new()
-    domain_hash[:domain_name]=@first_run_params[:default_domain]
+    domain_hash[:default_domain]=@first_run_params[:default_domain]
     if api.set_default_domain(domain_hash)  == false
       log_error("Fail to set default domain " + api.last_error() + " " + domain_hash.to_s)
       return false
