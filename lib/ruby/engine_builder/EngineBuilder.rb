80d87fea9fbb36ee4626497031b3fadef512475d
James Vodanovich <james@jvodan.com>
2015-08-16 18:09:09 +1000

    notes on params hash

--- lib/ruby/engine_builder/EngineBuilder.rb
+++ lib/ruby/engine_builder/EngineBuilder.rb
@@ -57,7 +57,8 @@ class EngineBuilder
   end
 
   def initialize(params,core_api)
-    
+ #   {:engine_name=>"phpmyadmin5", :host_name=>"phpmyadmin5", :domain_name=>"engines.demo", :http_protocol=>"HTTPS and HTTP", :memory=>"96", :variables=>{}, :attached_services=>[{:publisher_namespace=>"EnginesSystem", :type_path=>"filesystem/local/filesystem", :create_type=>"active", :parent_engine=>"phpmyadmin4", :service_handle=>"phpmyadmin4"}, {:publisher_namespace=>"EnginesSystem", :type_path=>"database/sql/mysql", :create_type=>"active", :parent_engine=>"phpmyadmin4", :service_handle=>"phpmyadmin4"}], :repository_url=>"https://github.com/EnginesBlueprints/phpmyadmin.git"}
+
     @container_name = params[:engine_name]
     @app_is_persistant= false
       #fixme
