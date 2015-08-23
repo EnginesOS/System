aebb1f9eb69525eef9a641426937b9f3f21750a7
James Vodanovich <james@jvodan.com>
2015-08-23 15:23:52 +1000

    debg

--- lib/ruby/managed_services/system_service.rb
+++ lib/ruby/managed_services/system_service.rb
@@ -19,9 +19,12 @@ class SystemService < ManagedService
       @docker_info = @core_api.inspect_container(self)
       if  @docker_info == false
         if has_image? == false
+          SystemUtils.log_output("pulling system service" + container_name.to_s,10)
              pull_image
            end
+        SystemUtils.log_output("creating system service" + container_name.to_s,10)
         @core_api.create_container(self)  
+        SystemUtils.log_output("created system service" + container_name.to_s,10)
         @docker_info = @core_api.inspect_container(self)
         if @docker_info == false
           p :panic
