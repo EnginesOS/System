c0cc31db06e1cacbc88a21aa7ae5ecf2d5bf717d
James Vodanovich <james@jvodan.com>
2015-08-24 12:30:17 +1000

    push first pass of rubocop

--- lib/ruby/engine_builder/builder_public.rb
+++ lib/ruby/engine_builder/builder_public.rb
@@ -1,7 +1,6 @@
 require 'securerandom' 
- 
 
-#@framework = framework
+# @framework = framework
 #   @runtime = runtime
 #   @databases = databases
 #   @setState = setState
@@ -25,63 +24,69 @@ require 'securerandom'
 #   @protocol=:http_and_https
 
 class BuilderPublic
-def initialize(builder)
- @builder = builder
-end
- def engine_name
-   @builder.container_name
- end
- def domain_name
-   @builder.domain_name
- end
+  def initialize(builder)
+    @builder = builder
+  end
+
+  def engine_name
+    @builder.container_name
+  end
+
+  def domain_name
+    @builder.domain_name
+  end
+
   def fqdn
     @builder.hostname + '.' + @builder.domain_name
   end
- def hostname 
-   @builder.hostname
- end
- 
- def http_protocol  
-   if @builder.http_protocol == nil
-     return ''
-   end
-   if @builder.http_protocol.include?('https')
-     return 'https'     
-   end
-   return 'http'
- end
- 
- def repoName
-   @builder.repoName
- end
- def webPort
-   @builder.webPort
- end
- def build_name
-   @builder.build_name
- end
- def runtime
-   @builder.runtime
- end     
 
- def set_environments 
-   @builder.set_environments
- end     
- 
- def engine_environment
-   @builder.engine_environment
- end
- 
- def blueprint
-   return @builder.blueprint
- end
+  def hostname
+    @builder.hostname
+  end
 
- def data_gid
-   return @builder.data_gid
- end
- 
- def memory
-   return @builder.memory
- end
+  def http_protocol
+    if @builder.http_protocol.nil? == true
+      return ''
+    end
+    if @builder.http_protocol.include?('https')
+      return 'https'
+    end
+    return 'http'
+  end
 
+  def repo_name
+    @builder.repo_name
+  end
+
+  def web_port
+    @builder.web_port
+  end
+
+  def build_name
+    @builder.build_name
+  end
+
+  def runtime
+    @builder.runtime
+  end
+
+  def set_environments
+    @builder.set_environments
+  end
+
+  def engine_environment
+    @builder.engine_environment
+  end
+
+  def blueprint
+    @builder.blueprint
+  end
+
+  def data_gid
+    @builder.data_gid
+  end
+
+  def memory
+    @builder.memory
+  end
 end
