363e13d5332e1bef59b02dadd46f9d2c058d1f01
James Vodanovich <james@jvodan.com>
2015-08-29 00:38:56 +1000

    correct

--- lib/ruby/api/system/engines_core.rb
+++ lib/ruby/api/system/engines_core.rb
@@ -200,7 +200,7 @@ class EnginesCore < ErrorsApi
   end
 
   def service_manager
-    @service_manager = ServiceManager.new(self) unless @service_manager.is_a?(Array)
+    @service_manager = ServiceManager.new(self) unless @service_manager.is_a?(ServiceManager)
     return @service_manager
   end
 
