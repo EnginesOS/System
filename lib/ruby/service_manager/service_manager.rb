c9b7c88b20e5fda78d01b1e963d490a9551ab5d3
James Vodanovich <james@jvodan.com>
2015-08-31 14:12:42 +1000

    carry  params[:remove_all_data]

--- lib/ruby/service_manager/service_manager.rb
+++ lib/ruby/service_manager/service_manager.rb
@@ -105,9 +105,10 @@ class ServiceManager  < ErrorsApi
     clear_error
     complete_service_query = ServiceManager.set_top_level_service_params(service_query,service_query[:parent_engine])
     service_hash = @system_registry.find_engine_service_hash(complete_service_query)
+    complete_service_query[:remove_all_data] = params[:remove_all_data]
     return log_error_mesg('Failed to to set top level params hash',service_hash) unless service_hash
     return log_error_mesg('failed to remove managed service',service_hash) unless remove_from_managed_service(service_hash)  
-    return test_registry_result(@system_registry.remove_from_services_registry(service_hash)) 
+    return true 
     rescue StandardError => e
       log_exception(e)
   end
