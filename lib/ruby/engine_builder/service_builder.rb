d309b9f5bb7007a3428cc15a6a5b96fa3c432e75
James Vodanovich <james@jvodan.com>
2015-09-11 09:40:15 +1000

    start of attaching existing services in builder

--- lib/ruby/engine_builder/service_builder.rb
+++ lib/ruby/engine_builder/service_builder.rb
@@ -20,46 +20,72 @@ class ServiceBuilder < ErrorsApi
   return true
 end
 
-def create_persistant_services(services,  environ)
+def create_persistant_services(services, environ, existing)
    service_cnt = 0
  
    services.each do |service_hash|
      free_orphan = false
      service_def = get_service_def(service_hash)
-     return false if service_def.nil?
+     return log_error_mesg('no matching service definition',self) if service_def.nil?
      if service_def[:persistant]    
        service_hash[:persistant] = true
-       service_hash = set_top_level_service_params(service_hash, @engine_name)
-     if @service_manager.match_orphan_service(service_hash) == true
-       service_hash = use_orphan(service_hash)
-       @first_build = false
-       free_orphan = true
-     elsif @service_manager.service_is_registered?(service_hash) == false
-       @first_build = true
-       service_hash[:fresh] = true
-     else # elseif over attach to existing true attached to existing
-       service_hash[:fresh] = false
-       return log_error_mesg('Failed to build cannot over write ' + service_hash[:service_handle].to_s + ' Service Found', self)
-     end
-     p :attach_service
-     p service_hash
-     @templater.fill_in_dynamic_vars(service_hash)
-      environ.concat(SoftwareServiceDefinition.service_environments(service_hash))
-     p :with_env
-     p service_hash
-     # FIXME: release orphan should happen latter unless use reoprhan on rebuild failure
-     if @service_manager.add_service(service_hash)
-       @attached_services.push(service_hash)    
-       release_orphan(service_hash) if free_orphan
-     else
-       return log_error_mesg('Failed to attach ' + @service_manager.last_error, service_hash)
-     end
+         process_persistant_service(service_hash)       
      end
      service_cnt += 1
    end
   return true
  end
 
+ def process_persistant_service(service_hash, environ, use_existing)
+   service_hash = set_top_level_service_params(service_hash, @engine_name)
+          s = match_service_to_existing(service_hash, use_existing) unless use_existing.nil?
+        if s != false
+          service_hash = s
+          @first_build = false
+          free_orphan = true if @service_manager.match_orphan_service(service_hash) == true
+        elsif @service_manager.match_orphan_service(service_hash) == true #auto orphan pick up
+          service_hash = use_orphan(service_hash)
+          @first_build = false
+          free_orphan = true
+        elsif @service_manager.service_is_registered?(service_hash) == false
+          @first_build = true
+          service_hash[:fresh] = true
+        else # elseif over attach to existing true attached to existing
+          service_hash[:fresh] = false
+          return log_error_mesg('Failed to build cannot over write ' + service_hash[:service_handle].to_s + ' Service Found', self)
+        end
+        p :attach_service
+        p service_hash
+        @templater.fill_in_dynamic_vars(service_hash)
+         environ.concat(SoftwareServiceDefinition.service_environments(service_hash))
+        p :with_env
+        p service_hash
+        # FIXME: release orphan should happen latter unless use reoprhan on rebuild failure
+        if @service_manager.add_service(service_hash)
+          @attached_services.push(service_hash)    
+          release_orphan(service_hash) if free_orphan
+        else
+          return log_error_mesg('Failed to attach ' + @service_manager.last_error, service_hash)
+        end
+ end
+ 
+ def match_service_to_existing(service_hash, use_existing)
+   use_existing.each do |existing_service|
+     if existing_service[:publisher_namespace] == service_hash[:publisher_namespace]\
+       && existing_service[:type_path] == service_hash[:type_path]
+         return use_active_service(service_hash, existing_service) if existing_service[:create_type] = 'active'
+         return use_orphan(existing_service)         
+     end
+  end
+  return false
+ end
+ 
+ def use_active_service(service_hash,existing_service )
+  s = @service_manager.get_service_entry(existing_service)
+  s[:shared]=true
+  return s
+ end
+ 
  def use_orphan(service_hash)
    p :attaching_orphan
     service_hash[:fresh] = false
