c90e854d2317bab2bb4ead97a04b849422fee219
James Vodanovich <james@engines.onl>
2015-10-18 11:10:33 +1100

    catch service hashes with engine_name instead of parent_name

--- lib/ruby/service_manager/service_manager.rb
+++ lib/ruby/service_manager/service_manager.rb
@@ -351,6 +351,7 @@ class ServiceManager  < ErrorsApi
 
 
   def ServiceManager.set_top_level_service_params(service_hash, container_name)
+    container_name = service_hash[:engine_name] if container_name == nil
     return SystemUtils.log_error_mesg('no set_top_level_service_params_nil_service_hash container_name:',container_name) if container_name.nil?
     return SystemUtils.log_error_mesg('no set_top_level_service_params_nil_container_name service_hash:',service_hash)  if service_hash.nil?
     service_def = SoftwareServiceDefinition.find(service_hash[:type_path],service_hash[:publisher_namespace])
