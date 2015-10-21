936386c5a2538fbc058fe69eadf237703eb423e1
James Vodanovich <james@engines.onl>
2015-10-21 13:27:35 +1100

    stubs for attach and detttach subservices

--- lib/ruby/api/public/engines_osapi.rb
+++ lib/ruby/api/public/engines_osapi.rb
@@ -644,14 +644,18 @@ class EnginesOSapi
 
   # service params and component objectname / and component name and parent name
   def attach_subservice(params)
-    return success(params[:service_handle], 'attach subservice') if @core_api.attach_subservice(params)
-    SystemUtils.log_error_mesg('attach subservice', params)
-    failed(params[:service_handle], @core_api.last_error, 'attach subservice')
+    p :attach_subservice
+    p params
+#    return success(params[:service_handle], 'attach subservice') if @core_api.attach_subservice(params)
+#    SystemUtils.log_error_mesg('attach subservice', params)
+    failed(params, @core_api.last_error, 'attach subservice')
   end
 
   def dettach_subservice(params)
-    return success(params[:service_handle], 'attach subservice') if @core_api.dettach_subservice(params)
-    SystemUtils.log_error_mesg('attach subservice', params)
+    p :attach_subservice
+      p params
+#    return success(params[:service_handle], 'attach subservice') if @core_api.dettach_subservice(params)
+#    SystemUtils.log_error_mesg('attach subservice', params)
     failed(params[:service_handle], @core_api.last_error, 'attach subservice')
   end
 
