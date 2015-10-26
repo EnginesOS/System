4fafba8ed856f9f3befd9fa00215eb4adb7aa677
James L Vodanovich <james@jvodan.com>
2015-10-26 03:44:05 +0000

    add ability to test update without pushing to git

--- bin/engines_system_update_status.sh
+++ bin/engines_system_update_status.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 cd /opt/engines
-
+if test -f /opt/engines/run/system/flags/test_engines_update
+ then
+   echo "Update Pending"
+	echo $status
+	exit 255
+ fi
+ 
 status=`git status -u no`
 echo $status |grep  up-to-date >/dev/null
 if test $? -ne 0
