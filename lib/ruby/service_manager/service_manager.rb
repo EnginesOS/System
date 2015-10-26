effbd4c0500194bb0aad9ab5d027b44a2caf3244
James L Vodanovich <james@jvodan.com>
2015-10-26 03:41:18 +0000

    also remove from engines regsitry on delete_service

--- lib/ruby/service_manager/service_manager.rb
+++ lib/ruby/service_manager/service_manager.rb
@@ -125,7 +125,8 @@ class ServiceManager  < ErrorsApi
     return log_error_mesg('Failed to match params to registered service',service_hash) unless service_hash
     service_hash[:remove_all_data] = service_query[:remove_all_data]
     return log_error_mesg('failed to remove managed service',service_hash) unless remove_from_managed_service(service_hash)
-    return test_registry_result(@system_registry.remove_from_services_registry(service_hash))
+    return remove_engine_from_managed_engines_registry(service_hash) if test_registry_result(@system_registry.remove_from_services_registry(service_hash))
+    return log_error_mesg('failed to remove managed service from service registry', service_hash)
     rescue StandardError => e
       log_exception(e)
   end
