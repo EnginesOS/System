d897c36560c3a00e8b09e4920e6c93708eb46094
James L Vodanovich <james@jvodan.com>
2015-10-30 02:16:51 +0000

    fix c n p error

--- lib/ruby/api/system/system_api.rb
+++ lib/ruby/api/system/system_api.rb
@@ -182,7 +182,7 @@ class SystemApi < ErrorsApi
     end
     yam1_file_name = SystemConfig.RunDir + service_type_dir + service_name + '/running.yaml'
     unless File.exist?(yam1_file_name)
-      return log_error_mesg('failed to create service file ', SystemConfig.RunDir + service_type_dir + '/' + service_name.to_s) unless ContainerStateFiles.build_running_service(service_name, SystemConfig.RunDir + service_type_dir + service_type_dir)
+      return log_error_mesg('failed to create service file ', SystemConfig.RunDir + service_type_dir + '/' + service_name.to_s) unless ContainerStateFiles.build_running_service(service_name, SystemConfig.RunDir + service_type_dir)
     end
     p service_type_dir
     yaml_file = File.read(yam1_file_name)
