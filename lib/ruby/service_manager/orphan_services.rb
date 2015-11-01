5918241d220ad3822fbc4aeebd8b79175281fc5c
James L Vodanovich <james@jvodan.com>
2015-11-01 22:50:52 +0000

    another moeth from service_manager

--- lib/ruby/service_manager/orphan_services.rb
+++ lib/ruby/service_manager/orphan_services.rb
@@ -7,10 +7,9 @@ def orphanate_service(params)
     log_exception(e)
  end
 
+ ## ????
 def release_orphan(params)
-  test_registry_result(@system_registry.release_orphan(params))   
-  rescue StandardError => e
-    log_exception(e)
+  remove_orphaned_service(params)
 end
 
   
@@ -34,5 +33,17 @@ end
     rescue StandardError => e
       log_exception(e)
   end
+
+  #@ removes underly service and remove entry from orphaned services
+  #@returns boolean indicating success
+  def remove_orphaned_service(service_query_hash)
+    clear_error
+    service_hash = retrieve_orphan(service_query_hash)
+    return log_error_mesg('failed to retrieve orphan service:' +  @last_error.to_s,service_hash)  if service_hash.nil? || service_hash == false
+    return test_registry_result(@system_registry.release_orphan(service_hash))   
+    rescue StandardError => e
+      log_exception(e)
+  end
+
   
   end
