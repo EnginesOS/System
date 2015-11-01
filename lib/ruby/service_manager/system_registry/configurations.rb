07182df829989845e78a88b11cf7e68f34d1c6c7
James L Vodanovich <james@jvodan.com>
2015-11-01 02:07:19 +0000

    refactor

---
+++ lib/ruby/service_manager/system_registry/configurations.rb
@@ -0,0 +1,27 @@
+# Configurations methods 
+  
+require_relative 'rset.rb'
+
+  def get_service_configurations_hashes(config_hash)
+    rest_get('/v0/system_registry/service/configurations/',{:params => config_hash })
+    end
+  
+    def get_service_configuration(config_hash)
+      rest_get('/v0/system_registry/services/configuration/',{:params => config_hash })
+      end
+      
+    def update_service_configuration(config_hash)
+      rest_put('/v0/system_registry/services/configuration/',config_hash )
+    end
+    
+    def rm_service_configuration(config_hash)
+      rest_delete('/v0/system_registry/services/configurations/',{:params => config_hash } )
+    end
+    
+    def add_service_configuration(config_hash)
+      rest_post('/v0/system_registry/services/configurations/',config_hash )
+      end
+      
+    def service_configurations_registry
+      rest_get('/v0/system_registry/services/configurations/tree', nil)
+    end
