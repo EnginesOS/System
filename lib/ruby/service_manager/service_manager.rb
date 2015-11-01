6e521724df00155bc0048da359388c95a97d1122
James L Vodanovich <james@jvodan.com>
2015-11-01 04:00:17 +0000

    roll back on the top level meth

--- lib/ruby/service_manager/service_manager.rb
+++ lib/ruby/service_manager/service_manager.rb
@@ -21,11 +21,41 @@ class ServiceManager  < ErrorsApi
      test_registry_result(@system_registry.get_service_entry(service_hash))
    end
    
-  def ServiceManager.set_top_level_service_params(service_hash, container_name) 
-    ServiceDefinitions.set_top_level_params(service_hash, container_name)
-  end
  
-  
+ 
+  def ServiceDefinitions.set_top_level_service_params(service_hash, container_name)
+     container_name = service_hash[:parent_engine] if service_hash.key?(:parent_engine)
+     container_name = service_hash[:engine_name] if container_name == nil    
+     return SystemUtils.log_error_mesg('no set_top_level_service_params_nil_service_hash container_name:',container_name) if container_name.nil?
+     return SystemUtils.log_error_mesg('no set_top_level_service_params_nil_container_name service_hash:',service_hash)  if service_hash.nil?
+     service_def = SoftwareServiceDefinition.find(service_hash[:type_path],service_hash[:publisher_namespace])
+     return SystemUtils.log_error_mesg('NO Service Definition File Found for:',service_hash) if service_def.nil?
+     service_hash[:service_container_name] = service_def[:service_container]
+     service_hash[:persistant] = service_def[:persistant]
+     service_hash[:parent_engine] = container_name      
+     service_hash[:container_type] = 'container' if service_hash.has_key?(:container_type) == false || service_hash[:container_type] ==nil
+     service_hash[:variables] = {} unless service_hash.has_key?(:variables)
+     service_hash[:variables][:parent_engine] = container_name
+       if service_def.key?(:priority)
+             service_hash[:priority] = service_def[:priority]
+           else
+             service_hash[:priority] = 0
+           end
+     return service_hash if service_hash.key?(:service_handle) && ! service_hash[:service_handle].nil?
+     
+     if service_def.key?(:service_handle_field) && !service_def[:service_handle_field].nil?
+     handle_field_sym = service_def[:service_handle_field].to_sym
+       p :handle_symbol
+       p service_def[:service_handle_field].to_sym
+       return SystemUtils.log_error_mesg('Missing Service Handle field in variables',handle_field_sym) unless service_hash[:variables].key?(handle_field_sym)
+       service_hash[:service_handle] = service_hash[:variables][handle_field_sym]
+     else
+       service_hash[:service_handle] = container_name
+     end    
+     return service_hash   
+       rescue StandardError => e
+         SystemUtils.log_exception(e)
+   end
 
   #@ Attach service called by builder and create service
   #if persisttant it is added to the Service Registry Tree
