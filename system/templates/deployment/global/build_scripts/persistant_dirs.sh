d990654a05bc43aac495b58d642eeb14910910ee
James L Vodanovich <james@jvodan.com>
2015-11-04 08:32:31 +0000

    persistant dir handling in a script

--- system/templates/deployment/global/build_scripts/persistant_dirs.sh
+++ system/templates/deployment/global/build_scripts/persistant_dirs.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+VOLDIR=$1
+shift
+
+	#$* some/were/to/path
+	#
+	# mkdir $voldir/some/were/to/path
+	#dirname = /some/were/to/
+	# mv /home/$path $VOLDIR/$dirname
+	#ln -s $voldir/$dirname /home/some/were/to/path
+	for path in $*
+      do
+
+		dirname=`dirname "$path" `
+		mkdir -p "$VOLDIR/$dirname"
+   			if [ ! -d "/home/$path"
+     			then 
+       			    mkdir -p "/home/$path" 
+   			fi
+
+		mv "/home/$path" "$VOLDIR/$dirname" 
+		ln -s "$VOLDIR/$path" "/home/$path"
+	done
