22a677900eb155b9754ddc4ec52b773c576cdc8f
James L Vodanovich <james@jvodan.com>
2015-11-04 21:30:28 +0000

    is_engines_system_upto_date return boolean not a String

--- lib/ruby/api/system/system_status.rb
+++ lib/ruby/api/system/system_status.rb
@@ -186,7 +186,9 @@ def self.is_base_system_upto_date?
  end
   def self.is_engines_system_upto_date?
     result = SystemUtils.execute_command('/opt/engines/bin/engines_system_update_status.sh')
-    return result[:stdout]
+    
+    return true if result[:stdout].include?('Up to Date')
+      return false
   rescue StandardError => e
     SystemUtils.log_exception(e)
     return result[:stderr] unless result.nil? 
