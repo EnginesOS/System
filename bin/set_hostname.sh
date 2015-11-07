59b603edeb006c63c969fb139446773af8603f9a
James L Vodanovich <james@jvodan.com>
2015-11-07 06:23:05 +0000

    single arg to set_hostname

--- bin/set_hostname.sh
+++ bin/set_hostname.sh
@@ -1,16 +1,4 @@
 #/bin/sh
 
 hostname=`echo  $SSH_ORIGINAL_COMMAND | awk '{print $2}'`
-domain_name=`echo  $SSH_ORIGINAL_COMMAND | awk '{print $3}'`
-echo $SSH_ORIGINAL_COMMAND
-echo $hostname $domain_name
-#hostname=$1
-#$domain_name=$2
-
-if ! test -z "$domain_name"
- then
-  hostname=${hostname}.$domain_name
-  fi
-  
-  echo sudo hostname $hostname
-   sudo hostname $hostname
+sudo hostname $hostname
