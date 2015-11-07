16ab9396e85f9dc196f723b935990657a08184ea
James L Vodanovich <james@jvodan.com>
2015-11-07 03:06:38 +0000

    get the first interface

--- bin/set_ip.sh
+++ bin/set_ip.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-gw_ifac=`netstat -nr |grep ^0.0.0.0 | awk '{print $8}'`
+gw_ifac=`netstat -nr |grep ^0.0.0.0 | awk '{print $8}' |head -1`
 
 /sbin/ifconfig $gw_ifac |grep "inet addr"  |  cut -f 2 -d: |cut -f 1 -d" " > /opt/engines/etc/net/ip
