08ee02f665ee10cb8769f223913ecb6c4dfdde0a
James L Vodanovich <james@jvodan.com>
2015-11-07 06:19:19 +0000

    dbg

--- bin/set_hostname.sh
+++ bin/set_hostname.sh
@@ -1,8 +1,9 @@
-#/bin/bash
+#/bin/sh
 
 hostname=`echo  $SSH_ORIGINAL_COMMAND | awk '{print $2}'`
 domain_name=`echo  $SSH_ORIGINAL_COMMAND | awk '{print $3}'`
-
+echo $SSH_ORIGINAL_COMMAND
+echo $hostname $domain_name
 #hostname=$1
 #$domain_name=$2
 
