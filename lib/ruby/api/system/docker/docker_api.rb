093c8b5c71377c9e5253c3acee86193663902640
James L Vodanovich <james@jvodan.com>
2015-11-07 10:51:46 +0000

    to_s the int

--- lib/ruby/api/system/docker/docker_api.rb
+++ lib/ruby/api/system/docker/docker_api.rb
@@ -119,12 +119,12 @@ class DockerApi < ErrorsApi
 
   def logs_container(container, count)
     clear_error
-    cmdline = 'docker logs --tail=' + count + ' ' + container.container_name
+    cmdline = 'docker logs --tail=' + count.to_s + ' ' + container.container_name
     result = SystemUtils.execute_command(cmdline)
     return result[:stderr].to_s + ' ' + result[:stdout].to_s 
   rescue StandardError => e
     log_exception(e)
-    return 'error retriving logs'
+    return 'error retriving logs ' + e.to_s
   end
 
   def inspect_container(container)
