1de092bb81fcc49976d304e3671e2bd7df893bed
James L Vodanovich <james@jvodan.com>
2015-11-07 21:41:25 +0000

    debug

--- lib/ruby/engine_builder/service_builder/service_checks.rb
+++ lib/ruby/engine_builder/service_builder/service_checks.rb
@@ -4,11 +4,15 @@ def required_services_are_running?
   
   @attached_services.each do |service_hash|  
    service_name = SoftwareServiceDefinition.get_software_service_container_name(service_hash)
+   p :checking_service
+   p service_name.to_s
     return service_not_running(service_name) unless @service_manager.is_service_running?(service_name)
   end
+  return true
 end
 
 def service_not_running(service_name)
   @last_error = service_name.to_s + ' Not Running'
+    return false
 end
 end
