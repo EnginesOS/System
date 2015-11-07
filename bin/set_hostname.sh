f2b3d24e03cb77fe56f4267b8ef9343e9017a079
James L Vodanovich <james@jvodan.com>
2015-11-07 06:16:09 +0000

    deal with aargs via ssh

--- bin/set_hostname.sh
+++ bin/set_hostname.sh
@@ -1,10 +1,14 @@
 #/bin/bash
 
-hostname=$1
+hostname=`echo  $SSH_ORIGINAL_COMMAND | awk '{print $2}'`
+domain_name=`echo  $SSH_ORIGINAL_COMMAND | awk '{print $3}'`
 
-if ! test -z "$2"
+#hostname=$1
+#$domain_name=$2
+
+if ! test -z "$domain_name"
  then
-  hostname=${hostname}.$2
+  hostname=${hostname}.$domain_name
   fi
   
   echo sudo hostname $hostname
