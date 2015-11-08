fa20e6de0397bceaf1abb1a0ff2b69c2ad4cb7de
James L Vodanovich <james@jvodan.com>
2015-11-08 07:50:26 +0000

    dbg

--- system/templates/deployment/global/build_scripts/persistant_dirs.sh
+++ system/templates/deployment/global/build_scripts/persistant_dirs.sh
@@ -11,15 +11,20 @@
 	for path in $*
       do
       path=`echo $path | sed "/[.][.]/s///g"` 
-      #echo $path
+      echo $path
  path=`echo $path | sed "/\/$/s///"`
 		dirname=`dirname "$path" `
 		mkdir -p "$VOLDIR/$dirname"
+		echo mkdir -p "$VOLDIR/$dirname"
+		touch  "$VOLDIR/at1"
    			if [ ! -d "/home/$path" ]
      			then 
        			    mkdir -p "/home/$path" 
+   					echo mkdir -p "/home/$path" 
    			fi
 
 		mv "/home/$path" "$VOLDIR/$dirname" 
+	echo "/home/$path" "$VOLDIR/$dirname" 
 		ln -s "$VOLDIR/$path" "/home/$path"
+		echo ln -s "$VOLDIR/$path" "/home/$path"
 	done
