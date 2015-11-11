4b2886c33bfa609c9960882851dd631669529013
James L Vodanovich <james@jvodan.com>
2015-11-11 08:06:51 +0000

    dbg

--- lib/ruby/containers/objects/environment_variable.rb
+++ lib/ruby/containers/objects/environment_variable.rb
@@ -49,6 +49,7 @@ class EnvironmentVariable
 
   def self.find_env_in(new_env,dest)
     dest.each do  |env|
+      puts '+++++++++++++++++++++++++++++'
       p env.to_s
       
       next unless env.is_a?(EnvironmentVariable)
