d246b3441d4613ca34bfa6bf3168c1e6b03b8f04
James L Vodanovich <james@jvodan.com>
2015-11-11 23:44:35 +0000

    track niliification or core

--- lib/ruby/engine_builder/engine_builder.rb
+++ lib/ruby/engine_builder/engine_builder.rb
@@ -41,7 +41,7 @@ class EngineBuilder < ErrorsApi
 
   def initialize(params, core_api)
     # {:engine_name=>'phpmyadmin5', :host_name=>'phpmyadmin5', :domain_name=>'engines.demo', :http_protocol=>'HTTPS and HTTP', :memory=>'96', :variables=>{}, :attached_services=>[{:publisher_namespace=>'EnginesSystem', :type_path=>'filesystem/local/filesystem', :create_type=>'active', :parent_engine=>'phpmyadmin4', :service_handle=>'phpmyadmin4'}, {:publisher_namespace=>'EnginesSystem', :type_path=>'database/sql/mysql', :create_type=>'active', :parent_engine=>'phpmyadmin4', :service_handle=>'phpmyadmin4'}], :repository_url=>'https://github.com/EnginesBlueprints/phpmyadmin.git'}
-    @core_api = core_api
+    @core_api = core_api.dup
     @core_api.freeze
     @mc = nil # Used in clean up only
     @build_params = params   
