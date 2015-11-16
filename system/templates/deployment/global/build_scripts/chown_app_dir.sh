90d9d76f027574e2fcb0bdc499e60325d1cf1e42
James L Vodanovich <james@jvodan.com>
2015-11-16 23:12:01 +0000

    tidy permission settings

--- system/templates/deployment/global/build_scripts/chown_app_dir.sh
+++ system/templates/deployment/global/build_scripts/chown_app_dir.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-echo PACKAGE_INSTALLER_RUN $PACKAGE_INSTALLER_RUN
+echo PACKAGE_INSTALLER_RUN "$PACKAGE_INSTALLER_RUN"
  
-if [ ! -d /home/app ]
- then 
-   mkdir -p /home/app 
-  fi
-  
- mkdir -p /home/fs ; mkdir -p /home/fs/local 
- chown -R $ContUser /home/app /home/fs /home/fs/local   
 
- 
+dirs=/home/fs /home/fs/local  /home/app
+
+for fir in $dirs
+ do
+  if [ ! -d $dir ]
+   then
+   	 mkdir -p $dir
+   fi
+   chown -R $ContUser $dir   
+ done
+
+
   
--- system/templates/deployment/global/build_scripts/set_data_permissions.sh
+++ system/templates/deployment/global/build_scripts/set_data_permissions.sh
@@ -7,3 +7,5 @@ id data-user | cut -f2 -d: |grep $data_uid >/dev/null
 
 chown -R $data_uid.$data_gid /home/app /home/fs_src
 chmod -R 770 /home/fs_src
+chmod g+rx ` find /home/fs_src -type d`
+chmod g+rx ` find /home/app -type d`
