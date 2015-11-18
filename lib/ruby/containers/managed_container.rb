2fdbea90cc65bd95a5007e304e8426d441684fcb
James L Vodanovich <james@jvodan.com>
2015-11-18 21:00:52 +0000

    seperate out voume creation stuff from service builder

--- lib/ruby/containers/managed_container.rb
+++ lib/ruby/containers/managed_container.rb
@@ -380,6 +380,10 @@ class ManagedContainer < Container
     vol = Volume.new(service_hash[:variables][:name], SystemConfig.LocalFSVolHome + '/' + service_hash[:parent_engine]  + '/' + service_hash[:variables][:name], service_hash[:variables][:engine_path], 'rw', permissions)  
     @volumes[service_hash[:variables][:name]] = vol
     save_state
+    rescue StandardError => e
+       p :add_volume
+       p service_hash
+      log_exception(e)
   end
 
   protected
--- lib/ruby/containers/objects/volume.rb
+++ lib/ruby/containers/objects/volume.rb
@@ -1,15 +1,27 @@
 require_relative 'static_service.rb'
 class Volume < StaticService #Latter will include group and perhaps other attributes
  
-  def initialize(name)
-   @name = name
-   @serviceType = 'fs'
-   @localpath = SystemConfig.LocalFSVolHome
-   @remotepath = SystemConfig.CONTFSVolHome
-   @mapping_permissions = 'rw'
-   @vol_permissions = nil
-  end
+#  def initialize(name)
+#   @name = name
+#   @serviceType = 'fs'
+#   @localpath = SystemConfig.LocalFSVolHome
+#   @remotepath = SystemConfig.CONTFSVolHome
+#   @mapping_permissions = 'rw'
+#   @vol_permissions = nil
+#  end
   
+  
+  def initialize(service_hash)
+    
+    @name = service_hash[:variables][:service_name]
+    @vol_permissions  = PermissionRights.new(service_hash[:parent_engine] , '', '')
+    @serviceType = 'fs'
+    @remotepath = service_hash[:variables][:engine_path]
+    @localpath = service_hash[:variables][:volume_src]
+    @mapping_permissions = 'rw'
+    
+  end
+    
   def initialize(name, localpath, remotepath, mapping_permissions, vol_permissions)
     @serviceType = 'fs'
     @name = name
@@ -30,6 +42,20 @@ class Volume < StaticService #Latter will include group and perhaps other attrib
 
   attr_reader :mapping_permissions, :name, :remotepath, :localpath, :user, :group, :vol_permissions, :mapping_permissions
  
+  def Volume.complete_service_hash(service_hash)
+    service_hash[:variables][:engine_path] = service_hash[:variables][:service_name] if service_hash[:variables][:engine_path].nil? || service_hash[:variables][:engine_path] == ''
+     if service_hash[:variables][:engine_path] == '/home/app/' || service_hash[:variables][:engine_path]  == '/home/app'     
+       service_hash[:variables][:engine_path] = '/home/app/'
+     else
+       service_hash[:variables][:engine_path] = '/home/fs/' + service_hash[:variables][:engine_path] unless service_hash[:variables][:engine_path].start_with?('/home/fs/') ||service_hash[:variables][:engine_path].start_with?('/home/app')
+     end
+     service_hash[:variables][:service_name] = service_hash[:variables][:engine_path].gsub(/\//,'_')
+     service_hash[:variables][:volume_src] = SystemConfig.LocalFSVolHome + '/' + service_hash[:parent_engine].to_s  + '/' + service_hash[:variables][:service_name].to_s unless service_hash[:variables].key?(:volume_src) && service_hash[:variables][:volume_src].to_s != ''
+   
+     service_hash[:variables][:volume_src].strip!
+     service_hash[:variables][:volume_src] = SystemConfig.LocalFSVolHome + '/' + service_hash[:parent_engine]  + '/' + service_hash[:variables][:volume_src] unless service_hash[:variables][:volume_src].start_with?(SystemConfig.LocalFSVolHome)
+    service_hash
+  end
 #  def permissions
 #    @mapping_permissions
 #  end
@@ -42,5 +68,5 @@ class Volume < StaticService #Latter will include group and perhaps other attrib
     backup_hash[:source_type] = 'fs'
     backup_hash[:source_name] = @name  
   end
-  
+ 
 end
