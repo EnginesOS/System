c0d528b2ffd41f55132855cca448da44fad05cef
James L Vodanovich <james@jvodan.com>
2015-11-21 04:41:29 +0000

    point to the new meths

--- lib/ruby/api/system/containers/container_api.rb
+++ lib/ruby/api/system/containers/container_api.rb
@@ -146,13 +146,13 @@ class ContainerApi < ErrorsApi
   end
 
   def register_with_dns(container)
-    service_hash = SystemUtils.create_dns_service_hash(container)
+    service_hash = create_dns_service_hash(container)
     return false if service_hash.is_a?(Hash) == false
     return @engines_core.create_and_register_service(service_hash)
   end
 
 def register_with_zeroconf(container)
-  service_hash = SystemUtils.create_zeroconf_service_hash(container)
+  service_hash = create_zeroconf_service_hash(container)
      return false if service_hash.is_a?(Hash) == false
   return @engines_core.create_and_register_service(service_hash)
 end
@@ -173,7 +173,7 @@ end
   end
 
   def remove_nginx_service(container)
-    service_hash = SystemUtils.create_nginx_service_hash(container)
+    service_hash = create_nginx_service_hash(container)
     @engines_core.dettach_service(service_hash)
   end
 
