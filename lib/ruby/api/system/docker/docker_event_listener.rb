6ed9be604d73c6fc0f8bac5ca17a14120ec946e4
James L Vodanovich <james@jvodan.com>
2015-11-22 06:10:44 +0000

    docker event listener ?

---
+++ lib/ruby/api/system/docker/docker_event_listener.rb
@@ -0,0 +1,10 @@
+module DockerEventListener
+  
+  #to check for event event_expected [Symbol] on container [ManagedContainer] container.container_id
+  def wait_for_docker_event(event_expected, container)
+    sleep 1 # delay one sec  to avoid rave condition between cmd completeion and actual docker process completing 
+    # Gui often reads before daemon completed task 
+    return true
+  end
+  
+end
