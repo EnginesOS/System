35af5a79192b573972a0022fcb537f692b6fcfdc
James L Vodanovich <james@jvodan.com>
2015-11-23 05:36:29 +0000

    .to_i

--- lib/ruby/api/system/containers/container_api/engines_api_system.rb
+++ lib/ruby/api/system/containers/container_api/engines_api_system.rb
@@ -18,7 +18,7 @@ module EnginesApiSystem
   
   def have_enough_ram?(container)
     free_ram = MemoryStatistics.avaiable_ram
-    ram_needed = SystemConfig.MinimumFreeRam + container.memory
+    ram_needed = SystemConfig.MinimumFreeRam .to_i + container.memory.to_i
     return true if  free_ram > ram_needed
     return false
   end
