6eb978ebab1daaaf29d28e3cb06f629ead369877
James L Vodanovich <james@jvodan.com>
2015-11-23 10:51:14 +0000

    no longer know ip of parents docker0 as a given

--- scripts/set_hostname.sh
+++ scripts/set_hostname.sh
@@ -4,4 +4,4 @@
 
 hostname=$1
 domain_name=$1
-ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/engines/.ssh/mgmt/set_hostname engines@172.17.42.1 /opt/engines/bin/set_hostname.sh $hostname $domain_name
+ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/engines/.ssh/mgmt/set_hostname engines@management.1 /opt/engines/bin/set_hostname.sh $hostname $domain_name
