aaee1dc26651763a513f7bdfc2b0d0cc2e3db5ed
James L Vodanovich <james@jvodan.com>
2015-11-23 21:23:28 +0000

    correct sh syntax

--- scripts/kill_docker_builder.sh
+++ scripts/kill_docker_builder.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pid=`ps -ax |grep -v grep |grep "docker run  -d" | awk '{ print $1}``
+pid=`ps -ax |grep -v grep |grep "docker build --force" | awk '{ print $1}'`
  if ! test -z "$pid"
   then
 	kill -HUP $pid
