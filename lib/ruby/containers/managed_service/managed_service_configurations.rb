50563f6641bd2663ebc758349de04dd5c0bb7bd9
James L Vodanovich <james@jvodan.com>
2015-11-25 01:40:33 +0000

    ?

--- lib/ruby/containers/managed_service/managed_service_configurations.rb
+++ lib/ruby/containers/managed_service/managed_service_configurations.rb
@@ -1,9 +1,9 @@
 module ManagedServiceConfigurations
-  
-  
+    
   def run_configurator(configurator_params)
     @container_api.run_configurator(self, configurator_params)
   end
+  
   def retrieve_configurator(configurator_params)
     @container_api.retrieve_configurator(self, configurator_params)
   end
