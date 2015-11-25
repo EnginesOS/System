08b260a277f17826544b96e13cb4a1cca0a3c1cf
James L Vodanovich <james@jvodan.com>
2015-11-25 01:12:03 +0000

    timeouts on configurators

--- lib/ruby/api/system/containers/service_api/service_api_configurations.rb
+++ lib/ruby/api/system/containers/service_api/service_api_configurations.rb
@@ -6,17 +6,43 @@ module ServiceApiConfigurations
     return log_error_mesg('service missing cont_userid ',params) if c.check_cont_uid == false
     cmd = 'docker exec -u ' + c.cont_userid + ' ' +  c.container_name + ' /home/configurators/read_' + params[:configurator_name].to_s + '.sh '
     result = {}
-    thr = Thread.new { result = SystemUtils.execute_command(cmd) }
-    thr.join
+      begin
+    Timeout.timeout(4) do 
+      thr = Thread.new { result = SystemUtils.execute_command(cmd) }
+      thr.join
+    end
+      rescue Timeout::Error
+        log_error_mesg('Timeout on  retrieving Configuration',cmd)
+        return {}
+     end
+      
     if result[:result] == 0
       variables = SystemUtils.hash_string_to_hash(result[:stdout])
       params[:variables] = variables
       return params
     end
-    log_error_mesg('Failed retrieve_configurator',result)
+    log_error_mesg('Error on retrieving Configuration',result)
     return {}
   end
 
+  def run_configurator(configurator_params)
+      return log_error_mesg('service not running ',configurator_params) unless is_running?
+      return log_error_mesg('service missing cont_userid ',configurator_params) if check_cont_uid == false
+      cmd = 'docker exec -u ' + @cont_userid.to_s + ' ' +  @container_name.to_s + ' /home/configurators/set_' + configurator_params[:configurator_name].to_s + '.sh \'' + SystemUtils.service_hash_variables_as_str(configurator_params).to_s + '\''
+      result = {}
+    begin
+  Timeout.timeout(4) do 
+      thr = Thread.new { result = SystemUtils.execute_command(cmd) }
+      thr.join
+    end
+      rescue Timeout::Error
+        log_error_mesg('Timeout on running configurator',cmd)
+        return {}
+     end
+      @last_error = result[:stderr] # Dont log just set
+      return result
+    end
+    
   def update_service_configuration(configuration)
     engines_core.update_service_configuration(configuration)
   end
--- lib/ruby/containers/managed_service/managed_service_configurations.rb
+++ lib/ruby/containers/managed_service/managed_service_configurations.rb
@@ -1,15 +1,9 @@
 module ManagedServiceConfigurations
+  
+  
   def run_configurator(configurator_params)
-    return log_error_mesg('service not running ',configurator_params) unless is_running?
-    return log_error_mesg('service missing cont_userid ',configurator_params) if check_cont_uid == false
-    cmd = 'docker exec -u ' + @cont_userid.to_s + ' ' +  @container_name.to_s + ' /home/configurators/set_' + configurator_params[:configurator_name].to_s + '.sh \'' + SystemUtils.service_hash_variables_as_str(configurator_params).to_s + '\''
-    result = {}
-    thr = Thread.new { result = SystemUtils.execute_command(cmd) }
-    thr.join
-    @last_error = result[:stderr] # Dont log just set
-    return result
+    @container_api.run_configurator(self, configurator_params)
   end
-
   def retrieve_configurator(configurator_params)
     @container_api.retrieve_configurator(self, configurator_params)
   end
