5dc4e68ba578f1d87b340f6697957160c74c3310
James L Vodanovich <james@jvodan.com>
2015-11-26 22:35:21 +0000

    fix arounf gw/management ip

--- scripts/set_hostname.sh
+++ scripts/set_hostname.sh
@@ -4,5 +4,5 @@
 
 hostname=$1
 domain_name=$1
-ip=`ifconfig docker0  |grep "inet addr:" |cut -f2 -d: |awk '{print $1}'`
+ip=`cat /opt/engines/etc/net/management`
 ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/engines/.ssh/mgmt/set_hostname engines@$ip /opt/engines/bin/set_hostname.sh $hostname $domain_name
