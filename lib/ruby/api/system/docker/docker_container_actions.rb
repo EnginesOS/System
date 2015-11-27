26767321140c4383be81f198cd3222c1f6473997
James L Vodanovich <james@jvodan.com>
2015-11-27 04:15:12 +0000

    break docker api up into modules

---
+++ lib/ruby/api/system/docker/docker_container_actions.rb
@@ -0,0 +1,75 @@
+module DockerContainerActions
+  require_relative 'docker_exec.rb'
+  
+  def create_container(container)
+    clear_error
+    commandargs = DockerCmdOptions.container_commandline_args(container)
+    commandargs = 'docker run  -d ' + commandargs
+    SystemUtils.debug_output('create cont', commandargs)
+    return wait_for_docker_event(:create, container) if run_docker_cmd(commandargs, container)
+    return false
+  rescue StandardError => e
+    container.last_error = ('Failed To Create ')
+    log_exception(e)
+  end
+
+  def start_container(container)
+    clear_error
+    commandargs = 'docker start ' + container.container_name
+    return wait_for_docker_event(:start, container) if run_docker_cmd(commandargs, container)
+    return false
+  rescue StandardError => e
+    log_exception(e)
+  end
+
+  def stop_container(container)
+    clear_error
+    commandargs = 'docker stop ' + container.container_name
+    return wait_for_docker_event(:stop, container) if run_docker_cmd(commandargs, container)
+    return false
+  rescue StandardError => e
+    log_exception(e)
+  end
+
+  def pause_container(container)
+    clear_error
+    commandargs = 'docker pause ' + container.container_name
+    return wait_for_docker_event(:pause, container) if run_docker_cmd(commandargs, container)
+    return false
+  rescue StandardError => e
+    log_exception(e)
+  end
+
+  def unpause_container(container)
+    clear_error
+    commandargs = 'docker unpause ' + container.container_name
+    return wait_for_docker_event(:unpause, container) if run_docker_cmd(commandargs, container)
+    return false
+  rescue StandardError => e
+    log_exception(e)
+  end
+
+  def signal_container_process(pid, signal, container)
+    clear_error
+    commandargs = 'docker exec ' + container.container_name + ' kill -' + signal + ' ' + pid.to_s
+    execute_docker_cmd(commandargs, container)
+  rescue StandardError => e
+    log_exception(e)
+  end
+
+  def destroy_container(container)
+    clear_error
+    commandargs = 'docker  rm ' + container.container_name
+    unless run_docker_cmd(commandargs, container)
+      log_error_mesg(container.last_error, container)
+      return false if image_exist?(container.image)
+    end
+    wait_for_docker_event(:rm, container)
+    clean_up_dangling_images
+    return true
+  rescue StandardError => e
+    container.last_error = 'Failed To Destroy ' + e.to_s
+    log_exception(e)
+  end
+
+end
