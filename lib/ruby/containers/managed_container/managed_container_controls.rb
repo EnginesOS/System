01be5edcef78bb1bb5b3ecc3fbc6397576c20d86
James L Vodanovich <james@jvodan.com>
2015-11-27 04:25:06 +0000

    call  clear error and set task into one meth

--- lib/ruby/containers/managed_container/managed_container_controls.rb
+++ lib/ruby/containers/managed_container/managed_container_controls.rb
@@ -1,18 +1,15 @@
 module ManagedContainerControls
   def destroy_container
     return false unless has_api?
-    clear_error
-    in_progress(:destroy) # this represents the state we want and not necessarily the one we get
+    prep_task(:destroy)
     return task_complete if super
     task_failed('destroy')
   end
 
   def setup_container
-    clear_error
+    prep_task(:stop)
     return false unless has_api?
     ret_val = false
-    state = read_state
-    in_progress(:stop)
     unless has_container?
       ret_val = @container_api.setup_container(self)
       expire_engine_info
@@ -25,9 +22,8 @@ module ManagedContainerControls
   end
 
   def create_container
-    clear_error
     return false unless has_api?
-    in_progress(:create)
+    prep_task(:create)
     return task_failed('create') unless super
     state = read_state
     return log_error_mesg('No longer running ' + state + ':' + @setState, self) unless state == 'running'
@@ -40,17 +36,15 @@ module ManagedContainerControls
   end
 
   def recreate_container
-    ret_val = false
-    in_progress(:recreate)
+    prep_task(:recreate)
     return task_failed('destroy/recreate') unless destroy_container
     return task_failed('create/recreate') unless create_container
     task_complete
   end
 
   def unpause_container
-    clear_error
     return false unless has_api?
-    in_progress(:unpause)
+    prep_task(:unpause)
     return task_failed('unpause') unless super
     register_with_dns # MUst register each time as IP Changes
     @container_api.register_non_persistant_services(self)
@@ -58,27 +52,25 @@ module ManagedContainerControls
   end
 
   def pause_container
-    clear_error
     return false unless has_api?
-    in_progress(:pause)
+    prep_task(:pause)
     return task_failed('pause') unless super
     @container_api.deregister_non_persistant_services(self)
     task_complete
   end
 
   def stop_container
-    clear_error
     return false unless has_api?
     in_progress(:stop)
+    clear_error
     @container_api.deregister_non_persistant_services(self)
     return task_failed('stop') unless super
     task_complete
   end
 
   def start_container
-    clear_error
-    return false unless has_api?
-    in_progress(:start)
+    return false unless has_api?    
+    prep_task(:start)
     return task_failed('start') unless super
     @restart_required = false
     register_with_dns # MUst register each time as IP Changes
@@ -87,7 +79,6 @@ module ManagedContainerControls
   end
 
   def restart_container
-
     in_progress(:restart)
     return task_failed('restart/stop') unless stop_container
     return task_failed('restart/start') unless start_container
@@ -96,7 +87,7 @@ module ManagedContainerControls
 
   def rebuild_container
     return false unless has_api?
-    in_progress(:rebuild)
+    prep_task(:rebuild)
     ret_val = @container_api.rebuild_image(self)
     expire_engine_info
     if ret_val == true
@@ -106,5 +97,11 @@ module ManagedContainerControls
     end
     return task_complete if ret_val
     task_failed('rebuild')
+  end
+  
+  private
+  def prep_task(action_sym)
+    in_progress(:action_sym)
+    clear_error
   end
 end
