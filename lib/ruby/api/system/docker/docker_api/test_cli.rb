cc1035789bafe03abd72f83ab0936a7e561b9fab
James L Vodanovich <james@jvodan.com>
2015-11-28 02:08:21 +0000

    f

--- lib/ruby/api/system/docker/docker_api/test_cli.rb
+++ lib/ruby/api/system/docker/docker_api/test_cli.rb
@@ -1,10 +1,10 @@
 require 'uri'
-require 'yajl/http_stream'
+
 
 socket = UNIXSocket.new('/var/run/docker.sock')
 socket.puts('GET /events')
 parser = Yajl::Parser.new
-hash = parser.parse(socket, :symbolize_keys => true) do |hash|
+hash = parser.parse(socket) do |hash|
 puts hash.inspect
 
-end
+end 
