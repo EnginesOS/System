4d85e2ceb5f01748978247edf4400b117e44c017
James L Vodanovich <james@jvodan.com>
2015-11-28 03:21:18 +0000

    up read timeout on even listener

--- lib/ruby/api/system/docker/docker_api/test_cli.rb
+++ lib/ruby/api/system/docker/docker_api/test_cli.rb
@@ -7,6 +7,7 @@ parser = Yajl::Parser.new
 #socket = UNIXSocket.new('/var/run/docker.sock')
 req = Net::HTTP::Get.new('/events')
 client = NetX::HTTPUnix.new('unix:///var/run/docker.sock')
+client.continue_timeout=36000
  client.request(req) { |resp|
 #p resp
    resp.read_body do |chunk|
