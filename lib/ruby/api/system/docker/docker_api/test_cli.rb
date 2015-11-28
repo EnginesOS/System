1a4e9b86c39860de25199103cc2bb6b564d79098
James L Vodanovich <james@jvodan.com>
2015-11-28 02:20:40 +0000

    f

--- lib/ruby/api/system/docker/docker_api/test_cli.rb
+++ lib/ruby/api/system/docker/docker_api/test_cli.rb
@@ -4,9 +4,11 @@ require 'socket'
 #socket = UNIXSocket.new('/var/run/docker.sock')
 req = Net::HTTP::Get.new('/events')
 client = NetX::HTTPUnix.new('unix:///var/run/docker.sock')
+resp = client.request(req)
+p resp.body
 
 parser = Yajl::Parser.new
-hash = parser.parse(client) do |hash|
+hash = parser.parse(resp.body) do |hash|
 puts hash.inspect
-
+  
 end 
