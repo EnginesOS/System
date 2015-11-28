c39d65afe81e95151d36ab7c0a6b002e8c9e94ce
James L Vodanovich <james@jvodan.com>
2015-11-28 00:01:29 +0000

    return empty hash on blueprint load failure as gui is no handle error result

--- lib/ruby/api/system/containers/container_api/engine_api_blueprint.rb
+++ lib/ruby/api/system/containers/container_api/engine_api_blueprint.rb
@@ -8,7 +8,7 @@ module EngineApiBlueprint
   def load_blueprint(container)
     blueprint_r = BlueprintApi.new
     blueprint = blueprint_r.load_blueprint(container)
-    log_error_mesg('failed to load blueprint', blueprint_r.last_error) unless blueprint.is_a?(Hash)
+   return log_error_mesg('failed to load blueprint', blueprint_r.last_error) unless blueprint.is_a?(Hash)
     return blueprint
   end
 
