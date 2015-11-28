be332f02fc6df27e958210e6363f7a588f252ea5
James L Vodanovich <james@jvodan.com>
2015-11-28 02:14:55 +0000

    try net_x

--- lib/ruby/api/system/docker/docker_api/test_cli.rb
+++ lib/ruby/api/system/docker/docker_api/test_cli.rb
@@ -1,10 +1,12 @@
 require 'yajl'
-
+require 'net_x/http_unix'
 require 'socket'
-socket = UNIXSocket.new('/var/run/docker.sock')
-socket.puts('GET /events')
+#socket = UNIXSocket.new('/var/run/docker.sock')
+req = Net::HTTP::Get.new('/events')
+client = NetX::HTTPUnix.new('unix:///var/run/docker.sock')
+
 parser = Yajl::Parser.new
-hash = parser.parse(socket) do |hash|
+hash = parser.parse(client) do |hash|
 puts hash.inspect
 
 end 
