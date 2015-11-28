7502e098a2edb42a6d1cb601777fe06f392d5900
James L Vodanovich <james@jvodan.com>
2015-11-28 02:27:17 +0000

    process in chunks

--- lib/ruby/api/system/docker/docker_api/test_cli.rb
+++ lib/ruby/api/system/docker/docker_api/test_cli.rb
@@ -1,13 +1,25 @@
 require 'yajl'
 require 'net_x/http_unix'
 require 'socket'
+
+parser = Yajl::Parser.new
+
 #socket = UNIXSocket.new('/var/run/docker.sock')
 req = Net::HTTP::Get.new('/events')
 client = NetX::HTTPUnix.new('unix:///var/run/docker.sock')
-resp = client.request(req)
-p resp.body
+ client.request(req) { |resp|
+p resp
+   resp.read_body do |chunk|
+     p chunk
+     hash = parser.parse(chunk) do |hash|
+     puts hash.inspect
+       
+     end 
 
-parser = Yajl::Parser.new
+
+   end
+ }
+
 hash = parser.parse(resp.body) do |hash|
 puts hash.inspect
   
