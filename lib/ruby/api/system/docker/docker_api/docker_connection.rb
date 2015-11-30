5e04849ae226d27ddef9b84cb543b8e6de25922c
James L Vodanovich <james@jvodan.com>
2015-11-30 23:48:11 +0000

    dont inspect if -i

--- lib/ruby/api/system/docker/docker_api/docker_connection.rb
+++ lib/ruby/api/system/docker/docker_api/docker_connection.rb
@@ -19,6 +19,7 @@ class DockerConnection
     p :test_inspect 
     p container.container_name
     p container.container_id.to_s
+    return nil if container.container_id == -1
     request='/containers/' + container.container_id.to_s + '/json'
    return make_request(request)
   end
