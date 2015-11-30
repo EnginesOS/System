7f2f17c12347cf6bdaef55f30fe19fd92931fa72
James L Vodanovich <james@jvodan.com>
2015-11-30 11:55:08 +0000

    dump usage in /var/log/apache2

--- lib/ruby/api/system/engines_system/base_os_system.rb
+++ lib/ruby/api/system/engines_system/base_os_system.rb
@@ -7,7 +7,7 @@ module BaseOsSystem
 
   def restart_system
     GC.start
-    ObjectSpace.dump_all(output: File.open('/var/log/apache/heap.json','w'))
+    ObjectSpace.dump_all(output: File.open('/var/log/apache2/heap.json','w'))
     
     res = Thread.new { system('ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/engines/.ssh/mgmt/restart_system engines@' + SystemStatus.get_management_ip + '  /opt/engines/bin/restart_system.sh') }
     # FIXME: check a status flag after sudo side post ssh run ie when we know it's definititly happenging
