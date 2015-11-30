36bd2f2c13734502850302445684e6906fd9b183
James L Vodanovich <james@jvodan.com>
2015-11-30 12:22:58 +0000

    freeze not work try dup

--- lib/ruby/api/system/engines_system/base_os_system.rb
+++ lib/ruby/api/system/engines_system/base_os_system.rb
@@ -7,13 +7,13 @@ module BaseOsSystem
 
   def restart_system
     GC.start
-    ObjectSpace.dump(@system_api.freeze,output: File.open('/var/log/apache2/system.json','w'))
-    ObjectSpace.dump(self, output: File.open('/var/log/apache2/engines.json','w'))
+    ObjectSpace.dump(@system_api.dup,output: File.open('/var/log/apache2/system.json','w'))
+    ObjectSpace.dump(self.dup, output: File.open('/var/log/apache2/engines.json','w'))
     ObjectSpace.dump_all(output: File.open('/var/log/apache2/heap.json','w'))
-    ObjectSpace.dump(@registry_handler.freeze,output: File.open('/var/log/apache2/registry_handler.json','w'))
-    ObjectSpace.dump(@container_api.freeze,output: File.open('/var/log/apache2/container_api.json','w'))
-    ObjectSpace.dump(@service_api.freeze,output: File.open('/var/log/apache2/service_api.json','w'))
-    ObjectSpace.dump(@docker_api.freeze,output: File.open('/var/log/apache2/docker_api.json','w'))
+    ObjectSpace.dump(@registry_handler.dup,output: File.open('/var/log/apache2/registry_handler.json','w'))
+    ObjectSpace.dump(@container_api.dup,output: File.open('/var/log/apache2/container_api.json','w'))
+    ObjectSpace.dump(@service_api.dup,output: File.open('/var/log/apache2/service_api.json','w'))
+    ObjectSpace.dump(@docker_api.dup,output: File.open('/var/log/apache2/docker_api.json','w'))
     res = Thread.new { system('ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/engines/.ssh/mgmt/restart_system engines@' + SystemStatus.get_management_ip + '  /opt/engines/bin/restart_system.sh') }
     # FIXME: check a status flag after sudo side post ssh run ie when we know it's definititly happenging
     return true if res.status == 'run'
