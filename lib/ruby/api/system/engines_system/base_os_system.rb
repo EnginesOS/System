a6ca0b95f084aafa7189595f2c3235929310c060
James L Vodanovich <james@jvodan.com>
2015-11-30 12:07:39 +0000

    correct ram dump paths

--- lib/ruby/api/system/engines_system/base_os_system.rb
+++ lib/ruby/api/system/engines_system/base_os_system.rb
@@ -10,10 +10,10 @@ module BaseOsSystem
     ObjectSpace.dump(@system_api,output: File.open('var/log/apache2/system.json','w'))
     ObjectSpace.dump(self, output: File.open('/var/log/apache2/engines.json','w'))
     ObjectSpace.dump_all(output: File.open('/var/log/apache2/heap.json','w'))
-    ObjectSpace.dump( @registry_handler,output: File.open('var/log/apache2/registry_handler.json','w'))
-    ObjectSpace.dump(@container_api,output: File.open('var/log/apache2/container_api.json','w'))
-    ObjectSpace.dump(@service_api,output: File.open('var/log/apache2/service_api.json','w'))
-    ObjectSpace.dump(@docker_api,output: File.open('var/log/apache2/docker_api.json','w'))
+    ObjectSpace.dump( @registry_handler,output: File.open('/var/log/apache2/registry_handler.json','w'))
+    ObjectSpace.dump(@container_api,output: File.open('/var/log/apache2/container_api.json','w'))
+    ObjectSpace.dump(@service_api,output: File.open('/var/log/apache2/service_api.json','w'))
+    ObjectSpace.dump(@docker_api,output: File.open('/var/log/apache2/docker_api.json','w'))
     res = Thread.new { system('ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/engines/.ssh/mgmt/restart_system engines@' + SystemStatus.get_management_ip + '  /opt/engines/bin/restart_system.sh') }
     # FIXME: check a status flag after sudo side post ssh run ie when we know it's definititly happenging
     return true if res.status == 'run'
