12af6357da6d093c13046e22ebe14298bc42bbab
James L Vodanovich <james@jvodan.com>
2015-12-03 00:20:25 +0000

    include preceeding space in uid search to avoid dyndns matching dns

--- scripts/_setup_service_key_dir.sh
+++ scripts/_setup_service_key_dir.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-uid=`grep $1 /opt/engines/etc/container_uids |awk '{print $3}'`
+uid=`grep " $1" /opt/engines/etc/container_uids |awk '{print $3}'`
 mkdir -p  /opt/engines/ssh/keys/services/$1
 chown $uid /opt/engines/ssh/keys/services/$1
