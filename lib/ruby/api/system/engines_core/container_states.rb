45230b2e9a74a0d67158762c03cbe6ec6bc78695
James L Vodanovich <james@jvodan.com>
2015-12-07 05:59:50 +0000

    dbg

--- lib/ruby/api/system/engines_core/container_states.rb
+++ lib/ruby/api/system/engines_core/container_states.rb
@@ -1,10 +1,16 @@
 module ContainerStates
   def get_engines_states
-     service_manager.get_engines_states
+  r=   service_manager.get_engines_states
+    p :get_engines_states
+            p r
+            r
    end
    
    def get_services_states
-     service_manager.get_services_states
+   r =  service_manager.get_services_states
+     p :get_services_states
+        p r
+        r
    end
  
 end
