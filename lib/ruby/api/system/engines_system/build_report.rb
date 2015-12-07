485a07e36c65d82d533aa377b84a9b5415be1860
James L Vodanovich <james@jvodan.com>
2015-12-07 04:40:35 +0000

    check for nil engine_name

--- lib/ruby/api/system/engines_system/build_report.rb
+++ lib/ruby/api/system/engines_system/build_report.rb
@@ -1,6 +1,7 @@
 module BuildReport
   def get_build_report(engine_name)
     clear_error
+    return log_error_mesg('get_build_report passed nil engine_name', engine_name ) if engine_name.nil?
     state_dir = SystemConfig.RunDir + '/containers/' + engine_name
     return File.read(state_dir + '/buildreport.txt') if File.exist?(state_dir + '/buildreport.txt')
     return 'Build Not Successful'
