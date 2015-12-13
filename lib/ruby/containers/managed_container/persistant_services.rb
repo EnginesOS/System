aebea5659c215243b26857d41b87fdc9e3cb7c70
James L Vodanovich <james@jvodan.com>
2015-12-13 23:01:24 +0000

    persistant services ash for templator

--- lib/ruby/containers/managed_container/persistant_services.rb
+++ lib/ruby/containers/managed_container/persistant_services.rb
@@ -1,8 +1,17 @@
 module PersistantServices
   
   def engine_persistant_services
-    p :engine_persistant_services
-    @container_api.engine_persistant_services(self)
+    services = @container_api.engine_persistant_services(self)
+    retval = ''
+    if services.is_a?(Array)
+      services.each do |service|
+        retval += ' ' + SystemUtils.service_hash_variables_as_str(service)
+      end
+    elsif services.is_a?(Hash)
+      retval = SystemUtils.service_hash_variables_as_str(services)
+    end
+    return retval
   end
+  
   
 end
