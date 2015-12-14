f3483cc21e2b293fea9c4c9b0ea106170601e6fe
James L Vodanovich <james@jvodan.com>
2015-12-14 00:58:34 +0000

    seperate services by ; (for backup)

--- lib/ruby/containers/managed_container/persistant_services.rb
+++ lib/ruby/containers/managed_container/persistant_services.rb
@@ -6,11 +6,15 @@ module PersistantServices
     if services.is_a?(Array)
       n=0
       services.each do |service|
-        retval += ' ' + service_to_str(service) + SystemUtils.service_hash_variables_as_str(service)
+        retval += ';' if n < 0
+        retval +=   service_to_str(service) #+ SystemUtils.service_hash_variables_as_str(service)
+        n=1
       end
     elsif services.is_a?(Hash)
       retval = service_to_str(service_hash) #SystemUtils.service_hash_variables_as_str(services)
     end
+    p :engine_persistant_services
+    p retval
     return retval
   end
   
@@ -19,9 +23,9 @@ module PersistantServices
 end
 
 def service_to_str(service_hash)
-  retval = service_hash[:publisher_namespace].to_s + ',' + service_hash[:type_path].to_s + ',' + service_hash[:type].to_s
+  retval = service_hash[:publisher_namespace].to_s + '/' + service_hash[:type_path].to_s 
   service_hash[:variables].each do |variable|
-    retval += ',' + variable[0].to_s + '%'  + variable[1].to_s
+    retval += ',' + variable[1].to_s
   end
   p :service_to_str
   p retval
