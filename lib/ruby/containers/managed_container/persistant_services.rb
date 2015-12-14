79dd09c3f979057302c6d25d29d6d44da12e2341
James L Vodanovich <james@jvodan.com>
2015-12-14 00:01:10 +0000

    fmt persistant services to fit in hash str with out use of = or : as this is a field within a hash string

--- lib/ruby/containers/managed_container/persistant_services.rb
+++ lib/ruby/containers/managed_container/persistant_services.rb
@@ -4,14 +4,26 @@ module PersistantServices
     services = @container_api.engine_persistant_services(self)
     retval = ''
     if services.is_a?(Array)
+      n=0
       services.each do |service|
         retval += ' ' + SystemUtils.service_hash_variables_as_str(service)
       end
     elsif services.is_a?(Hash)
-      retval = SystemUtils.service_hash_variables_as_str(services)
+      retval = service_to_str(service_hash) #SystemUtils.service_hash_variables_as_str(services)
     end
     return retval
   end
   
+  # patha-pathb-servicetype.service_handle.param1.param2.param3
   
+end
+
+def service_to_str(service_hash)
+  retval = service_hash[:publisher_namespace].to_s + ',' + service_hash[:type_path].to_s + ',' + service_hash[:type].to_s
+  service_hash[:variables].each do |variable|
+    retval += ',' + variable[0].to_s + '%'  + variable[1].to_s
+  end
+  retval+= ' '
+  retval
+    
 end
