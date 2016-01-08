e6c9bf7b8e62719dede9a5efb1591a4d0d8c09e6
James L Vodanovich <james@jvodan.com>
2016-01-08 00:36:48 +0000

    upgrade  not dist-upgrade

--- scripts/_update_base_os.sh
+++ scripts/_update_base_os.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get -y update
-env DEBIAN_FRONTEND=noninteractive   apt-get -q -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" dist-upgrade
+env DEBIAN_FRONTEND=noninteractive   apt-get -q -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade
 #DEBIAN_PRIORITY=critical
 apt-get install -y apt-transport-https    linux-image-extra-$(uname -r) lvm2 thin-provisioning-tools
 service docker restart 
