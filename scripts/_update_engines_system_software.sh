5e10acf00c2b03eb83da5b578bd5b5b5910b7205
James L Vodanovich <james@jvodan.com>
2016-01-28 20:52:21 +0000

    update scripts support in update, change services to new model

--- scripts/_update_engines_system_software.sh
+++ scripts/_update_engines_system_software.sh
@@ -4,4 +4,17 @@ chown engines /opt/engines/run/system/flags/update_engines_running
 cd /opt/engines
 git pull
  cp /opt/engines/system/updates/src/etc/sudoers.d/engines /etc/sudoers.d/engines 
+ chmod og-rw /etc/sudoers.d/engines 
+ 
+update_scripts=`ls /opt/engines/system/updates/to_run`
+for script in $update_scripts
+ do
+ 	if ! test -f /opt/engines/system/updates/have_run/$script
+ 		then
+ 		/opt/engines/system/updates/to_run/$script
+ 		echo ran /opt/engines/system/updates/to_run/$script
+ 		cp /opt/engines/system/updates/to_run/$script /opt/engines/system/updates/have_run/$script
+ 	fi
+ done
+ 
 exit $?
