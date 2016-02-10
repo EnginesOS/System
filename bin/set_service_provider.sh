5e928a5deb2c03eefb08e8204f00bc09a11fb10c
James L Vodanovich <james@jvodan.com>
2016-02-10 22:02:22 +0000

    rm first

--- bin/set_service_provider.sh
+++ bin/set_service_provider.sh
@@ -10,8 +10,8 @@
  #then
  #	rm /opt/engines/run/services/$1
  #fi
- 
+ rm -r /opt/engines/run/services-available/$1
 cp -r /opt/engines/run/services-available/$1/$2 /opt/engines/run/services/$1
 chgrp -R containers /opt/engines/run/services/$1
-chmod g+rwx /opt/engines/run/services-available/$1/
-chmod g+rwx /opt/engines/run/services-available/$1
+chmod g+rwx /opt/engines/run/services/$1
+
