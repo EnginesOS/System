eb86d7ad76424180919d53f84bd48c4c1eefaa95
James L Vodanovich <james@jvodan.com>
2016-02-14 00:51:03 +0000

    typos

--- etc/auth/scripts/ftp/add_service.sh
+++ etc/auth/scripts/ftp/add_service.sh
@@ -7,28 +7,15 @@ echo $TS: $SSH_ORIGINAL_COMMAND >> /var/log/ftp/add.log
 
 service_hash=`echo  $SSH_ORIGINAL_COMMAND | awk '{print $2}'`
 echo $service_hash
-new=`echo $service_hash | sed "/^:/s///" |  sed "/:$/s///"`
 
-service_hash=$new
+
+ echo $service_hash | /home/engines/bin/json_to_env >/tmp/.env
+ . /tmp/.env
 
 
 n=1
 
-fcnt=`echo $service_hash| grep -o : |wc -l`
 
-fcnt=`expr $fcnt + 1`
-
-        while test $fcnt -ge $n
-        do
-                nvp="`echo $service_hash |cut -f$n -d:`"
-                n=`expr $n + 1`
-                name=`echo $nvp |cut -f1 -d=`
-                value=`echo $nvp |cut -f2 -d=`
-                if test ${#name} -gt 0
-                	then
-                		export $name="$value"
-                	fi
-        done
                 
          if test "$rw_access" = "true"
           then
