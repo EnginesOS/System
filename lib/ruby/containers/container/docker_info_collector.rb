eed046a463495fa754e343f73c03985c427b8934
James L Vodanovich <james@jvodan.com>
2016-02-28 11:15:14 +1100

    docker_info_cache

--- lib/ruby/containers/container/docker_info_collector.rb
+++ lib/ruby/containers/container/docker_info_collector.rb
@@ -68,14 +68,17 @@ module DockerInfoCollector
   def collect_docker_info
     return false unless has_api?
     result = false
-    return false if @docker_info_cache == false
+    return false if @docker_info_cache == false 
     @docker_info_cache =  @container_api.inspect_container(self) if @docker_info_cache.nil?
-    if  @docker_info_cache == false
+    if @docker_info_cache == false
       @container_id = -1
       @docker_info_cache = nil if @setState != 'nocontainer'
-    elsif @container_id.to_s == '' || @container_id = -1
+    else
+      @docker_info_cache =  @docker_info_cache[0]
+    if @container_id.to_s == '' || @container_id = -1      
       @container_id = @docker_info_cache['Id']
     end
+  end
     #log_error_mesg('collect false from ', self)
     #@docker_info_cache = @last_result if result
     # result    
