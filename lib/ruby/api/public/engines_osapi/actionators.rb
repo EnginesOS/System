eed0e331888e526bc4f9e74180cb39d0cbb4afeb
James L Vodanovich <james@jvodan.com>
2016-02-29 10:19:17 +1100

    actionators

---
+++ lib/ruby/api/public/engines_osapi/actionators.rb
@@ -0,0 +1,18 @@
+module Actionators
+  def list_actionators(service)
+    if service.is_a?(Hash)
+      SoftwareServiceDefinition.software_service_definition(service)
+    else
+      service_def = SoftwareServiceDefinition.find(service.type_path,service.publisher_namespace)
+    end
+    return [] unless service_def.is_a?(Hash)
+    return [] unless service_def.key?(:actionators)
+    return [] unless service_def[:actionators].is_a?(Array)
+    return service_def[:actionators]
+  end
+  
+  def perform_service_action(service_name,actionator_name,params)
+    
+  end
+  
+end
