58e127ced7c0051bfefeafc683389146c3699b6a
James L Vodanovich <james@jvodan.com>
2016-03-02 09:06:44 +1100

    crashed re install  doesnt delete

--- lib/ruby/engine_builder/engine_builder.rb
+++ lib/ruby/engine_builder/engine_builder.rb
@@ -351,10 +351,13 @@ class EngineBuilder < ErrorsApi
     if @mc.is_a?(ManagedContainer)
       @mc.stop_container if @mc.is_running?
       @mc.destroy_container if @mc.has_container?
-      @mc.delete_image if @mc.has_image?
+      
+      @mc.delete_image if @mc.has_image? &&  ! @build_params[:reinstall].is_a?(TrueClass)
     end
-    return log_error_mesg('Failed to remove ' + @service_builder.last_error.to_s ,self) unless @service_builder.service_roll_back
-    return log_error_mesg('Failed to remove ' + @core_api.last_error.to_s ,self) unless @core_api.remove_engine(@build_params[:engine_name])
+    unless @build_params[:reinstall].is_a?(TrueClass)
+      return log_error_mesg('Failed to remove ' + @service_builder.last_error.to_s ,self) unless @service_builder.service_roll_back
+      return log_error_mesg('Failed to remove ' + @core_api.last_error.to_s ,self) unless @core_api.remove_engine(@build_params[:engine_name])
+    end
     #    params = {}
     #    params[:engine_name] = @build_name
     #    @core_api.delete_engine(params) # remove engine if created, removes from manged_engines tree (main reason to call)
