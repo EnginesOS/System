62f069f52ee8aedb5e48d712d22bbdd6900056d6
James L Vodanovich <james@jvodan.com>
2016-03-04 11:56:18 +1100

    updater tests

--- system/updates/to_run/system/01/updater.sh
+++ system/updates/to_run/system/01/updater.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo Done System Stuff
-    
+    chown -R 21000 /opt/engines/system/updates/failed/engines /opt/engines/system/updates/has_run/engines /opt/engines/system/updates/to_run/engines
---
+++ system/updates/to_run/system/02/updater.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+   chown -R 21000 /opt/engines/system/updates/failed/engines /opt/engines/system/updates/has_run/engines /opt/engines/system/updates/to_run/engines
+   
+echo Done System Stuff
+    
