a32ee214616665b6f17dc822b22a46be215a6b08
James L Vodanovich <james@jvodan.com>
2016-03-05 10:51:53 +1100

    now system meteor

--- system/templates/deployment/meteor/home/startwebapp.sh
+++ system/templates/deployment/meteor/home/startwebapp.sh
@@ -3,7 +3,7 @@
  if test $FRAMEWORK == 'meteor'
   then
   	cd /home/app
-  	~node/.meteor/meteor &
+  	meteor &
   	pid=$!
   	touch /engines/var/run/flags/startup_complete
   	echo $pid > /run/meteor.pid
