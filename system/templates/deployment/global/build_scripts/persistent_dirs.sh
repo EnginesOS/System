b0050a0ec3b185a82cc94fa0f14b209121ec381e
James L Vodanovich <james@jvodan.com>
2016-03-06 12:44:38 +1100

    more debug out

--- system/templates/deployment/global/build_scripts/persistent_dirs.sh
+++ system/templates/deployment/global/build_scripts/persistent_dirs.sh
@@ -11,7 +11,7 @@
 	
 	for path in $*
       do
-      echo $path |grep ^/usr/local/
+      echo $path |grep ^/usr/local/ >/dev/null
       if test $? -eq 0
        then
          path=`echo $path | sed "/\/usr\/local\//s///"`
@@ -19,17 +19,18 @@
        else
         prefix=/home/
       fi 
+      echo Prefix: $prefix
       path=`echo $path | sed "/[.][.]/s///g"` 
-      echo $path
+      echo Path: $path
  path=`echo $path | sed "/\/$/s///"`
 		dirname=`dirname "$path" `
 		mkdir -p "$VOLDIR/$dirname"
 		echo mkdir -p "$VOLDIR/$dirname"
-		touch  "$VOLDIR/at1"
+		#touch  "$VOLDIR/at1"
    			if [ ! -d "$prefix/$path" ]
      			then 
        			    mkdir -p "$prefix/$path" 
-   					echo mkdir -p "$prefix/$path" 
+   					echo Make Dir: mkdir -p "$prefix/$path" 
    			fi
 
 		mv "$prefix/$path" "$VOLDIR/$dirname" 
