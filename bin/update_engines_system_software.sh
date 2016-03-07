b381c14e9541b3237ac5a0ff4f80cd8294d51d2e
James L Vodanovich <james@jvodan.com>
2016-03-07 14:33:29 +1100

    log update

--- bin/update_engines_system_software.sh
+++ bin/update_engines_system_software.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 ts=`date +%d-%m-%Y-%H:%M`
 touch /var/log/engines/updates/engines_system_update_$ts.log
-nohup /opt/engines/bin/run_update_engines_system_software.sh  
-#>> /var/log/engines/updates/engines_system_update_$ts.log
+nohup /opt/engines/bin/run_update_engines_system_software.sh  >> /var/log/engines/updates/engines_system_update_$ts.log
 
