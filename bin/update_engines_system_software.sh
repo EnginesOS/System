2d1bc7544e32afcddad64d3856f5f8b5b3b51e3f
James L Vodanovich <james@jvodan.com>
2016-03-07 14:27:30 +1100

    log to stdout for test

--- bin/update_engines_system_software.sh
+++ bin/update_engines_system_software.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-nohup /opt/engines/bin/run_update_engines_system_software.sh 
+nohup /opt/engines/bin/run_update_engines_system_software.sh  
+#>> /var/log/engines/updates/engines_system_update_$ts.log
 
