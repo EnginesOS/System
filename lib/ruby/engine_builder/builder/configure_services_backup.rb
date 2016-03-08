2a53147e4211c501d488539b88ce5855f72b8667
James L Vodanovich <james@jvodan.com>
2016-03-09 10:36:16 +1100

    typo

--- lib/ruby/engine_builder/builder/configure_services_backup.rb
+++ lib/ruby/engine_builder/builder/configure_services_backup.rb
@@ -2,7 +2,7 @@ module ConfigureServicesBackup
   def configure_services_backup(services)
     services.each do |service|
       next unless service[:persistent] == true
-      script__src_dir = SystemConfig.BackupScriptsSrcRoot + '/' + service[:publisher_namespace] + '/' + service[:type_path]
+      script_src_dir = SystemConfig.BackupScriptsSrcRoot + '/' + service[:publisher_namespace] + '/' + service[:type_path]
       install_backup_scripts(service, script_src_dir)
       end
   end
