4f95c17d07263d4958419c8d4a1979a897c11e17
James L Vodanovich <james@jvodan.com>
2016-03-09 12:21:53 +1100

    handle stderr and display if action failed

--- system/templates/services/backup/EnginesSystem/database/sql/mysql/backup.sh
+++ system/templates/services/backup/EnginesSystem/database/sql/mysql/backup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-
-mysqldump -h $dbhost -u $dbuser --password=$dbpasswd $dbname 
+ 
+mysqldump -h $dbhost -u $dbuser --password=$dbpasswd $dbname 2>/tmp/mysqldump.errs
+if test $? -ne 0
+ then 
+ 	cat  /tmp/mysqldump.errs
+ 	exit -1
+ fi
+ 
+ exit 0
--- system/templates/services/backup/EnginesSystem/filesystem/local/filesystem/backup.sh
+++ system/templates/services/backup/EnginesSystem/filesystem/local/filesystem/backup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-tar -czpf - /home/fs/$volume_name
+
+tar -cpf - $VOLDIR  2>  /tmp/tar.errors.txt
+
+if test $? -ne 0
+ then
+ cat /tmp/tar.errors.txt
+exit -1
+ fi
+exit 0
+
