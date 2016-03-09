e5bf2de8c1d39f4de8dfe00670c654aef1079a4f
James L Vodanovich <james@jvodan.com>
2016-03-09 13:04:55 +1100

    aciotnators is a has not an array

--- lib/ruby/engine_builder/builder/engine_scripts_builder.rb
+++ lib/ruby/engine_builder/builder/engine_scripts_builder.rb
@@ -52,10 +52,10 @@ module EngineScriptsBuilder
    end
   
   def create_actionator_scripts
-    SystemDebug.debug(SystemDebug.builder,"creating actionators ",@blueprint_reader.actionators, @blueprint_reader.actionators.class.Name)
+    SystemDebug.debug(SystemDebug.builder,"creating actionators ",@blueprint_reader.actionators)
     return true if @blueprint_reader.actionators.nil?
     
-    @blueprint_reader.actionators.each do |actionator| 
+    @blueprint_reader.actionators.each_value do | actionator| 
       filename = SystemConfig.ActionatorDir + '/' + actionator[:name] + '.sh'
       SystemDebug.debug(SystemDebug.builder,"creating actionator " ,  actionator[:name])
        content = actionator[:content]
