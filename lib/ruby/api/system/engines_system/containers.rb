073c5f0641432954cf3841315d10e495794aa829
James L Vodanovich <james@jvodan.com>
2016-03-09 16:42:27 +1100

    enable actiosn debug

--- lib/ruby/api/system/engines_system/containers.rb
+++ lib/ruby/api/system/engines_system/containers.rb
@@ -154,6 +154,7 @@ rescue StandardError => e
 end
 
 def load_engine_actionators(container)
+  SystemDebug.debug(SystemDebug.actions,container,ContainerStateFiles.actionator_dir(container) + '/actionators.yaml')
   return [] unless File.exist?(ContainerStateFiles.actionator_dir(container) + '/actionators.yaml')
     yaml =  File.read(ContainerStateFiles.actionator_dir(container) + '/actionators.yaml')
   actionators = YAML::load(yaml)
