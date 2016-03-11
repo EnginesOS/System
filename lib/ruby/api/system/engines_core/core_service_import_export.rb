46ebb1f7b7b9ab8e2434497fe128dae0fb41e9ba
James L Vodanovich <james@jvodan.com>
2016-03-11 18:40:24 +1100

    fmt

--- lib/ruby/api/system/engines_core/core_service_import_export.rb
+++ lib/ruby/api/system/engines_core/core_service_import_export.rb
@@ -1,24 +1,21 @@
 module CoreServiceImportExport
-  
-  def export_service(service_hash) 
+  def export_service(service_hash)
     return false unless service_hash.key?(:persistent)
     return false unless service_hash[:persistent] == true
     return false unless service_hash.key?(parent_engine) == true
-    
-      if service_hash[:container_type] == container
-        engine = loadManagedEngine(service_hash[:parent_engine])
-      else
-        engine = loadManagedService(service_hash[:parent_engine])
-             
-      end
-      
-      engine.export_service(service_hash)
-  end
-  
-  def import_service(service_hash, data) 
-     
-    end
-    
 
-  
+    if service_hash[:container_type] == container
+      engine = loadManagedEngine(service_hash[:parent_engine])
+    else
+      engine = loadManagedService(service_hash[:parent_engine])
+
+    end
+
+    engine.export_service(service_hash)
+  end
+
+  def import_service(service_hash, data)
+
+  end
+
 end
