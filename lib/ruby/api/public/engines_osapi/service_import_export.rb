44925d30c14797488a775ba48e19e65f4af58b33
James L Vodanovich <james@jvodan.com>
2016-03-12 08:36:31 +1100

    engines_core

--- lib/ruby/api/public/engines_osapi/service_import_export.rb
+++ lib/ruby/api/public/engines_osapi/service_import_export.rb
@@ -1,11 +1,11 @@
 module ServiceImportExport
   
   def export_service(service_hash) 
-    engines_core.export_service(service_hash) 
+    @core_api.export_service(service_hash) 
   end
   
   def import_service(service_hash, data) 
-      engines_core.import_service(service_hash) 
+    @core_api.import_service(service_hash) 
     end
     
 
