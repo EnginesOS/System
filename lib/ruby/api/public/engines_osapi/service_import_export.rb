6993f68294b119e47dcbbf7bc3c9870b7923b6e6
James L Vodanovich <james@jvodan.com>
2016-03-12 16:00:37 +1100

    error handling for export import

--- lib/ruby/api/public/engines_osapi/service_import_export.rb
+++ lib/ruby/api/public/engines_osapi/service_import_export.rb
@@ -1,11 +1,15 @@
 module ServiceImportExport
   
   def export_service(service_hash) 
-    @core_api.export_service(service_hash) 
+   r = @core_api.export_service(service_hash)
+    return failed(params.to_s, @last_error, 'export service') unless t.is_a?(String) 
+    return r 
   end
   
   def import_service(params) 
-    @core_api.import_service(params) 
+  return  success(params.to_s,  'import service') if @core_api.import_service(params)
+    return failed(params.to_s, @last_error, 'import service')  
+     
     end
     
 
