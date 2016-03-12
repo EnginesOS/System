f4a1f2abe9d1ae734b0af5d83a869e4cd1d7b3d5
James L Vodanovich <james@jvodan.com>
2016-03-13 08:40:30 +1100

    acutally export something

--- lib/ruby/api/system/containers/container_api/engine_api_export_import.rb
+++ lib/ruby/api/system/containers/container_api/engine_api_export_import.rb
@@ -2,13 +2,19 @@ module EngineApiExportImport
   def export_service(service_hash)
     cmd_dir = SystemConfig.BackupScriptsRoot + '/' + service[:publisher_namespace] + '/' + service[:type_path] + '/' + service[:service_handle] + '/'
     cmd = 'docker exec  ' + service[:parent_engine] + ' ' + cmd_dir + '/backup.sh ' 
+      p :export
+      p cmd
         begin
+          result = {}
           Timeout.timeout(@@action_timeout) do
             thr = Thread.new { result = SystemUtils.execute_command(cmd) }
             thr.join
+           
+            return result[:stdout] if result[:result] == 0
+            return log_error("failed to export ",service_hash,result)
           end
         rescue Timeout::Error
-          log_error_mesg('Timeout on Running Action ',cmd)
+          log_error_mesg('Export Timeout on Running Action ',cmd)
           return {}
         end
 end
