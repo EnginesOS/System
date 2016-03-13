6e35ffc7d1aa782c4a817e881eb40205cf3801dd
James L Vodanovich <james@jvodan.com>
2016-03-13 12:04:59 +1100

    service

--- lib/ruby/api/system/containers/container_api/engine_api_export_import.rb
+++ lib/ruby/api/system/containers/container_api/engine_api_export_import.rb
@@ -1,9 +1,9 @@
 module EngineApiExportImport
   def export_service(service_hash)
     SystemDebug.debug(SystemDebug.export_import, :export_service, service_hash)
-    cmd_dir = SystemConfig.BackupScriptsRoot + '/' + service[:publisher_namespace] + '/' + service[:type_path] + '/' + service[:service_handle] + '/'
+    cmd_dir = SystemConfig.BackupScriptsRoot + '/' + service_hash[:publisher_namespace] + '/' + service_hash[:type_path] + '/' + service_hash[:service_handle] + '/'
     SystemDebug.debug(SystemDebug.export_import, :export_service, cmd)
-    cmd = 'docker exec  ' + service[:parent_engine] + ' ' + cmd_dir + '/backup.sh ' 
+    cmd = 'docker exec  ' + service_hash[:parent_engine] + ' ' + cmd_dir + '/backup.sh ' 
       
         begin
           result = {}
