24ee819ce10a3f2b38c3c6ba85e8b22a6ea7abc4
James L Vodanovich <james@jvodan.com>
2016-03-14 19:35:25 +1100

    more explicit perms

--- scripts/make_big_temp.sh
+++ scripts/make_big_temp.sh
@@ -2,5 +2,5 @@
 #FIXME check for .. and other nasties
 mkdir -p /opt/engines/tmp/$1
 chgrp  -R  containers /opt/engines/tmp/$1
-chmod 777 -R /opt/engines/tmp/$1
+chmod oug+rwx -R /opt/engines/tmp/$1
 
