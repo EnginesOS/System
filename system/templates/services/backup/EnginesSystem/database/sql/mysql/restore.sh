5cb21aa0b612b3bcc6ee5b669ec4fbddb9da90fa
James L Vodanovich <james@jvodan.com>
2016-03-14 14:14:06 +1100

    import scripts

--- system/templates/services/backup/EnginesSystem/database/sql/mysql/restore.sh
+++ system/templates/services/backup/EnginesSystem/database/sql/mysql/restore.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
-cat - |gzip -d | mysql -h $dbhost -u $dbuser --password $dbpasswd $dbname
+
+Archive=/tmp/archive 
+cd /tmp
+cat - > $Archive
+
+type=`file -i $Archive |grep application/gzip`
+if test $? -eq 0
+ then
+  extract="|gzip -d "
+  fi
+  
+	cat $Archive $extract | mysql -h $dbhost -u $dbuser --password $dbpasswd $dbname 2> /tmp/extract.err
+	if test $? -eq 0
+	  then
+	   rm  $Archive
+	  
+	   exit 0
+	   else
+	
+	    cat  tmp/extract.err
+	    echo  Rolled back >&2
+	 fi 
+
+
+
 
--- system/templates/services/backup/EnginesSystem/filesystem/local/filesystem/restore.sh
+++ system/templates/services/backup/EnginesSystem/filesystem/local/filesystem/restore.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
+Archive=/tmp/archive 
+cd /home/fs
+dirname=`basename $VOLDIR `
+cp -rp $VOLDIR /home/fs/$dirname.bak
+
+cat - > $Archive
 cd /
-cat - |gzip -d | tar -xpf - 
+type=`file -i $Archive |grep application/gzip`
+if test $? -eq 0
+ then
+  extract="|gzip -d "
+  fi
+  
+	cat $Archive $extract | tar -xpf - 2>/tmp/extract.err
+	if test $? -eq 0
+	  then
+	   rm  $Archive
+	   rm /home/fs/$dirname.bak
+	   exit 0
+	   else
+	    rm -r $VOLDIR
+	    mv /home/fs/$dirname.bak $VOLDIR
+	    cat  /tmp/extract.err
+	    echo  Rolled back >&2
+	 fi 
+
