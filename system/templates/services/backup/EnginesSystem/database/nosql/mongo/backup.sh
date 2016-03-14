7ba2fd658accd26a6023c76ea0f6fbb4034b30d4
James L Vodanovich <james@jvodan.com>
2016-03-14 11:50:02 +1100

    error report

--- system/templates/services/backup/EnginesSystem/database/nosql/mongo/backup.sh
+++ system/templates/services/backup/EnginesSystem/database/nosql/mongo/backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /tmp
-mongodump  -h mongo --password $dbpasswd -u $dbuser -d  $dbname  2>/tmp/mysqldump.errs
+mongodump  -h mongo --password $dbpasswd -u $dbuser -d  $dbname  2>/tmp/mongodump.errs
 if test $? -ne 0
  then 
- 	cat  /tmp/mysqldump.errs
+ 	cat  /tmp/mongodump.errs
  	exit -1
  fi
 tar -cpf - dump |gzip -c
