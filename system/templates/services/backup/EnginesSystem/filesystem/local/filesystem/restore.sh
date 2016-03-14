0570ddcde60acac81677a64b4d6f8b999d77dec8
James L Vodanovich <james@jvodan.com>
2016-03-15 07:18:18 +1100

    import script

--- system/templates/services/backup/EnginesSystem/filesystem/local/filesystem/restore.sh
+++ system/templates/services/backup/EnginesSystem/filesystem/local/filesystem/restore.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
-Archive=/big_tmp/archive 
+Archive=/big_tmp/archive
 cd /home/fs
 dirname=`basename $VOLDIR `
 cp -rp $VOLDIR /big_tmp/$dirname.bak
+ rm -r $VOLDIR/*
+ 
+if test -f  /tmp/extract.err
+ then
+rm /tmp/extract.err
+fi
 
 cat - > $Archive
 cd /
 type=`file -i $Archive |grep application/gzip`
 if test $? -eq 0
  then
-  extract="|gzip -d "
-  fi
-  
-	cat $Archive $extract | tar -xpf - 2>/tmp/extract.err
-	if test $? -eq 0
-	  then
-	   rm  $Archive
-	   rm -r /big_tmp/$dirname.bak
-	   exit 0
-	   else
-	    rm -r $VOLDIR
-	    mv /big_tmp/$dirname.bak $VOLDIR
-	    cat  /tmp/extract.err
-	    echo  Rolled back >&2
-	 fi 
+echo Gzip
+ cat  $Archive  | gzip -d  | tar -xpf  -  2>/tmp/extract.err
 
+echo xxx
+echo "cat $Archive | gzip -d | tar -xpf  -"
+
+else
+cat $Archive | tar -xpf  - 2>/tmp/extract.err
+
+  fi
+        if test $? -eq 0
+          then
+           rm  $Archive
+           rm -r /big_tmp/$dirname.bak
+           exit 0
+           else
+            rm -r $VOLDIR/*
+            cp -rp /big_tmp/$dirname.bak/ $VOLDIR
+            cat  /tmp/extract.err
+            echo  Rolled back >&2
+         fi
