dba88b833ce6f1abdb37895efe75652ab72d438e
James L Vodanovich <james@jvodan.com>
2016-03-15 18:56:27 +1100

    set ownsershio of /home/fs

--- system/templates/deployment/global/build_scripts/chown_app_dir.sh
+++ system/templates/deployment/global/build_scripts/chown_app_dir.sh
@@ -8,7 +8,8 @@ if [ ! -d /home/app ]
   fi
   
  mkdir -p /home/fs ; mkdir -p /home/fs/local 
- chown -R $ContUser /home/app /home/fs /home/fs/local   
+ chown -R $ContUser /home/app /home/fs/local
+ chown 21000 /home/fs 
 
  
 #echo PACKAGE_INSTALLER_RUN "$PACKAGE_INSTALLER_RUN"
