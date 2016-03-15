946f00a0a1b5076af1dcc4e686d75c36ef4f7636
James L Vodanovich <james@jvodan.com>
2016-03-15 14:12:07 +1100

    better naming for shared vol

--- lib/ruby/containers/managed_container/managed_container_volumes.rb
+++ lib/ruby/containers/managed_container/managed_container_volumes.rb
@@ -2,12 +2,14 @@ module ManagedContainerVolumes
   def add_volume(service_hash)
     permissions = PermissionRights.new(service_hash[:parent_engine] , '', '')
     vol = Volume.new(service_hash) #service_hash[:variables][:name], SystemConfig.LocalFSVolHome + '/' + service_hash[:parent_engine]  + '/' + service_hash[:variables][:name], service_hash[:variables][:engine_path], 'rw', permissions)
+    if service_hash[:shared] == true
+    @volumes[service_hash[:parent_engine] + '_' + service_hash[:service_name]] = vol
+      else
     @volumes[service_hash[:variables][:name]] = vol
+end
     save_state
   rescue StandardError => e
-    p :add_volume_failed
-    p service_hash
-    log_exception(e)
+    log_exception(e,service_hash)
   end
 
 
