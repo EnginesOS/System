ac7aa0ef9201e43a545ad62665bdb0e8741f855a
James L Vodanovich <james@jvodan.com>
2016-03-17 10:25:27 +1100

    shell syntax

--- scripts/services/backup/run_backup_on_engine.sh
+++ scripts/services/backup/run_backup_on_engine.sh
@@ -2,7 +2,7 @@
 
 backup_path=`cat -`
 echo backup path $backup_path
-engine=`echo $backup_path | cut -1 -d:`
-path=`echo $backup_path | cut -2 -d:`
+engine=`echo $backup_path | cut -f 1 -d:`
+path=`echo $backup_path | cut -f 2 -d:`
 echo backup cmd docker exec -i $engine /home/services/$path/backup.sh
 docker exec -i $engine /home/services/$path/backup.sh
