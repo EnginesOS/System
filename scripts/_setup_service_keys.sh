534f893946b6c0469c304d022ce63bcc59b440d5
James L Vodanovich <james@jvodan.com>
2016-03-17 09:55:15 +1100

    chown keys

--- scripts/_setup_service_keys.sh
+++ scripts/_setup_service_keys.sh
@@ -8,15 +8,15 @@ if ! test -d  ~engines/.ssh/${service}
  fi
  
 shift
-rm ~/.ssh/_${service}_keys
+
 for key in $*
  do 
     rm /opt/engines/etc/ssh/keys/services/$service/{$key,$key.pub}
 	ssh-keygen -f /opt/engines/etc/ssh/keys/services/$service/$key -N ''
 	uid=`/opt/engines/scripts/get_service_uid.sh $service`
-	chown /opt/engines/etc/ssh/keys/services/$service/{$key,$key.pub}
+	chown $uid /opt/engines/etc/ssh/keys/services/$service/{$key,$key.pub}
 	chmod og-rwx /opt/engines/etc/ssh/keys/services/$service/$key
 	pubkey=`cat /opt/engines/etc/ssh/keys/services/$service/$key.pub`
- 	echo "command=\"/opt/engines/scripts/services/$service/${key}.sh\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty  $pubkey " >>  ~engines/.ssh/${service}/${key}
- 	chown engines ~engines/.ssh/${service}/${key}
- 	done
+ 	echo "command=\"/opt/engines/scripts/services/$service/${key}.sh\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty  $pubkey " >  ~engines/.ssh/${service}/${key}_access
+ 	chown engines ~engines/.ssh/${service}/${key}_access
+ done
