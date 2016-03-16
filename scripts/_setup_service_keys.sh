76ffb6e676fb0638e59530f58f247d182165b17c
James L Vodanovich <james@jvodan.com>
2016-03-17 08:22:19 +1100

    service key creation

--- scripts/_setup_service_keys.sh
+++ scripts/_setup_service_keys.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-
-	ssh-keygen -f /opt/engines/etc/ssh/keys/services/$service/$1 -N ''
-	uid=`/opt/engines/scripts/get_service_uid.sh $1`
-	chown /opt/engines/etc/ssh/keys/services/$service/{$1,$1.pub}
-	chmod og-rwx /opt/engines/etc/ssh/keys/services/$service/$1	
+service=$1
+shift
+rm ~/.ssh/_${service}_keys
+for key in $*
+ do 
+	ssh-keygen -f /opt/engines/etc/ssh/keys/services/$service/$key -N ''
+	uid=`/opt/engines/scripts/get_service_uid.sh $key`
+	chown /opt/engines/etc/ssh/keys/services/$service/{$key,$key.pub}
+	chmod og-rwx /opt/engines/etc/ssh/keys/services/$service/$key
+	pubkey=`cat /opt/engines/etc/ssh/keys/services/$service/$key.pub`
+ 	echo "command=\"/opt/engines/scripts/services/${key}.sh\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty  $pubkey " >>  ~/.ssh/${service}_${key}
+ 	done
