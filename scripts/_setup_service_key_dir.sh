e10caef55c94b876db2ee47c0259f5f6417604e4
James L Vodanovich <james@jvodan.com>
2016-03-16 21:31:53 +1100

    fix ssh keys permissions

--- scripts/_setup_service_key_dir.sh
+++ scripts/_setup_service_key_dir.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 uid=`/opt/engines/scripts/get_service_uid.sh $1`
-mkdir -p  /opt/engines/ssh/keys/services/$1
-chown $uid /opt/engines/ssh/keys/services/$1
+mkdir -p   /opt/engines/etc/ssh/keys/services/$1
+chown $uid  /opt/engines/etc/ssh/keys/services/$1
