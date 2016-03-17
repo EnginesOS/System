af09a4bd5f8bb4265958085ba670749d96665846
James L Vodanovich <james@jvodan.com>
2016-03-18 09:18:03 +1100

    trace on osapi fail

--- lib/ruby/api/public/engines_osapi/return_objects.rb
+++ lib/ruby/api/public/engines_osapi/return_objects.rb
@@ -10,7 +10,8 @@ module ReturnObjects
     p item_name
     p cmd
     p mesg.to_s + ':' + last_api_error.to_s
-   p 'Debug:' + caller[1].to_s + ':' + caller[2].to_s + ':' + caller[3].to_s
+   #p 'Debug:' + caller[1].to_s + ':' + caller[2].to_s + ':' + caller[3].to_s
+    p 'Debug:' + caller.to_s
     EnginesOSapiResult.failed(item_name, mesg, cmd)
   end
 
