ad59eac2ec946dc6df3a5858811bb0ecfdd0f804
James L Vodanovich <james@jvodan.com>
2016-03-18 16:18:21 +1100

    just assume added

--- lib/ruby/service_manager/system_registry/api/shares.rb
+++ lib/ruby/service_manager/system_registry/api/shares.rb
@@ -8,7 +8,7 @@ module Shares
     end
  
     def add_share_to_managed_engines_registry(shared_service)
-      return false unless add_to_managed_engines_registry(shared_service)
+      add_to_managed_engines_registry(shared_service)
       rest_post('/v0/system_registry/shares/add',shared_service )
       
     end
