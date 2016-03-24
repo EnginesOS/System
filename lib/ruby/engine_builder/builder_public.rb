a3f29c4b7fe8d1d51d017bc7b2476be89b2569eb
James L Vodanovich <james@jvodan.com>
2016-03-25 10:33:24 +1100

    http_ahttps defaults to http, reenforce has http

--- lib/ruby/engine_builder/builder_public.rb
+++ lib/ruby/engine_builder/builder_public.rb
@@ -31,7 +31,7 @@ class BuilderPublic
 
   def http_protocol
     if @builder.build_params[:http_protocol] == 'http_https'
-      return 'https'
+      return 'http'
     end
     return @builder.build_params[:http_protocol]
   end
