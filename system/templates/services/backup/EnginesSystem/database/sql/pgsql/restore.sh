6e68141e71f8a65f71dcef159b2cb0c41ba236fc
James L Vodanovich <james@jvodan.com>
2016-03-25 14:34:47 +1100

    strip first line from dataseed

--- system/templates/services/backup/EnginesSystem/database/sql/pgsql/restore.sh
+++ system/templates/services/backup/EnginesSystem/database/sql/pgsql/restore.sh
@@ -12,19 +12,20 @@ if test $? -eq 0
  then
  cat $Archive| gzip -d| env  PGPASSWORD=$dbpasswd psql   -h $dbhost -U $dbuser  $dbname  2> /tmp/extract.err
  else
- cat $Archive $extract | env  PGPASSWORD=$dbpasswd psql   -h $dbhost -U $dbuser  $dbname  2> /tmp/extract.err
+ cat $Archive | env  PGPASSWORD=$dbpasswd psql   -h $dbhost -U $dbuser  $dbname  2> /tmp/extract.err
   fi
 	
 	if test $? -eq 0
 	  then
 	   rm  $Archive 
-	  
+	   rm  /tmp/extract.err
 	   exit 0
 	   else
 	
 	    cat  /tmp/extract.err
 	    echo  Rolled back >&2
-	    rm  $Archive 
+	    rm  $Archive
+	    rm  /tmp/extract.err
 	 fi 
 
 
