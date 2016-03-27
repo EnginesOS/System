2f270149fba72e4dab03a7751ebf01d8fd2c636b
James L Vodanovich <james@jvodan.com>
2016-03-27 12:41:32 +1100

    debug

--- system/templates/deployment/rails4/build_scripts/run_rake_tasks.sh
+++ system/templates/deployment/rails4/build_scripts/run_rake_tasks.sh
@@ -2,5 +2,6 @@
     for task in $*
      do
      mod=`echo $task | sed "/[;&]/s///g"`
+     echo "/usr/local/rbenv/shims/bundle exec rake $task"
      /usr/local/rbenv/shims/bundle exec rake $task
      done
