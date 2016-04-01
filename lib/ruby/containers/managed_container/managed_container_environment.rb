3a56f4cc5a64c53fcf97f574085fc3a9ed4a91d5
James L Vodanovich <james@jvodan.com>
2016-04-01 11:36:04 +1100

    dbg env change

--- lib/ruby/containers/managed_container/managed_container_environment.rb
+++ lib/ruby/containers/managed_container/managed_container_environment.rb
@@ -1,15 +1,18 @@
 module ManagedContainerEnvironment
   def update_environment(key,value, add=false)
+    SystemDebug.debug(SystemDebug.containers, :update_environment, key, value, @environments)
     return false if @environments.nil?
     
     @environments.each do |environment|
     if environment.name == key        
+      SystemDebug.debug(SystemDebug.containers, :update_environment, "Cahnged")
       environment.value = value
       return true
     end
     end
     
     if add == true
+      SystemDebug.debug(SystemDebug.containers, :update_environment, "added")
       env = EnvironmentVariable.new(key,value)
       @environments.push(env)
       return true
