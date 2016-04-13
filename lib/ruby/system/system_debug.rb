37c23b57297cbf9202792045684290f56a093a00
James L Vodanovich <james@jvodan.com>
2016-04-14 09:32:30 +1000

    change debug flags

--- lib/ruby/system/system_debug.rb
+++ lib/ruby/system/system_debug.rb
@@ -26,7 +26,7 @@ class SystemDebug
     @@debug_flags = 0
     require '/opt/engines/etc/debug/debug_flags.rb'
   else
-    @@debug_flags = @@templater| @@services | @@export_import |@@builder|@@container_events|@@execute
+    @@debug_flags = @@templater| @@services | @@export_import |@@builder|@@container_events|@@execute |@@services
   end
   #end
   #
