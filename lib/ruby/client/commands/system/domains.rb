07b16a849ac79e00db2151f1c21c9d3c0145a4bf
James L Vodanovich <james@jvodan.com>
2016-04-17 16:35:29 +1000

    fi

--- lib/ruby/client/commands/system/domains.rb
+++ lib/ruby/client/commands/system/domains.rb
@@ -7,7 +7,6 @@ when 'remove'
   @route += '/' + ARGV[3]
   perform_get
 when 'add'
-  @route += '/' + ARGV[2]
   params_data = read_stdin_data
   command_useage unless params_data.is_a?(String)
   perform_post(JSON.parse(params_data))
--- lib/ruby/server/api/system/domains.rb
+++ lib/ruby/server/api/system/domains.rb
@@ -1,20 +1,6 @@
 
-get '/v0/system/domains/:id' do
-  domain_name = @@core_api.domain_name(params[:id])
-  unless domain_name.is_a?(FalseClass)
-    return domain_name.to_json
-  else
-    return log_error('domain_name')
-  end
-end
 
-post '/v0/system/domain/:id' do
-  unless @@core_api.update_domain(params).is_a?(FalseClass)
-    return status(202)
-  else
-    return log_error('update_domain', params)
-  end
-end  
+
 
 post '/v0/system/domains/' do
   unless @@core_api.add_domain(params).is_a?(FalseClass)
