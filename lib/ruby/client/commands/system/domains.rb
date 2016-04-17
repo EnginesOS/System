8f540c3f0d285a9a03525052e8da6a55eb83a36b
James L Vodanovich <james@jvodan.com>
2016-04-17 15:53:56 +1000

    list domains

--- lib/ruby/client/commands/system/domains.rb
+++ lib/ruby/client/commands/system/domains.rb
@@ -1,19 +1,23 @@
-@route += '/' + ARGV[2]
+
 case ARGV[2]
 when 'view'
+  @route += '/' + ARGV[2]
   @route += '/' + ARGV[3]
   perform_get
 when 'list'
   perform_get
 when 'remove'
+  @route += '/' + ARGV[2]
   @route += '/' + ARGV[3]
   perform_get
 when 'update'
+  @route += '/' + ARGV[2]
   @route += '/' + ARGV[3]
   params_data = read_stdin_data
   command_useage unless params_data.is_a?(String)
   perform_post(JSON.parse(params_data))
 when 'add'
+  @route += '/' + ARGV[2]
   params_data = read_stdin_data
   command_useage unless params_data.is_a?(String)
   perform_post(JSON.parse(params_data))
