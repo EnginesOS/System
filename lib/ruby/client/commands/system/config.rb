c922c36ffa794142d362bfd20ee657e6c26e2a23
James L Vodanovich <james@jvodan.com>
2016-04-17 18:09:35 +1000

    config

--- lib/ruby/client/commands/system/config.rb
+++ lib/ruby/client/commands/system/config.rb
@@ -1,21 +1,22 @@
 case ARGV[2]
 when 'default_domain'
-
+@route += '/' + ARGV[2]
 when 'default_site'
-
+@route += '/' + ARGV[2]
 when 'hostname'
-  
+@route += '/' + ARGV[2]
 when 'remote_exception_logging'
-
+@route += '/' + ARGV[2]
 when 'set'
+
   case ARGV[3]
   when 'default_domain'
-
+@route += '/' + ARGV[3]
   when 'default_site'
-
+@route += '/' + ARGV[3]
   when 'hostname'
-    
+@route += '/' + ARGV[3]   
   when 'remote_exception_logging'
-    
+@route += '/' + ARGV[3]   
   end
 end
--- lib/ruby/server/api/system/do_first_run.rb
+++ lib/ruby/server/api/system/do_first_run.rb
@@ -4,7 +4,7 @@
 
 post '/v0/system/do_first_run' do
 
-  unless @@core_api.set_first_run_parameters(params).is_a?(FalseClass)
+  unless @@core_api.set_first_run_parameters(Utils.symbolize_keys(params)).is_a?(FalseClass)
     return status(202)
   else
     return log_error('set_first_run_parameters', @@core_api.last_error)
