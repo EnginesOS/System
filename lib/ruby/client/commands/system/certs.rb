ce65d63ff30b66790f85cc473d26cc70a5262a44
James L Vodanovich <james@jvodan.com>
2016-04-18 09:57:50 +1000

    post default cert

--- lib/ruby/client/commands/system/certs.rb
+++ lib/ruby/client/commands/system/certs.rb
@@ -6,6 +6,25 @@ end
 
 case ARGV[2]
 
+when 'default'
+@route += '/default'
+if ARGV.count < 6
+  command_useage('missing arguments')
+end
+command_useage('missing cert file ') unless File.exist?(ARGV[4])
+command_useage('missing key file ') unless  File.exist?(ARGV[5])
+
+pass = nil
+if ARGV.count == 7
+  pass=ARGV[6]
+end
+params = {}
+params[:domain_name] = ARGV[3]
+params[:certificate] = File.read(ARGV[4])
+params[:key] = File.read(ARGV[5])
+params[:password] = pass unless pass.nil?
+
+perform_post(params)
 
 when 'add'
 @route += '/'
