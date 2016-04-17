8ccf009d21a78669e51c1d78b997c820b9175aad
James L Vodanovich <james@jvodan.com>
2016-04-17 17:40:07 +1000

    bring symbolix stuff

--- lib/ruby/client/main.rb
+++ lib/ruby/client/main.rb
@@ -145,7 +145,7 @@ def rest_get(path,params=nil)
     retry_count = 0
    # STDERR.puts('Get Path:' + path.to_s + ' Params:' + params.to_s)
     r = RestClient.get(@base_url + path, params)
-    parse_rest_response(r)
+      p r
 
   rescue StandardError => e
     STDERR.puts e.to_s + ' with path:' + path + "\n" + 'params:' + params.to_s
@@ -157,7 +157,7 @@ def rest_post(path, params=nil)
   begin
     #STDERR.puts('Post Path:' + path.to_s + ' Params:' + params.to_s)
     r = RestClient.post(@base_url + path, params)
-    parse_rest_response(r)
+    p r
     exit
   rescue StandardError => e
     STDERR.puts e.to_s + ' with path:' + path + "\n" + 'params:' + params.to_s
@@ -168,7 +168,7 @@ def rest_delete(path, params=nil)
   begin
     #STDERR.puts('Post Path:' + path.to_s + ' Params:' + params.to_s)
     r = RestClient.delete(@base_url + path, params)
-    parse_rest_response(r)
+    p r
     exit
   rescue StandardError => e
     STDERR.puts e.to_s + ' with path:' + path + "\n" + 'params:' + params.to_s
