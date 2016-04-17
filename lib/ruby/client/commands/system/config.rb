d5b4383d9b32c4d2572201672abf469152ecdb20
James L Vodanovich <james@jvodan.com>
2016-04-17 18:13:27 +1000

    config

--- lib/ruby/client/commands/system/config.rb
+++ lib/ruby/client/commands/system/config.rb
@@ -1,22 +1,27 @@
 case ARGV[2]
-when 'default_domain'
-@route += '/' + ARGV[2]
-when 'default_site'
-@route += '/' + ARGV[2]
-when 'hostname'
-@route += '/' + ARGV[2]
-when 'remote_exception_logging'
-@route += '/' + ARGV[2]
+
 when 'set'
 
   case ARGV[3]
   when 'default_domain'
-@route += '/' + ARGV[3]
+    @route += '/' + ARGV[3]
   when 'default_site'
-@route += '/' + ARGV[3]
+    @route += '/' + ARGV[3]
   when 'hostname'
-@route += '/' + ARGV[3]   
+    @route += '/' + ARGV[3]
   when 'remote_exception_logging'
-@route += '/' + ARGV[3]   
+    @route += '/' + ARGV[3]
   end
+  perform_post(ARGV[3])
+
+when 'default_domain'
+  @route += '/' + ARGV[2]
+when 'default_site'
+  @route += '/' + ARGV[2]
+when 'hostname'
+  @route += '/' + ARGV[2]
+when 'remote_exception_logging'
+  @route += '/' + ARGV[2]
 end
+
+perform_get
