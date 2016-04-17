255f95a4fa513e24c5c699381492ef82032b74aa
James L Vodanovich <james@jvodan.com>
2016-04-17 11:55:58 +1000

    require paths

--- lib/ruby/client/commands/commands.rb
+++ lib/ruby/client/commands/commands.rb
@@ -1,18 +1,18 @@
 @route += '/' + ARGV[0]
 case ARGV[0]
 when 'containers'
-  require_relative 'commands/containers/commands.rb'
+  require_relative 'containers/commands.rb'
 when 'engines'
-  require_relative 'commands/containers/engines/commands.rb'
+  require_relative 'containers/engines/commands.rb'
 when 'services'
-  require_relative 'commands/containers/services/commands.rb'
+  require_relative 'containers/services/commands.rb'
 when 'registry'
-  require_relative 'commands/registry/commands.rb'
+  require_relative 'registry/commands.rb'
 when 'engines_builder'
-  require_relative 'commands/engines_builder/commands.rb'
+  require_relative 'engines_builder/commands.rb'
 when 'service_manager'
-  require_relative 'commands/service_manager/commands.rb'
+  require_relative 'service_manager/commands.rb'
 when 'system'
-  require_relative 'commands/system/commands.rb'
+  require_relative 'system/commands.rb'
 
 end
