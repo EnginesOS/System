e184a4dc24eccb0293cbe4379d5f644ef9673cad
James L Vodanovich <james@jvodan.com>
2016-04-17 15:11:29 +1000

    parse data as json

--- lib/ruby/client/commands/system/do_first_run.rb
+++ lib/ruby/client/commands/system/do_first_run.rb
@@ -1,5 +1,8 @@
-
-params = read_stdin_data
-command_useage unless params.is_a?(String)
-perform_post(params.to_json)
+begin
+params_data = read_stdin_data
+command_useage unless params_data.is_a?(String)
+perform_post(JSON.parse(params_data))
+rescue StandardError => e
+   log_error('Not a Json String',e,e.back_trace)
+end
   
