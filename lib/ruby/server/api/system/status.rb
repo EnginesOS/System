4ce4385c30aff5d4cbfc141a827a644e905f7351
James L Vodanovich <james@jvodan.com>
2016-04-17 13:09:26 +1000

    false not an error

--- lib/ruby/server/api/system/status.rb
+++ lib/ruby/server/api/system/status.rb
@@ -3,12 +3,9 @@
   #/system/status/first_run_has_run
 
 get '/v0/system/status/first_run_has_run' do
-  first_run_required = @@core_api.first_run_required?
-  unless first_run_required.is_a?(FalseClass)
-    return first_run_required.to_json
-  else
-    return log_error('first_run_required')
-  end
+  first_run_required = @@core_api.first_run_required? 
+    return first_run_required.to_json # no checky as true or false
+  
 end
 
 get '/v0/system/status' do
