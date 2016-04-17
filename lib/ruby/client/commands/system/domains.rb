be5987dffef4dc14329e1a2d8606175177d1e70a
James L Vodanovich <james@jvodan.com>
2016-04-17 16:08:53 +1000

    domain name routes

--- lib/ruby/client/commands/system/domains.rb
+++ lib/ruby/client/commands/system/domains.rb
@@ -1,17 +1,14 @@
 
 case ARGV[2]
 when 'view'
-  @route += '/' + ARGV[2]
   @route += '/' + ARGV[3]
   perform_get
 when 'list'
   perform_get
 when 'remove'
-  @route += '/' + ARGV[2]
   @route += '/' + ARGV[3]
   perform_get
 when 'update'
-  @route += '/' + ARGV[2]
   @route += '/' + ARGV[3]
   params_data = read_stdin_data
   command_useage unless params_data.is_a?(String)
