b8d71c9aa3841d5b157f3ecf00c512fa482d1917
James L Vodanovich <james@jvodan.com>
2016-04-17 12:59:56 +1000

    correct path

--- lib/ruby/server/api/system/status.rb
+++ lib/ruby/server/api/system/status.rb
@@ -2,7 +2,7 @@
   #/system/status Get
   #/system/status/first_run_has_run
 
-get '/v0/status/first_run_has_run' do
+get '/v0/system/status/first_run_has_run' do
   first_run_required = @@core_api.first_run_required
   unless first_run_required.is_a?(FalseClass)
     return first_run_required.to_json
