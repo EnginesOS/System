4db1bc03d9e92f20853c33fabb681664ee88044f
James L Vodanovich <james@jvodan.com>
2016-04-17 13:52:08 +1000

    metrics

--- lib/ruby/client/commands/system/metrics.rb
+++ lib/ruby/client/commands/system/metrics.rb
@@ -1,6 +1,14 @@
+
+command_useage unless ARGV[2].is_a?(String)
+
 case ARGV[2]
-when 'mem'
-when 'load'
-when 'mem_stat'
-when 'disk'
+#when 'mem'
+#when 'load'
+when 'mem_stats'
+@route += '/' + 'memory/statistics'
+#when 'disk'
+else
+  @route += '/' + ARGV[2]
 end
+
+perform_get
