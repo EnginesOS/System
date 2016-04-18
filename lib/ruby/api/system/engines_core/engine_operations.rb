e419687bc6d2f6832dbb2e1aa3036d84e7b1b4e9
James L Vodanovich <james@jvodan.com>
2016-04-19 08:43:51 +1000

    rm junl

--- lib/ruby/api/system/engines_core/engine_operations.rb
+++ lib/ruby/api/system/engines_core/engine_operations.rb
@@ -79,7 +79,7 @@ module EnginesOperations
   def set_engine_runtime_properties(params)
     engine_name = params[:engine_name]
     engine = loadManagedEngine(engine_name)
-    if engine.is_a?(EnginesOSapiResult)
+    if engine.is_a?(false)
       @last_error = engine.result_mesg
       return false
     end
