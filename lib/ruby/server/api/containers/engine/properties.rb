9d2fb7ac8e25346fa43043b391f56ff783d4a271
James L Vodanovich <james@jvodan.com>
2016-04-19 09:34:42 +1000

    fix

--- lib/ruby/server/api/containers/engine/properties.rb
+++ lib/ruby/server/api/containers/engine/properties.rb
@@ -2,8 +2,9 @@
 #/containers/engine/container_name/network_properties
 
 post '/v0/containers/engine/:id/properties/network' do
-  params[:engine_name] = params[:id]
 
+  engine = get_engine(params[:id])
+  p :LOADED
   r = @@core_api.set_engine_network_properties(engine, Utils.symbolize_keys(params))
 
   return log_error('set network properties', params) if r.is_a?(FalseClass)
@@ -11,8 +12,7 @@ post '/v0/containers/engine/:id/properties/network' do
 end
 
 post '/v0/containers/engine/:id/properties/runtime' do
-  engine = get_engine(params[:id])
-  p :LOADED
+  params[:engine_name] = params[:id]
   r =   @@core_api.set_engine_runtime_properties(Utils.symbolize_keys(params))
   return log_error('set run time properties', params) if r.is_a?(FalseClass)
   r.to_json
