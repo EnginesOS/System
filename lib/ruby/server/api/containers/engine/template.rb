68adc0a95236bf4d820ca738567ca40e18d3f325
James L Vodanovich <james@jvodan.com>
2016-04-18 18:07:44 +1000

    path for template

--- lib/ruby/server/api/containers/engine/template.rb
+++ lib/ruby/server/api/containers/engine/template.rb
@@ -1,6 +1,6 @@
 #/containers/engine/container_name/template
 
-post '/v0/system/template' do
+post '/v0/containers/engine/:id/template' do
   engine = get_engine(params[:id])
   resolved_string = @@core_api.get_resolved_string(params['string'],engine)
   return log_error('resolved_string', params) if resolved_string.is_a?(FalseClass)
