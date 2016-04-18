e586d6cf157af6d03086d50f95f803cc40683501
James L Vodanovich <james@jvodan.com>
2016-04-18 18:15:17 +1000

    engine_name

--- lib/ruby/server/api/containers/engine/template.rb
+++ lib/ruby/server/api/containers/engine/template.rb
@@ -2,7 +2,7 @@
 
 post '/v0/containers/engine/:id/template' do
   engine = get_engine(params[:id])
-  resolved_string = @@core_api.get_resolved_engine_string(params['string'],engine)
+  resolved_string = @@core_api.get_resolved_engine_string(params[:string],engine)
   return log_error('resolved_string', params) if resolved_string.is_a?(FalseClass)
   resolved_string.to_json
 end
