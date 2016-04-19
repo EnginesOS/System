34fa2646a8d316c09dd0071e5fd9cc5614914db7
James L Vodanovich <james@jvodan.com>
2016-04-20 09:29:10 +1000

    service

--- lib/ruby/server/api/containers/services/routes.rb
+++ lib/ruby/server/api/containers/services/routes.rb
@@ -1,1 +1,27 @@
 
+get '/v0/containers/services/' do
+  engines = @@core_api.getManagedServices
+  unless engines.is_a?(FalseClass)
+    return engines.to_json
+  else
+    return log_error('services')
+  end
+end
+
+get '/v0/containers/services/container_name' do
+  container_names = @@core_api.list_managed_services
+  unless container_names.is_a?(FalseClass)
+    return container_names.to_json
+  else
+    return log_error('container_name')
+  end
+end
+
+get '/v0/containers/services/state' do
+  states = @@core_api.get_services_states
+  unless states.is_a?(FalseClass)
+    return states.to_json
+  else
+    return log_error('states')
+  end
+end
