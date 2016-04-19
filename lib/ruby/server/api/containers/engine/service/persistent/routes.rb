920a1f964157c86f4a89b2de0d307d1cc84f4e13
James L Vodanovich <james@jvodan.com>
2016-04-20 08:15:57 +1000

    return r.b

--- lib/ruby/server/api/containers/engine/service/persistent/routes.rb
+++ lib/ruby/server/api/containers/engine/service/persistent/routes.rb
@@ -5,10 +5,9 @@ get '/v0/containers/engine/:id/service/persistent/:ns/*/export' do
   engine = get_engine(params[:id])
   return false if engine.is_a?(FalseClass)
    r = engine.export_service_data(hash)
-    p :exporting
- p r.class.name
+
   unless r.is_a?(FalseClass)
-    return r
+    return r.b
     #.to_json
   else
     return log_error(' export_service')
