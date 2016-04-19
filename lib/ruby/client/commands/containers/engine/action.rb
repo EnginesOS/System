0c57e773b6f96b338a17cb19c5afe09249fe31ce
James L Vodanovich <james@jvodan.com>
2016-04-19 13:45:38 +1000

    do action

--- lib/ruby/client/commands/containers/engine/action.rb
+++ lib/ruby/client/commands/containers/engine/action.rb
@@ -1,1 +1,8 @@
+if ARGV.count == 5
+  @route += '/' + ARGV[4]   
+  params_data = read_stdin_data
+  command_useage unless params_data.is_a?(String)
+  perform_post(JSON.parse(params_data))
+end
+  
 @route += '/' + ARGV[3]
