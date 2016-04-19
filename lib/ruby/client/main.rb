e5581de5ecf0d638ab0cb72da311b8c6d6b2e199
James L Vodanovich <james@jvodan.com>
2016-04-19 21:19:00 +1000

    betterdebug

--- lib/ruby/client/main.rb
+++ lib/ruby/client/main.rb
@@ -19,7 +19,7 @@ def parse_rest_response(r)
   return deal_with_jason(res)
 rescue  StandardError => e
   STDERR.puts e.to_s
-  STDERR.puts "Failed to parse rest response _" + res.to_s + "_"
+  STDERR.puts "Failed to parse rest response _" + r.to_s + "_"
   return false
 end
 
