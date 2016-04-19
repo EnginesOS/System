978bbb27ba905dbe72bfc740013682a4fb0fe4e7
James L Vodanovich <james@jvodan.com>
2016-04-20 07:53:07 +1000

    add exmport/import

--- lib/ruby/server/api/containers/engine/service/persistent/routes.rb
+++ lib/ruby/server/api/containers/engine/service/persistent/routes.rb
@@ -1,3 +1,33 @@
+
+get '/v0/containers/engine/:id/service/persistent/:ns/*/export' do
+  
+  hash = Utils.service_hash_from_params(params)
+  engine = get_engine(params[:id])
+  return false if engine.is_a?(FalseClass)
+   r = engine.export_service_data(hash)
+
+  unless r.is_a?(FalseClass)
+    return r.to_json
+  else
+    return log_error(' export_service')
+  end
+end
+
+get '/v0/containers/engine/:id/service/persistent/:ns/*/import' do
+  
+  hash = Utils.service_hash_from_params(params)
+  engine = get_engine(params[:id])
+  return false if engine.is_a?(FalseClass)
+  r = engine.export_service_data(hash)
+  
+
+
+  unless r.is_a?(FalseClass)
+    return r.to_json
+  else
+    return log_error(' import_service_')
+  end
+end
 get '/v0/containers/engine/:id/service/persistent/:ns/*' do
   
   hash = Utils.service_hash_from_params(params)
