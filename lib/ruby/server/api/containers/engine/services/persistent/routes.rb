20cc203b307e526bff1e2d3920c6e7767304a9f4
James L Vodanovich <james@jvodan.com>
2016-04-19 20:47:45 +1000

    find_engine_service_hash(hash)

--- lib/ruby/server/api/containers/engine/services/persistent/routes.rb
+++ lib/ruby/server/api/containers/engine/services/persistent/routes.rb
@@ -17,7 +17,7 @@ hash = {}
         hash[:type_path] = splats[0]    
 p hash
           
-  r = @@core_api.find_engine_service_hash(hash)
+  r = @@core_api.find_engine_services_hashes(hash)
 
   unless r.is_a?(FalseClass)
     return r.to_json
