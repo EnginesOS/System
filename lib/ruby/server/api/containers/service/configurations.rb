200eb6974b6ed42a1ebffe7d6ac479396b94bcec
James L Vodanovich <james@jvodan.com>
2016-04-20 13:29:03 +1000

    hash

--- lib/ruby/server/api/containers/service/configurations.rb
+++ lib/ruby/server/api/containers/service/configurations.rb
@@ -26,8 +26,10 @@ end
 post '/v0/containers/service/:id/configuration/:configurator_name' do
   service = get_engine(params[:id])
    return false if service.is_a?(FalseClass)
-  cparams = {}
-  cparams[:configurator_name] = params[:configurator_name]
+  cparams = Utils.symbolize_keys(params)
+  
+  #cparams[:configurator_name] = params[:configurator_name]
+    
    r = service.run_configurator(cparams)
   unless r.is_a?(FalseClass) 
       r.to_json
