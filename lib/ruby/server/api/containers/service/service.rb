bb23cc4a0052c915f826920c3055765ea282bed3
James L Vodanovich <james@jvodan.com>
2016-04-20 10:53:56 +1000

    service

--- lib/ruby/server/api/containers/service/service.rb
+++ lib/ruby/server/api/containers/service/service.rb
@@ -5,41 +5,24 @@
 #/containers/engine/container_name/build_report
 #/containers/engine/container_name/blueprint
 
-get '/v0/containers/engine/:id' do
-  engine = get_engine(params[:id])
-  unless engine.is_a?(FalseClass)
-    return engine.to_json
+get '/v0/containers/service/:id' do
+  service = get_service(params[:id])
+  unless service.is_a?(FalseClass)
+    return service.to_json
+  else
+    return log_error('service')
+  end
+end
+
+get '/v0/containers/service/:id/state' do
+  service = get_service(params[:id])
+ return false if service.is_a?(FalseClass)
+  r = service.read_state
+  unless r.is_a?(FalseClass)
+    return r.to_json
   else
     return log_error('engine')
   end
 end
 
-get '/v0/containers/engine/:id/state' do
-  engine = get_engine(params[:id])
- return false if engine.is_a?(FalseClass)
-  r = engine.read_state
-  unless r.is_a?(FalseClass)
-    return r.to_json
-  else
-    return log_error('engine')
-  end
-end
 
-get '/v0/containers/engine/:id/blueprint' do
-  engine = get_engine(params[:id])
-  return false if engine.is_a?(FalseClass)
-  r = engine.load_blueprint
-  unless r.is_a?(FalseClass)
-    return r.to_json
-  else
-    return log_error('blueprint')
-  end
-end
-get '/v0/containers/engine/:id/build_report' do
-  r = @@core_api.get_build_report(params[:id])
-  unless r.is_a?(FalseClass)
-    return r.to_json
-  else
-    return log_error('build_report')
-  end
-end
