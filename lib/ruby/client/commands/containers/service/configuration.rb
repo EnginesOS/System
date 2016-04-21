a1b872cf93613d778ce2c3108290d98d9f155667
James L Vodanovich <james@jvodan.com>
2016-04-21 10:31:29 +1000

    change var names

--- lib/ruby/client/commands/containers/service/configuration.rb
+++ lib/ruby/client/commands/containers/service/configuration.rb
@@ -3,6 +3,7 @@ case  ARGV[3]
 when 'run'
 @route += '/' + ARGV[4]
 params_data = read_stdin_json
+p params_data
 perform_post(params_data)
 end
 @route += '/' + ARGV[3]
--- lib/ruby/containers/managed_service/managed_service_configurations.rb
+++ lib/ruby/containers/managed_service/managed_service_configurations.rb
@@ -9,8 +9,8 @@ module ManagedServiceConfigurations
   
   def retrieve_configurator(configurator_params)
     configurator_params[:service_name] = @container_name
-    return log_error_mesg('service not running ',params) if is_running? == false
-    return log_error_mesg('service missing cont_userid ',params) if check_cont_uid == false
+    return log_error_mesg('service not running ',configurator_params) if is_running? == false
+    return log_error_mesg('service missing cont_userid ',configurator_params) if check_cont_uid == false
     @container_api.retrieve_configurator(self, configurator_params)
   end
   def get_service_configurations
