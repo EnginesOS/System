f74f7294b4812fc6784641e498c12bb1c4cb38f0
James L Vodanovich <james@jvodan.com>
2016-04-22 09:17:56 +1000

    fix

--- src/server/api/system/template.rb
+++ src/server/api/system/template.rb
@@ -1,8 +1,8 @@
 #/system/template/env P
 
-post '/v0/containers/engine/:engine_name/template' do
-  engine = get_engine(params[:engine_name])
-  cparams =  assemble_params(params, [:engine_name],  :string) 
+post '/v0/system/template' do
+ # engine = get_engine(params[:engine_name])
+  cparams =  assemble_params(params, [],  :string) 
   resolved_string = @@core_api.get_resolved_string(cparams[:string])
   return log_error('resolved_string', cparams) if resolved_string.is_a?(FalseClass)
   resolved_string.to_json
