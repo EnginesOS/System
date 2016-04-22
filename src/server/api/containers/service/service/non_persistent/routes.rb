e379a19a15f8f80aad14fc92d8425bfec847bd2d
James L Vodanovich <james@jvodan.com>
2016-04-22 10:59:20 +1000

    engine_service_hash_from_params

--- src/server/api/containers/service/service/non_persistent/routes.rb
+++ src/server/api/containers/service/service/non_persistent/routes.rb
@@ -1,15 +1,54 @@
+#
+#get '/v0/containers/service/:service_name/service/non_persistent/' do
+#  service = get_service(params[:service_name])
+#  r = @@core_api.list_non_persistent_services(service)
+#
+#  unless r.is_a?(FalseClass)
+#    return r.to_json
+#  else
+#    return log_error('pause')
+#  end
+#end
 
-get '/v0/containers/service/:service_name/service/non_persistent/' do
-  service = get_service(params[:service_name])
-  r = @@core_api.list_non_persistent_services(service)
+get '/v0/containers/engine/:engine_name/service/non_persistent/:ns/*/register' do
+  
+  hash = Utils.service_service_hash_from_params(params)
+  
+
+  r = @@core_api.force_register_attached_service(hash)
 
   unless r.is_a?(FalseClass)
     return r.to_json
   else
-    return log_error('pause')
+    return log_error(' register_service_hash')
   end
 end
 
+get '/v0/containers/engine/:engine_name/service/non_persistent/:ns/*/reregister' do
+  
+  hash = Utils.service_service_hash_from_params(params)
+ 
+  r = @@core_api.force_reregister_attached_service(hash)
+
+  unless r.is_a?(FalseClass)
+    return r.to_json
+  else
+    return log_error(' reregister_service_hash')
+  end
+end
+
+get '/v0/containers/engine/:engine_name/service/non_persistent/:ns/*/deregister' do
+  
+  hash = Utils.service_service_hash_from_params(params)
+ 
+ 
+ r = @@core_api.force_deregister_attached_service(hash)
+  unless r.is_a?(FalseClass)
+    return r.to_json
+  else
+    return log_error(' deregister_service_hash')
+  end
+end
 
 get '/v0/containers/service/:service_name/service/non_persistent/:ns/*' do
   #splats = params['splat']
@@ -31,4 +70,6 @@ p hash
   else
     return log_error(' find_engine_service_hash')
   end
+  
+  
 end
