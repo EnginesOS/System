91dc213c068763c7e27debe70b73ff3589340397
James L Vodanovich <james@jvodan.com>
2016-04-23 15:27:03 +1000

    core for configurators

--- lib/ruby/api/public/engines_api/system/config.rb
+++ lib/ruby/api/public/engines_api/system/config.rb
@@ -18,7 +18,7 @@ module Config
        service_param[:variables] = {}
        service_param[:variables][:hostname] = hostname
        service_param[:variables][:domain_name] = get_default_domain
-       update_service_configuration(service_param)
+     @core_api.update_service_configuration(service_param)
      end
  
    def set_default_site(params)
@@ -29,14 +29,14 @@ module Config
      service_param[:configurator_name] = 'default_site'
      service_param[:variables] = {}
      service_param[:variables][:default_site_url] = default_site_url
-     update_service_configuration(service_param)
+     @core_api.update_service_configuration(service_param)
    end
  
    def get_default_site()
      service_param = {}
      service_param[:service_name] = 'nginx'
      service_param[:configurator_name] = 'default_site'
-     config_params = retrieve_service_configuration(service_param)
+     config_params = @core_api.retrieve_service_configuration(service_param)
      if config_params.is_a?(Hash) == true && config_params.key?(:variables) == true
        vars = config_params[:variables]
        return vars[:default_site_url] if vars.key?(:default_site_url)
