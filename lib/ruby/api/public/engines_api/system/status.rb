9b6d8a80b7f6080796cb813fef6fc69b39e4604b
James L Vodanovich <james@jvodan.com>
2016-04-23 12:55:00 +1000

    req

--- lib/ruby/api/public/engines_api/system/status.rb
+++ lib/ruby/api/public/engines_api/system/status.rb
@@ -2,11 +2,11 @@ module SystemStatus
   
  
   def first_run_required?
-    require_relative '../../../system/first_run_wizard.rb'
+    require '/opt/engines/lib/ruby/api/system/first_run_wizard.rb'
     FirstRunWizard.required?
   end
   def system_status
-    require_relative '../../../system/system_status.rb'
+    require '/opt/engines/lib/ruby/api/system/system_status.rb'
     SystemStatus.system_status
   end 
    
