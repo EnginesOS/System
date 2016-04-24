981475a1b9c6fbc79e087a4c9800927c48ad3fc0
James L Vodanovich <james@jvodan.com>
2016-04-25 09:19:23 +1000

    typo

--- lib/ruby/api/public/engines_api/containers/persistent_services.rb
+++ lib/ruby/api/public/engines_api/containers/persistent_services.rb
@@ -2,6 +2,6 @@ module PublicApiContainersPersistentServices
  
     
    def list_persistent_services(engine)
-     @sservice_manager.list_persistent_services(engine)
+     @service_manager.list_persistent_services(engine)
    end
 end
