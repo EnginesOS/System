197a6ca4be19ffc102d84b5eac7df94934e0fe7f
James L Vodanovich <james@jvodan.com>
2016-04-28 09:51:50 +1000

    f

--- lib/ruby/api/public/engines_api/containers/engines/actionators.rb
+++ lib/ruby/api/public/engines_api/containers/engines/actionators.rb
@@ -1,7 +1,7 @@
 module PublicApiEngineActionators
   
   def list_engine_actionators(engine)
-      @system_api.list_engine_actionators(engine)
+      @system_api.load_engine_actionators(engine)
   end
   
    def get_engine_actionator(engine, action)
