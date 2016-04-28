f31cc8409d3c00b5c4f15a1737fe335bd9d30575
James L Vodanovich <james@jvodan.com>
2016-04-28 14:13:10 +1000

    dbg

--- src/server/api/containers/service/service_consumers.rb
+++ src/server/api/containers/service/service_consumers.rb
@@ -1,5 +1,6 @@
 get '/v0/containers/service/:service_name/consumers/:parent_engine' do
   service = get_service(params[:service_name])
+    p '/v0/containers/service/:service_name/consumers/:parent_engine'
     return false if service.is_a?(FalseClass)
     p params
   cparams =  Utils::Params.address_params(params, [:service_name,:parent_engine])
