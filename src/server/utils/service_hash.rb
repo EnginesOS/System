f7b138a4620fadb1a0f0fe932e153beb68440e3a
James L Vodanovich <james@jvodan.com>
2016-04-28 11:51:59 +1000

    f

--- src/server/utils/service_hash.rb
+++ src/server/utils/service_hash.rb
@@ -1,32 +1,31 @@
 module ServiceHash
-def self.service_hash_from_params(params, search)
-splats = params['splat']
- 
- hash = {}
- hash[:publisher_namespace] = params['ns']  
- 
-   unless search   
-     hash[:type_path] = File.dirname(splats[0])       
-     hash[:service_handle] = File.basename(splats[0])
-   else
-      type_path =  splats[0]
-end 
-end
+  def self.service_hash_from_params(params, search)
+    splats = params['splat']
 
-def self.engine_service_hash_from_params(params, search = false)
- hash = self.service_hash_from_params(params, search)
- hash[:parent_engine] = params['engine_name']
- hash[:container_type] = 'container'
- hash
-end
+    hash = {}
+    hash[:publisher_namespace] = params['ns']
 
-def self.service_service_hash_from_params(params, search = false)
-   hash = self.service_hash_from_params(params, search)
-   hash[:parent_engine] = params['service_name'] 
-   hash[:container_type] = 'service'
-    return hash  
-end
+    unless search
+      hash[:type_path] = File.dirname(splats[0])
+      hash[:service_handle] = File.basename(splats[0])
+    else
+      hash[:type_path] =  splats[0]
+    end
+    hash
+  end
 
+  def self.engine_service_hash_from_params(params, search = false)
+    hash = self.service_hash_from_params(params, search)
+    hash[:parent_engine] = params['engine_name']
+    hash[:container_type] = 'container'
+    hash
+  end
 
+  def self.service_service_hash_from_params(params, search = false)
+    hash = self.service_hash_from_params(params, search)
+    hash[:parent_engine] = params['service_name']
+    hash[:container_type] = 'service'
+    return hash
+  end
 
 end
