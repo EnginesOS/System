602f4a64a4c30352be62a904fb6a47bdb9eb73ec
James L Vodanovich <james@jvodan.com>
2016-04-28 17:29:08 +1000

    do use ap to stream file

--- src/server/api/engine_builder/engine_builder.rb
+++ src/server/api/engine_builder/engine_builder.rb
@@ -28,20 +28,30 @@ get '/v0/engine_builder/last_build/params' do
       return log_error(request)
     end
   end
-  out_h = nil
+ 
 get '/v0/engine_builder/follow', provides: 'text/event-stream'  do
+  build_log_file =  File.new(SystemConfig.BuildOutputFile, 'r')
+  
   stream :keep_open do |out|
-    out_h = out
-      settings.connections << out
-    @@engines_api.follow_build(out_h)
-      p out.to_s
-      out.callback { settings.connections.delete(out) }
-    end
-  r = @@engines_api.follow_build(out_h)
-#  r = @@engines_api.follow_build
-  unless r.is_a?(FalseClass)
-    return r.to_json
-  else
-    return log_error(request)
- end
+    begin
+    bytes = build_log_file.read_nonblock
+            rescue IO::WaitReadable
+                   retry
+            rescue EOFError
+           out << bytes
+            return 'OK'
+            build_log_file.close
+          rescue => e
+    out << bytes
+           
+            build_log_file.close
+            return 'Maybe ' + e.to_s
+          
+  out << bytes
+ 
+#
+rescue StandardError => e
+    return log_error(request,e)
+end
+  end
 end
