f9b6c94b5a2fa52feae44f11fa7330c5e4ecd3cb
James L Vodanovich <james@jvodan.com>
2016-04-30 17:26:21 +1000

    skip super

--- lib/ruby/api/system/engines_system/engines_system_error.rb
+++ lib/ruby/api/system/engines_system/engines_system_error.rb
@@ -2,7 +2,8 @@ require '/opt/engines/lib/ruby/system/engines_error.rb'
 class EnginesSystemError < EnginesError
  
   def initialize(message, type = :fail)
-        super
+    @error_mesg = message
+      @error_type = type
         @source = caller[1].to_s 
         @sub_system = 'engines_system'
       end
