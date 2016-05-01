d03b861bf3e6ff790891c6dd2a6cc5abc9ffe42e
James L Vodanovich <james@jvodan.com>
2016-05-01 10:25:57 +1000

    duh super

--- lib/ruby/api/system/engines_system/engines_system_error.rb
+++ lib/ruby/api/system/engines_system/engines_system_error.rb
@@ -2,6 +2,7 @@ require '/opt/engines/lib/ruby/system/engines_error.rb'
 class EnginesSystemError < EnginesError
  
   def initialize(message, type )
+    super
         @sub_system = 'engines_system'
       end
 end
