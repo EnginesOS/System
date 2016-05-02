5697e8cd4906f94a25c61f88181b92c7cf2853d4
James L Vodanovich <james@jvodan.com>
2016-05-02 12:34:17 +1000

    f

--- lib/ruby/service_manager/system_registry/engines_registry_client_error.rb
+++ lib/ruby/service_manager/system_registry/engines_registry_client_error.rb
@@ -2,7 +2,7 @@ require '/opt/engines/lib/ruby/system/engines_error.rb'
 class EnginesRegistryClientError < EnginesError
  
   def initialize(message, type, *objs )
-    super
+    super(message, type)
     @params = *objs
         @sub_system = 'engines_registry_client'
       end
