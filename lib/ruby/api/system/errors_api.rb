7425d9325c0d29a55f30c386291f85c57fac39f2
James L Vodanovich <james@jvodan.com>
2016-05-02 12:50:33 +1000

    default to throw global error unless over written

--- lib/ruby/api/system/errors_api.rb
+++ lib/ruby/api/system/errors_api.rb
@@ -8,21 +8,24 @@ class ErrorsApi
     @last_error = @last_error.to_s  + ':' + msg.to_s + ':' + objects.to_s.slice(0, 256)
     msg.to_s += caller_locations(1,3) if @debug
     SystemUtils.log_error_mesg(msg, objects)
+    return EnginesError.new(msg.to_s,:error)
   end
 
   def clear_error
     @last_error = ''
   end
 
-  def log_exception(*args)
-    e = args[0]
+  def log_exception(e, *args)
+
   SystemUtils.log_exception_to_bugcatcher(e) unless File.exists?(SystemConfig.NoRemoteExceptionLoggingFlagFile)
     @last_error = e.to_s + e.backtrace.to_s
     mesg = @last_error + ':'
     args.each do |arg|
       mesg += arg.to_s + ' '
     end
+   STDERR.puts(e.to_s + e.backtrace.to_s)
 
     SystemUtils.log_error_mesg('EXCEPTION:',mesg)
+    return EnginesError.new(mesg.to_s,:exception)
   end
 end
