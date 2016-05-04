7e19e662f95f33a7961cbd1fa6a4e0be2d439d7f
James L Vodanovich <james@jvodan.com>
2016-05-04 12:57:50 +1000

    dbg

--- tests/src/engines_test_expects.rb
+++ tests/src/engines_test_expects.rb
@@ -59,6 +59,7 @@ def read_stdin_data
  end
   end
  # puts "Read " + stdin_data.length.to_s + ' bytes ' + stdin_data
+  puts "Read " + stdin_data.length.to_s + ' bytes ' + stdin_data
   stdin_data
 rescue Timeout::Error
   puts "Timeout on data read from stdin"  
