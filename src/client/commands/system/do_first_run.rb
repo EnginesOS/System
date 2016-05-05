ae1b334999c73ba2587e3c0c6322a3d08003ab69
James L Vodanovich <james@jvodan.com>
2016-05-05 13:14:10 +1000

    f

--- src/client/commands/system/do_first_run.rb
+++ src/client/commands/system/do_first_run.rb
@@ -3,6 +3,6 @@ begin
   command_useage unless params_data.is_a?(String)
   perform_post(JSON.parse(params_data))
 rescue StandardError => e
-  log_error('Not a Json String',e,e.back_trace)
+  log_error('Not a Json String',e,e.backtrace)
 end
 
