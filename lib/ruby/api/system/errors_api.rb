226a42bffcd0e9e763f91574e79411d9101acd6c
James L Vodanovich <james@jvodan.com>
2016-05-13 09:00:57 +1000

    lets see the errrir

--- lib/ruby/api/system/errors_api.rb
+++ lib/ruby/api/system/errors_api.rb
@@ -23,8 +23,8 @@ class ErrorsApi
     args.each do |arg|
       mesg += arg.to_s + ' '
     end
-  # STDERR.puts(e.to_s + e.backtrace.to_s)
-
+  STDERR.puts(e.to_s + e.backtrace.to_s)
+    
     SystemUtils.log_error_mesg('EXCEPTION:',mesg)
     return EnginesError.new(mesg.to_s,:exception)
   end
