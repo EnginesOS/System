aceb0a2c55a8a5c13279240ef774c6b2dee7f897
James L Vodanovich <james@jvodan.com>
2016-05-14 11:46:23 +1000

    start of new event watcher

--- lib/ruby/api/system/docker/docker_api/docker_event_watcher.rb
+++ lib/ruby/api/system/docker/docker_api/docker_event_watcher.rb
@@ -1,12 +1,30 @@
 class DockerEventWatcher  < ErrorsApi
 
+  class EventListener
+    def initialize(listener, mask)
+     @object =  listener[0]
+     @method = listener[1]
+     @mask = mask
+    end
+    def name
+      return @object.object_id
+    end
+    
+    def trigger(hash)
+      return @object.method_defined?(@method).call(hash)
+    rescue StandardError => e
+     return e
+    end
+  end
 require 'yajl'
 require 'net_x/http_unix'
 require 'socket'
 
  def initialize(system)
- @system_api = system
+ #@system_api = system
  # FIXMe add conntection watcher that re establishes connection asap and continues trying after warngin ....
+   @event_listeners = {}
+   add_event_listener([system, :container_event])
  end
  
  def start
@@ -19,9 +37,14 @@ require 'socket'
    
   client.request(req) { |resp|
     chunk = ''
+    r = ''
    resp.read_body do |chunk|
      hash = parser.parse(chunk) do |hash|
        p hash
+       @event_listeners.values.each do |listener |
+       log_exeception(r) if (r = listener.trigger(hash)).is_a?(StandardError) 
+       end
+       
       @system_api.container_event(hash) # if hash.key?('from')     
     end 
    end
@@ -29,5 +52,12 @@ require 'socket'
    rescue StandardError => e
      log_exception(e,chunk)
 end 
+
+def add_event_listener(listener, event_mask = nil)
+  event = EventListener.new(listener,mask)
+  @event_listeners[event.name] = event 
+end
+
+
 
   end
