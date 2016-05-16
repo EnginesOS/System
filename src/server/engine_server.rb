755a61cdc18ecac588de6d133775f578f32fab94
James L Vodanovich <james@jvodan.com>
2016-05-16 18:02:30 +1000

    toekn

--- src/server/engine_server.rb
+++ src/server/engine_server.rb
@@ -103,40 +103,40 @@ begin
 
 #content_type :json
 require_relative 'api/routes.rb'
+  
+
+  use Warden::Manager do |config|
+      config.scope_defaults :default,
+      # Set your authorization strategy
+      strategies: [:access_token],
+      # Route to redirect to when warden.authenticate! returns a false answer.
+      action: '/unauthenticated'
+      config.failure_app = self
+  end
+  
+  Warden::Manager.before_failure do |env,opts|
+      env['REQUEST_METHOD'] = 'POST'
+  end
+  
+  # Implement your Warden stratagey to validate and authorize the access_token.
+  Warden::Strategies.add(:access_token) do
+      def valid?
+          # Validate that the access token is properly formatted.
+          # Currently only checks that it's actually a string.
+          request.env["HTTP_ACCESS_TOKEN"].is_a?(String)
+      end
+  
+      def authenticate!
+          # Authorize request if HTTP_ACCESS_TOKEN matches 'youhavenoprivacyandnosecrets'
+          # Your actual access token should be generated using one of the several great libraries
+          # for this purpose and stored in a database, this is just to show how Warden should be
+          # set up.
+          access_granted = (request.env["HTTP_ACCESS_TOKEN"] == 'test_token')
+          !access_granted ? fail!("Could not log in") : success!(access_granted)
+      end
   end
 
-#  use Warden::Manager do |config|
-#      config.scope_defaults :default,
-#      # Set your authorization strategy
-#      strategies: [:access_token],
-#      # Route to redirect to when warden.authenticate! returns a false answer.
-#      action: '/unauthenticated'
-#      config.failure_app = self
-#  end
-#  
-#  Warden::Manager.before_failure do |env,opts|
-#      env['REQUEST_METHOD'] = 'POST'
-#  end
-#  
-#  # Implement your Warden stratagey to validate and authorize the access_token.
-#  Warden::Strategies.add(:access_token) do
-#      def valid?
-#          # Validate that the access token is properly formatted.
-#          # Currently only checks that it's actually a string.
-#          request.env["HTTP_ACCESS_TOKEN"].is_a?(String)
-#      end
-#  
-#      def authenticate!
-#          # Authorize request if HTTP_ACCESS_TOKEN matches 'youhavenoprivacyandnosecrets'
-#          # Your actual access token should be generated using one of the several great libraries
-#          # for this purpose and stored in a database, this is just to show how Warden should be
-#          # set up.
-#          access_granted = (request.env["HTTP_ACCESS_TOKEN"] == 'test_token')
-#          !access_granted ? fail!("Could not log in") : success!(access_granted)
-#      end
-#  end
-
-
+  end
 
 rescue StandardError => e
   #log_error(e)
