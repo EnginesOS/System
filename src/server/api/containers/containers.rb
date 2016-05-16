e583562277f9b101428c71b9715d50c2cb17ab6b
James L Vodanovich <james@jvodan.com>
2016-05-16 11:21:39 +1000

    fmt

--- src/server/api/containers/containers.rb
+++ src/server/api/containers/containers.rb
@@ -10,54 +10,48 @@ get '/v0/containers/changed/' do
     return log_error(request, changed)
   end
 end
-  
-  get '/v0/containers/events/', provides: 'text/event-stream' do
 
-    stream :keep_open do |out|
-     
-      @events_stream = engines_api.container_events_stream
-      has_data = true
-     
-      parser = Yajl::Parser.new
-     
-      while has_data == true
-begin
-  require "timeout"
-        
-          timer =  EventMachine::PeriodicTimer.new(20) { out << "\0" }
-          bytes = @events_stream.rd.read_nonblock(2048)   
-          timer.cancel
-         # jason_event = parser.parse(bytes) 
-          begin
+get '/v0/containers/events/', provides: 'text/event-stream' do
+
+  stream :keep_open do |out|
+
+    @events_stream = engines_api.container_events_stream
+    has_data = true
+
+    parser = Yajl::Parser.new
+
+    while has_data == true
+      begin
+        require "timeout"
+
+        timer =  EventMachine::PeriodicTimer.new(20) { out << "\0" }
+        bytes = @events_stream.rd.read_nonblock(2048)
+        timer.cancel
+        # jason_event = parser.parse(bytes)
+        begin
           jason_event = JSON.parse(bytes)
-          rescue  JSON::ParserError => e
-            STDERR.puts('Failed to parse ' + bytes )
-            next 
-          end
-          #out <<'data:'     
-          out << jason_event.to_json
-          out << "\n\n"
-          STDERR.puts('EVENTS ' + jason_event.to_s + ' ' + jason_event.class.name)     
-          bytes = ''
-        
-        
-        rescue IO::WaitReadable
-          sleep 0.21
-          retry
-        rescue EOFError          
-          sleep 0.12
-          retry         
-        rescue IOError
-          has_data = false
-  @events_stream.stop unless @events_stream.nil?
+        rescue  JSON::ParserError => e
+          STDERR.puts('Failed to parse ' + bytes )
+          next
         end
-     
+        #out <<'data:'
+        out << jason_event.to_json
+        out << "\n\n"
+        STDERR.puts('EVENTS ' + jason_event.to_s + ' ' + jason_event.class.name)
+        bytes = ''
+      rescue IO::WaitReadable
+        sleep 0.21
+        retry
+      rescue EOFError
+        sleep 0.12
+        retry
+      rescue IOError
+        has_data = false
+        @events_stream.stop unless @events_stream.nil?
       end
-      
-      timer.cancel
+    end
+    timer.cancel
     @events_stream.stop unless @events_stream.nil?
-      end
   end
-  
-
+end
 
