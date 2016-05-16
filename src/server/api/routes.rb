1f96701ec73602b8f4c0ea75736abb83898cec68
James L Vodanovich <james@jvodan.com>
2016-05-16 16:44:59 +1000

    rm

--- src/server/api/routes.rb
+++ src/server/api/routes.rb
@@ -1,4 +1,4 @@
-require_relative 'auth/routes.rb'
+#require_relative 'auth/routes.rb'
 require_relative 'containers/routes.rb'
 require_relative 'registry/registry.rb'
 require_relative 'service_manager/routes.rb'
