3adead9673d1080381ada9185a5c7bf86c0d3218
James L Vodanovich <james@jvodan.com>
2016-05-16 10:26:48 +1000

    system service control

---
+++ bin/system_service.rb
@@ -0,0 +1,22 @@
+#!/home/engines/.rbenv/shims/ruby
+
+require '/opt/engines/lib/ruby/api/system/engines_core/engines_core.rb'
+
+
+core_api = EnginesCore.new  
+
+system_api = core_api.system_api
+
+service = system_api.loadSystemService(ARGV[0])
+
+case ARGV[1]
+when 'start'
+  p service.start_container
+when 'create'
+  p service.create_container
+when 'stop'
+  p service.stop_container
+when 'destroy'
+  p service.destroy_container
+    
+end
