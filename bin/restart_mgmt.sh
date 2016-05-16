0ae330bef42f978e2d73f50bd9955886bd011f22
James L Vodanovich <james@jvodan.com>
2016-05-16 15:50:40 +1000

    restart system as mgmt

--- bin/restart_mgmt.sh
+++ bin/restart_mgmt.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
  touch /opt/engines/run/system/flags/engines_restarting
- docker stop mgmt
- docker start mgmt
+ #docker stop mgmt
+ #docker start mgmt
+ docker stop system
+ docker start system
  res=$?
 rm	/opt/engines/run/system/flags/engines_restarting
 exit $res
