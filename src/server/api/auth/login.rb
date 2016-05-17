526854d26ee00f2d11dbae9025b96e45f1ce51a5
James L Vodanovich <james@jvodan.com>
2016-05-17 10:12:48 +1000

    dbg

--- src/server/api/auth/login.rb
+++ src/server/api/auth/login.rb
@@ -1,7 +1,9 @@
 get '/v0/login/:user_name/:password' do 
   #access_token = 'test_token'
   $token = 'test_token'
-  $token.to_json
+ p $token
+  p $token.to_json.to_s
+  return $token.to_json
 end
 
 get '/v0/logout' do
