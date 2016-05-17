5d4e36edd7070c99b54f95a4d8396a96fee31dc0
James L Vodanovich <james@jvodan.com>
2016-05-17 11:24:57 +1000

    '~

--- src/client/main.rb
+++ src/client/main.rb
@@ -302,14 +302,14 @@ end
 def login
   r = rest_get('/v0/system/login/test/test')
   ENV['access_token'] = r.body.gsub!(/\"/,'')
-  t = File.new('~/.engines_token','w+')
+  t = File.new(Dir.home + '/.engines_token','w+')
   t.write(ENV['access_token'])
   t.close  
 end
 
 def load_token
-  return false unless File.exist?('~/.engines_token')
-  ENV['access_token'].File.read('~/.engines_token')
+  return false unless File.exist?(Dir.home + '/.engines_token')
+  ENV['access_token'].File.read(Dir.home + '/.engines_token')
   ENV['access_token'].strip
 end
 
