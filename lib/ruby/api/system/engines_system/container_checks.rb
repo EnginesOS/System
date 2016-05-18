c5d40a747562c47b8e559301f21c18cec98d969b
James L Vodanovich <james@jvodan.com>
2016-05-18 11:18:06 +1000

    f

--- lib/ruby/api/system/engines_system/container_checks.rb
+++ lib/ruby/api/system/engines_system/container_checks.rb
@@ -12,8 +12,8 @@ module ContainerChecks
   def check_and_act(containers_status, ctype)
     result = {}
     containers_status.keys.each do |container_name|
-      STDERR.puts('CHECK  ' + container_name.to_s + ':' +containers_status[container_name].to_s )
-      if containers_status[container_name]['error'] == true
+     # STDERR.puts('CHECK  ' + container_name.to_s + ':' +containers_status[container_name].to_s )
+      if containers_status[container_name][:error] == true
       result[container_name] = act_on(result[container_name], ctype)
     else
         result[container_name] = 'ok'
