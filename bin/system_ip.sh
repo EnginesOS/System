b80dee9678e53c50cbd67d5d9c5badd97c5b3a1b
James L Vodanovich <james@jvodan.com>
2016-05-20 15:43:57 +1000

    ip address for system

---
+++ bin/system_ip.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if test $1 = '-i'
+	then
+		docker inspect system |grep  \"IPAddress\": | head -1 | awk '{print $2}' |sed "/\,/s///" |sed "/\"/s///g"
+	elif  test $1 = '-d'
+		then
+		 ifconfig  docker0  |grep "inet addr" |cut -f2 -d: |cut -f1 -d" "	
+	fi
