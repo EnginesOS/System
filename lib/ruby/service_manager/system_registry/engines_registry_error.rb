5f56b8a9a6206987de6d159741cdf6023c58b5b4
James L Vodanovich <james@jvodan.com>
2016-05-26 08:59:56 +1000

    no " around to_json

--- lib/ruby/service_manager/system_registry/engines_registry_error.rb
+++ lib/ruby/service_manager/system_registry/engines_registry_error.rb
@@ -20,7 +20,8 @@ class EnginesRegistryError < EnginesError
       
   def to_json(opt)
   #FixMe this is a kludge
-      '{"error_type":"' + @error_type.to_s + '","error_mesg":"' + @error_mesg.to_s + '","sub_system":"' + @sub_system.to_s + '","source":' + @source.to_s + ',"params":"' + @params.to_json + '"}'
+    STDERR.puts(      '{"error_type":"' + @error_type.to_s + '","error_mesg":"' + @error_mesg.to_s + '","sub_system":"' + @sub_system.to_s + '","source":' + @source.to_s + ',"params":' + @params.to_json + '}')
+      '{"error_type":"' + @error_type.to_s + '","error_mesg":"' + @error_mesg.to_s + '","sub_system":"' + @sub_system.to_s + '","source":' + @source.to_s + ',"params":' + @params.to_json + '}'
   end
   
  
