51b0beb60bd2fa5d3fa7acfcdfc2b519066c74d9
James L Vodanovich <james@jvodan.com>
2016-05-26 08:22:55 +1000

    orphans

--- src/client/commands/service_manager/commands.rb
+++ src/client/commands/service_manager/commands.rb
@@ -2,4 +2,6 @@
 case ARGV[1]
 when 'service_definitions'
   require_relative 'service_definitions.rb'
+when 'orphans'
+require_relative 'orphan_services.rb'
 end
--- src/server/api/service_manager/orphans.rb
+++ src/server/api/service_manager/orphans.rb
@@ -1,5 +1,4 @@
-#/service_manager/orphan_services
-#/service_manager/orphan_service/
+# @!group /service_manager/service_definitions/
 
 get '/v0/service_manager/orphan_services' do
   orphans = engines_api.get_orphaned_services_tree
@@ -10,8 +9,9 @@ else
 end
 end
 
-get '/v0/service_manager/orphan_services/:ns/:type_path' do
-  r = engines_api.retrieve_service_hash(service_hash)
+get '/v0/service_manager/orphan_services/:publisher_namespace/:type_path' do
+  cparams =  Utils::Params.assemble_params(params, [:publisher_namespace, :type_path], []) 
+  r = engines_api.retrieve_orphans(cparams)
 unless r.is_a?(EnginesError)
   return r.to_json
 else
@@ -19,8 +19,10 @@ else
 end
 end
 
-delete '/v0/service_manager/orphan_services/:ns/:type_path/:service_handle' do
-  service_hash = service_hash_from_params(params)
+delete '/v0/service_manager/orphan_services/:publisher_namespace/:type_path/:service_handle' do
+  cparams =  Utils::Params.assemble_params(params, [:publisher_namespace, :type_path, :service_handle], []) 
+  service_hash = engines_api.retrieve_orphan(cparams)
+  return log_error(request, service_hash) if service_hash.is_a?(EnginesError)
   r = engines_api.remove_orphaned_service(service_hash)
  
 unless r.is_a?(EnginesError)
@@ -29,3 +31,4 @@ else
   return log_error(request, r)
 end
 end
+# @!endgroup
