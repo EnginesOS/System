d64cf9f2e25916b05eba60d1da3e2ce0a6568db0
James L Vodanovich <james@jvodan.com>
2016-05-26 11:46:01 +1000

    5 args to address orphan

--- src/client/commands/service_manager/orphan_service.rb
+++ src/client/commands/service_manager/orphan_service.rb
@@ -1,9 +1,9 @@
 @route += '/orphan_service/'
 p @route
-if ARGV.count == 4
-  @route +=   ARGV[2] +  '/' + ARGV[3]
-elsif ARGV.count == 5
-@route +=   ARGV[2] +  '/' + ARGV[3]+  '/' + ARGV[4]
+if ARGV.count == 5
+  @route +=   ARGV[2] + '/' + ARGV[3] + '/' + ARGV[4]
+elsif ARGV.count == 6
+@route +=   ARGV[2] + '/' + ARGV[3] + '/' + ARGV[4] + '/' + ARGV[6]
 end 
 
 perform_get
