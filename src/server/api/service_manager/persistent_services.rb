f7c53a138afb857c2ffed02f1b3e986718ae3bdc
James L Vodanovich <james@jvodan.com>
2016-05-26 13:34:27 +1000

    docs

--- src/server/api/service_manager/persistent_services.rb
+++ src/server/api/service_manager/persistent_services.rb
@@ -1,32 +1,34 @@
 # @!group /service_manager/persistent_services/
 
-get '/v0/service_manager/persistent_service/:container_type/:publisher_namespace/*' do
- 
-  splats = params['splat']
-  pparams =  {}
-  pparams[:publisher_namespace] = params[:publisher_namespace]
-  pparams[:type_path] = File.dirname(splats[0])
-  pparams[:service_handle] = File.basename(pparams[:type_path])
-  pparams[:type_path] = File.dirname(pparams[:type_path])
-  pparams[:parent_engine] = File.basename(splats[0])
-pparams[:container_type]  = params[:container_type]
-  cparams =  Utils::Params.assemble_params(pparams, [:container_type, :publisher_namespace, :type_path, :service_handle, :parent_engine], [])
-  r = engines_api.retrieve_service_hash(cparams)
-
-  unless r.is_a?(EnginesError)
-    return r.to_json
-  else
-    return log_error(request, r)
-  end
-end
-
+#get '/v0/service_manager/persistent_service/:container_type/:publisher_namespace/*' do
+# 
+#  splats = params['splat']
+#  pparams =  {}
+#  pparams[:publisher_namespace] = params[:publisher_namespace]
+#  pparams[:type_path] = File.dirname(splats[0])
+#  pparams[:service_handle] = File.basename(pparams[:type_path])
+#  pparams[:type_path] = File.dirname(pparams[:type_path])
+#  pparams[:parent_engine] = File.basename(splats[0])
+#pparams[:container_type]  = params[:container_type]
+#  cparams =  Utils::Params.assemble_params(pparams, [:container_type, :publisher_namespace, :type_path, :service_handle, :parent_engine], [])
+#  r = engines_api.retrieve_service_hash(cparams)
+#
+#  unless r.is_a?(EnginesError)
+#    return r.to_json
+#  else
+#    return log_error(request, r)
+#  end
+#end
+# @method get_services_for_type
+# @overload get '/v0/service_manager/persistent_services/:publisher_namespace/:type_path'
+# Return array of services attached to the service :publisher_namespace/:type_path
+# @return Json Array|EnginesError.to_json
 get '/v0/service_manager/persistent_services/:publisher_namespace/*' do
  
   splats = params['splat']
   pparams =  {}
   pparams[:publisher_namespace] = params[:publisher_namespace]
-  pparams[:type_path] = splats[0]
-  
+  pparams[:type_path] = splats[0]  
 
   cparams =  Utils::Params.assemble_params(pparams, [:publisher_namespace, :type_path], [])
   r = engines_api.get_registered_against_service(cparams)
