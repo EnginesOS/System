fa6b120a9ce25b64af93a41828074dd3133fd2fc
James L Vodanovich <james@jvodan.com>
2016-05-26 12:35:02 +1000

    doc

--- src/server/api/service_manager/orphans.rb
+++ src/server/api/service_manager/orphans.rb
@@ -1,31 +1,34 @@
-# @!group /service_manager/service_definitions/
-
+# @!group /service_manager/orphan_services/
+# @method get_orphan_services
+# @overload get '/v0/service_manager/orphan_services/'
+# @return Json Array|EnginesError.to_json
 get '/v0/service_manager/orphan_services/' do
-  orphans = engines_api.get_orphaned_services_tree
-unless orphans.is_a?(EnginesError)
-  return orphans.to_json
-else
-  return log_error(request, orphans)
+  orphans = engines_api.get_orphaned_services
+  unless orphans.is_a?(EnginesError)
+    return orphans.to_json
+  else
+    return log_error(request, orphans)
+  end
 end
-end
-
+# @method get_orphan_services
+# @overload get '/v0/service_manager/orphan_services/:publisher_namespace/:type_path:'
+# @return Json Array|EnginesError.to_json
 get '/v0/service_manager/orphan_services/:publisher_namespace/*' do
-
   params[:type_path] = params['splat'][0] if params.key?('splat') && params['splat'].is_a?(Array)
-
   cparams =  Utils::Params.assemble_params(params, [:publisher_namespace, :type_path], [])
-
   r = engines_api.get_orphaned_services(cparams)
-unless r.is_a?(EnginesError)
-  return r.to_json
-else
-  return log_error(request, r)
+  unless r.is_a?(EnginesError)
+    return r.to_json
+  else
+    return log_error(request, r)
+  end
 end
-end
-
+# @method get_orphan_service
+# @overload get '/v0/service_manager/orphan_service/:publisher_namespace/:type_path:/:parent_engine/:service_handle'
+# @return Json|EnginesError.to_json
 get '/v0/service_manager/orphan_service/:publisher_namespace/*' do
-      p params
-      splats = params['splat']
+ 
+  splats = params['splat']
   pparams =  {}
   pparams[:publisher_namespace] = params[:publisher_namespace]
   pparams[:type_path] = File.dirname(splats[0])
@@ -33,27 +36,35 @@ get '/v0/service_manager/orphan_service/:publisher_namespace/*' do
   pparams[:type_path] = File.dirname(pparams[:type_path])
   pparams[:parent_engine] = File.basename(splats[0])
 
-cparams =  Utils::Params.assemble_params(pparams, [:publisher_namespace, :type_path, :service_handle, :parent_engine], []) 
-r = engines_api.retrieve_orphan(cparams)
+  cparams =  Utils::Params.assemble_params(pparams, [:publisher_namespace, :type_path, :service_handle, :parent_engine], [])
+  r = engines_api.retrieve_orphan(cparams)
 
-unless r.is_a?(EnginesError)
-return r.to_json
-else
-return log_error(request, r)
+  unless r.is_a?(EnginesError)
+    return r.to_json
+  else
+    return log_error(request, r)
+  end
 end
-end
-
+# @method delete_orphan_service
+# @overload delete '/v0/service_manager/orphan_service/:publisher_namespace/:type_path:/:parent_engine/:service_handle'
+# @return true|EnginesError.to_json
 delete '/v0/service_manager/orphan_service/:publisher_namespace/*' do
-  pparams = Utils::Params.service_hash_from_params(params, false)
-  cparams =  Utils::Params.assemble_params(pparams, [:publisher_namespace, :type_path, :service_handle], []) 
+  splats = params['splat']
+   pparams =  {}
+   pparams[:publisher_namespace] = params[:publisher_namespace]
+   pparams[:type_path] = File.dirname(splats[0])
+   pparams[:service_handle] = File.basename(pparams[:type_path])
+   pparams[:type_path] = File.dirname(pparams[:type_path])
+   pparams[:parent_engine] = File.basename(splats[0])
+   cparams =  Utils::Params.assemble_params(pparams, [:publisher_namespace, :type_path, :service_handle, :parent_engine], [])
   service_hash = engines_api.retrieve_orphan(cparams)
   return service_hash if service_hash.is_a?(EnginesError)
   r = engines_api.remove_orphaned_service(service_hash)
- 
-unless r.is_a?(EnginesError)
-  return r.to_json
-else
-  return log_error(request, r)
-end
+
+  unless r.is_a?(EnginesError)
+    return r.to_json
+  else
+    return log_error(request, r)
+  end
 end
 # @!endgroup
--- src/server/api/service_manager/service_definition.rb
+++ src/server/api/service_manager/service_definition.rb
@@ -1,10 +1,9 @@
 # @!group /service_manager/service_definitions/
 # @method get_service_definition
-# @overload get '/v0/service_manager/service_definitions/:provider/:type_path'
+# @overload get '/v0/service_manager/service_definitions/:publisher_namespace/:type_path'
 # return Json Hash for service definition addressed by
 #  :publisher_namespace :type_path
 # @return json|EnginesError.to_json
-
 get '/v0/service_manager/service_definitions/:publisher_namespace/:type_path' do
   cparams =  Utils::Params.assemble_params(params, [:publisher_namespace, :type_path], []) 
     p cparams 
