f9b5e1976768377cb91c059c95b3b73db5a36f85
James L Vodanovich <james@jvodan.com>
2016-05-26 12:12:29 +1000

    dbg

--- src/client/commands/service_manager/orphan_service.rb
+++ src/client/commands/service_manager/orphan_service.rb
@@ -1,9 +1,9 @@
 @route += '/orphan_service/'
-p @route
+
 if ARGV.count == 5
   @route +=   ARGV[2] + '/' + ARGV[3] + '/' + ARGV[4]
 elsif ARGV.count == 6
 @route +=   ARGV[2] + '/' + ARGV[3] + '/' + ARGV[4] + '/' + ARGV[5]
 end 
-
+p @route
 perform_get
