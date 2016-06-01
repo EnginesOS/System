6d59721ff1081a975bebedd6c38979dabea76245
James L Vodanovich <james@jvodan.com>
2016-06-01 13:57:51 +1000

    no tree from api

--- lib/ruby/api/public/engines_api/registry/registry.rb
+++ lib/ruby/api/public/engines_api/registry/registry.rb
@@ -1,21 +1,21 @@
 module PublicApiRegistry
   def get_orphaned_services_tree
-    @service_manager.get_orphaned_services_tree
+    @service_manager.get_orphaned_services_tree.to_h
   end
 
   def managed_service_tree
-    @service_manager.managed_service_tree
+    @service_manager.managed_service_tree.to_h
   end
 
   def get_managed_engine_tree
-   @service_manager.get_managed_engine_tree
+   @service_manager.get_managed_engine_tree.to_h
   end
 
   def get_configurations_tree
-    @service_manager.service_configurations_tree
+    @service_manager.service_configurations_tree.to_h
   end
 
   def get_shares_tree
-   @service_manager.shares_tree
+   @service_manager.shares_tree.to_h
   end
 end
