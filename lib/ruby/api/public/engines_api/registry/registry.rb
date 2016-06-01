74c5b0b5351a0bf1449ba757e70bc55ee4dca94b
James L Vodanovich <james@jvodan.com>
2016-06-01 14:20:58 +1000

    whips

--- lib/ruby/api/public/engines_api/registry/registry.rb
+++ lib/ruby/api/public/engines_api/registry/registry.rb
@@ -21,9 +21,10 @@ module PublicApiRegistry
   
   def as_hash(tree)
     h = { }
-     h[:name] = tree[:name]
-     h[:content] = tree[:content]
-     h[:children] =    []
+
+     h[:name] = tree.name
+     h[:content] = tree.content
+     h[:children] =  []
     tree.children do |child|
       h[:children].push(as_hash(child))
     end
