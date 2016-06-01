b1c7330d9448b3cd04008d34d26b86b251a99248
James L Vodanovich <james@jvodan.com>
2016-06-01 14:17:21 +1000

    treenode has

--- lib/ruby/api/public/engines_api/registry/registry.rb
+++ lib/ruby/api/public/engines_api/registry/registry.rb
@@ -1,21 +1,32 @@
 module PublicApiRegistry
   def get_orphaned_services_tree
-    @service_manager.get_orphaned_services_tree.to_h
+    as_hash(@service_manager.get_orphaned_services_tree)
   end
 
   def managed_service_tree
-    @service_manager.managed_service_tree.to_h
+    as_hash(@service_manager.managed_service_tree)
   end
 
   def get_managed_engine_tree
-   @service_manager.get_managed_engine_tree.to_h
+    as_hash(@service_manager.get_managed_engine_tree)
   end
 
   def get_configurations_tree
-    @service_manager.service_configurations_tree.to_h
+    as_hash( @service_manager.service_configurations_tree)
   end
 
   def get_shares_tree
-   @service_manager.shares_tree.to_h
+    as_hash(@service_manager.shares_tree)
   end
+  
+  def as_hash(tree)
+    h = { }
+     h[:name] = tree[:name]
+     h[:content] = tree[:content]
+     h[:children] =    []
+    tree.children do |child|
+      h[:children].push(as_hash(child))
+    end
+    h
+    end
 end
