788c7140fd078e1ce456e0b38e8b4b41bdeb86bf
James L Vodanovich <james@jvodan.com>
2016-06-01 10:12:54 +1000

    fix build error with non symbolised service hashes and write tests

--- lib/ruby/engine_builder/service_builder/persistent_service_builder.rb
+++ lib/ruby/engine_builder/service_builder/persistent_service_builder.rb
@@ -1,6 +1,7 @@
 module PersistantServiceBuilder
   def create_persistent_services(services, environ, use_existing)
-    services.each do |service_hash|
+    services.each do | sh |
+      service_hash =  SystemUtils.deal_with_jason(sh)
       service_def = SoftwareServiceDefinition.find(service_hash[:type_path], service_hash[:publisher_namespace])
       return log_error_mesg('no matching service definition',self) if service_def.nil?
       if service_def[:persistent]
