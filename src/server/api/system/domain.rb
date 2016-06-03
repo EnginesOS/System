acdbdf37dc47053bedeeb239d129f362c9186e68
James L Vodanovich <james@jvodan.com>
2016-06-03 16:18:29 +1000

    typo

--- src/server/api/system/domain.rb
+++ src/server/api/system/domain.rb
@@ -24,7 +24,7 @@ post '/v0/system/domains/:domain_name' do
  post_s = post_params(request)
  
  STDERR.puts(' post ' + post_s.to_s + ' params ' + params.to_s)
-  params.merge(post)
+  params.merge(post_s)
   p params
   cparams =  Utils::Params.assemble_params(params, [:domain_name], :all)
   r = engines_api.update_domain(cparams)
