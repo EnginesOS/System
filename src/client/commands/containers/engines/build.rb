5e75d522c6ecf2423c6b748c4f02e7e92916ab3f
James L Vodanovich <james@jvodan.com>
2016-06-03 10:42:54 +1000

    create additions

--- src/client/commands/containers/engines/build.rb
+++ src/client/commands/containers/engines/build.rb
@@ -1,3 +1,3 @@
 params_data = read_stdin_data
 command_useage unless params_data.is_a?(String)
-perform_post(JSON.parse(params_data))
+perform_post(JSON.parse(params_data, :create_additons => true ))
