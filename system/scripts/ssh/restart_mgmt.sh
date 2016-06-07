ba5d426ea0bbf9d73fafe450694883e6d5d828b5
James L Vodanovich <james@jvodan.com>
2016-06-08 08:47:59 +1000

    new way

--- system/scripts/ssh/restart_mgmt.sh
+++ system/scripts/ssh/restart_mgmt.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
  touch /opt/engines/run/system/flags/engines_restarting
 
- docker stop system
- docker start system
+ /opt/engines/bin/system_service.rb system stop
+ /opt/engines/bin/system_service.rb system destroy
+ sleep 60 #kludge need wait for
+  /opt/engines/bin/system_service.rb create
+
  res=$?
 rm	/opt/engines/run/system/flags/engines_restarting
 exit $res
