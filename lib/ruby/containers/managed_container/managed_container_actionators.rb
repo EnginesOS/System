3f3790534f08636c8a020a7f31cfebebe73b20c6
James L Vodanovich <james@jvodan.com>
2016-06-09 13:02:15 +1000

    add data para

--- lib/ruby/containers/managed_container/managed_container_actionators.rb
+++ lib/ruby/containers/managed_container/managed_container_actionators.rb
@@ -1,5 +1,5 @@
 module ManagedContainerActionators
- def perform_action(actionator_name,params)
-  @container_api.perform_action(self,actionator_name,params, data=nil)
+ def perform_action(actionator_name,params, data=nil)
+  @container_api.perform_action(self,actionator_name,params, data)
  end
 end
