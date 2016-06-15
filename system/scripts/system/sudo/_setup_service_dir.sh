b7a019ea7c374204fd255ef152421db27bd2bd27
James L Vodanovich <james@jvodan.com>
2016-06-15 15:12:40 +1000

    dir perms

--- system/scripts/system/sudo/_setup_service_dir.sh
+++ system/scripts/system/sudo/_setup_service_dir.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
  /opt/engines/scripts/_setup_service_key_dir.sh $1
- mkdir -p /opt/engines/run/services/$1/run
-chgrp containers  /opt/engines/run/services/$1/run
-chmod g+w  /opt/engines/run/services/$1/run
+ mkdir -p /opt/engines/run/services/$1/run/flags
+chgrp containers -R /opt/engines/run/services/$1/run
+chmod g+w -R  /opt/engines/run/services/$1/run
 
 uid=`/opt/engines/system/scripts/system/get_service_uid.sh $1`
 
