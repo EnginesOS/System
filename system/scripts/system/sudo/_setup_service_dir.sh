ac9f23da30bcc39ff8893fd8222de90b3940286f
James L Vodanovich <james@jvodan.com>
2016-06-15 15:15:34 +1000

    ch

--- system/scripts/system/sudo/_setup_service_dir.sh
+++ system/scripts/system/sudo/_setup_service_dir.sh
@@ -5,9 +5,13 @@
 chgrp containers -R /opt/engines/run/services/$1/run
 chmod g+w -R  /opt/engines/run/services/$1/run
 
+ mkdir -p /opt/engines/run/services/$1/run/f
+ 
+
 uid=`/opt/engines/system/scripts/system/get_service_uid.sh $1`
 
 mkdir -p /var/log/engines/services/$1
 chown -R $uid /var/log/engines/services/$1
+chown $uid /opt/engines/run/services/$1/run/f
 #no error
 exit
