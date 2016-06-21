6263776dfcebc922e9fc359e960d27836ba5c690
James L Vodanovich <james@jvodan.com>
2016-06-22 09:35:54 +1000

    rotate logs scripts

--- system/scripts/ssh/rotate_container_log.sh
+++ system/scripts/ssh/rotate_container_log.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-
-mv /var/lib/docker/containers/$1/$1-json.l /var/log/engines/raw/$1-json.last
+sudo  -n /opt/engines/system/scripts/ssh/sudo/_rotate_container_log.sh $1
+#mv /var/lib/docker/containers/$1/$1-json.l /var/log/engines/raw/$1-json.last
--- system/scripts/ssh/sudo/_rotate_container_log.sh
+++ system/scripts/ssh/sudo/_rotate_container_log.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-
- /opt/engines/scripts/_setup_service_key_dir.sh $1
- mkdir -p /opt/engines/run/services/$1/run
-chgrp containers  /opt/engines/run/services/$1/run
-chmod g+w  /opt/engines/run/services/$1/run
-
-uid=`/opt/engines/system/scripts/system/get_service_uid.sh $1`
-
-mkdir -p /var/log/engines/services/$1
-chown -R $uid /var/log/engines/services/$1
-#no error
-exit
+mv /var/lib/docker/containers/$1/$1-json.l /var/log/engines/raw/$1-json.last
+# /opt/engines/scripts/_setup_service_key_dir.sh $1
+# mkdir -p /opt/engines/run/services/$1/run
+#chgrp containers  /opt/engines/run/services/$1/run
+#chmod g+w  /opt/engines/run/services/$1/run
+#
+#uid=`/opt/engines/system/scripts/system/get_service_uid.sh $1`
+#
+#mkdir -p /var/log/engines/services/$1
+#chown -R $uid /var/log/engines/services/$1
+##no error
+#exit
