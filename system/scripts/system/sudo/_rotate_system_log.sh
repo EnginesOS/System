61bab98cb43c68010616c18525a1ce12fdf285ea
James L Vodanovich <james@jvodan.com>
2016-06-24 05:15:25 +1000

    dbg

--- system/scripts/system/sudo/_rotate_system_log.sh
+++ system/scripts/system/sudo/_rotate_system_log.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-system_cont_id=` docker inspect --format='{{.Id}}' system`
+system_cont_id=`docker inspect --format='{{.Id}}' system`
+echo system container id $system_cont_id >>/tmp/clean.log
 #`grep container_id /opt/engines/run/system_services/system/running.yaml |cut -f2 -d:`
 #grep container_id /opt/engines/run/system_services/system/running.yaml |cut -f2 -d:
 src=/var/lib/docker/containers/$system_cont_id/${system_cont_id}-json.log 
 dest=/var/log/engines/raw/${system_cont_id}-json.last
+
 if test -f $dest
  then
  	rm $dest
