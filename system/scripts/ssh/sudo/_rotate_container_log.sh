e916da891c423d361365504a7421f64843e3ff8a
James L Vodanovich <james@jvodan.com>
2016-06-23 11:24:56 +1000

    dbg

--- system/scripts/ssh/sudo/_rotate_container_log.sh
+++ system/scripts/ssh/sudo/_rotate_container_log.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo mv /var/lib/docker/containers/$1/$1-json.log /var/log/engines/raw/$1-json.last  &>>/tmp/clean.log
 mv /var/lib/docker/containers/$1/$1-json.log /var/log/engines/raw/$1-json.last  &>>/tmp/clean.log
 
 #exit
