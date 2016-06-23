3b9d4dfa0479782618ee570f584f39a7d6ed30ce
James L Vodanovich <james@jvodan.com>
2016-06-23 11:52:22 +1000

    debug

--- system/scripts/ssh/rotate_container_log.sh
+++ system/scripts/ssh/rotate_container_log.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
+echo sudo  -n /opt/engines/system/scripts/ssh/sudo/_rotate_container_log.sh $1  &>>/tmp/clean.log
 sudo  -n /opt/engines/system/scripts/ssh/sudo/_rotate_container_log.sh $1
