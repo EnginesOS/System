ec0d1169bd40a5490c4c0b90f5906dfb180f514a
James L Vodanovich <james@jvodan.com>
2016-06-24 15:42:33 +1000

    self::DockerApiCreateOptions.

--- lib/ruby/api/system/docker/docker_api/docker_api_create_options.rb
+++ lib/ruby/api/system/docker/docker_api/docker_api_create_options.rb
@@ -184,7 +184,7 @@ module DockerApiCreateOptions
 
   def logdir_mount(container)
     logdir_mount_hash = {}
-    logdir_mount_hash['Source'] = container_log_dir(container)
+    logdir_mount_hash['Source'] = self::DockerApiCreateOptions.container_log_dir(container)
     logdir_mount_hash['Destination'] = incontainer_logdir
     logdir_mount_hash['Mode'] = 'rw,Z'
     logdir_mount_hash['RW'] = true
@@ -193,16 +193,16 @@ module DockerApiCreateOptions
 
   def state_mount(container)
     state_mount_hash = {}
-    state_mount_hash['Source'] = ContainerStateFiles.container_state_dir(container) + '/run'
+    state_mount_hash['Source'] = self::DockerApiCreateOptions.container_state_dir(container) + '/run'
     state_mount_hash['Destination'] = '/engines/var/run'
     state_mount_hash['Mode'] = 'rw,Z'
     state_mount_hash['RW'] = true
     state_mount_hash
   end
 
-#  def self.container_state_dir(container)
-#    ContainerStateFiles.container_state_dir(container)
-#  end
+  def self.container_state_dir(container)
+    ContainerStateFiles.container_state_dir(container)
+  end
 
   def self.container_log_dir(container)
     SystemConfig.SystemLogRoot + '/' + container.ctype + 's/' + container.container_name
