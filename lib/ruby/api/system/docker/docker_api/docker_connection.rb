0e5c8b06216f18465ebde0fffa554d95428dbb27
James L Vodanovich <james@jvodan.com>
2016-06-24 13:12:14 +1000

    tail tail x lines no follow yet

--- lib/ruby/api/system/docker/docker_api/docker_connection.rb
+++ lib/ruby/api/system/docker/docker_api/docker_connection.rb
@@ -373,7 +373,7 @@ class DockerConnection < ErrorsApi
  
   def logs_container(container, count)
 #    GET /containers/4fa6e0f0c678/logs?stderr=1&stdout=1&timestamps=1&follow=1&tail=10&since=1428990821 HTTP/1.1
-    request = '/containers/' + container.container_id + '/logs?stderr=1&stdout=1&timestamps=1&follow=1&tail=' + count.to_s
+    request = '/containers/' + container.container_id + '/logs?stderr=1&stdout=1&timestamps=0&follow=1&tail=' + count.to_s
     make_request(request, nil,false) 
   end
   def image_exist_by_name?(image_name)
