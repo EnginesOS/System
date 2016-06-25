900bd86210ee75eac06fb948e20c5699f15e2828
James L Vodanovich <james@jvodan.com>
2016-06-25 13:42:20 +1000

    f

--- lib/ruby/api/system/docker/docker_api/docker_connection.rb
+++ lib/ruby/api/system/docker/docker_api/docker_connection.rb
@@ -37,7 +37,7 @@ class DockerConnection < ErrorsApi
              nil      
       else
       @mutex.synchronize {
-        size = -1 if size >= @body.length
+      size =  @body.length -1  if size >= @body.length
         
         b = @body.slice!(0,size)
         STDERR.puts(' write b ' + b.to_s + ' of ' + size.to_s + ' bytes  remaining str ' + @body.to_s )
