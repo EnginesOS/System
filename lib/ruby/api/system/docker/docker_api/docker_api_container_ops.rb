3d03f0e2bf7c4c45e12c7f87d2e10cffce3585f8
James L Vodanovich <james@jvodan.com>
2016-06-26 13:42:32 +1000

    f

--- lib/ruby/api/system/docker/docker_api/docker_api_container_ops.rb
+++ lib/ruby/api/system/docker/docker_api/docker_api_container_ops.rb
@@ -32,7 +32,7 @@ module DockerApiContainerOps
 
   def create_container(container)
     request_params = create_options(container)
-    request = '/containers/create?name=?' + container.container_name
+    request = '/containers/create?name=' + container.container_name
     r = make_post_request(request, container, request_params)
     STDERR.puts('DOCKER EXEC ' + r.to_s + ': for :' + container.container_name + ': with :' + request_params.to_s)
 
