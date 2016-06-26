ae4af3b5668dbd996854b506c3291cc7485c9489
James L Vodanovich <james@jvodan.com>
2016-06-27 08:48:06 +1000

    new get ip

--- bin/system_ip.sh
+++ bin/system_ip.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-if test $# -eq 0
-then
-a="-i"
-else
-a=$1
-fi
-
-if test $a = '-i'
-	then
-		docker inspect system |grep  \"IPAddress\": | head -1 | awk '{print $2}' |sed "/\,/s///" |sed "/\"/s///g"
-	elif  test $a = '-d'
-		then
-		 ifconfig  docker0  |grep "inet addr" |cut -f2 -d: |cut -f1 -d" "	
-	fi
+interface=`netstat -nr | grep ^0.0.0.0 | awk '{print $8}'`
+ifconfig  $interface  |grep "inet addr" |cut -f2 -d: |cut -f1 -d" "	
