7287071c52e3a1e4eb15e4a8660d1f1586f00d80
James L Vodanovich <james@jvodan.com>
2016-06-27 15:23:07 +1000

    correct sed str to strip release tag

--- lib/ruby/api/system/engines_system/docker_events.rb
+++ lib/ruby/api/system/engines_system/docker_events.rb
@@ -4,7 +4,7 @@ module DockerEvents
   def container_event(event_hash)
     return log_error_mesg('Nil event hash passed to container event','') if event_hash.nil?
     STDERR.puts('CONTAINER EVENTS' + event_hash.to_s)
-    event_hash['container_name'].gsub!(/:*.$/,'')
+    event_hash['container_name'].gsub!(/:.*$/,'')
     STDERR.puts('CONTAINER EVENTS' + event_hash.to_s)
     event_hash['container_name'] = container_name_from_id(event_hash['id']) unless event_hash.key?('container_name') 
     event_hash['container_name'] = container_name_from_id(event_hash['id']) if event_hash['container_name'].nil?
