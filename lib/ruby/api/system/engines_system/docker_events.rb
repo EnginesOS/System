5ff2f316d68d75eaa87e0d8177191d75d7cd0605
James L Vodanovich <james@jvodan.com>
2016-06-27 14:34:18 +1000

    clear :release from container name

--- lib/ruby/api/system/engines_system/docker_events.rb
+++ lib/ruby/api/system/engines_system/docker_events.rb
@@ -4,6 +4,7 @@ module DockerEvents
   def container_event(event_hash)
     return log_error_mesg('Nil event hash passed to container event','') if event_hash.nil?
     STDERR.puts(event_hash.to_s)
+    event_hash['container_name'].gsub!(/:*.$/,'')
     event_hash['container_name'] = container_name_from_id(event_hash['id']) unless event_hash.key?('container_name') 
     event_hash['container_name'] = container_name_from_id(event_hash['id']) if event_hash['container_name'].nil?
     event_hash['container_name'] = container_name_from_id(event_hash['id']) unless File.exist?(SystemConfig.RunDir + '/' + event_hash['container_type'].to_s + 's/' + event_hash['container_name'].to_s + '/config.yaml')
