0e5d283f33a18b5ac52c15857f4d747c82a3f42f
James L Vodanovich <james@jvodan.com>
2016-06-28 13:21:30 +1000

    f i c of e

--- lib/ruby/api/system/engines_system/docker_events.rb
+++ lib/ruby/api/system/engines_system/docker_events.rb
@@ -3,9 +3,9 @@ module DockerEvents
 
   def  fill_in_event_system_values(event_hash)
     cn_and_t = @engines_api.container_name_and_type_from_id(event_hash['id'])
-      return false if cn.nil?
+    return cn_and_t if cn_and_t.is_a?(EnginesError) 
     event_hash['container_name'] = cn_and_t[0]
-    event_hash['container_type'] = cn_and_t[0]
+    event_hash['container_type'] = cn_and_t[1]
     event_hash   
   end
   
@@ -13,17 +13,18 @@ module DockerEvents
     return log_error_mesg('Nil event hash passed to container event','') if event_hash.nil?
     STDERR.puts('1 CONTAINER EVENTS' + event_hash.to_s)
   
-    fill_in_event_system_values(event_hash)
+    r = fill_in_event_system_values(event_hash)
+    return r if r.is_a?(EnginesError) 
 
-    event_hash['container_name'] = container_name_from_id(event_hash['id']) unless event_hash.key?('container_name')
-    STDERR.puts('2 CONTAINER EVENTS' + event_hash.to_s)
-    event_hash['container_name'] = container_name_from_id(event_hash['id']) if event_hash['container_name'].nil?
-    STDERR.puts('3 CONTAINER EVENTS' + event_hash.to_s)
-    event_hash['container_name'].gsub!(/:.*$/,'')
-    STDERR.puts('4 CONTAINER EVENTS' + event_hash.to_s)
-    event_hash['container_name'] = container_name_from_id(event_hash['id']) unless File.exist?(SystemConfig.RunDir + '/' + event_hash['container_type'].to_s + 's/' + event_hash['container_name'].to_s + '/config.yaml')
-    STDERR.puts('5 CONTAINER EVENTS' + event_hash.to_s + ' ' + SystemConfig.RunDir + '/' + event_hash['container_type'].to_s + 's/' + event_hash['container_name'].to_s + '/config.yaml')
-    return no_container(event_hash) if event_hash['container_name'].nil?  
+#    event_hash['container_name'] = container_name_from_id(event_hash['id']) unless event_hash.key?('container_name')
+#    STDERR.puts('2 CONTAINER EVENTS' + event_hash.to_s)
+#    event_hash['container_name'] = container_name_from_id(event_hash['id']) if event_hash['container_name'].nil?
+#    STDERR.puts('3 CONTAINER EVENTS' + event_hash.to_s)
+#    event_hash['container_name'].gsub!(/:.*$/,'')
+#    STDERR.puts('4 CONTAINER EVENTS' + event_hash.to_s)
+#    event_hash['container_name'] = container_name_from_id(event_hash['id']) unless File.exist?(SystemConfig.RunDir + '/' + event_hash['container_type'].to_s + 's/' + event_hash['container_name'].to_s + '/config.yaml')
+#    STDERR.puts('5 CONTAINER EVENTS' + event_hash.to_s + ' ' + SystemConfig.RunDir + '/' + event_hash['container_type'].to_s + 's/' + event_hash['container_name'].to_s + '/config.yaml')
+#    return no_container(event_hash) if event_hash['container_name'].nil?  
    return no_container(event_hash) unless File.exist?(SystemConfig.RunDir + '/' + event_hash['container_type'] + 's/' + event_hash['container_name'] + '/config.yaml')
 
   inform_container(event_hash['container_name'] ,event_hash['container_type'] ,event_hash['status'],event_hash)
