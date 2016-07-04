54b4586b0563f43080c7bbb8e17c03e0d309356c
James L Vodanovich <james@jvodan.com>
2016-07-04 14:15:30 +1000

    side step the streaming

--- lib/ruby/api/system/docker/docker_api/docker_api_builder.rb
+++ lib/ruby/api/system/docker/docker_api/docker_api_builder.rb
@@ -63,8 +63,9 @@ module DockerApiBuilder
 #          archive_stream.set_source(build_archive_filename)
 #          
 #        end
-    f = File.new(build_archive_filename,'rb')
-        req.body_stream = f #archive_stream
+    #f = File.new(build_archive_filename,'rb')
+     #   req.body_stream = f #archive_stream
+    req.body = File.read(build_archive_filename)
     STDERR.puts( 'START ' + build_archive_filename.to_s + ' is '  + f.to_s)
         docker_socket.start {|http| http.request(req) 
         STDERR.puts( 'START ' + http.to_s )
