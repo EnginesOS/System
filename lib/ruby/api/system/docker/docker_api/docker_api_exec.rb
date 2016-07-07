c3bee26ec6de0cb801157e646a7b71e724865368
James L Vodanovich <james@jvodan.com>
2016-07-08 09:29:00 +1000

    s

--- lib/ruby/api/system/docker/docker_api/docker_api_exec.rb
+++ lib/ruby/api/system/docker/docker_api/docker_api_exec.rb
@@ -1,5 +1,91 @@
 module DockerApiExec
-
+  class DockerStreamHandler
+    attr_accessor :result
+      def initialize(data, result)
+        @data = data
+        @result = result
+        @result[:raw] = ''
+        @result[:stdout] = ''
+          @result[:stderr] = ''
+      end
+    def docker_stream_as_result(r, h)
+  
+         return h if r.nil?
+         
+      while r.length >0
+     if r[0].nil?
+      return h if r.length == 1
+      r = r[1..-1]
+      next
+      end
+       if r[0].start_with?("\u0001\u0000\u0000\u0000")
+        dst = :stdout
+       elsif r[0].start_with?("\u0002\u0000\u0000\u0000")
+         dst = :stderr
+       else
+         STDERR.puts('START ' + r[0..4].to_s)
+        dst = :stdout
+       end
+   #"\u0001\u0000\u0000\u0000\u0000\u0000\u0000\u000b{\"certs\":[\n\u0001\u0000\u0000\u0000\u0000\u0000\u0000\n\"engines\"\n\u0001\u0000\u0000\u0000\u0000\u0000\u0000\u0003]}\n
+       
+     STDERR.puts("CONTENT " + r.to_s)
+     p r
+       r = r[4..-1]
+       STDERR.puts(' R ' + r.to_s)
+       size = r[0,3]
+   STDERR.puts(' SIZE '  + size.to_s)
+       length = size.unpack("N")
+   STDERR.puts(' LENGTH '  + size.to_s + ' cn:' + length[0].class.name)
+       #length = length[0]
+       r = r[4..-1]
+       l = r.index("\u0000\u0000\u0000")
+       unless l.nil?
+       length =  l - 1
+       else
+         length = r.length
+       end
+       STDERR.puts(' problem ' + r.to_s + ' has ' + r.length.to_s + ' bytes and length ' + length.to_s ) if r.length < length
+       h[dst] += r[0..length-1]
+       r = r[length..-1]
+       end
+   
+      # FIXME need to get correct error status and set :stderr if app
+      h[:result] = 0
+      h
+     end 
+     def is_hijack?
+       false
+     end
+          def has_data?
+            return false if @data.nil?
+            return true unless @data.length == 0 
+            return false
+          end
+          
+      def process_response(chunk , c , t)
+        docker_stream_as_result(chunk, @result)
+      @result[:raw] = @result[:raw] + chunk
+  STDERR.puts( ' parse build res  ' + chunk.to_s )
+     rescue StandardError =>e
+          STDERR.puts( ' parse build res EOROROROROR ' + chunk.to_s + ' : ' +  e.to_s)
+                      return
+  
+      end
+    def process_request(*args)
+           STDERR.puts('PROCESS REQUEST with ')
+           return nil if @data.length == 0
+           if @data.length < Excon.defaults[:chunk_size]
+             return @data
+           else
+             return @data.slice!(0,Excon.defaults[:chunk_size])
+           end
+       
+      rescue StandardError => e
+        STDERR.puts('PROCESS REQUEST got ' + e.to_s)
+        return nil
+         end
+    end
+    
   def docker_exec(container, commands, log_error = true, data=nil)
     have_data = false
     have_data = true unless data.nil?
@@ -19,13 +105,16 @@ module DockerApiExec
       return docker_stream_as_result(r)
     end
     initheader = {'Transfer-Encoding' => 'chunked', 'content-type' => 'application/octet-stream' }
-      
-     req = Net::HTTP::Post.new(request, initheader)
-     
-    perform_data_request(req, container, request_params, data)
-      STDERR.puts('EXEC RESQU ' + r.to_s)
-      return r if r.is_a?(EnginesError)
-      docker_stream_as_result(r)
+    result = {}
+    stream_handler = DockerStreamHandler.new(data, result)
+    post_stream_request(request,options, stream_handler,  initheader  )
+    result
+#     req = Net::HTTP::Post.new(request, initheader)
+#     
+#    perform_data_request(req, container, request_params, data)
+#      STDERR.puts('EXEC RESQU ' + r.to_s)
+#      return r if r.is_a?(EnginesError)
+#      docker_stream_as_result(r)
    
     rescue StandardError => e
       STDERR.puts('DOCKER EXECep  ' + container.container_name + ': with :' + request_params.to_s)
@@ -33,50 +122,52 @@ module DockerApiExec
     end
     
     
-  class DataProducer
-      def initialize(data)
-        @mutex = Mutex.new
-        @body = data
-        @eof = false
-      end
-  
-      def eof!()
-        @eof = true
-      end
-  
-      def eof?()
-        @eof
-      end
-  
-      def size
-        @body.size
-      end
-  
-      def read(size, offset)
-        @mutex.synchronize {
-        STDERR.puts(' READ PARAm ' + offset.to_s + ',' + size.to_s + ' from ' + @body )
-        if @body.empty? && @eof
-          nil
-        else
-   
-            size =  @body.size - 1  if size >= @body.size
-  
-            b = @body.slice!(0,size)
-            STDERR.puts(' write b ' + b.to_s + ' of ' + size.to_s + ' bytes  remaining str ' + @body.to_s )
-            return b
-          end
-          }
-       
-      end
-  
-      def produce(data)
-        @mutex.synchronize {
-          @body = data
-          STDERR.puts(' body ' + @body.to_s)
-        }
-      end
-    end
-  
+#  class DataProducer
+#      def initialize(data)
+#        @mutex = Mutex.new
+#        @body = data
+#        @eof = false
+#      end
+#  
+#      def eof!()
+#        @eof = true
+#      end
+#  
+#      def eof?()
+#        @eof
+#      end
+#  
+#      def size
+#        @body.size
+#      end
+#  
+#      def read(size, offset)
+#        @mutex.synchronize {
+#        STDERR.puts(' READ PARAm ' + offset.to_s + ',' + size.to_s + ' from ' + @body )
+#        if @body.empty? && @eof
+#          nil
+#        else
+#   
+#            size =  @body.size - 1  if size >= @body.size
+#  
+#            b = @body.slice!(0,size)
+#            STDERR.puts(' write b ' + b.to_s + ' of ' + size.to_s + ' bytes  remaining str ' + @body.to_s )
+#            return b
+#          end
+#          }
+#       
+#      end
+#  
+#      def produce(data)
+#        @mutex.synchronize {
+#          @body = data
+#          STDERR.puts(' body ' + @body.to_s)
+#        }
+#      end
+#    end
+#  
+
+    
     def perform_data_request(req, container, params, data)
       producer = DataProducer.new(data)
 #      t1 = Thread.new do
@@ -99,53 +190,7 @@ module DockerApiExec
       log_exception(e)
     end
     
-  def docker_stream_as_result(r)
- h = {}
-   h[:stdout] = ''
-   h[:stderr] =  ''
-       return h if r.nil?
-       
-    while r.length >0
-   if r[0].nil?
-    return h if r.length == 1
-    r = r[1..-1]
-    next
-    end
-     if r[0].start_with?("\u0001\u0000\u0000\u0000")
-      dst = :stdout
-     elsif r[0].start_with?("\u0002\u0000\u0000\u0000")
-       dst = :stderr
-     else
-       STDERR.puts('START ' + r[0..4].to_s)
-      dst = :stdout
-     end
- #"\u0001\u0000\u0000\u0000\u0000\u0000\u0000\u000b{\"certs\":[\n\u0001\u0000\u0000\u0000\u0000\u0000\u0000\n\"engines\"\n\u0001\u0000\u0000\u0000\u0000\u0000\u0000\u0003]}\n
-     
-   STDERR.puts("CONTENT " + r.to_s)
-   p r
-     r = r[4..-1]
-     STDERR.puts(' R ' + r.to_s)
-     size = r[0,3]
- STDERR.puts(' SIZE '  + size.to_s)
-     length = size.unpack("N")
- STDERR.puts(' LENGTH '  + size.to_s + ' cn:' + length[0].class.name)
-     #length = length[0]
-     r = r[4..-1]
-     l = r.index("\u0000\u0000\u0000")
-     unless l.nil?
-     length =  l - 1
-     else
-       length = r.length
-     end
-     STDERR.puts(' problem ' + r.to_s + ' has ' + r.length.to_s + ' bytes and length ' + length.to_s ) if r.length < length
-     h[dst] += r[0..length-1]
-     r = r[length..-1]
-     end
- 
-    # FIXME need to get correct error status and set :stderr if app
-    h[:result] = 0
-    h
-   end
+
  
    private
    def create_docker_exec(container, commands, have_data)
