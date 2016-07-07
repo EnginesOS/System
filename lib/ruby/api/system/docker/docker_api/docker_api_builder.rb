73504ec4aa1a5951c5b0af2d99a3b97eb19c03ff
James L Vodanovich <james@jvodan.com>
2016-07-07 18:04:34 +1000

    content in body

--- lib/ruby/api/system/docker/docker_api/docker_api_builder.rb
+++ lib/ruby/api/system/docker/docker_api/docker_api_builder.rb
@@ -55,8 +55,8 @@ module DockerApiBuilder
      
 
     STDERR.puts( 'build_engine ' +  header.to_s)
-    stream_handler = DockerStreamHandler.new() #File.new(build_archive_filename,'r'))
-   
+    stream_handler = DockerStreamHandler.new(nil) #File.new(build_archive_filename,'r'))
+#   
   return post_stream_request('/build' , options, stream_handler,  header, File.read(build_archive_filename,'r') )
   
     req = Net::HTTP::Post.new('/build?' + options, header)
