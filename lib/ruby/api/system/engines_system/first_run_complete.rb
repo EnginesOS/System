806013843b2c83fc107ebf48d456161dbf09a1a9
James L Vodanovich <james@jvodan.com>
2016-07-13 16:41:58 +1000

    rm debg

--- lib/ruby/api/system/engines_system/first_run_complete.rb
+++ lib/ruby/api/system/engines_system/first_run_complete.rb
@@ -1,31 +1,29 @@
 module FirstRunComplete
-  
- def first_run_complete
-       return true if File.exist?(SystemConfig.FirstRunRan) == true
-       r = false
+  def first_run_complete
+    return true if File.exist?(SystemConfig.FirstRunRan) == true
+
     first_run = @engines_api.loadManagedService('firstrun')
     return first_run if first_run.is_a?(EnginesError)
     Thread.start do
       begin
-  first_run.stop_container
-  first_run.destroy_container
-   
-   mgmt = @engines_api.loadManagedService('mgmt')
-    
-   unless mgmt.create_service.is_a?(EnginesError)
-   
-   FileUtils.touch(SystemConfig.FirstRunRan)
-   disable_service('firstrun')
+        first_run.stop_container
+        first_run.destroy_container
 
-   end
-    
-   
-      rescue StandardError => e
-         STDERR.puts('FIRST RUN Thread Exception' + e.to_s + ':' + e.backtrace.to_s)
+        mgmt = @engines_api.loadManagedService('mgmt')
+
+        unless mgmt.create_service.is_a?(EnginesError)
+
+          FileUtils.touch(SystemConfig.FirstRunRan)
+          disable_service('firstrun')
+
         end
+
+      rescue StandardError => e
+        STDERR.puts('FIRST RUN Thread Exception' + e.to_s + ':' + e.backtrace.to_s)
+      end
     end
-    return true    
-   rescue StandardError => e
-      log_exception(e, event_hash)
- end
+    return true
+  rescue StandardError => e
+    log_exception(e, event_hash)
+  end
 end
