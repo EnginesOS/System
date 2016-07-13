30c8eef93a5c93d85ec5e1fba4efa1e67e7e21c0
James L Vodanovich <james@jvodan.com>
2016-07-13 16:21:20 +1000

    en/disable service now takes service_name

--- lib/ruby/api/system/engines_system/service_management.rb
+++ lib/ruby/api/system/engines_system/service_management.rb
@@ -4,12 +4,12 @@ module ServiceManagement
    return service if service.is_a?(EnginesError)
       
    return log_error_mesg("service container exists",service_name) unless service.read_state == 'nocontainer'
-   STDERR.puts("MOVING" +ContainerStateFiles.container_service_dir(container.container_name).to_s + ' to ' + ContainerStateFiles.container_disabled_service_dir(container.container_name).to_s )    
-   FileUtils.mv(ContainerStateFiles.container_service_dir(container.container_name),ContainerStateFiles.container_disabled_service_dir(container.container_name))
+   STDERR.puts("MOVING" +ContainerStateFiles.container_service_dir(service_name).to_s + ' to ' + ContainerStateFiles.container_disabled_service_dir(service_name).to_s )    
+   FileUtils.mv(ContainerStateFiles.container_service_dir(service_name),ContainerStateFiles.container_disabled_service_dir(service_name))
     
     rescue StandardError => e
       
-       log_exception(e, event_hash)
+       log_exception(e, service_name)
 end
 
   def enable_service(service_name)
