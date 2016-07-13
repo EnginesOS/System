d0e7bee54cabdd956b691e1967804040b15ad4ab
James L Vodanovich <james@jvodan.com>
2016-07-14 08:00:46 +1000

    reset first run support getting first run def from services-available

--- bin/reset_first_run.sh
+++ bin/reset_first_run.sh
@@ -3,7 +3,13 @@
 engines service mgmt stop
 engines service mgmt destroy
 
-mv /opt/engines/run/services-disabled/firstrun /opt/engines/run/services/firstrun 
+if test -d /opt/engines/run/services-disabled/firstrun
+ then
+	mv /opt/engines/run/services-disabled/firstrun /opt/engines/run/services/firstrun
+ elif  test -d /opt/engines/run/services-available/firstrun
+ then
+ 	mv /opt/engines/run/services-available/firstrun /opt/engines/run/services/firstrun
+ end
 
 rm /opt/engines/run/system/flags/first_ran 
 engines service firstrun create
