c012d5e389dee133634f3bbc7e0a876feda32ecc
James L Vodanovich <james@jvodan.com>
2016-07-13 12:26:55 +1000

    +x

--- bin/reset_first_run.sh
+++ bin/reset_first_run.sh
@@ -2,6 +2,7 @@
 
 engines service mgmt stop
 engines service mgmt destroy
+
 mv /opt/engines/run/services-disabled/firstrun /opt/engines/run/services/firstrun 
 
 rm /opt/engines/run/system/flags/first_ran 
