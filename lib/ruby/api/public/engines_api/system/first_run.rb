b51163165cbf1a3ec1f87b6c4bd642da2a08e1ca
James L Vodanovich <james@jvodan.com>
2016-07-13 14:27:32 +1000

    f

--- lib/ruby/api/public/engines_api/system/first_run.rb
+++ lib/ruby/api/public/engines_api/system/first_run.rb
@@ -1,13 +1,13 @@
 module PublicApiSystemFirstRun
-#  def set_first_run_parameters(params_from_gui)
-#    require '/opt/engines/lib/ruby/api/system/first_run_wizard/first_run_wizard.rb'
-#    params = params_from_gui.dup
-#     SystemDebug.debug(SystemDebug.first_run,params)
-#     first_run = FirstRunWizard.new(params)
-#     first_run.apply(@core_api)
-#     return log_error_message(first_run.last_error) unless first_run.sucess
-#     return first_run.sucess
-#  end
+  def set_first_run_parameters(params_from_wizard)
+    require '/opt/engines/lib/ruby/api/system/first_run_wizard/first_run_wizard.rb'
+    params = params_from_wizard.dup
+     SystemDebug.debug(SystemDebug.first_run,params)
+     first_run = FirstRunWizard.new(params)
+     first_run.apply(@core_api)
+     return log_error_message(first_run.last_error) unless first_run.sucess
+     return first_run.sucess
+  end
   
   def first_run_complete
     @system_api.first_run_complete
