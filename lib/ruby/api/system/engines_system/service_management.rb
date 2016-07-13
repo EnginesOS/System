0829c6031aef10b4b347cbd575c0bc63cc2a4f8a
James L Vodanovich <james@jvodan.com>
2016-07-13 15:44:28 +1000

    dbg

--- lib/ruby/api/system/engines_system/service_management.rb
+++ lib/ruby/api/system/engines_system/service_management.rb
@@ -3,7 +3,8 @@ module ServiceManagement
    service =  @engines_api.loadManagedService(service_name)
    return service if service.is_a?(EnginesError)
    
-   return log_error_mesg("service container exists",service_name) unless service.read_state = 'nocontainer'    
+   return log_error_mesg("service container exists",service_name) unless service.read_state = 'nocontainer'
+   STDERR.puts("MOVING" +ContainerStateFiles.container_service_dir(container.container_name).to_s + ' to ' + ContainerStateFiles.container_disabled_service_dir(container.container_name).to_s )    
    FileUtils.mv(ContainerStateFiles.container_service_dir(container.container_name),ContainerStateFiles.container_disabled_service_dir(container.container_name))
      
 end
