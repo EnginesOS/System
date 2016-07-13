dc4231ae213002e1ff23c7c0f3d6208d95a008f5
James L Vodanovich <james@jvodan.com>
2016-07-13 10:33:58 +1000

    first run destroy first run and create mgmt

--- lib/ruby/api/system/engines_system/first_run_complete.rb
+++ lib/ruby/api/system/engines_system/first_run_complete.rb
@@ -3,13 +3,13 @@ module FirstRunComplete
  def first_run_complete
        return true if File.exist?(SystemConfig.FirstRunRan) == true
        r = false
-    first_run = @engines_api.getManagedService('firstrun')
+    first_run = @engines_api.loadManagedService('firstrun')
     return first_run if first_run.is_a?(EnginesError)
     
    return r if ( r = first_run.stop).is_a?(EnginesError)
    return r if ( r = first_run.destroy).is_a?(EnginesError)
    
-   mgmt = @engines_api.getManagedService('mgmt')
+   mgmt = @engines_api.loadManagedService('mgmt')
    return r if ( r = mgmt.create).is_a?(EnginesError)
    if (r = mgmt.start) == true
    File.delete(SystemConfig.FirstRunRan)
