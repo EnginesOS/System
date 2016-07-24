ba852d630512d9f715f110663d96c21a8b44277b
James L Vodanovich <james@jvodan.com>
2016-07-24 13:17:21 +1000

    sleep

--- lib/ruby/api/system/containers/container_api/container_api_events.rb
+++ lib/ruby/api/system/containers/container_api/container_api_events.rb
@@ -2,19 +2,13 @@ module ContainerApiEvents
   
   def wait_for(state)
     
-   case state
-    when 'stopped'
-      @event_mutex = Mutex.new
-      @event_mutex.lock
-    @system_api.add_event_listener([self,'state_reached'.to_sym],1 | 512 | 256 , @container_id)
-    @event_mutex.lock
-    @event_mutex.unlock
-    end
-  @system_api.rm_event_listener([self,'state_reached'.to_sym])
-  end
+#   case state
+#    when 'stopped'
+#   
+#  end
+# FixME Big Kludge
+    sleep (10)
 
-  def state_reached(event_hash)
-    @event_mutex.unlock
   end
     
 end
