bd413ff1408f130ced86f7ddbcdb87accf543b3b
James L Vodanovich <james@jvodan.com>
2016-07-27 17:02:51 +1000

    dbg

--- system/templates/deployment/global/build_scripts/recursive_write_permissions.sh
+++ system/templates/deployment/global/build_scripts/recursive_write_permissions.sh
@@ -12,13 +12,21 @@ echo no suffix $directory
           if [ -h  /home/app/$directory ] 
             then 
             dest=`ls -la /home/app/$directory |cut -f2 -d'>'`
+             echo " chmod -R gu+rw $dest ;chgrp $data_gid -R $dest"
             chmod -R gu+rw $dest
+            chgrp $data_gid -R $dest
           elif [ ! -d /home/app/$directory ] 
             then 
+            echo " mkdir  -p /home/app/$directory "
+             echo "  chown $data_uid  /home/app/$directory "
+             echo "   chmod -R gu+rw /home/app/$directory " 
+             
               mkdir  -p /home/app/$directory
              chown $data_uid  /home/app/$directory
              chmod -R gu+rw /home/app/$directory 
           else
+          echo "   chmod -R gu+rw /home/app/$directory  ; chgrp $data_gid -R /home/app/$directory" 
+          chgrp $data_gid -R /home/app/$directory
           chmod -R gu+rw /home/app/$directory
           
         fi
