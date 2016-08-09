811ea648b76dfad335d2e315f036be228a14680f
James L Vodanovich <james@jvodan.com>
2016-08-09 14:11:52 +1000

    longer backtrace on errs

--- lib/ruby/api/system/docker/docker_api/engines_docker_api_error.rb
+++ lib/ruby/api/system/docker/docker_api/engines_docker_api_error.rb
@@ -1,7 +1,7 @@
 class EnginesDockerApiError < EnginesError
   def initialize(message, type = :fail)
       super
-      @source = caller[1].to_s 
+      @source = caller[1,4].to_s 
       @sub_system = 'docker_api'
     end
 end
