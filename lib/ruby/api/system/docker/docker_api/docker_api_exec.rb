2074d49d7f423876f768c8195d019277d0afb85c
James L Vodanovich <james@jvodan.com>
2016-08-15 13:58:27 +1000

    tidy

--- lib/ruby/api/system/docker/docker_api/docker_api_exec.rb
+++ lib/ruby/api/system/docker/docker_api/docker_api_exec.rb
@@ -1,95 +1,93 @@
 module DockerApiExec
 
   require_relative 'docker_utils.rb'
-  
   class DockerHijackStreamHandler
     attr_accessor :result, :data
-      def initialize(data)
-        @data = data
-        @result = {}
-        @result[:raw] = ''
-        @result[:stdout] = ''
-        @result[:stderr] = ''
-      end
-    
-     def is_hijack?
-       true
-     end
-          def has_data?
-            return false if  @data.nil?
-            return false if  @data.length == 0
-            return true
-          end
-          
-      def process_response()
-        return_result = @result
-         lambda do |chunk , c , t|
+    def initialize(data)
+      @data = data
+      @result = {}
+      @result[:raw] = ''
+      @result[:stdout] = ''
+      @result[:stderr] = ''
+    end
+
+    def is_hijack?
+      true
+    end
+
+    def has_data?
+      return false if  @data.nil?
+      return false if  @data.length == 0
+      return true
+    end
+
+    def process_response()
+      return_result = @result
+      lambda do |chunk , c , t|
         DockerUtils.docker_stream_as_result(chunk, return_result)
-           return_result[:raw] = return_result[:raw] + chunk.to_s
-  STDERR.puts( ' parse build res  ' + chunk.to_s )
-          end
-     rescue StandardError =>e
-          STDERR.puts( ' parse build res EOROROROROR ' + chunk.to_s + ' : ' +  e.to_s + ' ' + e.backtrace.to_s)
-          return        
+        return_result[:raw] = return_result[:raw] + chunk.to_s
+        STDERR.puts( ' parse exec_hj res  ' + chunk.to_s )
       end
+    rescue StandardError =>e
+      STDERR.puts( ' parse build res EOROROROROR ' + chunk.to_s + ' : ' +  e.to_s + ' ' + e.backtrace.to_s)
+      return
+    end
 
   end
-  
+
   class DockerStreamReader
     def is_hijack?
-         false
-       end
+      false
+    end
     attr_accessor :result
-         def initialize()
-          
-           @result = {}
-           @result[:raw] = ''
-           @result[:stdout] = ''
-           @result[:stderr] = ''
-         end
-   def process_response( )
-     return_result = @result
-            lambda do |chunk , c , t|
-           DockerUtils.docker_stream_as_result(chunk, return_result)
-              return_result[:raw] = return_result[:raw] + chunk.to_s
-     STDERR.puts( ' parse build res  ' + chunk.to_s )
-             end
-#     lambda do |chunk, remaining_bytes, total_bytes |  
-#       #@result[:raw] +=  chunk
-#       STDERR.puts('LABM ' + chunk.to_s )      
-#     end
-     
-  end  
+
+    def initialize()
+
+      @result = {}
+      @result[:raw] = ''
+      @result[:stdout] = ''
+      @result[:stderr] = ''
+    end
+
+    def process_response( )
+      return_result = @result
+      lambda do |chunk , c , t|
+        DockerUtils.docker_stream_as_result(chunk, return_result)
+        return_result[:raw] = return_result[:raw] + chunk.to_s
+        STDERR.puts( 'exec parse process  res  ' + chunk.to_s )
+      end
+    end
+
     def has_data?
-              return false 
-            end
+      return false
+    end
   end
-    
+
   def docker_exec(container, commands, log_error = true, data=nil)
     have_data = false
     have_data = true unless data.nil?
-    
-      r = create_docker_exec(container, commands, have_data)
-    
-      return r unless r.is_a?(Hash)
 
-      exec_id = r[:Id]
-      request_params = {}
-      request_params["Detach"] = false
-      request_params["Tty"] = false
-      request = '/exec/' + exec_id + '/start'
+    r = create_docker_exec(container, commands, have_data)
+
+    return r unless r.is_a?(Hash)
+
+    exec_id = r[:Id]
+    request_params = {}
+    request_params["Detach"] = false
+    request_params["Tty"] = false
+    request = '/exec/' + exec_id + '/start'
     request_params["User"] = ''
-        request_params["Privileged"] = false
-     request_params["AttachStdout"] = true
-       request_params["AttachStderr"] = true 
-         request_params["Container"] = container.container_name 
-         request_params["Cmd"] = commands
+    request_params["Privileged"] = false
     request_params["AttachStdout"] = true
     request_params["AttachStderr"] = true
-      
+    request_params["Container"] = container.container_name
+    request_params["Cmd"] = commands
+    request_params["AttachStdout"] = true
+    request_params["AttachStderr"] = true
+
     headers = {}
-       headers['Content-type'] = 'text/plain'
-      
+    headers['Content-type'] = 'text/plain'
+
     unless have_data == true
       result = {}
       stream_reader = DockerStreamReader.new
@@ -98,127 +96,71 @@ module DockerApiExec
       return r if r.is_a?(EnginesError)
       return stream_reader.result # DockerUtils.docker_stream_as_result(r, result)
     end
-  #  initheader = {'Transfer-Encoding' => 'chunked', 'content-type' => 'application/octet-stream' }
-     
+    #  initheader = {'Transfer-Encoding' => 'chunked', 'content-type' => 'application/octet-stream' }
+
     request_params["AttachStdin"] = true
     stream_handler = DockerHijackStreamHandler.new(data)
-   
+
     headers['Connection'] = 'Upgrade'
     headers['Upgrade'] = 'tcp'
-  
+
     post_stream_request(request, nil, stream_handler,  headers , request_params.to_json )
     STDERR.puts('EXEC RES ' + stream_handler.result.to_s)
     stream_handler.result
-#     req = Net::HTTP::Post.new(request, initheader)
-#     
-#    perform_data_request(req, container, request_params, data)
-#      STDERR.puts('EXEC RESQU ' + r.to_s)
-#      return r if r.is_a?(EnginesError)
-#      docker_stream_as_result(r)
    
-    rescue StandardError => e
-      STDERR.puts('DOCKER EXECep  ' + container.container_name + ': with :' + request_params.to_s)
-      log_exception(e)
+  rescue StandardError => e
+    STDERR.puts('DOCKER EXECep  ' + container.container_name + ': with :' + request_params.to_s)
+    log_exception(e)
+  end
+
+
+
+#  def perform_data_request(req, container, params, data)
+#    producer = DataProducer.new(data)
+#
+#    req.content_type = "application/octet-stream" #"text/plain"
+#    # req['Transfer-Encoding'] = 'chunked'
+#    # req.content_length = data.length
+#    req.body = params.to_json
+#
+#    #req.body_stream = producer
+#
+#    docker_socket.start {|http| http.request(req)
+#
+#      docker_socket.write(data)
+#
+#    }
+#  rescue StandardError => e
+#    log_exception(e)
+#  end
+
+  private
+
+  def create_docker_exec(container, commands, have_data)
+    commands = format_commands(commands)
+
+    request_params = {}
+    if have_data == false
+      request_params["AttachStdin"] = false
+      request_params["Tty"] =  false
+    else
+      request_params["AttachStdin"] = true
+      request_params["Tty"] =  false
     end
-    
-    
-#  class DataProducer
-#      def initialize(data)
-#        @mutex = Mutex.new
-#        @body = data
-#        @eof = false
-#      end
-#  
-#      def eof!()
-#        @eof = true
-#      end
-#  
-#      def eof?()
-#        @eof
-#      end
-#  
-#      def size
-#        @body.size
-#      end
-#  
-#      def read(size, offset)
-#        @mutex.synchronize {
-#        STDERR.puts(' READ PARAm ' + offset.to_s + ',' + size.to_s + ' from ' + @body )
-#        if @body.empty? && @eof
-#          nil
-#        else
-#   
-#            size =  @body.size - 1  if size >= @body.size
-#  
-#            b = @body.slice!(0,size)
-#            STDERR.puts(' write b ' + b.to_s + ' of ' + size.to_s + ' bytes  remaining str ' + @body.to_s )
-#            return b
-#          end
-#          }
-#       
-#      end
-#  
-#      def produce(data)
-#        @mutex.synchronize {
-#          @body = data
-#          STDERR.puts(' body ' + @body.to_s)
-#        }
-#      end
-#    end
-#  
+    request_params[ "AttachStdout"] =  true
+    request_params[ "AttachStderr"] =  true
+    request_params[ "DetachKeys"] =  "ctrl-p,ctrl-q"
+    request_params[ "Cmd"] =  commands
 
-    
-    def perform_data_request(req, container, params, data)
-      producer = DataProducer.new(data)
-#      t1 = Thread.new do
-#        producer.produce(data)
-#        producer.eof!
-#      end
-      req.content_type = "application/octet-stream" #"text/plain"
-     # req['Transfer-Encoding'] = 'chunked'
-     # req.content_length = data.length
-      req.body = params.to_json
-      
-      #req.body_stream = producer
+    request = '/containers/'  + container.container_id.to_s + '/exec'
+    r = post_request(request,  request_params)
+    STDERR.puts('DOCKER EXEC ' + r.to_s + ': for :' + container.container_name + ': with :' + request_params.to_s)
+    return r
+  end
 
-      docker_socket.start {|http| http.request(req) 
-      
-        docker_socket.write(data)
-        
-      }
-    rescue StandardError => e
-      log_exception(e)
-    end
-    
-
- 
-   private
-   def create_docker_exec(container, commands, have_data)
-     commands = format_commands(commands)
-          
-          request_params = {}
-          if have_data == false
-            request_params["AttachStdin"] = false
-            request_params["Tty"] =  false
-          else
-            request_params["AttachStdin"] = true
-            request_params["Tty"] =  false
-          end
-          request_params[ "AttachStdout"] =  true
-          request_params[ "AttachStderr"] =  true
-          request_params[ "DetachKeys"] =  "ctrl-p,ctrl-q"
-          request_params[ "Cmd"] =  commands
-      
-          request = '/containers/'  + container.container_id.to_s + '/exec'
-          r = post_request(request,  request_params)
-          STDERR.puts('DOCKER EXEC ' + r.to_s + ': for :' + container.container_name + ': with :' + request_params.to_s)
-          return r
-   end
-   
-     
-def format_commands(commands)
-   commands = [commands] unless commands.is_a?(Array)
+  def format_commands(commands)
+    commands = [commands] unless commands.is_a?(Array)
     commands
- end
- 
+  end
+
 end
--- lib/ruby/api/system/docker/docker_api/docker_utils.rb
+++ lib/ruby/api/system/docker/docker_api/docker_utils.rb
@@ -1,112 +1,104 @@
 module DockerUtils
-  
   def self.process_request(data , result)
-       to_send = data
-       return_result = result
-       STDERR.puts('PROCESS REQUEST init ' + to_send.to_s)
-       lambda do |socket|
-         STDERR.puts('PROCESS REQUEST Lambda')
-         write_thread = Thread.start do 
-           begin
-       STDERR.puts('PROCESS REQUEST write thread ' + to_send.to_s)
-            return socket.close_write if to_send.length == 0
-            if to_send.length < Excon.defaults[:chunk_size]
-              STDERR.puts('PROCESS REQUEST with single chunk ' + to_send.to_s)
-              r = to_send
-              to_send = ''
-              socket.send(r,0)
-              socket.close_write
-            else
-              socket.send(to_send.slice!(0,Excon.defaults[:chunk_size]),0)
-            end
-           rescue StandardError => e
-               STDERR.puts(e.to_s + ':' + e.backtrace.to_s)
-           end
-         end
-         read_thread = Thread.start do
-           begin
-             STDERR.puts('PROCESS REQUEST read thread')
-           while chunk = socket.readpartial(1024)
-             DockerUtils.docker_stream_as_result(chunk, return_result)
-             STDERR.puts('PROCESS REQUEST read thread' + return_result.to_s)
-           end          
-          rescue EOFError 
-             write_thread.kill
-            
-          rescue StandardError => e
-             STDERR.puts(e.to_s + ':' + e.backtrace.to_s)
-         end
-           write_thread.kill
-         end
-         
-         write_thread.join
-         read_thread.join
+    to_send = data
+    return_result = result
+    STDERR.puts('PROCESS REQUEST init ' + to_send.to_s)
+    lambda do |socket|
+      STDERR.puts('PROCESS REQUEST Lambda')
+      write_thread = Thread.start do
+        begin
+          STDERR.puts('PROCESS REQUEST write thread ' + to_send.to_s)
+          return socket.close_write if to_send.length == 0
+          if to_send.length < Excon.defaults[:chunk_size]
+            STDERR.puts('PROCESS REQUEST with single chunk ' + to_send.to_s)
+            r = to_send
+            to_send = ''
+            socket.send(r,0)
+            socket.close_write
+          else
+            socket.send(to_send.slice!(0,Excon.defaults[:chunk_size]),0)
+          end
+        rescue StandardError => e
+          STDERR.puts(e.to_s + ':' + e.backtrace.to_s)
+        end
       end
-     rescue StandardError => e
-       STDERR.puts('PROCESS Execp' + e.to_s + ' ' + e.backtrace.to_s )
-       
-     end
+      read_thread = Thread.start do
+        begin
+          STDERR.puts('PROCESS REQUEST read thread')
+          while chunk = socket.readpartial(1024)
+            DockerUtils.docker_stream_as_result(chunk, return_result)
+            STDERR.puts('PROCESS REQUEST read thread' + return_result.to_s)
+          end
+        rescue EOFError
+          write_thread.kill
 
+        rescue StandardError => e
+          STDERR.puts(e.to_s + ':' + e.backtrace.to_s)
+        end
+        write_thread.kill
+      end
+
+      write_thread.join
+      read_thread.join
+    end
+  rescue StandardError => e
+    STDERR.puts('PROCESS Execp' + e.to_s + ' ' + e.backtrace.to_s )
+
+  end
 
   def self.docker_stream_as_result(r, h)
     unmatched = false
-           return h if r.nil?
-           h[:stderr] = "" unless h.key?(:stderr)
-           h[:stdout] = "" unless h.key?(:stdout)
-               
-        while r.length >0
-       if r[0].nil?
+    return h if r.nil?
+    h[:stderr] = "" unless h.key?(:stderr)
+    h[:stdout] = "" unless h.key?(:stdout)
+
+    while r.length >0
+      if r[0].nil?
         return h if r.length == 1
         r = r[1..-1]
         next
-        end
-         if r.start_with?("\u0001\u0000\u0000\u0000")
-          r = r[8..-1]
-    STDERR.puts("Stdout CONTENT " + r.to_s)    
-          dst = :stdout
-         elsif r.start_with?("\u0002\u0000\u0000\u0000")
-           dst = :stderr
-           
-           r = r[8..-1]
-STDERR.puts("Stderr CONTENT " + r.to_s)
-
-         elsif r.start_with?("\u0000\u0000\u0000\u0000")
-          dst = :stdout
-          
-          r = r[8..-1]
-STDERR.puts("unlable stdout CONTENT " + r.to_s)
-
-         else         
+      end
+      if r.start_with?("\u0001\u0000\u0000\u0000")
+        r = r[8..-1]
+        STDERR.puts("Stdout CONTENT " + r.to_s)
+        dst = :stdout
+      elsif r.start_with?("\u0002\u0000\u0000\u0000")
+        dst = :stderr
+        r = r[8..-1]
+        STDERR.puts("Stderr CONTENT " + r.to_s)
+      elsif r.start_with?("\u0000\u0000\u0000\u0000")
+        dst = :stdout
+        r = r[8..-1]
+        STDERR.puts("unlable stdout CONTENT " + r.to_s)
+      else
         # r = r[7..-1]
-STDERR.puts(" umatched CONTENT " + r.to_s)
-          dst = :stdout
-          unmatched = true
-         end
-     #"\u0001\u0000\u0000\u0000\u0000\u0000\u0000\u000b{\"certs\":[\n\u0001\u0000\u0000\u0000\u0000\u0000\u0000\n\"engines\"\n\u0001\u0000\u0000\u0000\u0000\u0000\u0000\u0003]}\n
-         
-     
-         
-         return h if r.nil?
-         unless unmatched == true
-           
-         next_chunk = r.index("\u0000\u0000\u0000")
-         unless next_chunk.nil?
+        STDERR.puts(" umatched CONTENT " + r.to_s)
+        dst = :stdout
+        unmatched = true
+      end
+
+      return h if r.nil?
+      unless unmatched == true
+
+        next_chunk = r.index("\u0000\u0000\u0000")
+        STDERR.puts("Next Chunk " + next_chunk.to_s)
+        unless next_chunk.nil?
           length =  next_chunk - 2
-         else
-           STDERR.puts(' wnd of string')
-           length = r.length
-         end
-           length = r.length
-         end
-         
+        else
+          STDERR.puts('End of string')
+          length = r.length
+        end
+        length = r.length
+      end
+
       #   STDERR.puts(' problem ' + r.to_s + ' has ' + r.length.to_s + ' bytes and length ' + length.to_s ) if r.length < length
-         h[dst] += r[0..length-1]
-         r = r[length..-1]
-STDERR.puts(' still ave of string ' + r.to_s + ' with ' + r.length.to_s)
-         end
-     
-        # FIXME need to get correct error status and set :stderr if app
-        h[:result] = 0
-        h
-       end 
+      h[dst] += r[0..length-1]
+      r = r[length..-1]
+      STDERR.puts(' still ave of string ' + r.to_s + ' with ' + r.length.to_s)
+    end
+
+    # FIXME need to get correct error status and set :stderr if app
+    h[:result] = 0
+    h
+  end
 end
