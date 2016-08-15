9213fb7c1d6c4735a1a93b2897daa75d8ed4558b
James L Vodanovich <james@jvodan.com>
2016-08-15 14:04:48 +1000

    use flag files to determine when regisry and system are up

--- system/scripts/update/run_update_engines_system_software.sh
+++ system/scripts/update/run_update_engines_system_software.sh
@@ -11,13 +11,33 @@ docker stop system
 
 docker stop registry
 docker start registry 
-sleep 15
+count=0
+sleep 5
+  while ! test -f /opt/engines/run/system_services/registry/run/flags/startup_complete
+  do 
+  	sleep 5
+  	count=`expr $count + 5`
+  		if test $count -gt 120
+  		 then
+  		  echo "ERROR failed to start registry "
+  		  exit
+  		fi
+  done 
 rm /tmp/clean.log
 
   /opt/engines/system/scripts/system/rotate_system_log.sh 
 
 docker start system 
-   
+    while ! test -f /opt/engines/run/system_services/system/run/flags/startup_complete
+  do 
+  	sleep 5
+  	count=`expr $count + 5`
+  		if test $count -gt 120
+  		 then
+  		  echo "ERROR failed to start system "
+  		  exit
+  		fi
+  done 
 touch /opt/engines/run/system/flags/update_engines_run
 rm /opt/engines/run/system/flags/update_engines_running
 rm /opt/engines/run/system/flags/update_pending
