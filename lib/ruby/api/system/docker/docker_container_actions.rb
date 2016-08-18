06e47cc0c965d398d85ae4f720dbba0639527fdd
James L Vodanovich <james@jvodan.com>
2016-08-18 12:04:08 +1000

    params for docker_exec

--- lib/ruby/api/system/docker/docker_container_actions.rb
+++ lib/ruby/api/system/docker/docker_container_actions.rb
@@ -21,27 +21,18 @@ module DockerContainerActions
   end
 
   def stop_container(container)
-#    clear_error
-#    commandargs = 'docker stop ' + container.container_name
-#    run_docker_cmd(commandargs, container)
     @docker_comms.stop_container(container)
   rescue StandardError => e
     log_exception(e)
   end
 
   def pause_container(container)
-#    clear_error
-#    commandargs = 'docker pause ' + container.container_name
-#    run_docker_cmd(commandargs, container)
     @docker_comms.pause_container(container)
   rescue StandardError => e
     log_exception(e)
   end
 
   def unpause_container(container)
-#    clear_error
-#    commandargs = 'docker unpause ' + container.container_name
-#    run_docker_cmd(commandargs, container)
     @docker_comms.unpause_container(container)
   rescue StandardError => e
     log_exception(e)
@@ -52,16 +43,13 @@ module DockerContainerActions
  #   commandargs = 'docker exec ' + container.container_name + ' kill -' + signal + ' ' + pid.to_s
     cmds =['kill','-' + signal, pid]
     #execute_docker_cmd(commandargs, container)
-    @docker_comms.docker_exec(container, cmds, false)
+    @docker_comms.docker_exec({:container => container, :command_line=>cmds, :log_error=>false})
   rescue StandardError => e
     log_exception(e)
   end
 
   def destroy_container(container)
-#    clear_error
-#    commandargs = 'docker  rm ' + container.container_name
-#    unless run_docker_cmd(commandargs, container)
-#      log_error_mesg(container.last_error, container)
+
     @docker_comms.destroy_container(container)
       return false if @docker_comms.container_exist?(container)
     #end
