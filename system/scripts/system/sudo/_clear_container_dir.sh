fe587ec3a397fd08d45c7f2d4effc3ae25943075
James L Vodanovich <james@jvodan.com>
2016-09-02 15:51:33 +1000

    dont del just chown (safer)

--- system/scripts/system/sudo/_clear_container_dir.sh
+++ system/scripts/system/sudo/_clear_container_dir.sh
@@ -4,6 +4,7 @@ if test $# -gt 0
   then
   if test -d $1
    then
-	rm -r /opt/engines/run/$1
+	rm -r /opt/engines/run/containers/$1/run
+	chown engine.containers /opt/engines/run/containers/$1
   fi
 fi 
