cfbcc41a374d08caae0fab99b4b01d755f4c85a4
James L Vodanovich <james@jvodan.com>
2016-09-05 10:17:11 +1000

    use parent to_h and to_json

--- lib/ruby/service_manager/system_registry/engines_registry_error.rb
+++ lib/ruby/service_manager/system_registry/engines_registry_error.rb
@@ -17,12 +17,12 @@ class EnginesRegistryError < EnginesError
         @sub_system = 'engines_registry'
         @registry_source = error_hash[:source]
       end
-      
-  def to_json(opt)
-  #FixMe this is a kludge
-    STDERR.puts(      '{"error_type":"' + @error_type.to_s + '","error_mesg":"' + @error_mesg.to_s + '","sub_system":"' + @sub_system.to_s + '","source":' + @source.to_s + ',"params":' + @params.to_json + '}')
-      '{"error_type":"' + @error_type.to_s + '","error_mesg":"' + @error_mesg.to_s + '","sub_system":"' + @sub_system.to_s + '","source":' + @source.to_json + ',"params":' + @params.to_json + '}'
-  end
+#      
+#  def to_json(opt)
+#  #FixMe this is a kludge
+#    STDERR.puts(      '{"error_type":"' + @error_type.to_s + '","error_mesg":"' + @error_mesg.to_s + '","sub_system":"' + @sub_system.to_s + '","source":' + @source.to_s + ',"params":' + @params.to_json + '}')
+#      '{"error_type":"' + @error_type.to_s + '","error_mesg":"' + @error_mesg.to_s + '","sub_system":"' + @sub_system.to_s + '","source":' + @source.to_json + ',"params":' + @params.to_json + '}'
+#  end
   
  
 end
