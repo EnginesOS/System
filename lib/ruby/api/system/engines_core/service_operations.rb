99c90dae451b07c6e558ca443074dae9efa4baad
James L Vodanovich <james@jvodan.com>
2016-09-09 09:18:12 +1000

    dbg

--- lib/ruby/api/system/engines_core/service_operations.rb
+++ lib/ruby/api/system/engines_core/service_operations.rb
@@ -92,13 +92,13 @@ module ServiceOperations
 
   def create_and_register_managed_service(service_hash) 
     r = ''
-
+    return log_error_mesg('Attached Service passed no variables ' +  service_hash.to_s, service_hash) unless service_hash.key?(:variables)
     SystemDebug.debug(SystemDebug.services, "osapicreate_and_register_managed_service", service_hash)
     service_hash[:variables][:parent_engine] = service_hash[:parent_engine] unless service_hash[:variables].has_key?(:parent_engine)
     ServiceDefinitions.set_top_level_service_params(service_hash,service_hash[:parent_engine])
       
     return r unless ( r = check_engine_service_hash(service_hash))
-    return log_error_mesg('Attached Service passed no variables', service_hash) unless service_hash.key?(:variables)
+   
     if service_hash[:type_path] == 'filesystem/local/filesystem'
               engine = loadManagedEngine(service_hash[:parent_engine])         
               engine.add_volume(service_hash) if engine.is_a?(ManagedEngine)
