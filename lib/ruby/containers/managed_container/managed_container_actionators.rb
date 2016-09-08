5ad68d896d9a12dacac04118c1687a91e0131f08
James L Vodanovich <james@jvodan.com>
2016-09-08 15:20:21 +1000

    fmt

--- lib/ruby/containers/managed_container/managed_container_actionators.rb
+++ lib/ruby/containers/managed_container/managed_container_actionators.rb
@@ -1,7 +1,7 @@
 module ManagedContainerActionators
- def perform_action(actionator_name,params, data=nil)
-  @container_api.perform_action(self,actionator_name,params,data)
-   rescue StandardError => e
-            log_exception( e,'perform_engine_action',actionator_name,params)
- end
+  def perform_action(actionator_name,params, data=nil)
+    @container_api.perform_action(self,actionator_name,params,data)
+  rescue StandardError => e
+    log_exception(e ,'perform_engine_action',actionator_name,params)
+  end
 end
