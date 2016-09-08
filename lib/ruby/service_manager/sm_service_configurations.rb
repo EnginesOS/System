67beac2b60fe941df1d42321d148e0df00afbc42
James L Vodanovich <james@jvodan.com>
2016-09-08 13:00:57 +1000

    use of checks

--- lib/ruby/service_manager/sm_service_configurations.rb
+++ lib/ruby/service_manager/sm_service_configurations.rb
@@ -2,8 +2,8 @@ module SmServiceConfigurations
   def update_service_configuration(config_hash)
     #load service definition and from configurators definition and if saveable save
     service_definition = ServiceDefinitions.software_service_definition(config_hash)
+    return log_error_mesg('Missing Service definition file ', config_hash.to_s)  unless service_definition.is_a?(Hash)    
     config_hash[:no_save]  = service_definition[:no_save]
-    return log_error_mesg('Missing Service definition file ', config_hash.to_s)  unless service_definition.is_a?(Hash)
     SystemDebug.debug(SystemDebug.services,:update_service, service_definition)
     return log_error_mesg('Missing Configurators in service definition', config_hash.to_s) unless service_definition.key?(:configurators)
     configurators = service_definition[:configurators]
