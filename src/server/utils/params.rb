5191dc382b5e81d3ec5d9421753f8b8b33182644
James L Vodanovich <james@jvodan.com>
2016-09-08 12:44:29 +1000

    error

--- src/server/utils/params.rb
+++ src/server/utils/params.rb
@@ -1,12 +1,14 @@
+require '/opt/engines/lib/ruby/system/engines_error.rb'
 module Params
+ 
   def self.assemble_params(params, address_params, required_params, accept_params=nil )
 
     a_params = self.address_params(params, address_params)
 
     unless required_params.empty?
-      return EngineError.new('Missing Address Parameters ' + address_params.to_s + ' but only have:' + params.to_s) if a_params == false
+      return EngineError.new('Missing Address Parameters ' + address_params.to_s + ' but only have:' + params.to_s, :error,'api') if a_params == false
       r_params = self.required_params(params,required_params)
-      return EngineError.new('Missing Parameters ' + required_params.to_s + ' but only have:' + params.to_s) if r_params == false
+      return EngineError.new('Missing Parameters ' + required_params.to_s + ' but only have:' + params.to_s, :error,'api') if r_params == false
       a_params.merge!(r_params)
     end
     
