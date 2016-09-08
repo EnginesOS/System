32a787476e531da075b0b5c88d5fb3cdc24c36f2
James L Vodanovich <james@jvodan.com>
2016-09-08 12:50:54 +1000

    k

--- src/server/api/containers/service/configurations.rb
+++ src/server/api/containers/service/configurations.rb
@@ -30,7 +30,7 @@ end
 # @return [true]  apply service configuration Hash
 post '/v0/containers/service/:service_name/configuration/:configurator_name' do
   p_params = post_params(request)
-  cparams =  Utils::Params.assemble_params(p_params, [], [:variables,:service_name, :configurator_name])
+  cparams =  Utils::Params.assemble_params(p_params, [:service_name, :configurator_name], [:variables])
   return log_error(request, cparams, p_params) if cparams.is_a?(EnginesError)
   r = engines_api.update_service_configuration(cparams)
   return log_error(request, r, service.last_error) if r.is_a?(FalseClass) || r.is_a?(EnginesError)
