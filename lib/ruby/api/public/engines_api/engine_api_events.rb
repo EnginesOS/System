16716d6490150399913df89fe64258604090acb0
James L Vodanovich <james@jvodan.com>
2016-09-09 15:54:48 +1000

    f

--- lib/ruby/api/public/engines_api/engine_api_events.rb
+++ lib/ruby/api/public/engines_api/engine_api_events.rb
@@ -30,8 +30,8 @@ rescue StandardError => e
   def stop
     STDERR.puts('RM levent List')
     @system_api.rm_event_listener(self)
-    @wr.close if @wr.open?
-    @rd.close if @rd.open?
+    @wr.close if @wr.is_open?
+    @rd.close if @rd.is_open?
   end
   
   end
