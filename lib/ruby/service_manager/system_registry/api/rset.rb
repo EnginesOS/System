1ede4b74c34156f7a7c528a326475f1e16c5cf8d
James L Vodanovich <james@jvodan.com>
2016-09-09 18:07:54 +1000

    tidy

--- lib/ruby/service_manager/system_registry/api/rset.rb
+++ lib/ruby/service_manager/system_registry/api/rset.rb
@@ -17,7 +17,6 @@ end
 
 def rest_post(path,params)
   begin
-    #STDERR.puts('Post Path:' + path.to_s + ' Params:' + params.to_s)
     parse_rest_response(RestClient.post(base_url + path, params))
     rescue RestClient::ExceptionWithResponse => e   
       parse_error(e.response)
@@ -28,7 +27,6 @@ end
 
 def rest_put(path,params)
   begin
-    #  STDERR.puts('Put Path:' + path.to_s + ' Params:' + params.to_s)
     parse_rest_response(RestClient.put(base_url + path, params))
     rescue RestClient::ExceptionWithResponse => e      
       parse_error(e.response)
@@ -50,11 +48,8 @@ end
 private
 
 def parse_error(r)
-  r.strip!# (/^\n/,'')
- # STDERR.puts("RSPONSE:" +r.to_s)
-
-  res = json_parser.parse(r)#, :create_additions => true,:symbolize_keys => true)
-  #STDERR.puts("RSPONSE:" + res.to_s)
+  r.strip!
+  res = json_parser.parse(r)
   EnginesRegistryError.new(res)
   rescue  StandardError => e
   STDERR.puts(r.to_s)
@@ -69,92 +64,91 @@ def parse_rest_response(r)
   return true if r.to_s   == '' ||  r.to_s   == 'true'
   return false if r.to_s  == 'false'
   r.strip!
-  res = json_parser.parse(r)#, :create_additions => true,:symbolize_keys => true)
-  # STDERR.puts("RESPONSE "  + deal_with_jason(res).to_s)
-  return res #deal_with_jason(res)
+  res = json_parser.parse(r)
+  return res 
 rescue  StandardError => e
   STDERR.puts e.to_s
   STDERR.puts e.backtrace
   STDERR.puts "Failed to parse rest response _" + r.to_s + "_"
   return log_exception(e, r)
 end
-
-def deal_with_jason(res)
-  return symbolize_keys(res) if res.is_a?(Hash)
-  return symbolize_keys_array_members(res) if res.is_a?(Array)
-  return symbolize_tree(res) if res.is_a?(Tree::TreeNode)
-  return boolean_if_true_false_str(res) if res.is_a?(String)
-  return res
-rescue  StandardError => e
-  log_exception(e, res)
-end
-
-def boolean_if_true_false_str(r)
-  if  r == 'true'
-    return true
-  elsif r == 'false'
-    return false
-  end
-  return r
-rescue  StandardError => e
-  log_exception(e, r)
-end
-
-def symbolize_keys(hash)
-  hash.inject({}){|result, (key, value)|
-    new_key = case key
-    when String then key.to_sym
-    else key
-    end
-    new_value = case value
-    when Hash then symbolize_keys(value)
-    when Array then
-      newval = []
-      value.each do |array_val|
-        array_val = symbolize_keys(array_val) if array_val.is_a?(Hash)
-        array_val =  boolean_if_true_false_str(array_val) if array_val.is_a?(String)
-        newval.push(array_val)
-      end
-      newval
-    when String then
-      boolean_if_true_false_str(value)
-    else value
-    end
-    result[new_key] = new_value
-    result
-  }
-rescue  StandardError => e
-log_exception(e, hash)
-end
-
-def symbolize_keys_array_members(array)
-  return array if array.count == 0
-  return array unless array[0].is_a?(Hash)
-  retval = []
-  i = 0
-  array.each do |hash|
-    retval[i] = array[i]
-    next if hash.nil?
-    next unless hash.is_a?(Hash)
-    retval[i] = symbolize_keys(hash)
-    i += 1
-  end
-  return retval
-
-rescue  StandardError => e
-log_exception(e)
-end
-
-def symbolize_tree(tree)
-  nodes = tree.children
-  nodes.each do |node|
-    node.content = symbolize_keys(node.content) if node.content.is_a?(Hash)
-    symbolize_tree(node)
-  end
-  return tree
-rescue  StandardError => e
-  log_exception(e)
-end
+#
+#def deal_with_jason(res)
+#  return symbolize_keys(res) if res.is_a?(Hash)
+#  return symbolize_keys_array_members(res) if res.is_a?(Array)
+#  return symbolize_tree(res) if res.is_a?(Tree::TreeNode)
+#  return boolean_if_true_false_str(res) if res.is_a?(String)
+#  return res
+#rescue  StandardError => e
+#  log_exception(e, res)
+#end
+#
+#def boolean_if_true_false_str(r)
+#  if  r == 'true'
+#    return true
+#  elsif r == 'false'
+#    return false
+#  end
+#  return r
+#rescue  StandardError => e
+#  log_exception(e, r)
+#end
+#
+#def symbolize_keys(hash)
+#  hash.inject({}){|result, (key, value)|
+#    new_key = case key
+#    when String then key.to_sym
+#    else key
+#    end
+#    new_value = case value
+#    when Hash then symbolize_keys(value)
+#    when Array then
+#      newval = []
+#      value.each do |array_val|
+#        array_val = symbolize_keys(array_val) if array_val.is_a?(Hash)
+#        array_val =  boolean_if_true_false_str(array_val) if array_val.is_a?(String)
+#        newval.push(array_val)
+#      end
+#      newval
+#    when String then
+#      boolean_if_true_false_str(value)
+#    else value
+#    end
+#    result[new_key] = new_value
+#    result
+#  }
+#rescue  StandardError => e
+#log_exception(e, hash)
+#end
+#
+#def symbolize_keys_array_members(array)
+#  return array if array.count == 0
+#  return array unless array[0].is_a?(Hash)
+#  retval = []
+#  i = 0
+#  array.each do |hash|
+#    retval[i] = array[i]
+#    next if hash.nil?
+#    next unless hash.is_a?(Hash)
+#    retval[i] = symbolize_keys(hash)
+#    i += 1
+#  end
+#  return retval
+#
+#rescue  StandardError => e
+#log_exception(e)
+#end
+#
+#def symbolize_tree(tree)
+#  nodes = tree.children
+#  nodes.each do |node|
+#    node.content = symbolize_keys(node.content) if node.content.is_a?(Hash)
+#    symbolize_tree(node)
+#  end
+#  return tree
+#rescue  StandardError => e
+#  log_exception(e)
+#end
 
 def base_url
   'http://' + @core_api.get_registry_ip + ':4567'
