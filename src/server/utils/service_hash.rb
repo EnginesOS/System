11eda0fa8b1897de750c8bcc7bd8f6d4f7d190cb
James L Vodanovich <james@jvodan.com>
2016-09-15 14:17:40 +1000

    drop debug

--- src/server/utils/service_hash.rb
+++ src/server/utils/service_hash.rb
@@ -1,11 +1,8 @@
 module ServiceHash
   def self.service_hash_from_params(params, search)
     splats = params['splat']
-    p :params
-        p params
     hash = {}
     hash[:publisher_namespace] = params['publisher_namespace']
-
     unless search
       hash[:type_path] = File.dirname(splats[0])
       hash[:service_handle] = File.basename(splats[0])
@@ -16,8 +13,6 @@ module ServiceHash
   end
 
   def self.engine_service_hash_from_params(params, search = false)
-    p :params
-    p params
     hash = self.service_hash_from_params(params, search)
     hash[:parent_engine] = params['engine_name']
     hash[:container_type] = 'container'
@@ -25,8 +20,6 @@ module ServiceHash
   end
 
   def self.service_service_hash_from_params(params, search = false)
-    p :params
-    p params
     hash = self.service_hash_from_params(params, search)
     hash[:parent_engine] = params['service_name']
     hash[:container_type] = 'service'
