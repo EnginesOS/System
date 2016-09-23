e5ff6d2196443881a683475f154b28580bf2fffe
James L Vodanovich <james@jvodan.com>
2016-09-23 18:50:13 +1000

    f

--- lib/ruby/api/system/containers/service_api/service_api_docker_actions.rb
+++ lib/ruby/api/system/containers/service_api/service_api_docker_actions.rb
@@ -1,8 +1,8 @@
 module  ServiceApiDockerActions
   def destroy_container(container)
-    r =''
+    r = super
     STDERR.puts('destroy service')
-return r (r = super) unless r == true
+return r unless r == true
     @engines_core.clear_service_from_registry(self, :non_persistent)
   rescue StandardError => e
     container.last_error = 'Failed To Destroy ' + e.to_s
