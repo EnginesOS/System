94b52153359edaff411fac2146a51c1aa1a3ebff
James L Vodanovich <james@jvodan.com>
2016-09-23 11:54:09 +1000

    tidy

--- lib/ruby/api/system/build_controller.rb
+++ lib/ruby/api/system/build_controller.rb
@@ -27,7 +27,7 @@ class BuildController
   
   def build_engine(params)
     SystemDebug.debug(SystemDebug.builder, :builder_params, params)
-    return r if (r = @engine_builder.check_build_params(params)).is_a?(EnginesError)
+    STDERR.puts('Build param error ' + r.to_s) if (r = @engine_builder.check_build_params(params)).is_a?(EnginesError)
     @build_params = params
     SystemStatus.build_starting(@build_params)
     @engine_builder = get_engine_builder(@build_params)
--- lib/ruby/engine_builder/check_build_params.rb
+++ lib/ruby/engine_builder/check_build_params.rb
@@ -3,19 +3,21 @@ module CheckBuildParams
     return r unless (r = check_name(params)).is_a?(TrueClass)
     return r unless (r = check_host(params)).is_a?(TrueClass)    
     return true
+  rescue StandardError => e
+    log_exception(e)
   end
   
   private 
   
   def check_host(params)
-    return bad_param('Missing: Hostname', params) unless params.key?(:engine_name)
-    return bad_param('Invalid: Hostname', params) unless acceptable_chars(params[:engine_name])
+    return bad_param('Missing: Hostname', params) unless params.key?(:host_name)
+    return bad_param('Invalid: Hostname', params) unless acceptable_chars(params[:host_name])
     return true
   end
   
   def check_name(params)
     return bad_param('Missing: Engine Name', params) unless params.key?(:engine_name)
-    return bad_param('Invalid: Engine Name', params) unless  acceptable_chars(params[:engine_name])
+    return bad_param('Invalid: Engine Name', params) unless acceptable_chars(params[:engine_name])
     return true
   end
   
@@ -25,6 +27,7 @@ module CheckBuildParams
   end
   
   def acceptable_chars(str)
+    return false if str.nil?
     return false if str.match(/^[0-9]/)
     return true if str.match(/^[a-zA-Z0-9]+$/)
     return false
