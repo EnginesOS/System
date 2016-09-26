c7e36766985e20fb037ea11ecdfc0c7521cd95a7
James L Vodanovich <james@jvodan.com>
2016-09-26 10:29:25 +1000

    only add dns etc on first run

--- lib/ruby/containers/managed_container/managed_container_on_action.rb
+++ lib/ruby/containers/managed_container/managed_container_on_action.rb
@@ -1,62 +1,65 @@
 module ManagedContainerOnAction
-  
   def on_start(what)
     @container_mutex.synchronize {
-    SystemDebug.debug(SystemDebug.container_events,:ONSTART_CALLED,what)
-    @out_of_memory = false
-    save_state
+      SystemDebug.debug(SystemDebug.container_events,:ONSTART_CALLED,what)
+      @out_of_memory = false
+      save_state
       return true if @consumer_less
-   #return if what == 'create'
-    register_with_dns # MUst register each time as IP Changes   
-    add_nginx_service if @deployment_type == 'web' 
-    @container_api.register_non_persistent_services(self)
-    true
+      #return if what == 'create'
+      if  @has_run == false
+        register_with_dns # MUst register post each create as IP Changes
+        add_nginx_service if @deployment_type == 'web'
+      end
+      @has_run = true
+      @container_api.register_non_persistent_services(self)
+      true
     }
-    rescue StandardError => e
-       log_exception(e)
+  rescue StandardError => e
+    log_exception(e)
   end
-  
+
   def on_create(event_hash)
     @container_mutex.synchronize {
-      SystemDebug.debug(SystemDebug.container_events,:ON_Create_CALLED,event_hash)    
-        @container_id = event_hash[:id]
+      SystemDebug.debug(SystemDebug.container_events,:ON_Create_CALLED,event_hash)
+      @container_id = event_hash[:id]
       @out_of_memory = false
-      @had_out_memory =false
-          save_state
-          return true if @consumer_less
-       #return if what == 'create'
-        register_with_dns # MUst register each time as IP Changes    
-        
-       # @container_api.register_non_persistent_services(self)
-    SystemDebug.debug(SystemDebug.container_events,:ON_Create_Finised,event_hash)   
-    true 
-    }
-    rescue StandardError => e
-       log_exception(e)
-      end
-      
-    def on_stop(what)
-
-      SystemDebug.debug(SystemDebug.container_events,:ONStop_CALLED,what)
-      @had_out_memory = @out_of_memory
-      @out_of_memory = false
+      @had_out_memory = false
+      @has_run = false
       save_state
       return true if @consumer_less
-     deregister_with_dns # MUst register each time as IP Changes    
-      @container_api.deregister_non_persistent_services(self)
-      true
-      rescue StandardError => e
-         log_exception(e)
-    end
-    
-    def out_of_mem(what)
+      #return if what == 'create'
+      register_with_dns # MUst register each time as IP Changes
 
-      SystemDebug.debug(SystemDebug.container_events,:OUTOF_MEM_CALLED,what)
-      @out_of_memory = true
-      @had_out_memory = true    
-      save_state
-      rescue StandardError => e
-         log_exception(e)
-    end
-    
+      # @container_api.register_non_persistent_services(self)
+      SystemDebug.debug(SystemDebug.container_events,:ON_Create_Finised,event_hash)
+      true
+    }
+  rescue StandardError => e
+    log_exception(e)
+  end
+
+  def on_stop(what)
+
+    SystemDebug.debug(SystemDebug.container_events,:ONStop_CALLED,what)
+    @had_out_memory = @out_of_memory
+    @out_of_memory = false
+    save_state
+    return true if @consumer_less
+    deregister_with_dns # MUst register each time as IP Changes
+    @container_api.deregister_non_persistent_services(self)
+    true
+  rescue StandardError => e
+    log_exception(e)
+  end
+
+  def out_of_mem(what)
+
+    SystemDebug.debug(SystemDebug.container_events,:OUTOF_MEM_CALLED,what)
+    @out_of_memory = true
+    @had_out_memory = true
+    save_state
+  rescue StandardError => e
+    log_exception(e)
+  end
+
 end
