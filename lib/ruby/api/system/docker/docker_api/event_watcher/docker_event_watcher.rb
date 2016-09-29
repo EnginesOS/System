0bb963bd508e9f5a82447914015468c41faa5f69
James L Vodanovich <james@jdbgvodan.com>
2016-09-29 16:12:48 +1000

    dbg

--- lib/ruby/api/system/docker/docker_api/event_watcher/docker_event_watcher.rb
+++ lib/ruby/api/system/docker/docker_api/event_watcher/docker_event_watcher.rb
@@ -162,7 +162,6 @@ class DockerEventWatcher  < ErrorsApi
   end
 
   def add_event_listener(listener, event_mask = nil, container_id = nil)
-
     event = EventListener.new(listener,event_mask, container_id)
     @event_listeners[event.hash_name] = event
     SystemDebug.debug(SystemDebug.container_events,'ADDED listenter ' + listener.class.name + ' Now have ' + @event_listeners.keys.count.to_s + ' Listeners ')
