bfda564b367d25271387e3381504a46d888a468b
James L Vodanovich <james@jvodan.com>
2016-09-29 14:18:04 +1000

    tidy

--- src/client/main.rb
+++ src/client/main.rb
@@ -1,10 +1,9 @@
-
 if Process.euid != 21000
   STDERR.puts("This program can only be run be the engines user")
   exit
 end
 require 'rubygems'
- require 'excon'
+require 'excon'
 require 'json'
 require 'yajl'
 
@@ -14,8 +13,6 @@ def command_useage(mesg=nil)
   exit
 end
 
-
-
 def parse_rest_response(r)
   return false if r.code > 399
   return true if r.to_s   == '' ||  r.to_s   == 'true'
@@ -23,7 +20,7 @@ def parse_rest_response(r)
   return r.to_s if @raw
   parser = Yajl::Parser.new()
   res = parser.parse(r)
- # res = JSON.parse(r, :create_additions => true,:symbolize_keys => true)
+  # res = JSON.parse(r, :create_additions => true,:symbolize_keys => true)
   # STDERR.puts("RESPONSE "  + deal_with_jason(res).to_s)
   return deal_with_jason(res)
 rescue  StandardError => e
@@ -81,8 +78,6 @@ def perform_post(params, content_type='application/json')
   exit
 end
 
-
-
 def perform_delete(params=nil)
   #STDERR.puts  @route
   rest_delete(@route,params)
@@ -91,8 +86,7 @@ end
 
 require 'rest-client'
 
-
-#used by events 
+#used by events
 def get_json_stream(path)
   require 'yajl'
   chunk = ''
@@ -110,20 +104,20 @@ def get_json_stream(path)
           hash = parser.parse(chunk) do |hash|
             p hash.to_json
           end
-          #dont panic on bad json as it is the \0 keep alive 
-        rescue StandardError => e 
+          #dont panic on bad json as it is the \0 keep alive
+        rescue StandardError => e
           p e
           STDERR.puts('_'+ chunk + '_')
           next
         end
       end
-      
+
     }
     exit
   end
 rescue StandardError => e
   #Should goto to error hanlder but this is a script
-  p e.to_s  
+  p e.to_s
   p e.backtrace.to_s
 end
 
@@ -135,8 +129,8 @@ def get_stream(path, ostream=STDOUT)
   uri = URI(@base_url + path)
   req = Net::HTTP::Get.new(uri)
   req['Access_Token'] = ENV['access_token']
-  
-  Net::HTTP.start(uri.host, uri.port)  do |http|  
+
+  Net::HTTP.start(uri.host, uri.port)  do |http|
     http.read_timeout = 600
     http.request(req) { |resp|
       resp.read_body do |chunk|
@@ -168,59 +162,61 @@ def connection(content_type = 'application/json')
   headers = {}
   headers['content_type'] = content_type
   headers['ACCESS_TOKEN'] = load_token
-   @connection = Excon.new(@base_url,
-                           :debug_request => true,
-                           :debug_response => true,
-                           :persistent => true,
-                           :headers => headers) if @connection.nil?
-    @connection
-  rescue StandardError => e
-    STDERR.puts('Failed to open base url ' + @base_url.to_s)
- end
+  @connection = Excon.new(@base_url,
+  :debug_request => true,
+  :debug_response => true,
+  :persistent => true,
+  :headers => headers) if @connection.nil?
+  @connection
+rescue StandardError => e
+  STDERR.puts('Failed to open base url ' + @base_url.to_s)
+end
+
 def rest_del(uri,params=nil)
-  
-if params.nil?
-connection.request(:method => :delete,:path => uri) #,:body => params.to_json)
-else
-  connection.request(:method => :delete,:path => uri,:body => params.to_json)
+
+  if params.nil?
+    connection.request(:method => :delete,:path => uri) #,:body => params.to_json)
+  else
+    connection.request(:method => :delete,:path => uri,:body => params.to_json)
+  end
+rescue StandardError => e
+
+  STDERR.puts e.to_s + ' delete with path:' + uri + "\n" + 'params:' + params.to_s
+  STDERR.puts e.backtrace.to_s
 end
-    rescue StandardError => e
-  
-      STDERR.puts e.to_s + ' delete with path:' + uri + "\n" + 'params:' + params.to_s
-      STDERR.puts e.backtrace.to_s
- end
+
 def rest_get(uri,params=nil,time_out=120)
-  
-if params.nil?
-connection.request(:read_timeout => time_out,:method => :get,:path => uri) #,:body => params.to_json)
-else
-  connection.request(:read_timeout => time_out,:method => :get,:path => uri,:body => params.to_json)
+
+  if params.nil?
+    connection.request(:read_timeout => time_out,:method => :get,:path => uri) #,:body => params.to_json)
+  else
+    connection.request(:read_timeout => time_out,:method => :get,:path => uri,:body => params.to_json)
+  end
+rescue StandardError => e
+
+  STDERR.puts e.to_s + ' with path:' + uri + "\n" + 'params:' + params.to_s
+  STDERR.puts e.backtrace.to_s
 end
-    rescue StandardError => e
-  
-      STDERR.puts e.to_s + ' with path:' + uri + "\n" + 'params:' + params.to_s
-      STDERR.puts e.backtrace.to_s
- end
- 
+
 def handle_resp(resp, expect_json=true)
   parser = Yajl::Parser.new()
 
   #STDERR.puts(" RESPOSE " + resp.to_s)
   # STDERR.puts(" RESPOSE " + resp.status.to_s + " : " + resp.body  )
-   STDERR.puts("error:" + resp.status.to_s)  if resp.status  >= 400
-   return 'OK' if resp.status  == 204   # nodata but all good happens on del
-STDERR.puts "Un exepect response from system" + resp.status.to_s + ' ' + resp.body.to_s + ' ' + resp.headers.to_s    unless resp.status  == 200 ||  resp.status  == 201 || resp.status  == 202
-return resp.body.to_s unless expect_json == true
-   hashes = []
-   parser.parse(resp.body) do |hash |
-     hashes.push(hash)
-   end
+  STDERR.puts("error:" + resp.status.to_s)  if resp.status  >= 400
+  return 'OK' if resp.status  == 204   # nodata but all good happens on del
+  STDERR.puts "Un exepect response from system" + resp.status.to_s + ' ' + resp.body.to_s + ' ' + resp.headers.to_s    unless resp.status  == 200 ||  resp.status  == 201 || resp.status  == 202
+  return resp.body.to_s unless expect_json == true
+  hashes = []
+  parser.parse(resp.body) do |hash |
+    hashes.push(hash)
+  end
   return hashes[0].to_json
- rescue StandardError => e
-STDERR.puts e.to_s + ' with :' + resp.to_s
-   STDERR.puts e.backtrace.to_s
- end
- 
+rescue StandardError => e
+  STDERR.puts e.to_s + ' with :' + resp.to_s
+  STDERR.puts e.backtrace.to_s
+end
+
 #def rest_get(path,params=nil)
 #
 #  begin
@@ -244,14 +240,14 @@ def write_response(r)
     STDERR.puts 'nil response'
     return
   end
- #STDERR.puts( 'RESPONSE HEADER ' + r.headers.to_s)
+  #STDERR.puts( 'RESPONSE HEADER ' + r.headers.to_s)
   if r.headers['Content-Type'] == 'application/octet-stream'
     STDOUT.write( r.body.b)
     # STDERR.puts "as_binary"
   else
     expect_json=false
-   expect_json=true if r.body.start_with?('{')
-   puts handle_resp(r, expect_json)
+    expect_json=true if r.body.start_with?('{')
+    puts handle_resp(r, expect_json)
   end
 
 end
@@ -259,17 +255,17 @@ end
 def rest_post(uri, params, content_type,time_out = 120 )
 
   begin
-   unless params.nil?   
-  r =  connection(content_type).request(:read_timeout => time_out,:method => :post,:path => uri, :body => params.to_json) #,:body => params.to_json)
-   else
-     r =  connection(content_type).request(:read_timeout => time_out,:method => :post,:path => uri)
-   end
+    unless params.nil?
+      r =  connection(content_type).request(:read_timeout => time_out,:method => :post,:path => uri, :body => params.to_json) #,:body => params.to_json)
+    else
+      r =  connection(content_type).request(:read_timeout => time_out,:method => :post,:path => uri)
+    end
     write_response(r)
     exit
-        rescue StandardError => e
-      
-   STDERR.puts e.to_s + ' with path:' + uri + "\n" + 'params:' + params.to_s
-   STDERR.puts e.backtrace.to_s
+  rescue StandardError => e
+
+    STDERR.puts e.to_s + ' with path:' + uri + "\n" + 'params:' + params.to_s
+    STDERR.puts e.backtrace.to_s
 
     params[:api_vars][:data] = nil
     STDERR.puts e.to_s + ' with path:' + uri + "\n" + 'params:' + params.to_s
@@ -278,10 +274,10 @@ def rest_post(uri, params, content_type,time_out = 120 )
 end
 
 def rest_delete(uri, params=nil)
- # params = add_access(params)
+  # params = add_access(params)
   begin
-    if params.nil?  
-  r =  connection.request(:method => :delete,:path => uri) #,:body => params.to_json)
+    if params.nil?
+      r =  connection.request(:method => :delete,:path => uri) #,:body => params.to_json)
     else
       r =  connection.request(:method => :delete,:path => uri,:body => params.to_json)
     end
@@ -298,7 +294,7 @@ def login
   ENV['access_token'] = r.body.gsub!(/\"/,'')
   t = File.new(Dir.home + '/.engines_token','w+')
   t.write(ENV['access_token'])
-  t.close  
+  t.close
 end
 
 def load_token
@@ -309,22 +305,20 @@ def load_token
 end
 
 def  process_args
-  
+
 end
 
-@base_url = 'http://' + ENV['DOCKER_IP'] + ':2380'
 @host = ENV['DOCKER_IP']
-  @host = '127.0.0.1' if @host.length < 3
+@host = '127.0.0.1' if @host.length < 3
+@base_url = 'http://' +  @host + ':2380'
 @port = '2380'
 @route = "/v0"
 
 load_token
-
 login if ENV['access_token'].nil?
 
- process_args
-  
-  
+process_args
+
 require_relative 'commands/commands.rb'
 
 #require_relative 'rset.rb'
