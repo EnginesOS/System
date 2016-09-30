37fbe962a86f3f1524f8fe851f70c145b4de66d9
James L Vodanovich <james@jvodan.com>
2016-09-30 19:08:27 +1000

    utf8?

--- lib/ruby/api/system/docker/docker_api/docker_utils.rb
+++ lib/ruby/api/system/docker/docker_api/docker_utils.rb
@@ -134,8 +134,8 @@ module DockerUtils
 
     # This is actually set elsewhere after exec complete
     h[:result] = 0
-h[:stdout].force_encoding(Encoding::ASCII_8BIT) unless h[:stdout].nil?
-h[:stderr].force_encoding(Encoding::ASCII_8BIT) unless h[:stderr].nil?
+h[:stdout].force_encoding(Encoding::UTF_8) unless h[:stdout].nil?
+h[:stderr].force_encoding(Encoding::UTF_8) unless h[:stderr].nil?
 h
 rescue StandardError =>r
   log_exception(e)
