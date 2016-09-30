854dbcd2376a4cdfa314474ce5f8623911605d59
James L Vodanovich <james@jvodan.com>
2016-09-30 13:21:29 +1000

    tidy

--- system/templates/deployment/rails5/build_scripts/run_rake_task.sh
+++ system/templates/deployment/rails5/build_scripts/run_rake_task.sh
@@ -2,10 +2,3 @@
 
 
 /usr/local/rbenv/shims/bundle exec rake $*
-
-#    for task in $*
- #    do
-  #   mod=`echo $task | sed "/[;&]/s///g"`
-   #  echo "/usr/local/rbenv/shims/bundle exec rake $task"
-    # /usr/local/rbenv/shims/bundle exec rake $task
-     #done
