b36450f1f4264ef5267330a5554656e274893c81
James L Vodanovich <james@jvodan.com>
2016-10-12 12:31:11 +1100

    false  ||'false'

--- src/server/api/unauthenticated/routes.rb
+++ src/server/api/unauthenticated/routes.rb
@@ -44,6 +44,6 @@ post '/v0/unauthenticated/bootstrap/first_run/complete' do
   p_params = post_params(request)
   cparams =  Utils::Params.assemble_params(p_params, [], :all)
     i = true
-    i = false if cparams[:install_mgmt] == 'false'
+    i = false if cparams[:install_mgmt] == 'false' || cparams[:install_mgmt] == false
   engines_api.first_run_complete(i)  
 end
