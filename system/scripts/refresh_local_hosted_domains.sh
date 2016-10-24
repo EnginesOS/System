69e1a5f93bb90b7781d9ca7ad941ae5dc018cf7a
James L Vodanovich <james@jvodan.com>
2016-10-24 13:51:51 +1100

    update refresh domains

--- system/scripts/refresh_local_hosted_domains.sh
+++ system/scripts/refresh_local_hosted_domains.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
- docker exec dns /home/actionators/refresh_hosted_domains.sh :ip_type=lan:ip=$1:
+docker exec dns /home/actionators/refresh_hosted_domains.sh lan $1
  
-#su engines -c /opt/engines/system/scripts/get_ip.sh
 
 if test `id -u` -eq 21000
  then
- 	/opt/engines/scripts/restart_avahi.sh
+ 	/opt/engines/system/scripts/restart_avahi.sh
  else
-	su engines -l /opt/engines/scripts/restart_avahi.sh
+	su engines -l /opt/engines/system/scripts/restart_avahi.sh
 fi
