d3e1dfb044da6121697675ca3cdd44d66ce71b5b
James L Vodanovich <james@jvodan.com>
2016-10-27 14:16:33 +1100

    f

--- system/scripts/ssh/set_hostname.sh
+++ system/scripts/ssh/set_hostname.sh
@@ -2,14 +2,15 @@
 
 params=`cat -`
 
-hostname=`echo $params | cut -f1 -d . `
-domainname=`echo $params | cut -f 2- d .`
+hostname=`echo $params | cut -f1 -d. `
+domainname=`echo $params | cut -f 2- -d.`
 
 if ! test -z $domainname
  then
-	sudo -n hostname $hostname
-else
 	sudo -n hostname $hostname.$domainname
+else
+	sudo -n hostname $hostname
+	
 fi
-echo $params >/tmp/set_hostname
+echo params: $params >/tmp/set_hostname
 echo hostname $hostname.$domainname >> /tmp/set_hostname
