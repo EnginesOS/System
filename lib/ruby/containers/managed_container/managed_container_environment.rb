f1b1f045bd8d77e6b634d8de78071be8de6bf765
James L Vodanovich <james@jvodan.com>
2016-10-31 15:07:33 +1100

    f

--- lib/ruby/containers/managed_container/managed_container_environment.rb
+++ lib/ruby/containers/managed_container/managed_container_environment.rb
@@ -4,8 +4,8 @@ module ManagedContainerEnvironment
     return log_error_mesg('No envionment varaibles') if @environments.nil?
     
     @environments.each do |environment| 
-      STDERR.puts(' Evn ' + environment.name.to_s  + ':' + environment.value.to_s + ' for ' + key)    
-    if environment.name == key
+      STDERR.puts(' Evn ' + environment.name.to_s  + ':' + environment.value.to_s + ' for ' + key.to_s)    
+    if environment.name == key.to_s
      
       SystemDebug.debug(SystemDebug.containers, :update_environment, "Changed")
       return log_error_mesg(' variable ' + environment.name + ' immutable' )  if environment.immutable == true
