ba89aaa1486840fc21135f5415ce213b0e40c759
James L Vodanovich <james@jvodan.com>
2016-11-02 10:21:36 +1100

    fi

--- src/server/api/containers/engine/services/persistent_services.rb
+++ src/server/api/containers/engine/services/persistent_services.rb
@@ -31,7 +31,7 @@ end
 
 post '/v0/containers/engine/:engine_name/services/persistent/share/:owner/:publisher_namespace/*' do
   p_params = post_params(request)
-  path_hash = Utils::ServiceHash.engine_service_hash_from_params(params, false)
+  path_hash = Utils::ServiceHash.engine_service_hash_from_params(params, true)
   STDERR.puts('ADD Share ' + path_hash.to_s )
   p_params.merge!(path_hash)
   STDERR.puts('ADD Share ' + p_params.to_s )
@@ -51,7 +51,7 @@ end
 
 post '/v0/containers/engine/:engine_name/services/persistent/orphan/:owner/:publisher_namespace/*' do
   p_params = post_params(request)
-  path_hash = Utils::ServiceHash.engine_service_hash_from_params(params, false)
+  path_hash = Utils::ServiceHash.engine_service_hash_from_params(params, true)
   p_params.merge!(path_hash)
   STDERR.puts('ADD ORPHAN ' + p_params.to_s )
   cparams =  Utils::Params.assemble_params(p_params, [:engine_name,:owner,:publisher_namespace, :type_path, :service_handle], :all)
