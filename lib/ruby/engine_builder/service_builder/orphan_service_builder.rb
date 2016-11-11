e7d6d275502d2295a5119900b98784f4b1091663
James L Vodanovich <james@jvodan.com>
2016-11-11 17:08:46 +1100

    f

--- lib/ruby/engine_builder/service_builder/orphan_service_builder.rb
+++ lib/ruby/engine_builder/service_builder/orphan_service_builder.rb
@@ -1,6 +1,7 @@
 module OrphansServiceBuilder
   def use_orphan(service_hash)
-    build_vars = service_hash[:variables].dup
+    chash = service_hash.dup
+    build_vars = chash[:variables]
     SystemDebug.debug(SystemDebug.orphans,:attaching_orphan, service_hash)
     service_hash = @core_api.retrieve_orphan(service_hash)
     SystemDebug.debug(SystemDebug.orphans, :retrieved_orphan, service_hash)
