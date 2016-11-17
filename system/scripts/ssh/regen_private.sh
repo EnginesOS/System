c1e4118cd01c88585f8855f1ab38f3bcbb7459f1
James L Vodanovich <james@jvodan.com>
2016-11-17 14:07:26 +1100

    fix

--- system/scripts/ssh/regen_private.sh
+++ system/scripts/ssh/regen_private.sh
@@ -3,7 +3,7 @@ if test -f /home/engines/.ssh/console_access
  then
 	rm /home/engines/.ssh/console_access
 fi 
-ssh-keygen  -P \"\" -f /home/engines/.ssh/console_access >/dev/null
+ssh-keygen  -P "" -f /home/engines/.ssh/console_access >/dev/null
 cat   /home/engines/.ssh/console_access.pub > /home/engines/.ssh/authorized_keys.console_access
 cat /home/engines/.ssh/authorized_keys.console_access /home/engines/.ssh/authorized_keys.system  > /home/engines/.ssh/authorized_keys
 chmod og-rw  /home/engines/.ssh/authorized_keys
