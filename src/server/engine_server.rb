f99fdc134b6fde32bf833c114a77d673c28c69d6
James L Vodanovich <james@jvodan.com>
2016-12-15 14:18:17 +1100

    init token

--- src/server/engine_server.rb
+++ src/server/engine_server.rb
@@ -26,12 +26,17 @@ begin
               guid int
             );
           SQL
+    rescue
       rows = @auth_db.execute( "select authtoken from systemaccess" )
       STDERR.puts('init db')
       return if rows.count > 0
       STDERR.puts('init db')
       @auth_db.execute("INSERT INTO systemaccess (username, password, email, authtoken, uid,guid) 
-                        VALUES (?, ?, ?, ?, ?, ?)", ["admin", 'test', '', 'test_token_arandy',1,0])
+                        VALUES (?, ?, ?, ?, ?, ?)", ["test", 'test', '', 'test_token_arandy',2,0])
+    rows                        
+      toke = SecureRandom.hex(128)
+    @auth_db.execute("INSERT INTO systemaccess (username, password, email, authtoken, uid,guid)      
+                          VALUES (?, ?, ?, ?, ?, ?)", ["admin", 'EnginesDemo', '', toke.to_s ,1,0])                   
       STDERR.puts('init db')                 
     rescue StandardError => e
       STDERR.puts('init db error ' + e.to_s)
