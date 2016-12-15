63e596c4ab480bd10d637f6d88212b9cd56669ab
James L Vodanovich <james@jvodan.com>
2016-12-15 14:37:52 +1100

    dbg

--- src/server/engine_server.rb
+++ src/server/engine_server.rb
@@ -31,9 +31,9 @@ begin
       STDERR.puts('init db')
       #return if rows.count > 0
       STDERR.puts('init db')
-      @auth_db.execute("INSERT INTO systemaccess (username, password, email, authtoken, uid,guid) 
-                        VALUES (?, ?, ?, ?, ?, ?)", ["test", 'test', '', 'test_token_arandy',2,0])
-    rows                        
+#      @auth_db.execute("INSERT INTO systemaccess (username, password, email, authtoken, uid,guid) 
+#                        VALUES (?, ?, ?, ?, ?, ?)", ["test", 'test', '', 'test_token_arandy',2,0])
+#    rows                        
       toke = SecureRandom.hex(128)
     @auth_db.execute("INSERT INTO systemaccess (username, password, email, authtoken, uid,guid)      
                           VALUES (?, ?, ?, ?, ?, ?)", ["admin", 'EnginesDemo', '', toke.to_s ,1,0])                   
