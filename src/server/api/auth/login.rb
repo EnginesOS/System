351c1f910dd7405fbe767447a35614966583bbe1
James L Vodanovich <james@jvodan.com>
2016-12-15 12:57:10 +1100

    f

--- src/server/api/auth/login.rb
+++ src/server/api/auth/login.rb
@@ -5,6 +5,7 @@
 # @overload get '/v0/system/login/:user_name/:password'
 # @return [String] Authentication token
 get '/v0/system/login/:user_name/:password' do 
+  @auth_db = SQLite3::Database.new "/home/app/db/production.sqlite3"
   rows = @auth_db.execute( 'select authtoken from systemaccess where username=' + "'" + params[:user_name].to_s + 
     "' and password = '" +  params[:password].to_s + "'")
 
