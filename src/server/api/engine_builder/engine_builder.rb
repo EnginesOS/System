1db87e95480dd2c3deaf1c44d5824ff493395c20
James L Vodanovich <james@jvodan.com>
2016-12-18 16:51:57 +1100

    co deb

--- src/server/api/engine_builder/engine_builder.rb
+++ src/server/api/engine_builder/engine_builder.rb
@@ -66,11 +66,11 @@ get '/v0/engine_builder/follow_stream', provides: 'text/event-stream'  do
           out  << bytes          
           out  << '.'          
           bytes = ''
-          STDERR.puts('FOLLOW BUILDER GOT first EOF')
+          #         STDERR.puts('FOLLOW BUILDER GOT first EOF')
           sleep 2
-          STDERR.puts('FOLLOW BUILDER GOT 3nd EOF')
-          IO.select([build_log_file])       
-          STDERR.puts('FOLLOW BUILDER GOT 2nd EOF')
+#          STDERR.puts('FOLLOW BUILDER GOT 3nd EOF')
+#          IO.select([build_log_file])       
+#          STDERR.puts('FOLLOW BUILDER GOT 2nd EOF')
           retry if File.exist?(SystemConfig.BuildRunningParamsFile)
           if build_over == false         
             build_over = true
