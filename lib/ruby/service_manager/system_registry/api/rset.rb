0285ff04e33b52ccf3f546c87959f15783f307d0
James L Vodanovich <james@jvodan.com>
2016-12-18 11:11:41 +1100

    typo

--- lib/ruby/service_manager/system_registry/api/rset.rb
+++ lib/ruby/service_manager/system_registry/api/rset.rb
@@ -100,7 +100,7 @@ def parse_xcon_response(resp)
   return true if r.to_s   == '' ||  r.to_s   == 'true'
   return false if r.to_s  == 'false'
 
-  res = JSON.parse(r.body, :create_additions => true,:symbolize_keys => true)
+  res = JSON.parse(r, :create_additions => true,:symbolize_keys => true)
   # STDERR.puts("RESPONSE "  + deal_with_jason(res).to_s)
   return deal_with_jason(res)
 rescue  StandardError => e
