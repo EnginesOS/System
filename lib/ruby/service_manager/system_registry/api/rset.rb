1954b33e35a97710d5889aa8cbc4391801d3784f
James L Vodanovich <james@jvodan.com>
2016-12-18 14:28:23 +1100

    ty[o

--- lib/ruby/service_manager/system_registry/api/rset.rb
+++ lib/ruby/service_manager/system_registry/api/rset.rb
@@ -12,7 +12,7 @@ def connection(content_type = 'application/json')
  
   if @connection.nil?
     STDERR.puts('NEW REGISTRY CONNECTION ')
-  connection = Excon.new(base_url,
+  @connection = Excon.new(base_url,
   :debug_request => true,
   :debug_response => true,
   :ssl_verify_peer => false,
