d9966cb2c25e4821f26ab5abb9f19b7f7cfde15e
James L Vodanovich <james@jvodan.com>
2016-12-18 13:57:21 +1100

    rm db

--- src/client/client_login.rb
+++ src/client/client_login.rb
@@ -1,5 +1,5 @@
 def login
-  r = rest_get('/v0/system/login/admin/test')
+  r = rest_get('/v0/system/login/admin/EnginesDemo')
   puts r
   ENV['access_token'] = r.body.gsub!(/\"/,'')
   t = File.new(Dir.home + '/.engines_token','w+')
--- src/client/commands/system/login.rb
+++ src/client/commands/system/login.rb
@@ -1,4 +1,4 @@
 
 @route += '/'+ ARGV[2] + '/' + ARGV[3]
-puts @route 
+
 perform_get()
