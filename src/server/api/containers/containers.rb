c916c05a0fcd4e3950f412c5d8233a865b9539a3
James L Vodanovich <james@jvodan.com>
2017-01-16 14:50:48 +1100

    gu

--- src/server/api/containers/containers.rb
+++ src/server/api/containers/containers.rb
@@ -128,11 +128,13 @@ get '/v0/containers/events/stream', provides: 'text/event-stream' do
   
      begin
     STDERR.puts('REQUEST TO  /v0/containers/events/stream')
-     
-    stream :keep_open do |out  |
-      begin
-        #      STDERR.puts('OPEN EVENT STREAM')
        events_stream = engines_api.container_events_stream
+    stream :keep_open do |v[o,events_stream]  |
+      begin
+        out = v[0]
+       events_stream = v[1]
+        #      STDERR.puts('OPEN EVENT STREAM')
+        # events_stream = engines_api.container_events_stream
         save_curr_events_stream(events_stream )
         has_data = true
         while has_data == true
