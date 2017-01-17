fc6af439aab5358e8253d97d96bf5a6960675200
James L Vodanovich <james@jvodan.com>
2017-01-17 14:11:39 +1100

    use systemd to start/stop engines

--- sbin/engines_startup.sh
+++ sbin/engines_startup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
 
 /usr/sbin/service docker status |/usr/bin/grep running > /dev/null
 if ! /usr/bin/test $? -eq 0
  then
-	/usr/bin/apt-get upgrade -y linux-image-extra-$(uname -r) 
+	/usr/bin/apt-get  -y  upgrade linux-image-extra-$(uname -r) 
 	/usr/sbin/service docker start
  fi
 
