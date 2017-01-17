2f3a7e0de2b5078f85e1acbe890db588700c0213
James L Vodanovich <james@jvodan.com>
2017-01-17 13:36:32 +1100

    add paths

--- sbin/engines_startup.sh
+++ sbin/engines_startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-service docker status |grep running > /dev/null
-if ! test $? -eq 0
+/usr/sbin/service docker status |/usr/bin/grep running > /dev/null
+if ! /usr/bin/test $? -eq 0
  then
-	apt-get upgrade -y linux-image-extra-$(uname -r) 
-	service docker start
+	/usr/bin/apt-get upgrade -y linux-image-extra-$(uname -r) 
+	/usr/sbin/service docker start
  fi
 
-su  engines /opt/engines/bin/_engines_startup.sh
+/bin/su  engines /opt/engines/sbin/_engines_startup.sh
