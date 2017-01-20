3ded50858d4cc0bb2e022ad4aef4351ed21c4979
James L Vodanovich <james@jvodan.com>
2017-01-20 16:05:36 +1100

    sep out event mask

--- lib/ruby/api/system/docker/docker_api/event_watcher/docker_event_watcher.rb
+++ lib/ruby/api/system/docker/docker_api/event_watcher/docker_event_watcher.rb
@@ -1,22 +1,6 @@
 class DockerEventWatcher  < ErrorsApi
   class EventListener
-    @@container_event = 1
-    @@engine_target  = 2
-    @@service_target = 4
-    @@container_exec = 8
-    @@container_action = 16
-    @@image_event = 32
-    @@container_commit = 64
-    @@container_delete = 128
-    @@container_kill = 256
-    @@container_die = 512
-    @@container_event = 1024
-    @@container_pull = 2048
-    @@build_event = 4096
-    @@container_attach  = 8192
-
-    @@service_action = @@container_action | @@service_target
-    @@engine_action = @@container_action | @@engine_target
+    
 
     attr_accessor :container_id, :event_mask
     # @@container_id
@@ -32,7 +16,7 @@ class DockerEventWatcher  < ErrorsApi
     end
 
     def trigger(hash)
-      mask = event_mask(hash)
+      mask = EventMask.event_mask(hash)
       STDERR.puts('trigger  mask ' + mask.to_s + ' hash ' + hash.to_s + ' listeners mask' + @event_mask.to_s)
       SystemDebug.debug(SystemDebug.container_events,'trigger  mask ' + mask.to_s + ' hash ' + hash.to_s + ' listeners mask' + @event_mask.to_s)
       return  if  @event_mask == 0 || mask & @event_mask == 0
@@ -67,59 +51,12 @@ class DockerEventWatcher  < ErrorsApi
       end
     end
 
-    def event_mask(event_hash)
-      mask = 0
-      if event_hash[:Type] = 'container'
-        mask |= @@container_event
-        if event_hash.key?(:from)
-          return  mask |= @@build_event if event_hash[:from].nil?
-          return  mask |= @@build_event if event_hash[:from].length == 64
-          if event_hash[:from].start_with?('engines/')
-            mask |= @@service_target
-          else
-            mask |= @@engine_target
-          end
-        end
-        return  0  if event_hash[:status].nil?
-
-        if event_hash[:status].start_with?('exec')
-          mask |= @@container_exec
-        elsif event_hash[:status] == 'delete'
-          mask |= @@container_delete
-        elsif event_hash[:status] == 'destroy'
-          mask |= @@container_delete | @@container_action
-        elsif event_hash[:status] == 'commit'
-          mask |= @@container_commit
-        elsif event_hash[:status] == 'pull'
-          mask |= @@container_pull
-          #        elsif event_hash['status'] == 'delete'
-          #          mask |= @@container_delete
-        elsif event_hash[:status] == 'die'
-          mask |= @@container_die
-        elsif event_hash[:status] == 'kill'
-          mask |= @@container_kill
-        elsif event_hash[:status] == 'attach'
-          mask |= @@container_attach
-        else
-          mask |= @@container_action
-        end
-      elsif event_hash[:Type] = 'image'
-        mask |= @@image_event
-      elsif event_hash[:Type] = 'network'
-        mask |= @@network_event
-      end
-
-      return mask
-      rescue StandardError => e
-           SystemDebug.debug(SystemDebug.container_events,event_hash.to_s + ':' + e.to_s + ':' +  e.backtrace.to_s)
-           return e
-    end
-  end
+end
   require 'yajl'
   require 'net_x/http_unix'
   require 'socket'
   require 'json'
-
+  require_relative 'event_mask.rb'
   def initialize(system, event_listeners = nil )
     @system = system
     # FIXMe add conntection watcher that re establishes connection asap and continues trying after warngin ....
@@ -263,6 +200,7 @@ end
 
     @event_listeners.values.each do |listener|
       unless listener.container_id.nil?
+        STDERR.puts('matching ' + listener.container_id.to_s)
         next unless hash[:id] == listener.container_id
       end
       log_exeception(r) if (r = listener.trigger(hash)).is_a?(StandardError)
---
+++ lib/ruby/api/system/docker/docker_api/event_watcher/event_mask.rb
@@ -0,0 +1,68 @@
+class EventMask
+  
+  @@container_event = 1
+  @@engine_target  = 2
+  @@service_target = 4
+  @@container_exec = 8
+  @@container_action = 16
+  @@image_event = 32
+  @@container_commit = 64
+  @@container_delete = 128
+  @@container_kill = 256
+  @@container_die = 512
+  @@container_event = 1024
+  @@container_pull = 2048
+  @@build_event = 4096
+  @@container_attach  = 8192
+
+  @@service_action = @@container_action | @@service_target
+  @@engine_action = @@container_action | @@engine_target
+  
+  def self.event_mask(event_hash)
+    mask = 0
+    if event_hash[:Type] = 'container'
+      mask |= @@container_event
+      if event_hash.key?(:from)
+        return  mask |= @@build_event if event_hash[:from].nil?
+        return  mask |= @@build_event if event_hash[:from].length == 64
+        if event_hash[:from].start_with?('engines/')
+          mask |= @@service_target
+        else
+          mask |= @@engine_target
+        end
+      end
+      return  0  if event_hash[:status].nil?
+
+      if event_hash[:status].start_with?('exec')
+        mask |= @@container_exec
+      elsif event_hash[:status] == 'delete'
+        mask |= @@container_delete
+      elsif event_hash[:status] == 'destroy'
+        mask |= @@container_delete | @@container_action
+      elsif event_hash[:status] == 'commit'
+        mask |= @@container_commit
+      elsif event_hash[:status] == 'pull'
+        mask |= @@container_pull
+        #        elsif event_hash['status'] == 'delete'
+        #          mask |= @@container_delete
+      elsif event_hash[:status] == 'die'
+        mask |= @@container_die
+      elsif event_hash[:status] == 'kill'
+        mask |= @@container_kill
+      elsif event_hash[:status] == 'attach'
+        mask |= @@container_attach
+      else
+        mask |= @@container_action
+      end
+    elsif event_hash[:Type] = 'image'
+      mask |= @@image_event
+    elsif event_hash[:Type] = 'network'
+      mask |= @@network_event
+    end
+
+    return mask
+    rescue StandardError => e
+         SystemDebug.debug(SystemDebug.container_events,event_hash.to_s + ':' + e.to_s + ':' +  e.backtrace.to_s)
+         return e
+  end
+end
