cf7fcce1aadfffb021ec5874de800d828c636336
James L Vodanovich <james@jvodan.com>
2017-02-08 13:15:53 +1100

    catch partials

--- lib/ruby/api/system/docker/docker_api/event_watcher/docker_event_watcher.rb
+++ lib/ruby/api/system/docker/docker_api/event_watcher/docker_event_watcher.rb
@@ -135,14 +135,21 @@ class DockerEventWatcher  < ErrorsApi
     parser = nil
 
     client.request(req) do |resp|
+      json_part = nil
       resp.read_body do |chunk|
         begin
           # parser = FFI_Yajl::Parser.new({:symbolize_keys => true}) if parser.nil?
           #   STDERR.puts('event  cunk ' + chunk.to_s )
           r = ''
           chunk.strip!
-          json_str = chunk
-          STDERR.puts('DOCKER SENT INCOMPLETE json ' + json_str.to_s ) unless json_str.end_with?('}')
+          chunk = json_part + chunk unless json_part.nil?
+          unless chunk.end_with?('}')
+          STDERR.puts('DOCKER SENT INCOMPLETE json ' + chunk.to_s )
+            json_part = chunk
+            next
+          else
+            json_part = nil
+          end 
           #      hash =  parser.parse(chunk)# do |hash|
           hash =  SystemUtils.deal_with_jason(JSON.parse(chunk, :create_additons => true ))
           next unless hash.is_a?(Hash)
