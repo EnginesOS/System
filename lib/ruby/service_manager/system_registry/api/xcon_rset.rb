d2b26a41319397e09fd849c3eb449677ed68e379
James L Vodanovich <james@jvodan.com>
2017-02-08 15:17:57 +1100

    delete content-type if not posting/query

--- lib/ruby/service_manager/system_registry/api/xcon_rset.rb
+++ lib/ruby/service_manager/system_registry/api/xcon_rset.rb
@@ -49,9 +49,10 @@ def rest_get(path,params = nil,time_out=120, _headers = nil)
   
   STDERR.puts(' GET ' + path.to_s + '?' + q.to_s )
 #  headers = {'Content-Type' =>'application/json', 'Accept' => '*/*'} if headers.nil?
-    q = {} if q.nil?
+  #  q = {} if q.nil?
     lheaders = headers
     lheaders.merge(_headers) unless _headers == nil
+  _headers.delete('Content-Type' ) if  q.nil?
 req = {:time_out => time_out,:method => :get,:path => path, :headers => lheaders }
   req[:query] = q unless q.nil?
 
