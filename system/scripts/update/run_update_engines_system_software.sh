63da72ee54f33b8870ea426d86d42a50216449e3
James L Vodanovich <james@jvodan.com>
2017-02-09 11:01:27 +1100

    f

--- system/scripts/update/run_update_engines_system_software.sh
+++ system/scripts/update/run_update_engines_system_software.sh
@@ -10,7 +10,7 @@ docker stop registry
 sleep 5
 
   /opt/engines/system/scripts/system/rotate_system_log.sh system >& /tmp/_rotate_system_log.system
- /opt/engines/system/scripts/system/rotate_system_log.sh registry   >& /tmp/rotate_system_log.registry
+# /opt/engines/system/scripts/system/rotate_system_log.sh registry   >& /tmp/rotate_system_log.registry
 echo "Restarting"
 
 docker start registry 
