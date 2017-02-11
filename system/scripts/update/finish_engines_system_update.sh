6d9aed5054cea4152c1cc37b0822576a5b200036
James L Vodanovich <james@jvodan.com>
2017-02-11 12:33:55 +1100

    f

--- system/scripts/update/finish_engines_system_update.sh
+++ system/scripts/update/finish_engines_system_update.sh
@@ -22,12 +22,12 @@ if ! test -d  $system_updates_dir/$update_id
   		for service in $services
   		 do
   		 	/opt/engines/bin/engines service $service stop >> $system_updates_dir/$update_id/update_log
-  		 	if ! test `/opt/engines/bin/engines service  $service state  |grep running |cut -f2 -d:` = "running"
-  		 	 then  		 	 
+  		 #	if  test `/opt/engines/bin/engines service  $service state  |grep running` == stopped 
+  		 #	 then  		 	 
   		 		image=`grep image /opt/engines/run/services/$service/running.yaml | cut -f2 -d" "`
   				docker pull $image
   		 		/opt/engines/bin/engines service $service recreate  >>  $system_updates_dir/$update_id/update_log
-  		 	fi
+  		 #	fi
   		 done
   fi
   
