adc4855ab0591f48358aab369f1630217a1edff4
James L Vodanovich <james@jvodan.com>
2017-02-13 17:34:17 +1100

    == ! =

--- lib/ruby/engine_builder/docker_file_builder/docker_file_builder.rb
+++ lib/ruby/engine_builder/docker_file_builder/docker_file_builder.rb
@@ -321,7 +321,7 @@ class DockerFileBuilder
     write_line('')
     set_user('0')
     @blueprint_reader.archives_details.each do |archive_details|      
-    next if archive_details[:extraction_command] = 'docker'
+    next if archive_details[:extraction_command] == 'docker'
       source_url = archive_details[:source_url].to_s
       package_name = archive_details[:package_name].to_s
       destination = archive_details[:destination].to_s
