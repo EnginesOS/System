987bed146b0f24ad3c2dff12fb83baf674947c49
James L Vodanovich <james@jvodan.com>
2017-02-21 10:45:46 +1100

    fmt

--- lib/ruby/api/system/engines_core/service_manager_operations.rb
+++ lib/ruby/api/system/engines_core/service_manager_operations.rb
@@ -8,27 +8,31 @@ module ServiceManagerOperations
   def deregister_non_persistent_services(engine)
     service_manager.deregister_non_persistent_services(engine)
   end
+
   def find_engine_service_hash(params)
     params[:container_type] = 'container'
     service_manager.find_engine_service_hash(params)
   end
-  
-  def  find_engine_services_hashes(hash) 
-    hash[:container_type] = 'service'  
-       
-    service_manager.find_engine_services_hashes(hash) 
+
+  def  find_engine_services_hashes(hash)
+    hash[:container_type] = 'service'
+
+    service_manager.find_engine_services_hashes(hash)
   end
-  
+
   def find_service_service_hash(params)
     params[:container_type] = 'service'
     service_manager.find_engine_service_hash(params)
   end
+
   def list_persistent_services(engine)
-      service_manager.list_persistent_services(engine)
-    end
+    service_manager.list_persistent_services(engine)
+  end
+
   def list_non_persistent_services(engine)
-      service_manager.list_non_persistent_services(engine)
-    end
+    service_manager.list_non_persistent_services(engine)
+  end
+
   def load_and_attach_services(dirname, container)
     service_manager.load_and_attach_services(dirname, container)
   end
@@ -44,22 +48,26 @@ module ServiceManagerOperations
   def rollback_shared_service(service_hash)
     service_manager.rollback_shared_service(service_hash)
   end
+
   def rollback_orphan_service(service_hash)
-      service_manager.rollback_orphan_service(service_hash)
-    end
+    service_manager.rollback_orphan_service(service_hash)
+  end
+
   def get_service_entry(service_hash)
     service_manager.get_service_entry(service_hash)
   end
+
   def retreive_cron_entry(cronjob, container)
     service_manager.get_cron_entry(cronjob, container)
   end
+
   def taken_hostnames
 
     sites = []
     hashes = service_manager.all_engines_registered_to('nginx')
 
-    return sites unless hashes.is_a?(Array)    
-   
+    return sites unless hashes.is_a?(Array)
+
     hashes.each do |service_hash|
       SystemDebug.debug(SystemDebug.services,  'service_hash is a' + service_hash.class.name)
       next unless service_hash.is_a?(Hash)
