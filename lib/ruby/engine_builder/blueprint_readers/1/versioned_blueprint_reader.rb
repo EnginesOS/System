e103aa9a00b7c8583cbcda4f23c0c26ffbec028d
James L Vodanovich <james@jvodan.com>
2017-02-23 17:32:32 +1100

    support schema 1 blueprints

--- lib/ruby/engine_builder/blueprint_readers/1/versioned_blueprint_reader.rb
+++ lib/ruby/engine_builder/blueprint_readers/1/versioned_blueprint_reader.rb
@@ -1,3 +1,13 @@
 require_relative '../blueprint_reader.rb'
 class VersionedBlueprintReader < BluePrintReader
+  def read_lang_fw_values
+     log_build_output('Read Framework Settings')
+     @framework = @blueprint[:software][:base][:framework]
+ 
+     @runtime = ''
+     @memory = @blueprint[:software][:base][:required_memory]
+   rescue StandardError => e
+     SystemUtils.log_exception(e)
+   end
+  
 end
