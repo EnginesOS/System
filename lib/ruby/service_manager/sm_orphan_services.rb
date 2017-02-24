a3534cd2325c210b9ee55b116a8b41bf4e44f217
James L Vodanovich <james@jvodan.com>
2017-02-24 18:43:27 +1100

    f

--- lib/ruby/service_manager/sm_orphan_services.rb
+++ lib/ruby/service_manager/sm_orphan_services.rb
@@ -42,8 +42,9 @@ def match_orphan_service(service_hash)
 end
 
   def retrieve_orphan(params)
-    r = system_registry_client.retrieve_orphan(params)
     STDERR.puts('retrice ORPHA ' + params.to_s)
+    r = system_registry_client.retrieve_orphan(params)
+  
     test_registry_result(r)   
     rescue StandardError => e
       log_exception(e)
