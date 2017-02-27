3aff889e3af0da08340afcbcd752b417e9fe8098
James L Vodanovich <james@jvodan.com>
2017-02-27 14:36:50 +1100

    f

--- lib/ruby/engine_builder/blueprint_readers/1/versioned_blueprint_reader.rb
+++ lib/ruby/engine_builder/blueprint_readers/1/versioned_blueprint_reader.rb
@@ -10,6 +10,12 @@ class VersionedBlueprintReader < BluePrintReader
      SystemUtils.log_exception(e)
    end
   
-  
+  def read_scripts
+    return unless @blueprint[:software].key?(:scripts)
+    @custom_start_script =  @blueprint[:software][:scripts][:start].gsub(/\r/, '') if @blueprint[:software][:scripts].key?(:start)
+    @custom_stop_script =  @blueprint[:software][:scripts][:shutdown].gsub(/\r/, '') if @blueprint[:software][:scripts].key?(:shutdown)
+    @custom_install_script =  @blueprint[:software][:install].gsub(/\r/, '') if @blueprint[:software][:scripts].key?(:install)
+    @custom_post_install_script =  @blueprint[:software][:scripts][:post_install].gsub(/\r/, '') if  @blueprint[:software][:scripts].key?(:post_install)
+  end
    
 end
