b6d1116b0320494b492c157b02bf171574634469
James L Vodanovich <james@jvodan.com>
2017-03-02 10:57:54 +1100

    ber

--- lib/ruby/service_manager/system_registry/api/configurations.rb
+++ lib/ruby/service_manager/system_registry/api/configurations.rb
@@ -3,22 +3,37 @@ module Configurations
  # require_relative 'xcon_rset.rb'
   def get_service_configurations_hashes(config_hash)
     rest_get('/v0/system_registry/service/configurations/' + config_hash[:service_name] )
+    rescue StandardError => e
+       STDERR.puts( 'Failed To /v0/system_registry/service/configurations/  ' + config_hash.to_s)
+        SystemUtils.log_exception(e)
   end
 
   def get_service_configuration(config_hash)
     rest_get('/v0/system_registry/services/configuration/' + config_hash[:service_name] + '/' + config_hash[:configurator_name] )
+    rescue StandardError => e
+       STDERR.puts( 'Failed To /v0/system_registry/services/configuration/  ' + config_hash.to_s)
+        SystemUtils.log_exception(e)
   end
 
   def update_service_configuration(config_hash)
     rest_post('/v0/system_registry/services/configuration/update/' + config_hash[:service_name] + '/' + config_hash[:configurator_name] , {:api_vars => config_hash} )
+    rescue StandardError => e
+       STDERR.puts( 'Failed To /v0/system_registry/services/configuration/update/  ' + config_hash.to_s)
+        SystemUtils.log_exception(e)
   end
 
   def rm_service_configuration(config_hash)
     rest_delete('/v0/system_registry/services/configurations/del/' + config_hash[:service_name] + '/' + config_hash[:configurator_name]  )
+    rescue StandardError => e
+       STDERR.puts( 'Failed To /v0/system_registry/services/configurations/del/  ' + config_hash.to_s)
+        SystemUtils.log_exception(e)
   end
 
   def add_service_configuration(config_hash)
     rest_post('/v0/system_registry/services/configurations/add/' + config_hash[:service_name] + '/' + config_hash[:configurator_name] , {:api_vars => config_hash} )
+    rescue StandardError => e
+       STDERR.puts( 'Failed To /v0/system_registry/services/configurations/add/  ' + config_hash.to_s)
+        SystemUtils.log_exception(e)
   end
 
   def service_configurations_registry
--- lib/ruby/service_manager/system_registry/api/engines.rb
+++ lib/ruby/service_manager/system_registry/api/engines.rb
@@ -7,6 +7,10 @@ module Engines
        r += '/' + params[:service_handle] 
        r += '/' + params[:type_path] 
        rest_get(r)
+    rescue StandardError => e
+       STDERR.puts( 'Failed To /v0/system_registry/engine/service/ ' + params.to_s)
+        SystemUtils.log_exception(e)
+      
   end
 
   def find_engine_services_hashes(params)
@@ -14,21 +18,36 @@ module Engines
   r = '/v0/system_registry/engine/services/'  + params[:container_type] + '/' + params[:parent_engine] 
     r += '/' + params[:type_path] if params.key?(:type_path)
     rest_get(r)
+    rescue StandardError => e
+       STDERR.puts( 'Failed To /v0/system_registry/engine/services/ ' + params.to_s)
+        SystemUtils.log_exception(e)
+      
   end
 
   def get_engine_nonpersistent_services(params)
     params[:persistent] = false
     rest_get('/v0/system_registry/engine/services/nonpersistent/',{:params => params })
+    rescue StandardError => e
+       STDERR.puts( 'Failed To /v0/system_registry/engine/services/nonpersistent/  ' + params.to_s)
+        SystemUtils.log_exception(e)
+      
   end
 
   def get_engine_persistent_services(params)
     params[:persistent] = true
     rest_get('/v0/system_registry/engine/services/persistent/',{:params => params })
+    rescue StandardError => e
+       STDERR.puts( 'Failed To /v0/system_registry/engine/services/persistent/  ' + params.to_s)
+        SystemUtils.log_exception(e)
+      
   end
 
   def add_to_managed_engines_registry(service_hash)
     SystemDebug.debug(SystemDebug.services,'sm add_to_managed_engines_registry ', service_hash)
     rest_post('/v0/system_registry/engine/services/add',service_hash )
+  rescue StandardError => e
+    STDERR.puts( 'Failed To /v0/system_registry/engine/services/add  ' + service_hash.to_s)
+     SystemUtils.log_exception(e)
   end
 
   def remove_from_managed_engines_registry(params)
@@ -38,6 +57,10 @@ module Engines
           r += '/' + params[:type_path] 
     rest_delete(r) 
    # rest_delete('/v0/system_registry/engine/services/del',{:params => params })
+        rescue StandardError => e
+           STDERR.puts( 'Failed To /v0/system_registry/engine/services/del/  ' + params.to_s)
+            SystemUtils.log_exception(e)
+          
   end
 
   def update_registered_managed_engine(params)
@@ -45,6 +68,10 @@ module Engines
              r += '/' + params[:service_handle] 
              r += '/' + params[:type_path] 
     rest_put(r,{:api_vars => params })
+    rescue StandardError => e
+       STDERR.puts( 'Failed To /v0/system_registry/engine/services/update/  ' + params.to_s)
+        SystemUtils.log_exception(e)
+      
   end
 
   def managed_engines_registry
