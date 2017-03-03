575547fd3cd3acfdd7ec0dad3981c001a225feb9
James L Vodanovich <james@jvodan.com>
2017-03-03 14:47:49 +1100

    f

--- lib/ruby/service_manager/system_registry/engines_registry_utils.rb
+++ lib/ruby/service_manager/system_registry/engines_registry_utils.rb
@@ -14,7 +14,7 @@ end
 def address_params(hash,param_symbols)
   r = ''
   param_symbols.each do | sym |
-    STDERR.puts('Hash Missing key!:'  + sym.to_s) unless hash.key?(sym)
+    STDERR.puts('Hash Missing key!:'  + sym.to_s + ' in ' + hash.to_s) unless hash.key?(sym)
     r += '/' + hash[sym]
   end
   r  
