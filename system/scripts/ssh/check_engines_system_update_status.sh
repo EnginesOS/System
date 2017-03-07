c1b448bba5f119ca16298d644ca55609109479c7
James L Vodanovich <james@jvodan.com>
2017-03-07 21:26:51 +1100

    fmt

--- system/scripts/ssh/check_engines_system_update_status.sh
+++ system/scripts/ssh/check_engines_system_update_status.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 cd /opt/engines
-
- 
 release=`cat /opt/engines/release`
 status=` git remote show origin |grep $release`
 echo $status | grep  'local out of date' >/dev/null
+
 if test $? -eq 0
  then
   echo $status > /opt/engines/run/system/flags/update_pending
 	echo "Update Pending"
 	echo $status
-	exit 255
+	exit 127
  else
   rm -f /opt/engines/run/system/flags/update_pending
 fi
+
 echo "System Up to Date"
 rm -f /opt/engines/run/system/flags/update_pending
+
 exit 0
