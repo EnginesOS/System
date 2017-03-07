f6d0ee782e102e320942192f86e4d81198463461
James L Vodanovich <james@jvodan.com>
2017-03-07 18:23:28 +1100

    clear flag

--- system/scripts/ssh/sudo/_update_base_os.sh
+++ system/scripts/ssh/sudo/_update_base_os.sh
@@ -10,3 +10,4 @@ env DEBIAN_FRONTEND=noninteractive  apt-get -q -y -o "Dpkg::Options::=--force-co
 touch /opt/engines/run/system/flags/run_post_system_update
 cp /etc/os-release /opt/engines/etc/os-release-host 
 #/opt/engines/system/scripts/system/update_system_status_flags.sh
+rm /opt/engines/run/system/flags/base_os_update_pending
