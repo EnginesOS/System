3721ddfa83b99ac5698cf182c9a7852284ea66c7
James L Vodanovich <james@jvodan.com>
2017-03-08 08:27:25 +1100

    fmt

--- lib/ruby/api/system/engines_system/container_schedules.rb
+++ lib/ruby/api/system/engines_system/container_schedules.rb
@@ -14,28 +14,26 @@ module ContainerSchedules
   end
 
   def create_cron_service(container, schedule)
-    
+
     @engines_api.create_and_register_service( {publisher_namespace: 'EnginesSystem', type_path: 'cron',
-     variables: { action_type: 'schedule', 
-                                  cron_job: schedule_instruction(schedule[:timespec]),
-                                   title: schedule[:lable],
-                                   :when => cron_line(schedule),
-                                   parent_engine: container.container_name } }   ) 
-                                                               
+      variables: { action_type: 'schedule',
+      cron_job: schedule_instruction(schedule[:timespec]),
+      title: schedule[:lable],
+      :when => cron_line(schedule),
+      parent_engine: container.container_name } }   )
   end
-  
+
   def schedule_instruction(schedule)
     return schedule[:action] unless  schedule[:action] == "action"
     #r = schedule[:actionator]
-    schedule[:actionator]  
-    
+    schedule[:actionator]
+
   end
-  
+
   def cron_line(timespec)
-   
+
     timespec[:minute] + ' ' + timespec[:hour] + ' ' + timespec[:day_of_month] + ' ' + timespec[:month] + ' ' + timespec[:day_of_week]
-     
-    
+
   end
-  
+
 end
