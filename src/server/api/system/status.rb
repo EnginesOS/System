e78e077f702ea3f6df49624551ff3ddd3a36417e
James L Vodanovich <james@jvodan.com>
2017-03-07 19:58:18 +1100

    f

--- src/server/api/system/status.rb
+++ src/server/api/system/status.rb
@@ -18,6 +18,7 @@ end
 
 get '/v0/system/status' do
   s_status = SystemStatus.system_status
+  STDERR.puts( s_status.to_s)
   return log_error(request,s_status ) if s_status.is_a?(EnginesError)
   status(202)
   s_status.to_json
