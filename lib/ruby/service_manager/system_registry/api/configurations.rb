acb0b89bf77abee533f5276f85565812a18e5674
James L Vodanovich <james@jvodan.com>
2017-03-11 09:27:58 +1100

    f

--- lib/ruby/service_manager/system_registry/api/configurations.rb
+++ lib/ruby/service_manager/system_registry/api/configurations.rb
@@ -4,7 +4,7 @@ module Configurations
   def get_service_configurations_hashes(config_hash)
     #  rest_get('service/configurations/' + config_hash[:service_name] )
     r = 'service/configurations'  
-        r += address_params(params,[:service_name] ) 
+        r += address_params(config_hash,[:service_name] ) 
     rest_get(r)
     rescue StandardError => e
        STDERR.puts( 'Failed To service/configurations/  ' + config_hash.to_s)
@@ -14,7 +14,7 @@ module Configurations
   def get_service_configuration(config_hash)
    # rest_get('services/configuration/' + config_hash[:service_name] + '/' + config_hash[:configurator_name] )
     r = 'service/configurations'  
-        r += address_params(params,[:service_name,:configurator_name] ) 
+        r += address_params(config_hash,[:service_name,:configurator_name] ) 
     rest_get(r)
     rescue StandardError => e
        STDERR.puts( 'Failed To services/configuration/  ' + config_hash.to_s)
@@ -24,7 +24,7 @@ module Configurations
   def update_service_configuration(config_hash)
    # rest_post('services/configuration/update/' + config_hash[:service_name] + '/' + config_hash[:configurator_name] , {:api_vars => config_hash} )
     r = 'service/configuration/update'  
-        r += address_params(params,[:service_name,:configurator_name] ) 
+        r += address_params(config_hash,[:service_name,:configurator_name] ) 
     rest_post(r, {:api_vars => config_hash})
     rescue StandardError => e
        STDERR.puts( 'Failed To services/configuration/update/  ' + config_hash.to_s)
@@ -34,7 +34,7 @@ module Configurations
   def rm_service_configuration(config_hash)
     #   rest_delete('services/configurations/del/' + config_hash[:service_name] + '/' + config_hash[:configurator_name]  ) )
     r = 'service/configurations/del'  
-        r += address_params(params,[:service_name,:configurator_name] ) 
+        r += address_params(config_hash,[:service_name,:configurator_name] ) 
     rest_delete(r)
     
     rescue StandardError => e
@@ -44,7 +44,7 @@ module Configurations
 
   def add_service_configuration(config_hash)
     r = 'service/configurations/add'  
-          r += address_params(params,[:service_name,:configurator_name] ) 
+          r += address_params(config_hash,[:service_name,:configurator_name] ) 
       rest_post(r, {:api_vars => config_hash})
     rescue StandardError => e
        STDERR.puts( 'Failed add To services/configurations/add/  ' + config_hash.to_s)
