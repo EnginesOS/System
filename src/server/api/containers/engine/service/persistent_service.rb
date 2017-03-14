fdfe66e16d55edd78430d081563f426e7c095233
James L Vodanovich <james@jvodan.com>
2017-03-14 14:08:47 +1100

    f

--- src/server/api/containers/engine/service/persistent_service.rb
+++ src/server/api/containers/engine/service/persistent_service.rb
@@ -23,79 +23,39 @@ end
 # @param :data data to import
 # @return [true]
 post '/v0/containers/engine/:engine_name/service/persistent/:publisher_namespace/*/overwrite' do
-#  p_params = request.env["rack.input"].read
-#  STDERR.puts(' upload post '  + p_params.to_s + ' params '  + params.to_s)
+  #  p_params = request.env["rack.input"].read
+  #  STDERR.puts(' upload post '  + p_params.to_s + ' params '  + params.to_s)
   hash = {}
   hash[:service_connection] =  engine_service_hash_from_params(params)
   engine = get_engine(params[:engine_name])
   hash[:datafile] = params['file'][:tempfile]
 
- # hash[:data] = Base64.encode64( p_params['api_vars']['data'])
+  # hash[:data] = Base64.encode64( p_params['api_vars']['data'])
   return log_error(request, engine, hash) if engine.is_a?(EnginesError)
   r = engine.import_service_data(hash,File.new(hash[:datafile].path,'rb'))
   return log_error(request, r, engine.last_error) if r.is_a?(EnginesError)
   return_text(r)
 end
-# @method engine_import_persistent_service_file
-# @overload post '/v0/containers/engine/:engine_name/service/persistent/:publisher_namespace/:type_path/:service_handle/import_file'
-# import the service data gzip optional
-# @param
-# @return [true]
-#post '/v0/containers/engine/:engine_name/service/persistent/:publisher_namespace/*/import_file' do
-##  p_params = request.env["rack.input"].read
-# # STDERR.puts(' upload post '  + p_params.to_s + ' params '  + params.to_s)
-#  hash = {}
-#  hash[:service_connection] =  engine_service_hash_from_params(params)
-#  engine = get_engine(params[:engine_name])
-# # hash[:data] = p_params['api_vars']['data']
-#  file = p_params[:file][:tempfile]
-#  return log_error(request, engine, hash) if engine.is_a?(EnginesError)
-#  r = engine.import_service_data(hash, file)
-#  return log_error(request, r, engine.last_error) if r.is_a?(EnginesError)
-#  content_type 'text/plain' 
-#  r.to_s
-#end
-# @method engine_replace_persistent_service_file
-# @overload post '/v0/containers/engine/:engine_name/service/persistent/:publisher_namespace/:type_path/:service_handle/replace_file'
-# import the service data gzip optional after dropping/deleting existing data
-# @param :data data to import
-# @return [true]
-#post '/v0/containers/engine/:engine_name/service/persistent/:publisher_namespace/*/replace_file' do
-# # p_params =request.env["rack.input"].read
-# # STDERR.puts(' upload post '  + p_params.to_s + ' params '  + params.to_s)
-#  hash = {}
-#  hash[:service_connection] =  engine_service_hash_from_params(params)
-#  engine = get_engine(params[:engine_name])
-#  hash[:import_method] = :replace
-#  hash[:datafile] = params[:tempfile]
-#  file = p_params[:file][:tempfile]
-#  return log_error(request, engine, hash) if engine.is_a?(EnginesError)
-#  r = engine.import_service_data(hash,file)
-#  return log_error(request, r, engine.last_error) if r.is_a?(EnginesError)
-#  content_type 'text/plain' 
-#  r.to_s
-#end
+
 # @method engine_replace_persistent_service
 # @overload post '/v0/containers/engine/:engine_name/service/persistent/:publisher_namespace/:type_path/:service_handle/replace'
 # import the service data gzip optional after dropping/deleting existing data
 # @param :data data to import
 # @return [true]
 post '/v0/containers/engine/:engine_name/service/persistent/:publisher_namespace/*/replace' do
-#  p_params = request.env["rack.input"].read
-#  STDERR.puts(' upload post '  + p_params.to_s + ' params '  + params.to_s)
+  #  p_params = request.env["rack.input"].read
+  #  STDERR.puts(' upload post '  + p_params.to_s + ' params '  + params.to_s)
   hash = {}
   hash[:service_connection] =  engine_service_hash_from_params(params)
   engine = get_engine(params[:engine_name])
   hash[:import_method] = :replace
   hash[:datafile] = params['file'][:tempfile]
   return log_error(request, engine, hash) if engine.is_a?(EnginesError)
- # hash[:data] =Base64.encode64( p_params['api_vars']['data'])
+  # hash[:data] =Base64.encode64( p_params['api_vars']['data'])
   r = engine.import_service_data(hash, File.new(hash[:datafile].path,'rb'))
   return log_error(request, r, engine.last_error) if r.is_a?(EnginesError)
   return_text(r)
 end
-
-
 
 # @method get_engine_persistent_service
 # @overload get '/v0/containers/engine/:engine_name/services/persistent/'
@@ -117,11 +77,10 @@ end
 
 post '/v0/containers/engine/:engine_name/service/persistent/:publisher_namespace/*' do
   p_params = post_params(request)
-   path_hash = engine_service_hash_from_params(params, false)
-   p_params.merge!(path_hash)
-   cparams =  Utils::Params.assemble_params(p_params, [:parent_engine,:publisher_namespace, :type_path, :service_handle], :all)
-   return log_error(request,cparams,p_params) if cparams.is_a?(EnginesError)
- 
+  path_hash = engine_service_hash_from_params(params, false)
+  p_params.merge!(path_hash)
+  cparams = assemble_params(p_params, [:parent_engine,:publisher_namespace, :type_path, :service_handle], :all)
+  return log_error(request,cparams,p_params) if cparams.is_a?(EnginesError)
   r = engines_api.update_attached_service(cparams)
   return log_error(request, r, hash) if r.is_a?(EnginesError)
   return_text(r)
