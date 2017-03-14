036c78fa1955150893e5092facd445c5ab12a706
James L Vodanovich <james@jvodan.com>
2017-03-14 16:44:00 +1100

    f

--- lib/ruby/api/system/engines_core/service_operations.rb
+++ lib/ruby/api/system/engines_core/service_operations.rb
@@ -33,12 +33,12 @@ module ServiceOperations
 
   #Attach the service defined in service_hash [Hash]
   #@return boolean indicating sucess
-  def create_and_register_service(service_hash, overwrite = false)
+  def create_and_register_service(service_hash)
     r = ''
   #  service_hash = SystemUtils.symbolize_keys(service_hash)
 
     SystemDebug.debug(SystemDebug.services, :attach_ing_create_and_egister_service, service_hash)
-    return r unless ( r = create_and_register_managed_service(service_hash, overwrite))
+    return r unless ( r = create_and_register_managed_service(service_hash))
 
     true
   rescue StandardError => e
