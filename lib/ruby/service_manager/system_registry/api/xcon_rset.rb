862ae632c48c3d7e3be8683d6deaea6ab9489c34
James L Vodanovich <james@jvodan.com>
2017-03-14 14:54:44 +1100

    nil is nil

--- lib/ruby/service_manager/system_registry/api/xcon_rset.rb
+++ lib/ruby/service_manager/system_registry/api/xcon_rset.rb
@@ -17,7 +17,6 @@ def connection(content_type = nil)
   :ssl_verify_peer => false,
   :persistent => true,
   :headers => headers)
-
 rescue StandardError => e
   STDERR.puts('Failed to open base url to registry' + @base_url.to_s)
   STDERR.puts e.backtrace.to_s
@@ -38,14 +37,13 @@ end
 def rest_get(path,params = nil,time_out=120, _headers = nil)
   cnt = 0
   q = query_hash(params)
-  STDERR.puts(' GET ' + path.to_s + '?' + q.to_s )
+  STDERR.puts(' REG GET ' + path.to_s + '?' + q.to_s )
   SystemDebug.debug(SystemDebug.registry,'GET ', path.to_s + '?' + q.to_s)
   lheaders = headers
   lheaders.merge(_headers) unless _headers == nil
   lheaders.delete('Content-Type' ) if  q.nil?
   req = {time_out: time_out, method: :get, path: @route_prefix + path, headers: lheaders }
   req[:query] = q unless q.nil?
-
   r = connection.request(req)
   parse_xcon_response(r)
 rescue  Excon::Error::Socket => e
@@ -53,7 +51,6 @@ rescue  Excon::Error::Socket => e
   STDERR.puts e.class.name + ' with path:' + path.to_s + "\n" + 'params:' + q.to_s + ':::' + req.to_s  + ':' + e.to_s
   cnt+=1
   retry if cnt< 5
-
 end
 
 def time_out
@@ -105,7 +102,6 @@ private
 
 
 def parse_xcon_response(resp)
-
   raise RegistryException.new({status: 500 , error_mesg: 'Server Error', exception: :exception})  if resp.nil?
   STDERR.puts('1 ' + resp.status.to_s + ':' + resp.headers.to_s + " __ " + resp.body.to_s)
   if resp.status > 399
@@ -122,7 +118,7 @@ def parse_xcon_response(resp)
   STDERR.puts('2 ' + resp.status.to_s + ':' + resp.body.to_s)
   #return parse_error(resp) if resp.status > 399
   r = resp.body
-  return false if r.nil?
+  return if r.nil?
   r.strip!
   return r if resp.headers['Content-Type'] == 'plain/text'
   r = deal_with_json(r)
