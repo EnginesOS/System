a84165076b0b1cadaffd4ea14ca028847f33a946
James L Vodanovich <james@jvodan.com>
2017-03-14 17:48:35 +1100

    bump version

--- lib/ruby/system/system_config/system_version.rb
+++ lib/ruby/system/system_config/system_version.rb
@@ -1,14 +1,12 @@
 module SystemVersion
   @@api_version = '0.2'
-  @@engines_system_version = '0.2'
+  @@engines_system_version = '0.3'
+  def SystemConfig.api_version
+    return @@api_version
+  end
 
-  
- def SystemConfig.api_version
-return @@api_version
-end
-
-def SystemConfig.engines_system_version
-return @@engines_system_version
-end
+  def SystemConfig.engines_system_version
+    return @@engines_system_version
+  end
 
 end
