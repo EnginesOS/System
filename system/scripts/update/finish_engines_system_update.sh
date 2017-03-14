42f067c51c390deb68a21c310790265d861eaa6a
James L Vodanovich <james@jvodan.com>
2017-03-14 17:27:13 +1100

    again

--- system/scripts/update/finish_engines_system_update.sh
+++ system/scripts/update/finish_engines_system_update.sh
@@ -14,18 +14,18 @@ if ! test -d  $system_updates_dir/$update_id
  then
    exit
   fi
- Applying update $update_id >>& $system_updates_dir/$update_id/update_log 		 
+ Applying update $update_id &>> $system_updates_dir/$update_id/update_log 		 
  
  if test -f  $system_updates_dir/$update_id/services
   then
   	services=`cat  $system_updates_dir/$update_id/services`
   		for service in $services
   		 do
-  		 	/opt/engines/bin/engines service $service stop >>& $system_updates_dir/$update_id/update_log 		 	 
+  		 	/opt/engines/bin/engines service $service stop &>> $system_updates_dir/$update_id/update_log 		 	 
   		 	image=`grep image /opt/engines/run/services/$service/running.yaml | cut -f2 -d" "`
   			docker pull $image
   			rm /opt/engines/run/services/$service/running.yaml*
-  		 	/opt/engines/bin/engines service $service recreate  >>&  $system_updates_dir/$update_id/update_log
+  		 	/opt/engines/bin/engines service $service recreate  &>>  $system_updates_dir/$update_id/update_log
 
   		 done
   fi
@@ -35,25 +35,25 @@ if ! test -d  $system_updates_dir/$update_id
   	services=`cat  $system_updates_dir/$update_id/system_services`
   		for service in $services
   		 do
-  		    echo "Recreate Service $service" >>& $system_updates_dir/$update_id/update_log
+  		    echo "Recreate Service $service" &>> $system_updates_dir/$update_id/update_log
   		    
-  		  	docker pull engines/$service:` cat /opt/engines/release`  >>& $system_updates_dir/$update_id/update_log
+  		  	docker pull engines/$service:` cat /opt/engines/release`  &>> $system_updates_dir/$update_id/update_log
  			rm /opt/engines/run/system_services/$service/running.yaml*
  			#sleep 30
- 			/opt/engines/bin/system_service.rb $service stop >>& $system_updates_dir/$update_id/update_log
- 			/opt/engines/bin/system_service.rb $service destroy >>& $system_updates_dir/$update_id/update_log
- 			/opt/engines/bin/system_service.rb $service create >>& $system_updates_dir/$update_id/update_log
+ 			/opt/engines/bin/system_service.rb $service stop &>> $system_updates_dir/$update_id/update_log
+ 			/opt/engines/bin/system_service.rb $service destroy &>> $system_updates_dir/$update_id/update_log
+ 			/opt/engines/bin/system_service.rb $service create &>> $system_updates_dir/$update_id/update_log
   		 done
   fi
   
   if test -f  $system_updates_dir/$update_id/updater.sh
    then
- 		 $system_updates_dir/$update_id/updater.sh >>  $system_updates_dir/$update_id/update_log
+ 		 $system_updates_dir/$update_id/updater.sh &>>  $system_updates_dir/$update_id/update_log
  		if test $? -ne 0
  		 then
  		 echo Problem with $update_id
  		 cat  $system_updates_dir/$update_id/update_log
- 		 mv  $system_updates_dir/$update_id /opt/engines/system/updates/failed/engines
+ 		 cp -rp  $system_updates_dir/$update_id /opt/engines/system/updates/failed/engines
  		 exit
  		  
  		 fi
