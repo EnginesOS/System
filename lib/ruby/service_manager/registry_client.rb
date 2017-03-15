e5e0a9afe0e24aa2014c4dd302ae12beae1f082f
James L Vodanovich <james@jvodan.com>
2017-03-15 20:41:18 +1100

    te

--- lib/ruby/service_manager/registry_client.rb
+++ lib/ruby/service_manager/registry_client.rb
@@ -1,5 +1,5 @@
-module RegistryClient
-  def system_registry_client
-    @system_registry ||= SystemRegistryClient.new(@core_api)
-  end
-end
+#module RegistryClient
+#  def system_registry_client
+#    @system_registry ||= SystemRegistryClient.new(@core_api)
+#  end
+#end
--- lib/ruby/service_manager/sm_service_forced_methods.rb
+++ lib/ruby/service_manager/sm_service_forced_methods.rb
@@ -5,8 +5,6 @@ module SmServiceForcedMethods
     clear_error
     add_to_managed_service(service_hash)
     system_registry_client.add_to_services_registry(service_hash)
-  rescue StandardError => e
-    handle_exception(e)
   end
 
   def deregister_non_persistent_service(service_hash)
@@ -14,24 +12,18 @@ module SmServiceForcedMethods
     service_hash = set_top_level_service_params(service_hash,service_hash[:parent_engine])
     remove_from_managed_service(service_hash)
     system_registry_client.remove_from_services_registry(service_hash)
-  rescue StandardError => e
-    handle_exception(e)
   end
 
   def force_register_attached_service(service_query)
     complete_service_query = set_top_level_service_params(service_query, service_query[:parent_engine])
     service_hash = system_registry_client.find_engine_service_hash(complete_service_query)
     add_to_managed_service(service_hash)
-  rescue StandardError => e
-    handle_exception(e)
   end
 
   def force_deregister_attached_service(service_query)
     complete_service_query = set_top_level_service_params(service_query, service_query[:parent_engine])
     service_hash = system_registry_client.find_engine_service_hash(complete_service_query)
     remove_from_managed_service(service_hash)
-  rescue StandardError => e
-    handle_exception(e)
   end
 
   def force_reregister_attached_service(service_query)
@@ -39,8 +31,6 @@ module SmServiceForcedMethods
     service_hash = system_registry_client.find_engine_service_hash(complete_service_query)
     remove_from_managed_service(service_hash)
     add_to_managed_service(service_hash)
-  rescue StandardError => e
-    handle_exception(e)
   end
 
 end
