9810a41dbbf748cc160c765e0ec2b69b89b31385
James L Vodanovich <james@jvodan.com>
2017-03-15 16:30:25 +1100

    f

--- lib/ruby/service_manager/service_manager.rb
+++ lib/ruby/service_manager/service_manager.rb
@@ -12,7 +12,7 @@ class ServiceManager  < ErrorsApi
   require_relative 'sm_service_control.rb'
   require_relative 'sm_engine_services.rb'
   require_relative 'sm_service_forced_methods.rb'
- # require_relative 'sm_registry_tree.rb'
+ require_relative 'sm_registry_tree.rb'
   require_relative 'sm_orphan_services.rb'
   require_relative 'sm_subservices.rb'
   require_relative 'sm_service_info.rb'
@@ -32,12 +32,12 @@ class ServiceManager  < ErrorsApi
   include SMSubservices
   include SmServiceInfo
   include SmServiceForcedMethods
-  #include SmRegistryTree
+  include SmRegistryTree
   include SmOrphanServices
   include SmEngineServices
   include SMAttachedServices
   include SmAttachStaticServices
-
+  
   include SmServiceControl
   include SmServiceConfigurations
   include SharedServices
@@ -46,7 +46,8 @@ class ServiceManager  < ErrorsApi
   require '/opt/engines/lib/ruby/exceptions/registry_exception.rb'
   require '/opt/engines/lib/ruby/managed_services/service_definitions/service_top_level.rb'
   
-  private 
+  private
+
   def system_registry_client
      @system_registry ||= SystemRegistryClient.new(@core_api)
    end
