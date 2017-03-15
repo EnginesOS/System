2419e401d7da1ae4be3b3d732620e55da6900b3d
James L Vodanovich <james@jvodan.com>
2017-03-15 16:03:22 +1100

    set_top_level meth

---
+++ lib/ruby/managed_services/service_definitions/service_top_level.rb
@@ -0,0 +1,32 @@
+def set_top_level_service_params(service_hash, container_name)
+   container_name = service_hash[:parent_engine] if service_hash.key?(:parent_engine)
+   container_name = service_hash[:engine_name] if container_name == nil
+   return SystemUtils.log_error_mesg('no set_top_level_service_params_nil_service_hash container_name:',container_name) if container_name.nil?
+   return SystemUtils.log_error_mesg('no set_top_level_service_params_nil_container_name service_hash:',service_hash)  if service_hash.nil?
+   service_def = SoftwareServiceDefinition.find(service_hash[:type_path],service_hash[:publisher_namespace])
+   return service_def if service_def.is_a?(EnginesError)
+   service_hash[:service_container_name] = service_def[:service_container]
+   service_hash[:persistent] = service_def[:persistent]
+   service_hash[:parent_engine] = container_name
+   service_hash[:container_type] = 'container' if service_hash.has_key?(:container_type) == false || service_hash[:container_type] ==nil
+   service_hash[:variables] = {} unless service_hash.has_key?(:variables)
+   service_hash[:variables][:parent_engine] = container_name
+   if service_def.key?(:priority)
+     service_hash[:priority] = service_def[:priority]
+   else
+     service_hash[:priority] = 0
+   end
+   return service_hash if service_hash.key?(:service_handle) && ! service_hash[:service_handle].nil?
+
+   if service_def.key?(:service_handle_field) && !service_def[:service_handle_field].nil?
+     handle_field_sym = service_def[:service_handle_field].to_sym
+     return SystemUtils.log_error_mesg('Missing Service Handle field in variables',handle_field_sym) unless service_hash[:variables].key?(handle_field_sym)
+     service_hash[:service_handle] = service_hash[:variables][handle_field_sym]
+   else
+     service_hash[:service_handle] = container_name
+   end
+
+   service_hash
+ rescue StandardError => e
+   SystemUtils.log_exception(e)
+ end
