0f3d4efbac341b924d4fc3b975065b690a704c28
James L Vodanovich <james@jvodan.com>
2017-03-16 11:05:29 +1100

    dbg

--- lib/ruby/service_manager/system_registry/engines_registry_utils.rb
+++ lib/ruby/service_manager/system_registry/engines_registry_utils.rb
@@ -1,10 +1,12 @@
 module EnginesRegistryUtils
-  def address_params(hash,param_symbols)
+  def address_params(hash, param_symbols)
     r = ''
     param_symbols.each do | sym |
+      STDERR.puts('address_params sym ' + sym.to_s)
       return r unless hash.key?(sym)
       r += '/' + hash[sym].to_s
      # SystemDebug.debug(SystemDebug.services,r.to_s)
+      STDERR.puts('address_params val ' + r.to_s)
     end
     SystemDebug.debug(SystemDebug.services,r.to_s)
     r
