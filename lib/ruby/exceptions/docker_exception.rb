600ebdcda83152e2ed4cedaf6f2343026bce6fcb
James L Vodanovich <james@jvodan.com>
2017-03-16 17:05:26 +1100

    sort inherietaba withc exception

--- lib/ruby/exceptions/docker_exception.rb
+++ lib/ruby/exceptions/docker_exception.rb
@@ -1,4 +1,4 @@
-class DockerException < StandardError
+class DockerException < EnginesException
   attr_reader :status
   def initialize( hash)
     @status = hash[:status] if hash.is_a?(Hash)
--- lib/ruby/exceptions/registry_exception.rb
+++ lib/ruby/exceptions/registry_exception.rb
@@ -1,4 +1,4 @@
-class EnginesException < StandardError
+class RegistryException < EnginesException
   attr_reader :status
   def initialize( hash)
     @status = hash[:status] if hash.is_a?(Hash)
