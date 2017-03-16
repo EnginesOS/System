4df74d2c1cda4b1d8c33e9e99bc5adb089da5647
James L Vodanovich <james@jvodan.com>
2017-03-16 19:48:40 +1100

    f

--- lib/ruby/api/system/engines_system/container_checks.rb
+++ lib/ruby/api/system/engines_system/container_checks.rb
@@ -1,26 +1,30 @@
 module ContainerChecks
   def containers_check_and_act
-    
+
     services_status = get_services_status
-   results = check_and_act(services_status, 'service')
+    results = check_and_act(services_status, 'service')
     engines_status = get_engines_status
     results.merge!(check_and_act(engines_status, 'container'))
-      results
+    results
   end
   protected
-  
+
   def check_and_act(containers_status, ctype)
     result = {}
-    containers_status.keys.each do |container_name|    
+    containers_status.keys.each do |container_name|
+
       if containers_status[container_name][:error] == true
-      result[container_name] = act_on(container_name, ctype)
-    else
+        begin
+          result[container_name] = act_on(container_name, ctype)
+        rescue EnginesError => e
+        end
+      else
         result[container_name] = 'ok'
-     end       
-   end
-result
+      end
+    end
+    result
   end
-  
+
   def act_on(container_name, ctype)
     if ctype == 'container'
       container = loadManagedEngine(container_name)
@@ -29,14 +33,13 @@ result
     else
       container = loadSystemService(container_name)
     end
-    
+
     return container if container.is_a?(EnginesError)
-    
+
     r = container.correct_current_state
     return r if r.is_a?(EnginesError)
-    
-     'fixed'
-    
-  
+
+    'fixed'
+
   end
 end
