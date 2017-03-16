fc1234cfe495cb70d33f17c35cdb130df82c7b9e
James L Vodanovich <james@jvodan.com>
2017-03-16 11:12:26 +1100

    f

--- lib/ruby/service_manager/system_registry/api/engines.rb
+++ lib/ruby/service_manager/system_registry/api/engines.rb
@@ -31,7 +31,7 @@ module Engines
     SystemDebug.debug(SystemDebug.services, 'sm add_to_managed_engines_registry ', service_hash)
     STDERR.puts('sm add_to_managed_engines_registry ' + service_hash.to_s)
     r = 'engine/services/add'
-    r += address_params(service_hash, full_path)
+    r += address_params(service_hash, [:container_type, :parent_engine, :service_handle, :publisher_namespace, :type_path])
     STDERR.puts('sm add_to_managed_engines_registry ' + r.to_s)
     rest_post(r,{:api_vars => service_hash} )
   end
@@ -40,13 +40,13 @@ module Engines
     params[:container_type] = 'container' unless params.key?(:container_type)
     STDERR.puts('PARAMAS FOR DELEparams' + params.to_s)
     r = 'engine/services/del'
-    r += address_params(params, full_path)
+    r += address_params(params, [:container_type, :parent_engine, :service_handle, :publisher_namespace, :type_path])
     rest_delete(r)
   end
 
   def update_registered_managed_engine(params)
     r = 'engine/services/update'
-    r += address_params(params, full_path)
+    r += address_params(params, [:container_type, :parent_engine, :service_handle, :publisher_namespace, :type_path])
     rest_post(r,{:api_vars => params })
   end
 
