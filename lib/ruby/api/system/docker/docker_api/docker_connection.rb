3b2554bc75da43a9c80e8e75f94b0ac2ff1efd73
James L Vodanovich <james@jvodan.com>
2017-03-17 18:31:47 +1100

    dbg

--- lib/ruby/api/system/docker/docker_api/docker_connection.rb
+++ lib/ruby/api/system/docker/docker_api/docker_connection.rb
@@ -142,6 +142,7 @@ class DockerConnection < ErrorsApi
 
   def get_request(uri,  expect_json = true, rheaders = nil, timeout = 60)
     SystemDebug.debug(SystemDebug.docker,' Get ' + uri.to_s)
+    STDERR.puts('GET TRUE REQUEST ' + caller[0..5].to_s)  if url.start_with?('/containers/true/') 
     rheaders = default_headers if rheaders.nil?
     r = connection.request(request_params({method: :get,path: uri,read_timeout: timeout,headers: rheaders}))
     return handle_resp(r,expect_json)
