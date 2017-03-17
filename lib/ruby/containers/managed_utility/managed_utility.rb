3b42175fb3201e2a2640cfe34c4cefe74d749751
James L Vodanovich <james@jvodan.com>
2017-03-17 18:53:50 +1100

    f

--- lib/ruby/containers/managed_utility/managed_utility.rb
+++ lib/ruby/containers/managed_utility/managed_utility.rb
@@ -35,6 +35,7 @@ class ManagedUtility< ManagedContainer
     @container_mutex.synchronize {
       SystemDebug.debug(SystemDebug.container_events,:ON_Create_CALLED,event_hash)
       @container_id = event_hash[:id]
+        STDERR.puts('ID SET YTO' + @container_id.to_s )
       @out_of_memory = false
       @had_out_memory = false
       save_state
