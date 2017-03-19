631239c73297315ca0dc12666d45925119c46097
James L Vodanovich <james@jvodan.com>
2017-03-20 10:57:57 +1100

    f

--- lib/ruby/service_manager/private/shared_volumes.rb
+++ lib/ruby/service_manager/private/shared_volumes.rb
@@ -1,5 +1,5 @@
 def attach_shared_volume(shared_service)
-  engine = @core_api.loadManagedEngine(shared_service[:parent_engine])
+  engine = @core_api.loadManagedEngine(shared_service[:existing_service][:parent_engine])
   #used by the builder whn no engine to add volume to def
   #  return engine unless engine.is_a?(ManagedEngine)
   # Volume.complete_service_hash(shared_service)
