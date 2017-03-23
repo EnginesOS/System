7871d00a991c898e50c34eec0a472a584be7283e
James L Vodanovich <james@jvodan.com>
2017-03-23 15:29:10 +1100

    tkludge until excaptions

--- lib/ruby/engine_builder/engine_builder.rb
+++ lib/ruby/engine_builder/engine_builder.rb
@@ -178,6 +178,7 @@ class EngineBuilder < ErrorsApi
   def wait_for_engine
     cnt = 0
     lcnt = 5
+    return @container unless @container.is_a?(ManagedEngine)
     log_build_output('Starting Engine')
     while @container.is_startup_complete? == false && @container.is_running?
       cnt += 1
