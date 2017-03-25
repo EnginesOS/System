22a7bf7622757572cad56308fe09ad4271a4d29b
James L Vodanovich <james@jvodan.com>
2017-03-25 16:47:11 +1100

    f

--- lib/ruby/api/system/containers/container_api/engine_api_image_actions.rb
+++ lib/ruby/api/system/containers/container_api/engine_api_image_actions.rb
@@ -1,7 +1,7 @@
 module EngineApiImageActions
   def delete_image(container)
     clear_error
-    @system_api.delete_engine(container)
+    @system_api.rm_engine_from_cache(container.container_name)
     volbuilder = @engines_core.loadManagedUtility('fsconfigurator')
     @docker_api.delete_image(container) if @docker_api.image_exist?(container.image)
     @system_api.delete_container_configs(volbuilder, container)
