e5aaa3a3c72c4a919e39d8826283a1b556be97da
James L Vodanovich <james@jvodan.com>
2017-03-27 08:49:37 +1100

    blo

--- lib/ruby/api/system/engines_system/build_report.rb
+++ lib/ruby/api/system/engines_system/build_report.rb
@@ -11,7 +11,7 @@ module BuildReport
     raise EnginesException.new(error_hash('get_build_report passed nil engine_name', engine_name)) if engine_name.nil?
     c = container_state_dir(FakeContainer.new(engine_name))
     File.read(c + '/buildreport.txt') if File.exist?(c + '/buildreport.txt')
-    raise EnginesException.new(error_hash('No Build Report'))
+    raise EnginesException.new(error_hash('No Build Report:' + c + '/buildreport.txt'))
   end
 
   def save_build_report(container, build_report)
