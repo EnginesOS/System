d3c3abdc7f70dec88bfd77fbde68747edda38953
James L Vodanovich <james@jvodan.com>
2017-03-27 08:21:48 +1100

    f

--- lib/ruby/api/system/engines_core/core_build_controller.rb
+++ lib/ruby/api/system/engines_core/core_build_controller.rb
@@ -29,8 +29,7 @@ module CoreBuildController
   def build_engine(params)
     @build_controller = BuildController.new(self)  unless @build_controller
     @build_thread = Thread.new { @build_controller.build_engine(params) }
-    return false unless @build_thread.alive?
-    true
+    return true if @build_thread.alive?
     raise EnginesException.new(error_hash(params[:engine_name], 'Build Failed to start'))
   end
 end
