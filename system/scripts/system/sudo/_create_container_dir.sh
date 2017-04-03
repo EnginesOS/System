16f9424e4dd88a6ae9e825ae37b1acd3be015cc2
James L Vodanovich <james@jvodan.com>
2017-04-03 11:42:29 +1000

    dbg

--- system/scripts/system/sudo/_create_container_dir.sh
+++ system/scripts/system/sudo/_create_container_dir.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+echo create_container_dir /opt/engines/run/containers/$1 >/tmp/create_container_dir
 if test $# -eq 1
   then
-  if ! test -d /opt/engines/run/containers/$1
+  if ! test -d /opt/engines/run/containers/$1 &>> /tmp/create_container_dir
    then
-	mkdir /opt/engines/run/containers/$1
+	mkdir /opt/engines/run/containers/$1 &>>  /tmp/create_container_dir
   fi
   
-  chown engines.containers /opt/engines/run/containers/$1
+  chown -R engines.containers /opt/engines/run/containers/$1 &>>  /tmp/create_container_dir
 fi 
