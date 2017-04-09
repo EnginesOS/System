59533195728fb9878ed2d8d5663447e9ed2dd90d
James L Vodanovich <james@jvodan.com>
2017-04-10 09:35:55 +1000

    system services have no attached services so dont attempt to remove

--- lib/ruby/api/system/containers/service_api/service_api_docker_actions.rb
+++ lib/ruby/api/system/containers/service_api/service_api_docker_actions.rb
@@ -1,7 +1,7 @@
 module  ServiceApiDockerActions
   def destroy_container(container)
     super
-    @engines_core.clear_service_from_registry(container, :non_persistent)
+    @engines_core.clear_service_from_registry(container, :non_persistent) unless $PROGRAM_NAME.end_with?('system_service.rb')
   end
 
 end
