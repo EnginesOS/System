237fbecbcbd1026329d264253507833ff135194a
James L Vodanovich <james@jvodan.com>
2017-04-18 08:37:52 +1000

    use X11COMMAND o start app on ssh login

--- system/templates/deployment/x11/build_scripts/finalise_envionment.sh
+++ system/templates/deployment/x11/build_scripts/finalise_envionment.sh
@@ -3,6 +3,11 @@
 apt-get -y remove  build-essential gcc make 
 apt-get -y clean 
 
+if ! test -z $command
+ then
+  echo "$command &" >/home/app/.profile
+fi 
+
 mkdir  -p /var/log/apache2/ 
 chown www-data /var/log/apache2/ 
 mkdir  -p /run/apache2/ 
