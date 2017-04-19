7870bc65d3232b7b6251783b27675539e125576c
James L Vodanovich <james@jvodan.com>
2017-04-19 12:04:52 +1000

    get_service_actionator

--- system/templates/deployment/x11/build_scripts/finalise_envionment.sh
+++ system/templates/deployment/x11/build_scripts/finalise_envionment.sh
@@ -8,7 +8,3 @@ if ! test -z $X11COMMAND
   echo "$X11COMMAND &" >/home/app/.profile
 fi 
 
-mkdir  -p /var/log/apache2/ 
-chown www-data /var/log/apache2/ 
-mkdir  -p /run/apache2/ 
-chown www-data /run/apache2/
