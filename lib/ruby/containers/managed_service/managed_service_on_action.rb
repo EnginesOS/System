63176d25457df4108413e1317fa671df863868d8
James L Vodanovich <james@jvodan.com>
2017-04-25 12:43:18 +1000

    f

--- lib/ruby/containers/managed_service/managed_service_on_action.rb
+++ lib/ruby/containers/managed_service/managed_service_on_action.rb
@@ -1,7 +1,29 @@
 module ManagedServiceOnAction
   def on_start(event_hash)
     SystemDebug.debug(SystemDebug.container_events,:ON_start_MS,event_hash)
-    super
+    @container_mutex.synchronize {
+         set_running_user
+         STDERR.puts('ONSTART_CALLED' + container_name.to_s + ';' + what.to_s)
+         SystemDebug.debug(SystemDebug.container_events,:ONSTART_CALLED, what)
+         @out_of_memory = false
+         if @consumer_less
+           @has_run = true
+           return save_state
+         end
+         # MUst register post each start as IP Changes (different post reboot)
+         register_with_dns
+         if  @has_run == false
+           add_nginx_service if @deployment_type == 'web'
+         end
+         @has_run = true
+         save_state
+         begin
+         @container_api.register_non_persistent_services(self)
+         rescue
+          return on_stop(nil) unless is_running?
+         end
+         save_state
+       }
     wait_for_startup
     service_configurations = @container_api.pending_service_configurations_hashes({service_name: @container_name, publisher_namespace: @publisher_namespace, type_path: @type_path })
     if service_configurations.is_a?(Array)
@@ -33,7 +55,17 @@ module ManagedServiceOnAction
 
   def on_create(event_hash)
     SystemDebug.debug(SystemDebug.container_events,:ON_Create_MS,event_hash)
-    super
+    @container_mutex.synchronize {
+         SystemDebug.debug(SystemDebug.container_events,:ON_Create_CALLED,event_hash)
+         @container_id = event_hash[:id]
+         @out_of_memory = false
+         @had_out_memory = false
+         @has_run = false
+         @container_api.apply_schedules(self)
+         save_state    
+         SystemDebug.debug(SystemDebug.container_events, :ON_Create_Finised, event_hash)
+       }
+       start_container
     @created = true
   end
 
@@ -46,11 +78,21 @@ module ManagedServiceOnAction
   end
   
   def on_stop(what)
-    super(what)
+    SystemDebug.debug(SystemDebug.container_events, :ONStop_CALLED, what)
+       @had_out_memory = @out_of_memory
+       @out_of_memory = false
+       save_state
+       #return true if @consumer_less    
+       # deregister_with_dns # Really its in the following nowMUst register each time as IP Changes
+       @container_api.deregister_non_persistent_services(self)
+    
   end
 
   def out_of_mem(what)
-    super(what)
+    SystemDebug.debug(SystemDebug.container_events, :OUTOF_MEM_CALLED, what)
+       @out_of_memory = true
+       @had_out_memory = true
+       save_state
   end
 
 end
