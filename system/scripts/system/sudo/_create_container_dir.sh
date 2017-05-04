d855feb96c51328fa8ae0f59aeebf90e4cb8d364
James L Vodanovich <james@jvodan.com>
2017-05-04 18:25:36 +1000

    env names \"

--- system/scripts/system/sudo/_create_container_dir.sh
+++ system/scripts/system/sudo/_create_container_dir.sh
@@ -10,6 +10,7 @@ if test $# -eq 1
    then
 	mkdir /opt/engines/etc/ssh/keys/containers/$1  &>>  /tmp/create_container_dir
 	chown -R engines.containers /opt/engines/etc/ssh/keys/containers/$1
+	chmod g+w /opt/engines/etc/ssh/keys/containers/$1
   fi
   
   chown -R engines.containers /opt/engines/run/containers/$1 &>>  /tmp/create_container_dir
