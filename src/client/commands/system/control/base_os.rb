aba7c1813efb0c8fe7d3cb91d844e9cf66cf4e3e
James L Vodanovich <james@jvodan.com>
2017-05-08 18:38:18 +1000

    timezone and locale commands

--- src/client/commands/system/control/base_os.rb
+++ src/client/commands/system/control/base_os.rb
@@ -6,8 +6,25 @@ when 'update'
 when 'shutdown'
   @route += '/' + ARGV[3]
   params = {}
-  params[:reason] =  ARGV[4]
+  params[:reason] = ARGV[4]
   perform_post(params)
-
+when 'timezone'
+  @route += '/' + ARGV[3] 
+when 'locale' 
+  @route += '/' + ARGV[3]
+when 'set'
+  case ARGV[4]
+when 'timezone'
+@route += '/' + ARGV[3]
+params = {}
+ params[:timezone] = ARGV[5]
+   perform_post(params)
+when 'locale' 
+@route += '/' + ARGV[3]
+params = {}
+ params[:country_code] = ARGV[5]
+ params[:lang_code]  = ARGV[6]
+ perform_post(params)
+end
 end
 perform_get
