8382967d94d6a09e59d53766e94dd18958cc1f19
James L Vodanovich <james@jvodan.com>
2017-05-10 11:11:49 +1000

    f

--- src/client/commands/system/control/base_os.rb
+++ src/client/commands/system/control/base_os.rb
@@ -9,22 +9,22 @@ when 'shutdown'
   params[:reason] = ARGV[4]
   perform_post(params)
 when 'timezone'
-  @route += '/' + ARGV[3] 
-when 'locale' 
+  @route += '/' + ARGV[3]
+when 'locale'
   @route += '/' + ARGV[3]
 when 'set'
   case ARGV[4]
-when 'timezone'
-@route += '/' + ARGV[3]
-params = {}
- params[:timezone] = ARGV[5]
-   perform_post(params)
-when 'locale' 
-@route += '/' + ARGV[3]
-params = {}
- params[:country_code] = ARGV[5]
- params[:lang_code]  = ARGV[6]
- perform_post(params)
-end
+  when 'timezone'
+    @route += '/' + ARGV[4]
+    params = {}
+    params[:timezone] = ARGV[5]
+    perform_post(params)
+  when 'locale'
+    @route += '/' + ARGV[4]
+    params = {}
+    params[:country_code] = ARGV[5]
+    params[:lang_code]  = ARGV[6]
+    perform_post(params)
+  end
 end
 perform_get
