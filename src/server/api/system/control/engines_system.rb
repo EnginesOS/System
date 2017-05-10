a46d6531b5c99a08ee9d4c09154df0e3a84b7f1f
James L Vodanovich <james@jvodan.com>
2017-05-10 17:37:55 +1000

    fmt

--- src/server/api/system/control/engines_system.rb
+++ src/server/api/system/control/engines_system.rb
@@ -1,4 +1,5 @@
 # @!group /system/control/engines_system
+
 # @method update_engines_system
 # @overload get '/v0/system/control/engines_system/update'
 # Update the engines system
@@ -6,6 +7,7 @@
 #  false > no udpdate available
 #  EnginesError > an error occurred
 # @return [true|false]
+# test cd /opt/engines/tests/engines_tool/system/control/engines_system; make update
 get '/v0/system/control/engines_system/update' do
   begin
     return_text(engines_api.update_engines_system_software)
@@ -13,10 +15,12 @@ get '/v0/system/control/engines_system/update' do
     send_encoded_exception(request: request, exception: e)
   end
 end
+
 # @method restart_engines_system
 # @overload get '/v0/system/control/engines_system/restart'
 #  Restart the engines system
 # @return [true]
+# test cd /opt/engines/tests/engines_tool/system/control/engines_system; make restart
 get '/v0/system/control/engines_system/restart' do
   begin
     return_text(engines_api.restart_engines_system_service)
@@ -24,10 +28,12 @@ get '/v0/system/control/engines_system/restart' do
     send_encoded_exception(request: request, exception: e)
   end
 end
+
 # @method recreate_engines_system
 # @overload get '/v0/system/control/engines_system/recreate'
 #  Recreate the engines system container
 # @return [true]
+# test cd /opt/engines/tests/engines_tool/system/control/engines_system; make recreate
 get '/v0/system/control/engines_system/recreate' do
   begin
     return_text(engines_api.recreate_engines_system_service)
@@ -35,6 +41,7 @@ get '/v0/system/control/engines_system/recreate' do
     send_encoded_exception(request: request, exception: e)
   end
 end
+
 # @method dump_engines_system_heap_stats
 # @overload get '/v0/system/control/engines_system/heap_stats'
 #  dump the heap stats engines system post CG output is written to /tmp/big/heap.dump
@@ -48,6 +55,13 @@ get '/v0/system/control/engines_system/heap_stats' do
     send_encoded_exception(request: request, exception: e)
   end
 end
+
+# @method dump_engines_system_heap_stats
+# @overload get '/v0/system/control/engines_system/heap_stats'
+#  dump the heap stats engines system post CG output is written to /tmp/big/heap.dump
+#  admin has access to this via ssh login
+#  the path is /opt/engines/tmp/system_service/system/heap.dump
+# @return [String]
 get '/v0/system/control/engines_system/dump_threads' do
   begin
     return_text(engines_api.dump_threads)
@@ -55,4 +69,5 @@ get '/v0/system/control/engines_system/dump_threads' do
     send_encoded_exception(request: request, exception: e)
   end
 end
+
 # @!endgroup
