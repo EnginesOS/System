5385b3098b562f7b3597cf17d9cbd6944f9542d4
James L Vodanovich <james@jvodan.com>
2017-05-15 09:44:23 +1000

    use olsontz time

--- lib/ruby/api/system/engines_system/system_host/base_os_system.rb
+++ lib/ruby/api/system/engines_system/system_host/base_os_system.rb
@@ -45,6 +45,16 @@ module BaseOsSystem
   end
   
   def get_timezone
-    Time.now.getlocal.zone    
+    #Time.now.getlocal.zone    
+    olsontz = `if [ -f /etc/timezone ]; then
+         cat /etc/timezone
+       elif [ -h /etc/localtime ]; then
+         readlink /etc/localtime | sed "s/\\/usr\\/share\\/zoneinfo\\///"
+       else
+         checksum=\`md5sum /etc/localtime | cut -d' ' -f1\`
+         find /usr/share/zoneinfo/ -type f -exec md5sum {} \\; | grep "^$checksum" | sed "s/.*\\/usr\\/share\\/zoneinfo\\///" | head -n 1
+       fi`.chomp
+       return "  " if olsontz.nil?
+       return olsontz
   end
 end
