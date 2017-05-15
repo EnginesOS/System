c91c494cfcc51981c2074cd6c0aae28e453493f1
James L Vodanovich <james@jvodan.com>
2017-05-15 17:23:11 +1000

    handle recreate of syslog

--- system/install/first_start.bash
+++ system/install/first_start.bash
@@ -11,10 +11,12 @@ system_service.rb registry destroy >>&/tmp/first_start.log
 system_service.rb registry create >>&/tmp/first_start.log
 system_service.rb system destroy >>&/tmp/first_start.log
 system_service.rb system create >>&/tmp/first_start.log
-engines service syslog create >>&/tmp/first_start.log
 engines service dns stop >>&/tmp/first_start.log
 engines service dns destroy  >>&/tmp/first_start.log
 engines service dns create >>&/tmp/first_start.log
+engines service syslog stop >>&/tmp/first_start.log
+engines service syslog destroy >>&/tmp/first_start.log
+engines service syslog create >>&/tmp/first_start.log
 engines service auth stop >>&/tmp/first_start.log
 engines service auth destroy >>&/tmp/first_start.log
 engines service auth create >>&/tmp/first_start.log
