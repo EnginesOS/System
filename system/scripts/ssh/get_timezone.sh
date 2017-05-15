7e9f43fe26a58c7d2d7744b19de689021f17bb7c
James L Vodanovich <james@jvodan.com>
2017-05-15 11:34:33 +1000

    f

--- system/scripts/ssh/get_timezone.sh
+++ system/scripts/ssh/get_timezone.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-if [ -f /etc/timezone ]; then
-      cat /etc/timezone
-    elif [ -h /etc/localtime ]; then
-      readlink /etc/localtime | sed "s/\\/usr\\/share\\/zoneinfo\\///"
-    else
-      checksum=\`md5sum /etc/localtime | cut -d' ' -f1\`
-      find /usr/share/zoneinfo/ -type f -exec md5sum {} \\; | grep "^$checksum" | sed "s/.*\\/usr\\/share\\/zoneinfo\\///" | head -n 1
-    fi
+     
+ readlink /etc/localtime | sed "s/\/usr\/share\/zoneinfo\///" | sed "s/\.\.//"
+ 
