fc2c0c8c2037e3350f62651b418a6b5898027a9b
James L Vodanovich <james@jvodan.com>
2017-05-15 20:19:27 +1000

    f

--- system/install/first_start.bash
+++ system/install/first_start.bash
@@ -5,32 +5,32 @@ if test -f /opt/engines/run/system/flags/first_start_complete
   exit 127
  fi
   
-system_service.rb system stop  >&/tmp/first_start.log
-system_service.rb registry stop >>&/tmp/first_start.log
-system_service.rb registry destroy >>&/tmp/first_start.log
-system_service.rb registry create >>&/tmp/first_start.log
-system_service.rb system destroy >>&/tmp/first_start.log
-system_service.rb system create >>&/tmp/first_start.log
-engines service dns stop >>&/tmp/first_start.log
-engines service dns destroy  >>&/tmp/first_start.log
-engines service dns create >>&/tmp/first_start.log
-engines service syslog stop >>&/tmp/first_start.log
-engines service syslog destroy >>&/tmp/first_start.log
-engines service syslog create >>&/tmp/first_start.log
-engines service cert_auth stop >>&/tmp/first_start.log
-engines service cert_auth destroy >>&/tmp/first_start.log
-engines service cert_auth create >>&/tmp/first_start.log
-engines service auth stop >>&/tmp/first_start.log
-engines service auth destroy >>&/tmp/first_start.log
-engines service auth create >>&/tmp/first_start.log
-engines service cron create >>&/tmp/first_start.log
-engines service mysql_server create >>&/tmp/first_start.log
-engines service backup create >>&/tmp/first_start.log
-engines service logrotate create >>&/tmp/first_start.log
-engines service ftp create >>&/tmp/first_start.log
-engines service backup create >>&/tmp/first_start.log
-engines service nginx create >>&/tmp/first_start.log
-engines service smtp create >>&/tmp/first_start.log
+system_service.rb system stop  >/tmp/first_start.log
+system_service.rb registry stop >>/tmp/first_start.log
+system_service.rb registry destroy >>/tmp/first_start.log
+system_service.rb registry create >>/tmp/first_start.log
+system_service.rb system destroy >>/tmp/first_start.log
+system_service.rb system create >>/tmp/first_start.log
+engines service dns stop >> /tmp/first_start.log
+engines service dns destroy  >>/tmp/first_start.log
+engines service dns create >> /tmp/first_start.log
+engines service syslog stop >>/tmp/first_start.log
+engines service syslog destroy >> /tmp/first_start.log
+engines service syslog create >> /tmp/first_start.log
+engines service cert_auth stop >>/tmp/first_start.log
+engines service cert_auth destroy >>/tmp/first_start.log
+engines service cert_auth create >>/tmp/first_start.log
+engines service auth stop >>/tmp/first_start.log
+engines service auth destroy >>/tmp/first_start.log
+engines service auth create >>/tmp/first_start.log
+engines service cron create >>/tmp/first_start.log
+engines service mysql_server create >>/tmp/first_start.log
+engines service backup create >>/tmp/first_start.log
+engines service logrotate create >>/tmp/first_start.log
+engines service ftp create >>/tmp/first_start.log
+engines service backup create >>/tmp/first_start.log
+engines service nginx create >>/tmp/first_start.log
+engines service smtp create >>/tmp/first_start.log
  touch /opt/engines/run/system/flags/first_start_complete
  cat /tmp/first_start.log
  
