6718ab1937015e344770bec5366d72fe4bc4c514
James L Vodanovich <james@jvodan.com>
2017-05-19 11:51:45 +1000

    match by container name

--- lib/ruby/api/system/docker/docker_api/event_watcher/docker_event_watcher.rb
+++ lib/ruby/api/system/docker/docker_api/event_watcher/docker_event_watcher.rb
@@ -211,7 +211,9 @@ class DockerEventWatcher  < ErrorsApi
     @event_listeners.values.each do |listener|
       unless listener.container_name.nil?
           STDERR.puts('matching ' + listener.container_name.to_s + ' with ' + hash[:id].to_s)
-        next unless hash[:id] == listener.container_name
+        next unless hash.key?(:Attributes)
+        next unless hash[:Attributes].key?(:container_name)         
+        next unless hash[:Attributes][:container_name] == listener.container_name
       end
       log_exception(r) if (r = listener.trigger(hash)).is_a?(StandardError)
     end
