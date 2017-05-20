6110be8d297f02f79f56a8d9a6da19dd11b5b5b2
James L Vodanovich <james@jvodan.com>
2017-05-21 09:42:39 +1000

    f

--- lib/ruby/api/system/memory_statistics.rb
+++ lib/ruby/api/system/memory_statistics.rb
@@ -59,7 +59,7 @@ module MemoryStatistics
       else
         STDERR.puts('no_cgroup_file for ' + container.container_name + ':' + path.to_s, path)
         SystemUtils.log_error_mesg('no_cgroup_file for ' + container.container_name + ':' + path.to_s, path)
-        ret_val  = self.empty_container_result
+        ret_val  = self.empty_container_result(container)
       end
     end
     ret_val
@@ -72,7 +72,7 @@ module MemoryStatistics
     }
   end
 
-  def self.empty_container_result
+  def self.empty_container_result(container)
     {
       maximum: 0,
       current: 0,
