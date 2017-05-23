5d838e693fd0ea7189c88709d4e0cdcb30ff65fc
James L Vodanovich <james@jvodan.com>
2017-05-23 12:53:35 +1000

    use wait for in recreate

--- system/scripts/ssh/_recreate_system_service.sh
+++ system/scripts/ssh/_recreate_system_service.sh
@@ -2,11 +2,18 @@
  touch /opt/engines/run/system/flags/engines_restarting
 
  /opt/engines/bin/system_service.rb system stop
+ /opt/engines/bin/system_service.rb system wait_for stop 10
  /opt/engines/bin/system_service.rb system destroy
- sleep 6
+ /opt/engines/bin/system_service.rb system wait_for destroy 10
+
   
   /opt/engines/bin/system_service.rb system create
-
- res=$?
+res=`/opt/engines/bin/system_service.rb system wait_for_startup 45`
+ if test $res = 'false'
+  then
+   res = 127
+  else
+   res 0
+  fi
 rm	/opt/engines/run/system/flags/engines_restarting
 exit $res
