c4b584ef59bfc16ba1f830f215522b2e5afa5eac
James L Vodanovich <james@jvodan.com>
2017-05-23 13:05:23 +1000

    use system service to control system

--- system/scripts/ssh/_restart_system_service.sh
+++ system/scripts/ssh/_restart_system_service.sh
@@ -2,9 +2,15 @@
  touch /opt/engines/run/system/flags/engines_restarting
 
  /opt/engines/bin/system_service.rb system stop
- /opt/engines/bin/system_service.rb system wait_for stop 10
+ /opt/engines/bin/system_service.rb system wait_for stop 30
  /opt/engines/bin/system_service.rb system start
- /opt/engines/bin/system_service.rb system wait_for_startup 45
+res=`/opt/engines/bin/system_service.rb system wait_for_startup 45`
+ if test $res = 'false'
+  then
+   res = 127
+  else
+   res 0
+  fi
 
  res=$?
 rm	/opt/engines/run/system/flags/engines_restarting
