fb5ad85750fbe304ef2a26fa5c2b3ee4fb28a426
James L Vodanovich <james@jvodan.com>
2017-05-23 12:59:06 +1000

    use system and not docker for restart

--- system/scripts/ssh/_restart_system_service.sh
+++ system/scripts/ssh/_restart_system_service.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
  touch /opt/engines/run/system/flags/engines_restarting
 
- docker stop system
- docker start system
+ /opt/engines/bin/system_service.rb system stop
+ /opt/engines/bin/system_service.rb system wait_for stop 10
+ /opt/engines/bin/system_service.rb system start
+ /opt/engines/bin/system_service.rb system wait_for_startup 45
+
  res=$?
 rm	/opt/engines/run/system/flags/engines_restarting
 exit $res
