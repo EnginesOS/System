94820eb06aded2ab4c31cbb2156c190527f2bba6
James L Vodanovich <james@jvodan.com>
2017-05-24 11:27:29 +1000

    move volume builder to container_api

---
+++ lib/ruby/api/system/containers/container_api/volume_builder.rb
@@ -0,0 +1,13 @@
+module ContainerApiVolumeBuilder
+  def run_volume_builder(container, username)
+    volbuilder = @core_api.loadManagedUtility('fsconfigurator')
+    result = volbuilder.execute_command(:setup_engine, {
+    volume: '/',
+    fw_user: username.to_s,
+    target: container.container_name,
+    target_container: container.container_name,
+    data_gid: container.data_gid.to_s
+  })
+    raise EngineBuilderException.new(error_hash('volbuild problem ' + result.to_s, result)) unless result[:result] == 0
+  end
+end
