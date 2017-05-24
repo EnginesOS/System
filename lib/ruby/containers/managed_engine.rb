3685c7173f6a2766988888c90f8512478d7aad18
James L Vodanovich <james@jvodan.com>
2017-05-24 12:14:50 +1000

    fmt

--- lib/ruby/containers/managed_engine.rb
+++ lib/ruby/containers/managed_engine.rb
@@ -1,7 +1,7 @@
 class ManagedEngine < ManagedContainer
   require '/opt/engines/lib/ruby/containers/managed_container.rb'
 
-  def initialize(build_params, runtime_params , core_api)
+  def initialize(build_params, runtime_params, core_api)
     @container_mutex = Mutex.new
     @memory = build_params[:memory]
     @hostname = build_params[:host_name]
@@ -49,11 +49,11 @@ class ManagedEngine < ManagedContainer
 
   def on_start(event_hash)
     set_running_user
-  #  STDERR.puts('ONSTART_CALLED' + container_name.to_s + ';' + event_hash.to_s)
+    #  STDERR.puts('ONSTART_CALLED' + container_name.to_s + ';' + event_hash.to_s)
     #    STDERR.puts('ONS TART @service_builder.run_volume_builder  is a' +  @volume_service_builder.to_s )
     if @volume_service_builder == true
-    #  STDERR.puts('Running @service_builder.run_volume_builder ' )
-      @container_api.run_volume_builder(self, @cont_userid) 
+      #  STDERR.puts('Running @service_builder.run_volume_builder ' )
+      @container_api.run_volume_builder(self, @cont_userid)
       @volume_service_builder = false
       @save_container = false
     end
@@ -63,7 +63,7 @@ class ManagedEngine < ManagedContainer
   def volume_service_builder=(is_built)
     #raise EnginesException,ew('Error alread run', :error) unless @volume_service_builder.nil?
     #   STDERR.puts(' SET @service_builder.run_volume_builder ' +  builder.to_s )
-       @volume_service_builder = is_built
+    @volume_service_builder = is_built
   end
 
   def load_blueprint
@@ -72,7 +72,7 @@ class ManagedEngine < ManagedContainer
   end
 
   def plugins_path
-    return '/plugins/'
+    '/plugins/'
   end
 
   def extract_plugins
