4815247bdd09d7f17a81330482fcfab9390f3546
James L Vodanovich <james@jvodan.com>
2017-05-26 12:35:52 +1000

    f force meth names

--- lib/ruby/service_manager/sm_service_forced_methods.rb
+++ lib/ruby/service_manager/sm_service_forced_methods.rb
@@ -12,19 +12,19 @@ module SmServiceForcedMethods
     system_registry_client.remove_from_services_registry(service_hash)
   end
 
-  def force_register_attached_service(service_query)
+  def force_register_non_persistent_service(service_query)
     complete_service_query = set_top_level_service_params(service_query, service_query[:parent_engine])
     service_hash = retrieve_engine_service_hash(complete_service_query)
     add_to_managed_service(service_hash)
   end
 
-  def force_deregister_attached_service(service_query)
+  def force_deregister_non_persistent_service(service_query)
     complete_service_query = set_top_level_service_params(service_query, service_query[:parent_engine])
     service_hash = retrieve_engine_service_hash(complete_service_query)
     remove_from_managed_service(service_hash)
   end
 
-  def force_reregister_attached_service(service_query)
+  def force_reregister_non_persistent_service(service_query)
     complete_service_query = set_top_level_service_params(service_query, service_query[:parent_engine])
     service_hash = retrieve_engine_service_hash(complete_service_query)
     remove_from_managed_service(service_hash)
