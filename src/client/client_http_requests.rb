1d563438a1d938dd95370ebd6e8b9bed4d684c17
James L Vodanovich <james@jvodan.com>
2017-06-01 09:21:50 +1000

    add retries

--- src/client/client_http_requests.rb
+++ src/client/client_http_requests.rb
@@ -5,70 +5,11 @@ require 'excon'
 
 require 'yajl'
 
-##used by events
-#def get_json_stream(path)
-#  require 'yajl'
-#  chunk = ''
-#
-#  uri = URI(@base_url + path)
-#  Net::HTTP.start(uri.host, uri.port)  do |http|
-#    req = Net::HTTP::Get.new(uri)
-#    req['access_token'] = ENV['access_token']
-#    req['HTTP_access_token'] = ENV['access_token']
-#    parser = Yajl::Parser.new(:symbolize_keys => true)
-#    http.request(req) { |resp|
-#      resp.read_body do |chunk|
-#        begin
-#          next if chunk == "\0" || chunk == "\n"
-#          hash = parser.parse(chunk) do |hash|
-#            p hash.to_json
-#          end
-#          #dont panic on bad json_parser as it is the \0 keep alive
-#        rescue StandardError => e
-#          p e
-#          STDERR.puts('_'+ chunk + '_')
-#          next
-#        end
-#      end
-#
-#    }
-#    exit
-#  end
-#rescue StandardError => e
-#  #Should goto to error hanlder but this is a script
-#  p e.to_s
-#  p e.backtrace.to_s
-#end
-
-## Used By builder command
-#def get_stream(path, ostream=STDOUT)
-#  #require 'yajl'
-#  chunk = ''
-#
-#  uri = URI(@base_url + path)
-#  req = Net::HTTP::Get.new(uri)
-#  req['Access_Token'] = ENV['access_token']
-#
-#  Net::HTTP.start(uri.host, uri.port)  do |http|
-#    http.read_timeout = 600
-#    http.request(req) { |resp|
-#      resp.read_body do |chunk|
-#        #hash = parser.parse(chunk) do |hash|
-#        ostream.write(chunk)
-#        #end
-#      end
-#    }
-#    exit
-#  end
-#rescue StandardError => e
-#  p e.to_s
-#  p chunk.to_s
-#  p e.backtrace.to_s
-#end
 
 
 
 def connection( content_type = 'application/json_parser')
+  retries = 0
   headers = {}
   headers['content_type'] = content_type
   headers['ACCESS_TOKEN'] = load_token
@@ -82,7 +23,12 @@ def connection( content_type = 'application/json_parser')
   :headers => headers) if @connection.nil?
   @connection
 rescue StandardError => e
-  STDERR.puts('Failed to open base url ' + @base_url.to_s)
+   if retries < 10
+     retries +=1
+     sleep 1
+     retry
+   end
+  STDERR.puts('Failed to open base url ' + @base_url.to_s + ' after ' + retries.to_s = ' attempts')
 end
 
 def rest_del(uri, params=nil, time_out=23)
