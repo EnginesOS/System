15f30c684403541787c91fbcf2211afca81a9737
James L Vodanovich <james@jvodan.com>
2017-06-05 11:37:17 +1000

    use framework_specific

--- lib/ruby/engine_builder/blueprint_readers/1/versioned_blueprint_reader.rb
+++ lib/ruby/engine_builder/blueprint_readers/1/versioned_blueprint_reader.rb
@@ -168,5 +168,15 @@ class VersionedBlueprintReader < BluePrintReader
     read_schedules
     read_repos
   end
+def read_apache_htaccess_files
+  @apache_htaccess_files = @blueprint[:software][:framework_specific][:apache_htaccess_files] if @blueprint[:software][:apache_htaccess_files].is_a?(Array)
+end
 
+def read_custom_php_inis
+  @custom_php_inis = @blueprint[:software][:framework_specific][:custom_php_inis] if @blueprint[:software][:custom_php_inis].is_a?(Array)
+end
+
+def read_apache_httpd_configurations
+  @apache_httpd_configurations = @blueprint[:software][:framework_specific][:apache_httpd_configurations] if  @blueprint[:software][:apache_httpd_configurations].is_a?(Array)
+end
 end
