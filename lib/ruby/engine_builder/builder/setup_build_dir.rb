2c72e2bf7bef3d769c964df32b457d2c97bb34d6
James L Vodanovich <james@jvodan.com>
2017-06-05 12:35:26 +1000

    htaccess_files

--- lib/ruby/engine_builder/builder/setup_build_dir.rb
+++ lib/ruby/engine_builder/builder/setup_build_dir.rb
@@ -61,7 +61,6 @@ end
 
 def create_httaccess
   if @blueprint_reader.apache_htaccess_files
-    FileUtils.mkdir_p(basedir + File.dirname(SystemConfig.htaccessSourceDir))
     @blueprint_reader.apache_htaccess_files.each do |htaccess_hash|
       write_software_file(SystemConfig.htaccessSourceDir + htaccess_hash[:directory] + '/.htaccess', htaccess_hash[:content])
     end
