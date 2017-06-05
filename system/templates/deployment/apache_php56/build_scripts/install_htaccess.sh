2e2a17492c57313b0a73091df7aa2f5a598466ca
James L Vodanovich <james@jvodan.com>
2017-06-05 17:05:20 +1000

    f

--- system/templates/deployment/apache_php56/build_scripts/install_htaccess.sh
+++ system/templates/deployment/apache_php56/build_scripts/install_htaccess.sh
@@ -10,13 +10,13 @@ htaccess_files=`find /home/engines/htaccess_files/ -type f |grep -v keep_me`
                 dest_file=`echo $file | sed "/^.*htaccess_files\//s///"`
                 dest_dir=`dirname $dest_file`
                 
-                mkdir -p $dest_dir
+                mkdir -p app/$dest_dir
                 if test -h $dest_file
                 then
                 	dest_file=`ls -l $dest_file |cut -f2 -d">"`
                 fi
             
-                 cp $file $dest_file
+                 cp $file app/$dest_file
         done
 fi
       
