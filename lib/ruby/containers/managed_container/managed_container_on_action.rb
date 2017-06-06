335c349cd7e1ba0900f02a5788560fd2a78a4db4
James L Vodanovich <james@jvodan.com>
2017-06-06 11:31:55 +1000

    rm dbg

--- lib/ruby/containers/managed_container/managed_container_on_action.rb
+++ lib/ruby/containers/managed_container/managed_container_on_action.rb
@@ -10,10 +10,8 @@ module ManagedContainerOnAction
         @has_run = true
         return save_state
       end
-      # MUst register post each start as IP Changes (different post reboot)
-      STDERR.puts(' Container ' + self.container_name.to_s + ' reg dns ' + self.conf_register_dns.to_s)
-      register_with_dns
-      STDERR.puts(' HAS RUN ' + @has_run.to_s + ' Deplyment type ' + @deployment_type.to_s)
+      # MUst register post each start as IP Changes (different post reboot)     
+      register_with_dns    
       if  @has_run == false
         add_nginx_service if @deployment_type == 'web'
       end
