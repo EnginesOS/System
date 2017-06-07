ae6260b358ca44adeb0b9b02e29bd00b2feda2fc
James L Vodanovich <james@jvodan.com>
2017-06-07 10:20:55 +1000

    make sure system responds to reboot request

--- system/scripts/ssh/restart_base_os.sh
+++ system/scripts/ssh/restart_base_os.sh
@@ -2,7 +2,7 @@
  touch /opt/engines/run/system/flags/engines_rebooting
  if ! test -f /etc/engines_reboot_disabled
  	then
-		sudo  -n   /opt/engines/system/scripts/ssh/sudo/_restart_base_os.sh 
+		sudo  -n   /opt/engines/system/scripts/ssh/sudo/_restart_base_os.sh & 
 		echo restarting
 		exit
 	fi
