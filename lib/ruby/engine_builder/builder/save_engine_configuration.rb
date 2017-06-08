42c81ef72c04a20b83b15cdf2d810696da0298e2
James L Vodanovich <james@jvodan.com>
2017-06-08 14:58:10 +1000

    write serialized object not puts

--- lib/ruby/engine_builder/builder/save_engine_configuration.rb
+++ lib/ruby/engine_builder/builder/save_engine_configuration.rb
@@ -20,14 +20,13 @@ module SaveEngineConfiguration
     return true if actionators.nil?
     FileUtils.mkdir_p(ContainerStateFiles.actionator_dir(container)) unless Dir.exist?(ContainerStateFiles.actionator_dir(container))
     serialized_object = YAML.dump(actionators)
-
     f = File.new(ContainerStateFiles.actionator_dir(container) + '/actionators.yaml', File::CREAT | File::TRUNC | File::RDWR, 0644)
-    f.puts(serialized_object)
+    f.write(serialized_object)
     f.flush()
     f.close
   end
 
-  def  write_services(mc, attached_services)
+  def write_services(mc, attached_services)
   end
 
   def write_variables(mc, environments)
