3f56f7983a086c4ffa2e3a76809a812e0f89611c
James L Vodanovich <james@jvodan.com>
2017-06-09 12:48:51 +1000

    f

--- lib/ruby/engine_builder/config_file_writer.rb
+++ lib/ruby/engine_builder/config_file_writer.rb
@@ -8,7 +8,7 @@ class ConfigFileWriter
   end
 
   def self.write_templated_file(templater, filename, content)
-    return SystemUtils.log_error("NO Content ", filename , content) if content.nil?
+    return SystemUtils.log_error('NO Content for ' + filename.to_s) if content.nil?
     content.gsub!(/\r/, '')
     dir = File.dirname(filename)
     FileUtils.mkdir_p(dir) unless Dir.exist?(dir)
