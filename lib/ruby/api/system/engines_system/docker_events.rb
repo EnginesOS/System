66b384ef46bf35383ae422b17fb0d758d4948ebf
James L Vodanovich <james@jvodan.com>
2017-06-11 13:15:05 +1000

    t

--- lib/ruby/api/system/engines_system/docker_events.rb
+++ lib/ruby/api/system/engines_system/docker_events.rb
@@ -52,9 +52,10 @@ module DockerEvents
       unless is_aready?(what, container.read_state)
         # STDERR.puts(' Wait on READ ' + container.container_name.to_s + ' for ' + what )
         begin
-          d =  pipe_in.read
+          d = pipe_in.read
           puts.STDERR.puts(' READ ' + d.to_s)
         rescue
+          puts.STDERR.puts(' READ RESCUE')
         end
       end
       pipe_in.close unless pipe_in.closed?
