01200d9195afd3f4dcf582e0479395960148e56e
James L Vodanovich <james@jvodan.com>
2017-06-12 10:36:23 +1000

    dbg debug

--- lib/ruby/engine_builder/blueprint_readers/blueprint_reader.rb
+++ lib/ruby/engine_builder/blueprint_readers/blueprint_reader.rb
@@ -222,7 +222,7 @@ class BluePrintReader
     @pecl_modules = []
     @npm_modules = []
     @lua_modules = []
-
+    
     pkg_modules = @blueprint[:software][:modules]
     return true unless pkg_modules.is_a?(Array)  # not an error just nada
     pkg_modules.each do |pkg_module|
