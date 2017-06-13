773aeaa0001008e8f7abc4c0b15de4c0ccfc8a52
James L Vodanovich <james@jvodan.com>
2017-06-13 16:33:19 +1000

    f

--- src/client/commands/containers/utility.rb
+++ src/client/commands/containers/utility.rb
@@ -3,6 +3,6 @@
 case ARGV[2]
 when 'wait_for'
   @route += '/' + ARGV[1] + '/' + ARGV[2]
-  @route += '/' + ARGV[3] if ARGV[1].count == 4
+  @route += '/' + ARGV[3] if ARGV[1].length == 4
   perform_get
 end
