7bcd43b32acae7e859803bb7bbd0cc6c36ca76c3
James L Vodanovich <james@jvodan.com>
2017-06-16 15:49:25 +1000

    tidy

--- system/templates/deployment/global/engines/functions/start_functions.sh
+++ system/templates/deployment/global/engines/functions/start_functions.sh
@@ -1,32 +1,36 @@
 function wait_for_debug {
 if ! test -z "$Engines_Debug_Run"
  then
-		echo "Stopped by Sleeping for 500 seconds to allow debuging"
-  	 	sleep 500 &
-  	 	echo -n " $!" >> $PID_FILE
-  	 	wait
-  	 fi  	 
+  if test "$Engines_Debug_Run" = true
+   then  
+     echo "Stopped by Sleeping for 500 seconds to allow debuging"
+     sleep 500 &
+     echo -n " $!" >> $PID_FILE
+     wait
+   fi
+fi  	 
  }
   
 function volume_setup {	 
 if test  ! -f /engines/var/run/flags/volume_setup_complete
-   then
-   echo "Waiting for Volume setup to Complete "
+ then
+   echo "Waiting for Volume setup to Complete"
  	while test ! -f /engines/var/run/flags/volume_setup_complete
  	  do
  	  echo  "."
  		sleep 4
  	 done
- 	 echo "Volume setup to Complete "
+ echo "Volume setup to Complete "
 fi
 }
- function dynamic_persistence {	  
+
+function dynamic_persistence {	  
 if test -f "$VOLDIR/.dynamic_persistence"
   then
 	if ! test -f /home/app/.dynamic_persistence_restored
-	then
+	  then
  		/home/engines/scripts/restore_dynamic_persistence.sh
- 		 echo "Dynamic persistence restore Complete "
+ 		echo "Dynamic persistence restore Complete"
  	fi
  fi
 }
@@ -35,18 +39,18 @@ if test -f "$VOLDIR/.dynamic_persistence"
 function first_run {
 if ! test -f /engines/var/run/flags/first_run_done
  then
-	 touch /engines/var/run/flags/first_run_done
+   touch /engines/var/run/flags/first_run_done
  else		
-	   if test -f /home/engines/scripts/post_install.sh
-		then 				
-			echo "Has Post install"
-			 if ! test -f /engines/var/run/flags/post_install.done
-			  then
-				echo "Running Post Install"
-				/bin/bash /home/engines/scripts/post_install.sh 							
-				touch /engines/var/run/flags/post_install.done
-			  fi
-		fi
+	if test -f /home/engines/scripts/post_install.sh
+	 then 				
+	   echo "Has Post install"
+		 if ! test -f /engines/var/run/flags/post_install.done
+		  then
+			echo "Running Post Install"
+			/bin/bash /home/engines/scripts/post_install.sh 							
+			touch /engines/var/run/flags/post_install.done
+		 fi
+	fi
 fi	
 }
 
@@ -55,9 +59,9 @@ if test -f /engines/var/run/flags/restart_required
  then
   if test -f /engines/var/run/flags/started_once
    then
-  		rm -rf /engines/var/run/flags/restart_required
+  	rm -rf /engines/var/run/flags/restart_required
   else
-  	touch  /engines/var/run/flags/started_once
+    touch  /engines/var/run/flags/started_once
   fi
 fi
 }
@@ -66,12 +70,12 @@ function custom_start {
 #if not blocking continues
 if test -f /home/engines/scripts/custom_start.sh
  then
-	 echo "Custom start"	   
-	 result=`/home/engines/scripts/custom_start.sh`
-	  if test "$result" = "exit"
-		then 
-			wait_for_debug
-			exit
+   echo "Custom start"	   
+   result=`/home/engines/scripts/custom_start.sh`
+	 if test "$result" = "exit"
+	  then 
+		wait_for_debug
+		exit
 	  fi
 fi
 }
@@ -86,58 +90,56 @@ fi
 
 function start_apache {
 mkdir -p /var/log/apache2/ >& /dev/null
-
-	if test -f /home/engines/scripts/blocking.sh 
-		then
-		   /usr/sbin/apache2ctl -DFOREGROUND &		  
-		   /home/engines/scripts/blocking.sh  &
-		   echo  -n " $!" >> $PID_FILE
-	else		
-		  /usr/sbin/apache2ctl -DFOREGROUND &
-	fi
+if test -f /home/engines/scripts/blocking.sh 
+ then
+   /usr/sbin/apache2ctl -DFOREGROUND &		  
+   /home/engines/scripts/blocking.sh  &
+   echo  -n " $!" >> $PID_FILE
+ else		
+   /usr/sbin/apache2ctl -DFOREGROUND &
+fi
 sleep 2
 apache_pid=`cat /var/run/apache2/apache2.pid`
 echo -n " $apache_pid" >> $PID_FILE
 }
 
 function configure_passenger {
-       cp /home/ruby_env /home/.env_vars
-        for env_name in `cat /home/app.env `
-  	     do
-   		   if ! test -z  "${!env_name}"
-            then
-        	  #val=`echo ${!env_name} | sed "/ /s//\\ /g"`
-  	      	  echo  "passenger_env_var $env_name \"${!env_name}\";"   >> /home/.env_vars
-  	       fi
-  	     done
-       echo " passenger_env_var RAILS_ENV $RAILS_ENV;" >> /home/.env_vars
-       echo " passenger_env_var SECRET_KEY_BASE $SECRET_KEY_BASE;" >> /home/.env_vars
+cp /home/ruby_env /home/.env_vars
+  for env_name in `cat /home/app.env `
+  	do
+   	  if ! test -z  "${!env_name}"
+        then
+  	      echo  "passenger_env_var $env_name \"${!env_name}\";" >> /home/.env_vars
+  	  fi
+  	done
+echo " passenger_env_var RAILS_ENV $RAILS_ENV;" >> /home/.env_vars
+echo " passenger_env_var SECRET_KEY_BASE $SECRET_KEY_BASE;" >> /home/.env_vars
  }
  
 function start_nginx {
-	mkdir /var/log/nginx >& /dev/null
-     if test -f /home/ruby_env 
-      then
-        configure_passenger
-	fi	
-	if test -f /home/engines/scripts/blocking.sh 
-	 then
-		nginx &
-	    echo -n " $!" >>  $PID_FILE
-		 /home/engines/scripts/blocking.sh  &
-		 echo -n " $!" >>  $PID_FILE
+mkdir /var/log/nginx >& /dev/null
+ if test -f /home/ruby_env 
+   then
+     configure_passenger
+  fi	
+  if test -f /home/engines/scripts/blocking.sh 
+	then
+	  nginx &
+	  echo -n " $!" >>  $PID_FILE
+	  /home/engines/scripts/blocking.sh  &
+	  echo -n " $!" >>  $PID_FILE
 	else		
-		nginx &
-	    echo -n " $!" >>  $PID_FILE
-	fi
+	  nginx &
+	  echo -n " $!" >>  $PID_FILE
+  fi
 }
 
 function launch_app {
-	 /home/startwebapp.sh 
-	   if test -f /home/engines/scripts/blocking.sh
-		 then
-		 	/home/engines/scripts/blocking.sh &
-			blocking_pid=$!
-		 	echo -n " $blocking_pid " >>  $PID_FILE
-	  fi
+/home/startwebapp.sh 
+ if test -f /home/engines/scripts/blocking.sh
+   then
+	/home/engines/scripts/blocking.sh &
+	blocking_pid=$!
+	echo -n " $blocking_pid " >>  $PID_FILE
+ fi
 }
