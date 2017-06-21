2c4658e1718aeb81a74c2a2b0e315ec5d154f3eb
James L Vodanovich <james@jvodan.com>
2017-06-22 08:56:01 +1000

    f

--- lib/ruby/api/system/engines_core/engines_core_preferences.rb
+++ lib/ruby/api/system/engines_core/engines_core_preferences.rb
@@ -13,7 +13,7 @@ module EnginesCorePreferences
 
   def set_hostname(hostname)
     update_service_configuration( {
-      service_name: 'mgmt',
+      service_name: 'system',
       configurator_name: 'hostname',
       variables: {
       hostname: hostname,
