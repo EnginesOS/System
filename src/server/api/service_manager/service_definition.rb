1a338bfcd499d753896b02cc70f7c4edacafd65c
James L Vodanovich <james@jvodan.com>
2017-06-22 10:06:41 +1000

    drop debug

--- src/server/api/service_manager/service_definition.rb
+++ src/server/api/service_manager/service_definition.rb
@@ -8,7 +8,6 @@ get '/v0/service_manager/service_definitions/:publisher_namespace/*' do
   begin
     params[:type_path] = params['splat'][0]
     cparams = assemble_params(params, [:publisher_namespace, :type_path], [])
-      STDERR.puts('params ' + params.to_s + ' cparams ' + cparams.to_s)
   return_json(engines_api.get_service_definition(cparams))
   rescue StandardError => e
     send_encoded_exception(request: request, exception: e)
