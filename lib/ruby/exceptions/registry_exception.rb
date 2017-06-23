bd35d64b59406264137f871f0b1484a71b4ba98d
James L Vodanovich <james@jvodan.com>
2017-06-23 17:11:07 +1000

    f

--- lib/ruby/exceptions/registry_exception.rb
+++ lib/ruby/exceptions/registry_exception.rb
@@ -2,7 +2,7 @@ class RegistryException < EnginesException
   attr_reader :status
   def initialize( hash)
     @status = hash[:status] if hash.is_a?(Hash)
-    STDERR.puts('RegistryException ' + hash.to_s) unless @level == :warning 
+    STDERR.puts('RegistryException ' + hash.to_s) unless hash[:error_type] == :'warning' 
     super(hash)
   end
 end
