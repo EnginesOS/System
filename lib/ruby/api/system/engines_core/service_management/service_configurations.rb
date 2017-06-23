be95b70f22b0b326970a77800abb8be382ef3a46
James L Vodanovich <james@jvodan.com>
2017-06-23 17:03:18 +1000

    fxi and rmdbg

--- lib/ruby/api/system/engines_core/service_management/service_configurations.rb
+++ lib/ruby/api/system/engines_core/service_management/service_configurations.rb
@@ -93,7 +93,6 @@ module ServiceConfigurations
       end
       # set config on reunning service
       configurator_result = service.run_configurator(service_param)
-STDERR.puts( " COFNGITO RES " + configurator_result.to_s )
       raise EnginesException.new(error_hash('Service configurator erro@core_api.r Got:' + configurator_result.to_s, " For:" +service_param.to_s)) unless configurator_result.is_a?(Hash)
         service_manager.update_service_configuration(service_param)
       raise EnginesException.new(error_hash('Service configurator error @core_ap Got:' + configurator_result.to_s, " For:" +service_param.to_s )) unless configurator_result[:result] == 0 || configurator_result[:stderr].start_with?('Warning')
