2f8031acaace2b72387bc1eab2736a0ffac86a39
James L Vodanovich <james@jvodan.com>
2017-06-26 17:43:18 +1000

    user

--- src/client/commands/system/user.rb
+++ src/client/commands/system/user.rb
@@ -1,13 +1,13 @@
 @route += ''
 
 if ARGV[2] == 'change'
-
   case ARGV[3]
   when 'email'
-    pargs = { user_name: ARGV[4], current_password: ARGV[5], email: ARGV[6]}
+    pargs = { current_password: ARGV[5], email: ARGV[6]}
   when 'password'
-    pargs = { user_name: ARGV[4], current_password: ARGV[5], new_password: ARGV[6]}
+    pargs = { current_password: ARGV[5], new_password: ARGV[6]}
   end
+@route += '/' + ARGV[4]
   perform_post(pargs)
 else
   @route += '/' + ARGV[2]
