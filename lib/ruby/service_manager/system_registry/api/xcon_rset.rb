ed41df9f6f629f6be9930daff2bd107225cdea6b
James L Vodanovich <james@jvodan.com>
2017-06-27 17:29:03 +1000

    dbg

--- lib/ruby/service_manager/system_registry/api/xcon_rset.rb
+++ lib/ruby/service_manager/system_registry/api/xcon_rset.rb
@@ -27,7 +27,7 @@ rescue StandardError => e
 end
 
 def reopen_connection
-  # STDERR.puts('re open connec' )
+  # STDERR.puts('re open connec')
   @connection.reset
   @connection = nil
   @connection = connection
@@ -46,10 +46,9 @@ def rest_get(path,params = nil, time_out = 120, _headers = nil)
   req[:query] = q unless q.nil?
   r = connection.request(req)
   parse_xcon_response(r)
-rescue EOFError
-STDERR.puts(e.class.name + 'EOFError with path:')
+
 rescue Excon::Error::Socket => e
-STDERR.puts(e.class.name + 'Excon::Error::Socket error:' + e.socket_error.to_s)
+STDERR.puts(e.class.name + 'Excon::Error::Socket error:' + e.socket_error.instance_methods.to_s)
 unless e.socket_error.to_s == 'end of file reached'
   reopen_connection
   STDERR.puts(e.class.name + 'Excon::Error::Socket with path:' + path.to_s + "\n" + 'params:' + q.to_s + ':::' + req.to_s  + ':' + e.to_s)
