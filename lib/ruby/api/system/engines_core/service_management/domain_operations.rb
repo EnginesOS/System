6bf11949e56fd61f91e6919f67639dac08849544
James L Vodanovich <james@jvodan.com>
2017-06-27 10:54:21 +1000

    f

--- lib/ruby/api/system/engines_core/service_management/domain_operations.rb
+++ lib/ruby/api/system/engines_core/service_management/domain_operations.rb
@@ -82,17 +82,17 @@ module DomainOperations
 
       if params[:internal_only]
         service_hash[:variables][:ip_type] = 'lan'
-        service_hash[:variables][:ip] =  get_lan_ip_for_hosted_dns()
+        service_hash[:variables][:ip] = get_lan_ip_for_hosted_dns()
       else
         service_hash[:variables][:ip_type] = 'gw'
-        service_hash[:variables][:ip] =  get_ext_ip_for_hosted_dns()
+        service_hash[:variables][:ip] = get_ext_ip_for_hosted_dns()
       end
       STDERR.puts(' COMPLETEd DNS HASH ' + service_hash.to_s )
       begin
         dettach_service(service_hash)
-      ensure
-        create_and_register_service(service_hash)
+      rescue
       end
+      create_and_register_service(service_hash)
     else
       begin
         dettach_service(service_hash)
