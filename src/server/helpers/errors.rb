c6d6430dfbd5cd26269b3f9bbb65a666a2eb3bc7
James L Vodanovich <james@jvodan.com>
2017-06-28 09:12:06 +1000

    tidy

---
+++ src/server/helpers/errors.rb
@@ -0,0 +1,59 @@
+def send_encoded_exception(api_exception)#request, error_object, *args)
+  api_exception[:exception] = fake_exception(api_exception) unless api_exception[:exception].is_a?(Exception)
+  status_code = 404
+  status_code = api_exception[:status] if api_exception.key?(:status)
+
+  if request.is_a?(String)
+    error_mesg = {
+      route: request,
+      error_object: {}
+    }
+  else
+    error_mesg = {
+      route: request.fullpath,
+      method: request.request_method,
+      query: request.query_string,
+      params: request.params,
+      error_object: {}
+    }
+  end
+  STDERR.puts('send_encoded_exception with request ' + api_exception.to_s)
+  if api_exception[:exception].is_a?(EnginesException)
+    error_mesg[:error_object] = api_exception[:exception].to_h
+    error_mesg[:params] = api_exception[:params].to_s
+  elsif api_exception[:exception].is_a?(Exception)
+    error_mesg[:error_object] = {error_mesg: api_exception[:exception].to_s, error_type: :failure}
+    error_mesg[:source] = api_exception[:exception].backtrace.to_s
+    #  error_mesg[:error_mesg] = api_exception[:exception].to_s
+    status_code = 500
+  elsif api_exception[:exception].to_s == 'unauthorised'
+    status_code = 401
+  end
+  STDERR.puts error_mesg.to_s
+  return_json(error_mesg, status_code)
+rescue Exception => e
+  STDERR.puts e.to_s + '  ' + e.backtrace.to_s
+  #  send_encoded_exception(request: 'send_encoded_exception', exception: e, status: 500)
+end
+
+def fake_exception(api_exception)
+  STDERR.puts('faking it' + api_exception.to_s)
+  STDERR.puts(caller[0..10].to_s)
+  if api_exception.to_s == 'unauthorised'
+    status_code = 403
+    STDERR.puts('faking unauthorised')
+  else
+    status_code = 404
+  end
+  status_code = api_exception[:status] if api_exception.key?(:status)
+  error_mesg = {
+    error_object: {}
+  }
+  if request.is_a?(String)
+    error_mesg[:route] = request
+  else
+    error_mesg[:route] = request.fullpath
+  end
+  error_mesg[:error_object] = api_exception[:exception].to_s
+  return_text(error_mesg, status_code)
+end
---
+++ src/server/helpers/warden.rb
@@ -0,0 +1,35 @@
+use Warden::Manager do |config|
+   config.scope_defaults :default,
+   strategies: [:access_token], # Set your authorization strategy
+   action: '/v0/unauthenticated' # Route to redirect to when warden.authenticate! returns a false answer.
+   config.failure_app = self.class
+ end
+
+ # Implement your Warden stratagey to validate and authorize the access_token.
+ Warden::Strategies.add(:access_token) do
+   def valid?
+     request.env['HTTP_ACCESS_TOKEN'].is_a?(String)
+   end
+
+   def is_token_valid?(token, ip = nil)
+     $engines_api.is_token_valid?(token, ip)
+   end
+   
+   def failed
+   #  status(401)
+     #   send_encoded_exception(request: request, exception: 'unauthorised', params: params)
+     #   STDERR.puts('FAILED ')
+    # status (401)
+     send_encoded_exception(request: request, exception: 'unauthorised', params: params)
+   #  redirect! '/v0/unauthenticated'
+     #throw(:warden, action: '/v0/unauthenticated')
+   end
+   
+   
+   def authenticate!
+     STDERR.puts('NO HTTP_ACCESS_TOKEN in header ') if request.env['HTTP_ACCESS_TOKEN'].nil?
+     access_granted = is_token_valid?(request.env['HTTP_ACCESS_TOKEN'])
+     !access_granted ? fail!(action: '/v0/unauthenticated', mesg: 'Could not log in') : success!(access_granted)
+   #  !access_granted ? failed : success!(access_granted)
+   end
+ end
