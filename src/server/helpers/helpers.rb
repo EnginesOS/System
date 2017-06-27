dd2c967be44dbed53bcf9f9a7f4e1b260c16e5f6
James L Vodanovich <james@jvodan.com>
2017-06-28 08:14:45 +1000

    f

--- src/server/helpers/helpers.rb
+++ src/server/helpers/helpers.rb
@@ -118,12 +118,15 @@ helpers do
       #$
       $engines_api.is_token_valid?(token, ip)
     end
-
+    def failed
+      STDERR.puts('FAILED ')
+      throw(:warden)
+    end
     def authenticate!
       STDERR.puts('NO HTTP_ACCESS_TOKEN in header ') if request.env['HTTP_ACCESS_TOKEN'].nil?
       access_granted = is_token_valid?(request.env['HTTP_ACCESS_TOKEN'])
    #   !access_granted ? fail!('Could not log in') : success!(access_granted)
-      !access_granted ? throw(:warden) : success!(access_granted)
+      !access_granted ? failed : success!(access_granted)
     end
   end
 
