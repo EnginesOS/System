c5726301990a46618e574bf96164fdf68ac01b0b
James L Vodanovich <james@jvodan.com>
2017-06-28 10:21:01 +1000

    debug

--- src/server/helpers/warden.rb
+++ src/server/helpers/warden.rb
@@ -7,6 +7,7 @@ use Warden::Manager do |config|
   failure_action = env["warden.options"][:action].to_sym
       STDERR.puts('Its a :AMBDA action ' + failure_action.to_s)
       STDERR.puts('Its a :AMBDA env' + env.to_s)
+      STDERR.puts('Its a :AMBDA SessionsController' + SessionsController.to_s)
       STDERR.puts('Its a :AMBDA action' + SessionsController.action(failure_action).to_s)
  SessionsController.action(failure_action).call(env) 
       STDERR.puts('It was a :AMBDA ' + SessionsController.to_s)
