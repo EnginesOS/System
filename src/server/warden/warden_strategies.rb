a4a450d43273e24a6289abf4b7bf9814696ec1ab
James L Vodanovich <james@jvodan.com>
2017-06-29 08:46:44 +1000

    t

--- src/server/warden/warden_strategies.rb
+++ src/server/warden/warden_strategies.rb
@@ -11,12 +11,12 @@
 # Implement Warden stratagey to validate and authorize the access_token.
 Warden::Strategies.add(:access_token) do
   def valid?
-    STDERR.puts('Valid ' + request.env['HTTP_ACCESS_TOKEN'].to_s)
+   # STDERR.puts('Valid ' + request.env['HTTP_ACCESS_TOKEN'].to_s)
     request.env['HTTP_ACCESS_TOKEN'].is_a?(String)
   end
 
   def is_token_valid?(token, ip = nil)
-    STDERR.puts('token ' + token.to_s)
+  #  STDERR.puts('token ' + token.to_s)
       $engines_api.is_token_valid?(token, ip)
   end
 
@@ -24,7 +24,7 @@ Warden::Strategies.add(:access_token) do
     #  status(401)
     #   send_encoded_exception(request: request, exception: 'unauthorised', params: params)
     #    STDERR.puts('FAILED ')
-    fail!(action: '/v0/unauthenticated', message: 'Could not log in')
+    fail!('Could not log in')
     # STDERR.puts('FAILED ')
     # warden.custom_failure!
     # send_encoded_exception(request: request, exception: 'unauthorised', params: params)
@@ -33,6 +33,7 @@ Warden::Strategies.add(:access_token) do
     # warden.custom_failure!
     # render :json => {:success => false, :errors => ["Login Failed"]}
     #   end
+    env['warden'].custom!( [403, {'Error-Message' => 'invalid token'}, 'err'])
       throw(:warden)
   end
 
