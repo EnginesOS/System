fec962db80b5cfbb36c7555c88073d4db3ecaf7c
James L Vodanovich <james@jvodan.com>
2017-06-28 11:22:44 +1000

    t

--- src/server/helpers/warden.rb
+++ src/server/helpers/warden.rb
@@ -7,7 +7,8 @@ use Warden::Manager do |config|
      failure_action = env["warden.options"][:action].to_sym
      STDERR.puts('Its a :AMBDA action ' + failure_action.to_s)
      STDERR.puts('Its a :AMBDA env' + env.to_s)
-      redirect! '/v0/unauthenticated'
+      env['warden'].custom_failure!
+      #  redirect! '/v0/unauthenticated'
      #  STDERR.puts('_______' + caller.to_s)
     # redirect! '/v0/unauthenticated'
       STDERR.puts('_______')
