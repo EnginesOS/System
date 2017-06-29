82f882c706ceb765c5a0ab0ada409e898ff5dbd4
James L Vodanovich <james@jvodan.com>
2017-06-29 10:39:33 +1000

    remove debug

--- src/server/engine_server.rb
+++ src/server/engine_server.rb
@@ -8,7 +8,7 @@ begin
   require 'yajl'
   require 'ffi_yajl'
   require '/opt/engines/lib/ruby/system/system_debug.rb'
- # require '/opt/engines/lib/ruby/system/deal_with_json.rb'
+  # require '/opt/engines/lib/ruby/system/deal_with_json.rb'
   require '/opt/engines/lib/ruby/api/public/engines_api/engines_api.rb'
 
   require '/opt/engines/lib/ruby/api/system/first_run_wizard/first_run_wizard.rb'
@@ -21,9 +21,9 @@ begin
   STDERR.puts('++++')
   FileUtils.touch('/engines/var/run/flags/startup_complete')
   @@last_error = ''
-  
+
   require 'warden'
-  #require_relative 'warden/warden_config.rb'
+  require_relative 'warden/warden_config.rb'
   require_relative 'warden/warden_strategies.rb'
 
   before do
@@ -37,27 +37,19 @@ begin
     pass if request.path.start_with?('/v0/system/do_first_run') && FirstRunWizard.required?
     env['warden'].authenticate!(:access_token)
   end
-  def unauthenticated
-     STDERR.puts('Server Strat unauth')
-   end
+
   class FailureApp
-   
     def call(env)
-      STDERR.puts 'failure: ' + env['REQUEST_METHOD'] + ' ' + env['REQUEST_URI']
       env['warden'].custom_failure!
-      STDERR.puts('env[‘PATH_INFO’] ' + env['PATH_INFO'].to_s)
-    #  env['warden'].custom
-    #  [302, {'Location' => '/v0/unauthenticated'},'']
-      #[403,{"Error-Message" => "invalid token"} ,{"Error-Message" => "invalid token"}.to_json ] #'{"Error-Message" => "invalid token"}']
-      [403,{"Content-Type"=>"text/plain",  "Content-Length"=>"12", "Server"=>"thin","Error-Message" => "Invalid Token"},['Invalid Token']]
+      [403,{"Content-Type"=>"text/plain",  "Content-Length"=>"13", "Server"=>"thin","Error-Message" => "Invalid Token"},['Invalid Token']]
+    end
   end
-end
+
   class Application < Sinatra::Base
     @events_s = nil
     set :sessions, true
     set :logging, true
     set :run, true
-    
     require_relative 'helpers/helpers.rb'
     require_relative 'api/routes.rb'
   rescue StandardError => e
@@ -66,9 +58,7 @@ end
     STDERR.puts('Unhandled Exception' + e.to_s + '\n' + e.backtrace.to_s )
     r.to_json
   end
-def unauthenticated
-   STDERR.puts('Sinatra Strat unauth')
- end
+
   def source_is_service?(request, service_name)
     service = get_service(service_name)
     if request.ip.to_s == service.get_ip_str
--- src/server/warden/warden_strategies.rb
+++ src/server/warden/warden_strategies.rb
@@ -1,50 +1,20 @@
-#require 'warden'
-
-#Warden::Manager.before_failure do |env,opts|
-#   # Sinatra is very sensitive to the request method
-#   # since authentication could fail on any type of method, we need
-#   # to set it for the failure app so it is routed to the correct block
-#   puts "============== #{opts.inspect}"
-#   env['REQUEST_METHOD'] = "POST"
-#end
-
 # Implement Warden stratagey to validate and authorize the access_token.
 Warden::Strategies.add(:access_token) do
   def valid?
-   # STDERR.puts('Valid ' + request.env['HTTP_ACCESS_TOKEN'].to_s)
-    STDERR.puts('NO HTTP_ACCESS_TOKEN in header ') if request.env['HTTP_ACCESS_TOKEN'].nil?
+    # STDERR.puts('NO HTTP_ACCESS_TOKEN in header ') if request.env['HTTP_ACCESS_TOKEN'].nil?
     request.env['HTTP_ACCESS_TOKEN'].is_a?(String)
   end
 
   def is_token_valid?(token, ip = nil)
-  #  STDERR.puts('token ' + token.to_s)
-      $engines_api.is_token_valid?(token, ip)
+    $engines_api.is_token_valid?(token, ip)
   end
-  
+
   def unauthenticated
     STDERR.puts('Warden Strat unauth')
   end
 
-  def failed
-    #  status(401)
-    #   send_encoded_exception(request: request, exception: 'unauthorised', params: params)
-    #    STDERR.puts('FAILED ')
-    fail!('Could not log in')
-    # STDERR.puts('FAILED ')
-    # warden.custom_failure!
-    # send_encoded_exception(request: request, exception: 'unauthorised', params: params)
-    #    redirect! '/v0/unauthenticated'
-    #  def failure
-    # warden.custom_failure!
-    # render :json => {:success => false, :errors => ["Login Failed"]}
-    #   end
-   # env['warden'].custom!( [403, {'Error-Message' => 'invalid token'}, 'err'])
-      throw(:warden)
-  end
-
   def authenticate!
     access_granted = is_token_valid?(request.env['HTTP_ACCESS_TOKEN'], request.env['REMOTE_ADDR'])
-     !access_granted ? fail!('Could not log in') : success!(access_granted)
-    #     !access_granted ? failed : success!(access_granted)
+    !access_granted ? fail!('Could not log in') : success!(access_granted)
   end
 end
