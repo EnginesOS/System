d78215fa252b660453d940f49052f42e7327bbf2
James L Vodanovich <james@jvodan.com>
2017-06-30 08:36:59 +1000

    f

--- lib/ruby/api/system/engines_core/containers/engine_operations.rb
+++ lib/ruby/api/system/engines_core/containers/engine_operations.rb
@@ -95,7 +95,7 @@ module EnginesOperations
   def clear_lost_engines
     r = []
     engines_tree = service_manager.managed_engines_registry
-    STDERR.puts('Engine Tree' + engines_tree.print_tree)
+    STDERR.puts('Engine Tree' + engines_tree.to_s)
     engines_tree['Application'].children.each do |engine_node|
       STDERR.puts('Engine Node' + engine_node.name)
       begin
