cb4102e0c07192dd003f9719a1b510563cf88b9d
James L Vodanovich <james@jvodan.com>
2017-07-03 18:53:23 +1000

    fmt

--- src/client/commands/system/certs.rb
+++ src/client/commands/system/certs.rb
@@ -5,60 +5,56 @@ end
 
 case ARGV[2]
 when 'default'
-
-@route += '/' + ARGV[2] + '/' + ARGV[3]
-@route +=  '/' + ARGV[4] if ARGV.count == 5
-perform_post(nil)
+  @route += '/' + ARGV[2] + '/' + ARGV[3]
+  @route +=  '/' + ARGV[4] if ARGV.count == 5
+  perform_post(nil)
 when 'system_ca'
-perform_get
+  perform_get
 when 'view'
-perform_get
+  perform_get
 when 'generate'
-@route += '/generate'
-params_data = read_stdin_data
-
-   perform_post(json_parser.parse(params_data))
- 
+  @route += '/generate'
+  params_data = read_stdin_data
+  perform_post(json_parser.parse(params_data))
 when 'default'
   @route += '/default'
   if ARGV.count < 6
     command_usage('missing arguments')
+  else
+    command_usage('missing cert file ') unless File.exist?(ARGV[4])
+    command_usage('missing key file ') unless  File.exist?(ARGV[5])
+
+    pass = nil
+    if ARGV.count == 7
+      pass=ARGV[6]
+    end
+    params = {
+      domain_name: ARGV[3],
+      certificate: File.read(ARGV[4]),
+      key: File.read(ARGV[5])
+    }
+    params[:password] = pass unless pass.nil?
+    perform_post(params)
   end
-  command_usage('missing cert file ') unless File.exist?(ARGV[4])
-  command_usage('missing key file ') unless  File.exist?(ARGV[5])
-
-  pass = nil
-  if ARGV.count == 7
-    pass=ARGV[6]
-  end
-  params = {}
-  params[:domain_name] = ARGV[3]
-  params[:certificate] = File.read(ARGV[4])
-  params[:key] = File.read(ARGV[5])
-  params[:password] = pass unless pass.nil?
-
-  perform_post(params)
-
 when 'add'
   @route += '/'
   if ARGV.count < 6
     command_usage('missing arguments')
+  else
+    command_usage('missing cert file ') unless File.exist?(ARGV[4])
+    command_usage('missing key file ') unless File.exist?(ARGV[5])
+
+    pass = nil
+    pass = ARGV[6]if ARGV.count == 7
+
+    params = {
+      domain_name: ARGV[3],
+      certificate: File.read(ARGV[4]),
+      key: File.read(ARGV[5])
+    }
+    params[:password] = pass unless pass.nil?
+    perform_post(params)
   end
-  command_usage('missing cert file ') unless File.exist?(ARGV[4])
-  command_usage('missing key file ') unless  File.exist?(ARGV[5])
-
-  pass = nil
-  if ARGV.count == 7
-    pass=ARGV[6]
-  end
-  params = {}
-  params[:domain_name] = ARGV[3]
-  params[:certificate] = File.read(ARGV[4])
-  params[:key] = File.read(ARGV[5])
-  params[:password] = pass unless pass.nil?
-
-  perform_post(params)
-
 when 'remove'
   @route += '/' + ARGV[3]
   perform_delete
