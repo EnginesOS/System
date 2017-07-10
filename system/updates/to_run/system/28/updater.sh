2d5ee35ea72c927da4c14b2e89f9ea44d9f9e35a
James L Vodanovich <james@jvodan.com>
2017-07-10 11:43:14 +1000

    swtich to new certs store

---
+++ system/updates/to_run/system/28/updater.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+cd /var/lib/engines/cert_auth/
+
+if  test -f public/certs/system_system_engines.crt
+then
+ mkdir -p public/keys/systems/system
+ mkdir -p public/certs/systems/system
+ mv public/certs/system_system_engines.crt  public/certs/systems/system
+ mv public/keys/system_system_engines.key public/keys/systems/system
+ 
+ fi
+ 
+ if test -f public/certs/service_ivpn_ipvpn.crt
+  then
+  	mkdir -p public/keys/services/ivpn
+  	mkdir -p public/certs/services/ivpn
+  	mv public/certs/service_ivpn_ipvpn.crt public/certs/services/ivpn/ipvpn.crt
+  	mv public/keys/service_ivpn_ipvpn.key public/keys/services/ivpn/ipvpn.key
+  
+  fi
