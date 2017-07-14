c4c50283c0720a3e5ddf90fb7081f0c66d889dca
James L Vodanovich <james@jvodan.com>
2017-07-14 13:59:13 +1000

    dbg

--- lib/ruby/api/system/docker/docker_api/docker_utils.rb
+++ lib/ruby/api/system/docker/docker_api/docker_utils.rb
@@ -10,12 +10,14 @@ module DockerUtils
             STDERR.puts('COPY STREAMS ')
             IO.copy_stream(@stream_reader.i_stream, socket) unless @stream_reader.i_stream.eof?
           else
-            STDERR.puts('send data as chunks ')
+            STDERR.puts('send data:' + data.class.name)
             unless stream_reader.data.nil? ||  stream_reader.data.length == 0
               if stream_reader.data.length < Excon.defaults[:chunk_size]
+                STDERR.puts('send data as one chunk ')
                 socket.send(stream_reader.data, 0)
                 stream_reader.data = ''
               else
+                STDERR.puts('send data as chunks ')
                 while stream_reader.data.length != 0
                   if stream_reader.data.length < Excon.defaults[:chunk_size]
                     socket.send(stream_reader.data.slice!(0, Excon.defaults[:chunk_size]), 0)
