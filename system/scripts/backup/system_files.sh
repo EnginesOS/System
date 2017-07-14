9fd85de16712f43bc9059469387eaa125a5e7732
James L Vodanovich <james@jvodan.com>
2017-07-14 12:10:29 +1000

    tidy

--- system/scripts/backup/system_files.sh
+++ system/scripts/backup/system_files.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo -n /opt/engines/system/scripts/backup/sudo/_system_files.sh
+cat - | sudo -n /opt/engines/system/scripts/backup/sudo/_system_files.sh
 
 #tar -czpf - /opt/engines/etc /opt/engines/run
