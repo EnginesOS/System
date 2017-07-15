78824547eff2a3a3f1e7133c15dab51a2b9bec46
James L Vodanovich <james@jvodan.com>
2017-07-15 11:50:31 +1000

    reset container ids post restore

--- system/scripts/restore/sudo/_system_files.sh
+++ system/scripts/restore/sudo/_system_files.sh
@@ -3,6 +3,10 @@
 cd /
 tar -xzpf - opt/engines
 
+for config in `find /opt/engines/ -name running.yaml`
+ do
+  sed -i.bak  "s/container_id.*//" $config
+done
 #need to reset stuff that is dynamic
 
 /opt/engines/system/scripts/startup/set_ip.sh
