ae252105295dcfc2845890f76c8510ba3897e2cf
James L Vodanovich <james@jvodan.com>
2017-07-24 14:03:16 +1000

    tw

--- system/templates/services/backup/EnginesSystem/database/sql/mysql/backup.sh
+++ system/templates/services/backup/EnginesSystem/database/sql/mysql/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-mysqldump -h $dbhost -u $dbuser --password=$dbpasswd $dbname |gzip -c 2>/tmp/mysqldump.errs
+mysqldump -h $dbhost -u $dbuser --password=$dbpasswd $dbname 2>/tmp/mysqldump.errs
 if test $? -ne 0
  then 
  	cat  /tmp/mysqldump.errs  >&2
