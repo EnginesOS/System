fa10067d2fc9f46c03817396e6cf4bf065f161f5
James L Vodanovich <james@jvodan.com>
2017-07-27 10:08:46 +1000

    tidy

--- src/client/client_http_stream.rb
+++ src/client/client_http_stream.rb
@@ -9,12 +9,12 @@ def get_json_stream(path)
 
   uri = URI(@base_url + path)
   options = nil
-options = { :use_ssl => true, uri.scheme => 'https', :verify_mode => OpenSSL::SSL::VERIFY_NONE} if @use_https == true
+options = { use_ssl: true, uri.scheme => 'https', verify_mode: OpenSSL::SSL::VERIFY_NONE} if @use_https == true
   Net::HTTP.start(uri.host, uri.port, options)  do |http|
     req = Net::HTTP::Get.new(uri)
     req['access_token'] = ENV['access_token']
     req['HTTP_access_token'] = ENV['access_token']
-    parser = FFI_Yajl::Parser.new({:symbolize_keys => true})
+    parser = FFI_Yajl::Parser.new({symbolize_keys: true})
     http.request(req) { |resp|
       resp.read_body do |chunk|
         begin
@@ -47,7 +47,7 @@ def get_stream(path, ostream = STDOUT)
   uri = URI(@base_url + path)
   req = Net::HTTP::Get.new(uri)
   req['Access_Token'] = ENV['access_token']
-  options = {use_ssl => uri.scheme == 'https', :verify_mode => OpenSSL::SSL::VERIFY_NONE} if @use_https == true
+  options = {use_ssl: true, uri.scheme => 'https', verify_mode: OpenSSL::SSL::VERIFY_NONE} if @use_https == true
   Net::HTTP.start(uri.host, uri.port,   options)   do |http|
     http.read_timeout = 600
     http.request(req) { |resp|
