ead52fa28e8e2064f7ee0aad1a80fce39d1897fb
James L Vodanovich <james@jvodan.com>
2017-10-16 11:14:09 +1100

    setup cert dir on service create

--- system/scripts/system/sudo/_setup_service_dir.sh
+++ system/scripts/system/sudo/_setup_service_dir.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
- /opt/engines/system/scripts/system/sudo/_setup_service_key_dir.sh $1
- mkdir -p /opt/engines/run/services/$1/run/flags
+ 
+uid=`/opt/engines/system/scripts/system/get_service_uid.sh $1`
+
+/opt/engines/system/scripts/system/sudo/_setup_service_key_dir.sh $1
+mkdir -p /opt/engines/run/services/$1/run/flags
 chgrp containers -R /opt/engines/run/services/$1/run
 chmod g+w -R  /opt/engines/run/services/$1/run
 
- mkdir -p /opt/engines/run/services/$1/run/f
- 
+if ! test -d /var/lib/engines/cert_auth/public/certs/services/$1
+ then
+   mkdir -p  /var/lib/engines/cert_auth/public/certs/services/$1
+   chown  $uid /var/lib/engines/cert_auth/public/certs/services/$1
+fi
 
-uid=`/opt/engines/system/scripts/system/get_service_uid.sh $1`
 
 mkdir -p /var/log/engines/services/$1
 chown -R $uid /var/log/engines/services/$1
