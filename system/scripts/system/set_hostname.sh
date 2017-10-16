c0bebc7e8c724360832d7b156747d2cd8a198915
James L Vodanovich <james@jvodan.com>
2017-10-17 08:49:16 +1100

    change ssh key dir to sensible name

--- system/scripts/system/set_hostname.sh
+++ system/scripts/system/set_hostname.sh
@@ -4,6 +4,6 @@
 
 hostname=`echo $1 | cut -f 1 -d.`
 domain_name=`echo $1 | cut -f 2- -d.`
-echo $hostname.$domain_name | ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/engines/.ssh/mgmt/set_hostname engines@control /opt/engines/bin/set_hostname.sh 
+echo $hostname.$domain_name | ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/engines/.ssh/system/set_hostname engines@control /opt/engines/bin/set_hostname.sh 
 
-echo ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/engines/.ssh/mgmt/set_hostname engines@control /opt/engines/bin/set_hostname.sh $hostname $domain_name > /tmp/sethost.ssh
+echo ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/engines/.ssh/system/set_hostname engines@control /opt/engines/bin/set_hostname.sh $hostname $domain_name > /tmp/sethost.ssh
