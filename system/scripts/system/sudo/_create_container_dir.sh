5bc4aa54048a6144c9f22715e086674cb81b3f3e
James L Vodanovich <james@jvodan.com>
2017-11-09 18:52:57 +1100

    fix post containers -> apps

--- system/scripts/system/sudo/_create_container_dir.sh
+++ system/scripts/system/sudo/_create_container_dir.sh
@@ -6,11 +6,11 @@ if test $# -eq 1
    then
 	mkdir /opt/engines/run/apps/$1 &>>  /tmp/create_container_dir
   fi
-   if ! test -d /opt/engines/etc/ssh/keys/containers/$1 
+   if ! test -d /opt/engines/etc/ssh/keys/apps/$1 
    then
-	mkdir /opt/engines/etc/ssh/keys/containers/$1  &>>  /tmp/create_container_dir
-	chown -R engines.containers /opt/engines/etc/ssh/keys/containers/$1
-	chmod g+w /opt/engines/etc/ssh/keys/containers/$1
+	mkdir /opt/engines/etc/ssh/keys/apps/$1  &>>  /tmp/create_container_dir
+	chown -R engines.containers /opt/engines/etc/ssh/keys/apps/$1
+	chmod g+w /opt/engines/etc/ssh/keys/apps/$1
   fi
   
   chown -R engines.containers /opt/engines/run/apps/$1 &>>  /tmp/create_container_dir
