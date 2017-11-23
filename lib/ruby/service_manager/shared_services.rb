1ae6853af0497ceaa90da95c2d6b15227eb6da9f
James L Vodanovich <james@jvodan.com>
2017-11-24 09:58:52 +1100

    f cnp e

--- lib/ruby/service_manager/shared_services.rb
+++ lib/ruby/service_manager/shared_services.rb
@@ -1,7 +1,7 @@
 module SharedServices
   require_relative 'private/shared_volumes.rb'
   def share_service_to_engine(shared_service_params)
-    raise EnginesException.new(warning_hash("Cannot share non sharable service", service_hash)) unless SoftwareServiceDefinition.is_sharable?(shared_service_params[:existing_service])     
+    raise EnginesException.new(warning_hash("Cannot share non sharable service", shared_service_params[:existing_service])) unless SoftwareServiceDefinition.is_sharable?(shared_service_params[:existing_service])     
     #    STDERR.puts( 'share_service_to_engine ' + shared_service_params.to_s)
     existing_service = shared_service_params[:existing_service]
     shared_service = shared_service_params.dup
