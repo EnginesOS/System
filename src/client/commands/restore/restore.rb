ef263852974339a0efb06d19982c842b64d8bde5
James L Vodanovich <james@jvodan.com>
2017-11-27 18:50:59 +1100

    restore is a put

--- src/client/commands/restore/restore.rb
+++ src/client/commands/restore/restore.rb
@@ -1,3 +1,9 @@
  
 @route += '/' + ARGV[0] + '/' + ARGV[1] + '/' + '/' + ARGV[2]
-perform_get
+
+content_type='application/octet-stream'
+STDERR.puts  @route
+params = {data: read_stdin_data}
+
+
+perform_post(params, content_type)
