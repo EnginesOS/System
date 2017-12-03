677fd47a8b32f5e5e06160f45301098439af665f
James L Vodanovich <james@jvodan.com>
2017-12-03 14:16:58 +1100

    ensure fh closed

--- lib/ruby/api/public/engines_api/system/gui_prefs.rb
+++ lib/ruby/api/public/engines_api/system/gui_prefs.rb
@@ -1,15 +1,20 @@
 module GuiPrefs
-  
   def set_container_icon_url(container, url)
     url_f = File.new(ContainerStateFiles.container_state_dir(container) + '/icon.url', 'w+')
-    url_f.puts(url)    
+    url_f.puts(url)
     url_f.close
+  rescue StandardError => e
+    url_f.close unless url_f.nil?
+    raise e
   end
-  
+
   def container_icon_url(container)
     url_f = File.new(ContainerStateFiles.container_state_dir(container) + '/icon.url', 'r')
-      url = url_f.gets(url)
-      url_f.close
-      url.strip
-    end   
+    url = url_f.gets(url)
+    url_f.close
+    url.strip
+  rescue StandardError => e
+    url_f.close unless url_f.nil?
+    raise e
+  end
 end
