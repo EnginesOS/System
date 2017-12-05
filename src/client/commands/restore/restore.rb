1b0c2618c9bf8ebe2e7319c3e302575086910b8c
James L Vodanovich <james@jvodan.com>
2017-12-05 12:39:02 +1100

    f

--- src/client/commands/restore/restore.rb
+++ src/client/commands/restore/restore.rb
@@ -1,5 +1,5 @@
  
-@route += '/' + ARGV[0] + '/' + ARGV[1] + '/' + '/' + ARGV[2]
+@route += '/' + ARGV[0] + '/' + ARGV[1] + '/' + ARGV[2] + '/' + ARGV[3]
 
 content_type='application/octet-stream'
 STDERR.puts  @route
