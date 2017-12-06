097f6a8a6014a404c169de7a189f7569328f13ad
James L Vodanovich <james@jvodan.com>
2017-12-06 13:34:26 +1100

    read direct from file

--- src/client/commands/restore/restore.rb
+++ src/client/commands/restore/restore.rb
@@ -1,8 +1,12 @@
- 
 @route += '/' + ARGV[0] + '/' + ARGV[1] + '/' + ARGV[2] + '/' + ARGV[3]
 
 content_type='application/octet-stream'
 STDERR.puts  @route
-stream_put(STDIN)
+if ARGV.length == 5
+  f = FILE.new(ARGV[4],'r')
+  stream_put(r)
+else
+  stream_put(STDIN)
+end
 #params = {data: read_stdin_data}
 #perform_put(params, content_type)
