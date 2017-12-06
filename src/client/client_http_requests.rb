0c0e116405e49757cf9cb9e8325528e6d1bd9886
James L Vodanovich <james@jvodan.com>
2017-12-06 16:31:23 +1100

    set ssl

--- src/client/client_http_requests.rb
+++ src/client/client_http_requests.rb
@@ -43,10 +43,9 @@ def stream_connection(uri_s, stream_reader)
   }
   uri = URI(@base_url + uri_s)
   STDERR.puts('uri ' + uri.to_s)
-  conn = Net::HTTP.new(uri.host, uri.port, use_ssl: true)
-  
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  conn = Net::HTTP.new(uri.host, uri.port, use_ssl: true)  
+  conn.use_ssl = true
+  conn.verify_mode = OpenSSL::SSL::VERIFY_NONE
   request = Net::HTTP::Put.new(uri.request_uri, headers)
   request.body_stream = stream_reader
   conn.request(request)
