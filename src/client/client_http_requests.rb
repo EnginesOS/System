8c20a5982c27e94dd32378aa9497ecf8ea0f9b14
James L Vodanovich <james@jvodan.com>
2017-12-06 16:19:27 +1100

    ssl

--- src/client/client_http_requests.rb
+++ src/client/client_http_requests.rb
@@ -45,6 +45,8 @@ def stream_connection(uri_s, stream_reader)
   uri = URI(@base_url + uri_s)
   STDERR.puts('uri ' + uri.to_s)
   conn = Net::HTTP.new(uri.host, uri.port)
+  conn.use_ssl = true
+  conn.verify_mode = OpenSSL::SSL::VERIFY_NONE
   request = Net::HTTP::Put.new uri.request_uri, {'Transfer-Encoding' => 'chunked', 'content-type' => 'application/octet-stream'}
   request.body_stream = stream_reader
   conn.request(request)
