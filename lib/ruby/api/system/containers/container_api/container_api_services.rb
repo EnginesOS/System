e01d120a8545aa7730c893aac57ef52ec5411f3b
James L Vodanovich <james@jvodan.com>
2017-12-22 20:12:58 +1100

    test registry derived mounts

--- lib/ruby/api/system/containers/container_api/container_api_services.rb
+++ lib/ruby/api/system/containers/container_api/container_api_services.rb
@@ -2,8 +2,9 @@ module ContainerApiServices
   #
   # @param match [Hash]
   # @param container [ManagedContainer]
-  def engine_services(container, match)
+  def container_services(container, match)
     match[:parent_engine] = container.container_name 
+    match[:container_type] = container.ctype
     engines_core.find_engine_services(match)
   end
 end
--- lib/ruby/containers/managed_container/managed_container_services.rb
+++ lib/ruby/containers/managed_container/managed_container_services.rb
@@ -1,5 +1,5 @@
 module ManagedContainerServices
   def services 
-   @container_api.engine_services(self, match)
+   @container_api.container_services(self, match)
   end
 end
