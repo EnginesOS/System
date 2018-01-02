a948175d273a65bb71e3c7e7e6b327ee9dfaa19e
James L Vodanovich <james@jvodan.com>
2018-01-02 14:07:52 +1100

    missing param

--- lib/ruby/containers/managed_service/managed_service_configurations.rb
+++ lib/ruby/containers/managed_service/managed_service_configurations.rb
@@ -11,7 +11,7 @@ module ManagedServiceConfigurations
   def retrieve_configurator(configurator_params)
     configurator_params[:service_name] = @container_name
     if  is_running? == false
-      retrieve_service_configuration
+      retrieve_service_configuration(configurator_params)
     else
       raise EnginesException.new(error_hash('service missing cont_userid ', configurator_params)) if check_cont_uid == false
       @container_api.retrieve_configurator(self, configurator_params)
