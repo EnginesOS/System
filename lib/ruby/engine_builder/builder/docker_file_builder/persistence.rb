86452d70b15cf42617050b914ae5fe0da407087c
James L Vodanovich <james@jvodan.com>
2018-01-04 18:12:29 +1100

    apply templater when reading in

--- lib/ruby/engine_builder/builder/docker_file_builder/persistence.rb
+++ lib/ruby/engine_builder/builder/docker_file_builder/persistence.rb
@@ -6,7 +6,7 @@ def write_persistent_files
      src_paths = @blueprint_reader.persistent_files
      unless src_paths.nil?
        src_paths.each do |p_file|
-         p_file[:volume_name] = templater.process_templated_string(p_file[:volume_name])         
+        # p_file[:volume_name] = templater.process_templated_string(p_file[:volume_name])         
          path = p_file[:path]
          dir = File.dirname(path)
          file = File.basename(path)
@@ -27,7 +27,7 @@ def write_persistent_dirs
     paths = ''
     write_comment('#Persistant Dirs')
     @blueprint_reader.persistent_dirs.each do |p_dir|
-      p_dir[:volume_name] = templater.process_templated_string(p_dir[:volume_name])      
+      #p_dir[:volume_name] = templater.process_templated_string(p_dir[:volume_name])      
       path = p_dir[:path]
       path.chomp!('/')
       paths += p_dir[:volume_name].to_s + ':' + path + ' ' unless path.nil?
