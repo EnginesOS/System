3bb716b0a24aa5adea08fda6ab48f333da15a89c
James L Vodanovich <james@jvodan.com>
2018-01-04 12:55:24 +1100

    dont over service name

--- lib/ruby/containers/objects/volume.rb
+++ lib/ruby/containers/objects/volume.rb
@@ -26,7 +26,7 @@ class Volume < StaticService #Latter will include group and perhaps other attrib
       service_hash[:variables][:engine_path] = '/home/fs/' + service_hash[:variables][:engine_path] unless service_hash[:variables][:engine_path].start_with?('/home/fs/') ||service_hash[:variables][:engine_path].start_with?('/home/app')
     end
 
-    service_hash[:variables][:service_name] = service_hash[:variables][:engine_path].gsub(/\//,'_')
+   # service_hash[:variables][:service_name] = service_hash[:variables][:engine_path].gsub(/\//,'_')
 
     unless service_hash[:variables].key?(:volume_src)
       service_hash[:variables][:volume_src] = self.default_volume_name(service_hash)
