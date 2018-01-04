9cc6013e7c846c98244a97627b6f617d4714eeaf
James L Vodanovich <james@jvodan.com>
2018-01-04 11:34:29 +1100

    devubg

--- lib/ruby/engine_builder/service_builder/local_file_service_builder.rb
+++ lib/ruby/engine_builder/service_builder/local_file_service_builder.rb
@@ -3,8 +3,9 @@ module LocalFileServiceBuilder
     SystemDebug.debug(SystemDebug.builder, 'Add File Service ' + service_hash[:variables][:name].to_s + ' ' + service_hash.to_s)
     #  Default to engine
     @app_is_persistent = true if service_hash[:variables][:engine_path] == '/home/app/' || service_hash[:variables][:engine_path] == '/home/app'
-    service_hash = Volume.complete_service_hash(service_hash)
     SystemDebug.debug(SystemDebug.builder,:complete_VOLUME_service_hash, service_hash)
+    service_hash = Volume.complete_service_hash(service_hash)
+    SystemDebug.debug(SystemDebug.builder,:completed_VOLUME_service_hash, service_hash)
     write_vol_map(service_hash)
 #    if service_hash[:share] == true
 #      @volumes[service_hash[:service_owner] + '_' + service_hash[:variables][:volume_name]] = Volume.volume_hash(service_hash)
