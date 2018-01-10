1ee9fa41655fa577dd943325724937b13d0bf7c5
James L Vodanovich <james@jvodan.com>
2018-01-10 20:01:56 +1100

    persistence via map files

--- system/templates/deployment/global/build_scripts/persistent_dirs.sh
+++ system/templates/deployment/global/build_scripts/persistent_dirs.sh
@@ -1,37 +1,52 @@
 #!/bin/bash
-set
-for path in $*
-  do
- Volume=`echo $path |cut -f1 -d:`
- VOLDIR=`cat /home/fs/volumes/$Volume`
- echo $VOLDIR |grep /home/fs/ >/dev/null
- if test $? -ne 0
-  then
-  VOLDIR=/home/fs/$VOLDIR
- fi
- path=`echo $path |cut -f2 -d:`
-   echo $path |grep ^/usr/local/ >/dev/null
-    if test $? -eq 0
-     then
-      path=`echo $path | sed "/\/usr\/local\//s///"`
-      prefix=/usr/local/
-     else
-      prefix=/home/
-    fi 
-  echo Prefix: $prefix
-  path=`echo $path | sed "/[.][.]/s///g"` 
-  echo Path: $path
-  path=`echo $path | sed "/\/$/s///"`
-  dirname=`dirname "$path" `
-  mkdir -p "$VOLDIR/$dirname"
-  echo mkdir -p "$VOLDIR/$dirname"
-   	 if [ ! -d "$prefix/$path" ]
-      then 
-        mkdir -p "$prefix/$path" 
-   		echo Make Dir: mkdir -p "$prefix/$path" 
-   	 fi
-  mv "$prefix/$path" "$VOLDIR/$dirname" 
-  echo "mv $prefix / $path" "$VOLDIR / $dirname" 
-  ln -s "$VOLDIR/$path" "$prefix/$path"
-  echo ln -s "$VOLDIR/$path" "$prefix/$path"
-done
+
+for file  in `cat /home/fs/vol_file_maps | awk '{ print $1}'`
+ do 
+   volume=`grep "$file " /home/fs/vol_file_maps| awk '{print $2}'`	
+   dest_path=`cat /home/fs/volumes/$volume`
+   destination=$dest_path/$file 
+   if ! test -d `dirname $destination`
+    then
+    	mkdir -p $destination
+    fi
+ 	cp -np /home/$file $destination 
+ 	rm /home/$file 
+ 	ln -s $dest_path/$file $dest_path/$file 
+ done
+#set
+#
+#for path in $*
+#  do
+# Volume=`echo $path |cut -f1 -d:`
+# VOLDIR=`cat /home/fs/volumes/$Volume`
+# echo $VOLDIR |grep /home/fs/ >/dev/null
+# if test $? -ne 0
+#  then
+#  VOLDIR=/home/fs/$VOLDIR
+# fi
+# path=`echo $path |cut -f2 -d:`
+#   echo $path |grep ^/usr/local/ >/dev/null
+#    if test $? -eq 0
+#     then
+#      path=`echo $path | sed "/\/usr\/local\//s///"`
+#      prefix=/usr/local/
+#     else
+#      prefix=/home/
+#    fi 
+#  echo Prefix: $prefix
+#  path=`echo $path | sed "/[.][.]/s///g"` 
+#  echo Path: $path
+#  path=`echo $path | sed "/\/$/s///"`
+#  dirname=`dirname "$path" `
+#  mkdir -p "$VOLDIR/$dirname"
+#  echo mkdir -p "$VOLDIR/$dirname"
+#   	 if [ ! -d "$prefix/$path" ]
+#      then 
+#        mkdir -p "$prefix/$path" 
+#   		echo Make Dir: mkdir -p "$prefix/$path" 
+#   	 fi
+#  mv "$prefix/$path" "$VOLDIR/$dirname" 
+#  echo "mv $prefix / $path" "$VOLDIR / $dirname" 
+#  ln -s "$VOLDIR/$path" "$prefix/$path"
+#  echo ln -s "$VOLDIR/$path" "$prefix/$path"
+#done
--- system/templates/deployment/global/build_scripts/persistent_files.sh
+++ system/templates/deployment/global/build_scripts/persistent_files.sh
@@ -1,32 +1,48 @@
 #!/bin/bash
-set
-for path in $*
- do
-Volume=`echo $path |cut -f1 -d:`
- VOLDIR=`cat /home/fs/volumes/$Volume`
-echo $Volume to $VOLDIR
- echo $VOLDIR |grep /home/fs/ >/dev/null
- if test $? -ne 0
-  then
-  VOLDIR=/home/fs/$VOLDIR
- fi
- path=`echo $path |cut -f2 -d:`
-   path=`echo $path | sed "/[.][.]/s///g"` 
-   echo $path
-   path=`echo $path | sed "/\/$/s///"`
-   dir=`dirname $path`
-   mkdir -p /home/$dir
-   echo "mkdir -p /home/$dir"
-   echo file $path is in $dir
- 	if [ ! -f /home/$path ]
-  	  then 
-  		echo "touch  /home/$path"
-    	touch  /home/$path
-     fi  	
-  echo mkdir -p $VOLDIR/$dir
-  mkdir -p $VOLDIR/$dir
-  echo "mv /home/$path /$VOLDIR/$dir"
-  mv /home/$path /$VOLDIR/$dir	
-  echo "ln -s  $VOLDIR/$path /home/$path"
-  ln -s  $VOLDIR/$path /home/$path
-done
+
+
+for file  in `cat /home/fs/vol_file_maps | awk '{ print $1}'`
+ do 
+   volume=`grep "$file " /home/fs/vol_file_maps| awk '{print $2}'`	
+   dest_path=`cat /home/fs/volumes/$volume`
+   destination=$dest_path/$file 
+   if ! test -d `dirname $destination`
+    then
+    	mkdir -p $destination
+    fi
+ 	cp -np /home/$file $destination 
+ 	rm /home/$file 
+ 	ln -s $dest_path/$file $dest_path/$file 
+ done
+ 
+#set
+#for path in $*
+# do
+#Volume=`echo $path |cut -f1 -d:`
+# VOLDIR=`cat /home/fs/volumes/$Volume`
+#echo $Volume to $VOLDIR
+# echo $VOLDIR |grep /home/fs/ >/dev/null
+# if test $? -ne 0
+#  then
+#  VOLDIR=/home/fs/$VOLDIR
+# fi
+# path=`echo $path |cut -f2 -d:`
+#   path=`echo $path | sed "/[.][.]/s///g"` 
+#   echo $path
+#   path=`echo $path | sed "/\/$/s///"`
+#   dir=`dirname $path`
+#   mkdir -p /home/$dir
+#   echo "mkdir -p /home/$dir"
+#   echo file $path is in $dir
+# 	if [ ! -f /home/$path ]
+#  	  then 
+#  		echo "touch  /home/$path"
+#    	touch  /home/$path
+#     fi  	
+#  echo mkdir -p $VOLDIR/$dir
+#  mkdir -p $VOLDIR/$dir
+#  echo "mv /home/$path /$VOLDIR/$dir"
+#  mv /home/$path /$VOLDIR/$dir	
+#  echo "ln -s  $VOLDIR/$path /home/$path"
+#  ln -s  $VOLDIR/$path /home/$path
+#done
