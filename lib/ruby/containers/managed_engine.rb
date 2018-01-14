688f2fd1afafa9495e26ba9e513d745fbd6e3ed9
James L Vodanovich <james@jvodan.com>
2018-01-15 10:36:58 +1100

    dbg and reg dns before volbuilder

--- lib/ruby/containers/managed_engine.rb
+++ lib/ruby/containers/managed_engine.rb
@@ -50,6 +50,7 @@ class ManagedEngine < ManagedContainer
 
   def on_start(event_hash)
     set_running_user
+    register_with_dns
     #  STDERR.puts('ONSTART_CALLED' + container_name.to_s + ';' + event_hash.to_s)
         STDERR.puts('ONS ME TART @service_builder.run_volume_builder  is a' +  @volume_service_builder.to_s )    
     if @volume_service_builder == true
@@ -58,12 +59,13 @@ class ManagedEngine < ManagedContainer
       {type_path: 'filesystem/local/filesystem'
       })
     if vols.is_a?(Array) && vols.length > 0 
+      STDERR.puts('RuN VOLBUILER')
            @container_api.run_volume_builder(self, @cont_userid, 'all')
       end
       @volume_service_builder = false
       @save_container = false
     end
-    register_with_dns
+    STDERR.puts('VOLBUILER RAN')
     super
   end
 
