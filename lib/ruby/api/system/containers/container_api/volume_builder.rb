f05d3fa16a28ce7d61898f890d62b096245d2b82
James L Vodanovich <james@jvodan.com>
2018-01-15 11:41:58 +1100

    dbg

--- lib/ruby/api/system/containers/container_api/volume_builder.rb
+++ lib/ruby/api/system/containers/container_api/volume_builder.rb
@@ -7,7 +7,12 @@ module ContainerApiVolumeBuilder
       target: container.container_name,
       target_container: container.container_name,
       data_gid: container.data_gid.to_s
-    })
+    }) 
+    STDERR.puts('Wait for create ' + volbuilder.container_name + ' is in ' + volbuilder.status.to_s)
+    volbuilder.wait_for('create', 10)
+    STDERR.puts('Wait for start ' + volbuilder.container_name + ' is in ' + volbuilder.status.to_s)
+    volbuilder.wait_for('start', 10)
+    STDERR.puts('Wait for stop ' + volbuilder.container_name + ' is in ' + volbuilder.status.to_s)
     volbuilder.wait_for('stop', 90)
     STDERR.puts('Stopped ' + volbuilder.container_name + ' is in ' + volbuilder.status.to_s)
     volbuilder.destroy_container
