18afb724cdf12b74d49ec0016767718a3e41a39c
James L Vodanovich <james@jvodan.com>
2018-01-28 12:46:24 +1100

    chunk it

--- src/client/client_http_requests.rb
+++ src/client/client_http_requests.rb
@@ -38,7 +38,8 @@ def stream_file(uri_s, src_f)
   headers = {
      'content_type' => 'application/octet-stream',
      'ACCESS_TOKEN' => load_token,
-     'Content-Length' => src_f.size.to_s
+    'Transfer-Encoding' => 'chunked'
+     #'Content-Length' => src_f.size.to_s
   }
   uri = URI(@base_url + uri_s)
   STDERR.puts('uri ' + uri.to_s)
@@ -48,9 +49,12 @@ def stream_file(uri_s, src_f)
   request = Net::HTTP::Put.new(uri.request_uri, headers)
   STDERR.puts request.inspect
   request.body_stream = src_f
- r = conn.request(request)
+ #r = conn.request(request)
+  conn.start do |http| 
+  r = http.request(request)
  STDERR.puts('STREAM RESULT ' + r.inspect)
-  write_response(r)
+  end
+  #write_response(r)
     exit
   rescue StandardError => e
   STDERR.puts('socket stream closed ' + e.to_s + e.backtrace.to_s)
