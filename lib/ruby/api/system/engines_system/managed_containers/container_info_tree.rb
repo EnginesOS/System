923d13ff267fd17120f5e73ba51693d1bcd4281f
James L Vodanovich <james@jvodan.com>
2018-02-01 12:15:54 +1100

    fmt

--- lib/ruby/api/system/engines_system/managed_containers/container_info_tree.rb
+++ lib/ruby/api/system/engines_system/managed_containers/container_info_tree.rb
@@ -2,13 +2,13 @@ module ContainerInfoTree
   def write_info_tree(c, keys)
     unless File.exists?(container_info_tree_dir(c))
       FileUtils.mkdir_p(container_info_tree_dir(c))
-    end        
+    end
     keys.each do |k, v|
       next if v.nil?
       kf = File.new(container_info_tree_dir(c) + '/' + k.to_s,'w')
       kf.write(v.to_s)
       kf.close
-     end
+    end
   end
 
   def remove_info_tree(c)
@@ -16,25 +16,24 @@ module ContainerInfoTree
       FileUtils.rm_f(container_info_tree_dir(c))
     end
   end
+
   class FakeContainer
     attr_reader :container_name, :ctype
     def initialize(p)
       @container_name = p[:name]
-      @ctype  = p[:ctype]   
+      @ctype  = p[:ctype]
     end
-    end
-    
+  end
+
   def init_container_info_dir(p)
-    if p.is_a?(Hash)    
+    if p.is_a?(Hash)
       c = FakeContainer.new(p)
       keys = p[:keys]
     else
       c = p
-    keys = {uid: c.cont_user_id} 
+      keys = {uid: c.cont_user_id}
     end
-    
     write_info_tree(c, keys)
+  end
 
-end
-       
 end
