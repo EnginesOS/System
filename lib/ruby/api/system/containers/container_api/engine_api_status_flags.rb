c400a2ac801ba653c37b8c9fad4494242deb4bd6
James L Vodanovich <james@jvodan.com>
2018-02-03 16:19:35 +1100

    debug file/dr create on system service inotidfy use

--- lib/ruby/api/system/containers/container_api/engine_api_status_flags.rb
+++ lib/ruby/api/system/containers/container_api/engine_api_status_flags.rb
@@ -30,12 +30,15 @@ module EngineApiStatusFlags
 
   def wait_for_startup(c, timeout = 5)
     r = false
-    state_file_name = @system_api.container_state_dir(c) +'/run/flags/state'
-    if ! File.exist?(@system_api.container_state_dir(c) +'/run/flags')
-      FileUtils.mkdir_p(@system_api.container_state_dir(c) +'/run/flags')
+    fd = @system_api.container_state_dir(c) +'/run/flags'
+    state_file_name = fd + '/state'
+    if ! File.exist?(fd)
+      FileUtils.mkdir_p(fd)
+      STDERR.puts('wait_for_startup made ' + fd)
     end
     if ! File.exist?(state_file_name)
       FileUtils.touch(state_file_name)
+      STDERR.puts('wait_for_startup touched ' + state_file_name)
     end
     if c.is_running?
       if is_startup_complete?(c)
@@ -43,7 +46,7 @@ module EngineApiStatusFlags
       else
         begin
           Timeout::timeout(timeout) do
-            sfn = @system_api.container_state_dir(c) + '/run/flags/startup_complete'
+            sfn = fd + '/startup_complete'
             begin
               require 'rb-inotify'
               notifier = INotify::Notifier.new
