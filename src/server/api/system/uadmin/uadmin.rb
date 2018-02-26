969d70b40ee9c67da5390c86547c3ba65642ffdc
James L Vodanovich <james@jvodan.com>
2018-02-26 13:23:22 +1100

    f dbg

--- src/server/api/system/uadmin/uadmin.rb
+++ src/server/api/system/uadmin/uadmin.rb
@@ -3,6 +3,7 @@ get '/v0/system/uadmin/*' do
     STDERR.puts(' Get')
     require_relative 'uadmin_verbs.rb'
     STDERR.puts(' Getting')
+    STDERR.puts('I got ' + params.to_s)
     uadmin_response(uadmin_get(params[:splat][0]))
   rescue StandardError => e
     send_encoded_exception(request: request, exception: e)
@@ -12,7 +13,6 @@ end
 put '/v0/system/uadmin/*' do
   begin
     require_relative 'uadmin_verbs.rb'
-    STDERR.puts(params.to_s)
     uadmin_response(uadmin_put(params[:splat][0], request.env['rack.input']))
   rescue StandardError => e
     send_encoded_exception(request: request, exception: e)
