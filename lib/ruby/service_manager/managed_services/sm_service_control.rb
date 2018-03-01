2f5da2ef787a30632c57c04752cc13addce71f08
James L Vodanovich <james@jvodan.com>
2018-03-01 14:44:02 +1100

    f

--- lib/ruby/service_manager/managed_services/sm_service_control.rb
+++ lib/ruby/service_manager/managed_services/sm_service_control.rb
@@ -103,8 +103,8 @@ module SmServiceControl
       
     STDERR.puts('RESOLVING ' + new_vars.to_s)
     def resolve_field_val(fld_name, new_vars)
-      name = match.sub!(/_Engines_Fields\(/, '')
-         name.sub!(/[\)]/, '')
+      fld_name.sub!(/_Engines_Fields\(/, '')
+      fld_name.sub!(/[\)]/, '')
       STDERR.puts('RESOLVE FLD ' + fld_name.to_s + ' = ' + new_vars[fld_name].to_s)
       val=''
       unless fld_name.nil?
