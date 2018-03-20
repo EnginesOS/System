0d142b343558aa7e03d49c680e3105487dc3ea22
James L Vodanovich <james@jvodan.com>
2018-03-20 15:30:43 +1100

    f

--- system/updates/to_run/system/39/updater.sh
+++ system/updates/to_run/system/39/updater.sh
@@ -2,4 +2,4 @@
 if ! test -f /opt/engines/etc/services/mapping/ManagedEngine/secrets.yaml 
  then
   ln -s /opt/engines/etc/services/providers/EnginesSystem/secrets/secrets.yaml /opt/engines/etc/services/mapping/ManagedEngine/
-rm  
+fi
