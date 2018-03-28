815392a93d615ce752972f83e287ecd4dc9c15dd
James L Vodanovich <james@jvodan.com>
2018-03-28 11:13:17 +1100

    apache want /var/log/apache2 regardless of settings (or prior to)

--- system/templates/deployment/apache_php/build_scripts/finalise_environment.sh
+++ system/templates/deployment/apache_php/build_scripts/finalise_environment.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-#mkdir  -p /var/log/apache2/ 
-#chown www-data /var/log/apache2/ 
-#mkdir  -p /run/apache2/ 
-#chown www-data /run/apache2/
+mkdir  -p /var/log/apache2/ 
+chown www-data /var/log/apache2/ 
+mkdir  -p /run/apache2/ 
+chown www-data /run/apache2/
