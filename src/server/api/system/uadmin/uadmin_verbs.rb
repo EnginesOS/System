8d6fec27f3eb4f061f85adc538e281279826220e
James L Vodanovich <james@jvodan.com>
2018-04-06 13:12:12 +1000

    f

--- src/server/api/system/uadmin/uadmin_verbs.rb
+++ src/server/api/system/uadmin/uadmin_verbs.rb
@@ -88,6 +88,7 @@ def uadmin_post(splat, body, params)
   params.merge!(body)
   r = {method: :post,
  # query: clean_api_vars(params),
+  query: body.to_json,
   headers: rheaders,
   path: build_uri(splat),
   body: body.to_json}
