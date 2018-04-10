79befdfaf8a906ef403001679091a7fa2502c0c9
James L Vodanovich <james@jvodan.com>
2018-04-10 11:13:17 +1000

    return count of listeners and not every listener

--- lib/ruby/api/system/engines_system/docker_events.rb
+++ lib/ruby/api/system/engines_system/docker_events.rb
@@ -171,7 +171,7 @@ module DockerEvents
   end
 
   def start_docker_event_listener(listeners = nil)
-    SystemDebug.debug(SystemDebug.container_events, ' Start EVENT LISTENER THREAD !!!!!!!!!!!!!!!!!!!!!!!!!!!!! with ' + listeners.to_s)
+    SystemDebug.debug(SystemDebug.container_events, ' Start EVENT LISTENER THREAD !!!!!!!!!!!!!!!!!!!!!!!!!!!!! with n ' + listeners.count.to_s)
     @docker_event_listener = DockerEventWatcher.new(self, listeners)
     @event_listener_thread.exit unless @event_listener_thread.nil?
     @event_listener_thread = Thread.new do
