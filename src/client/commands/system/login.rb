279bd51f46d855c5f08755acb7fbd197e67ff51b
James L Vodanovich <james@jvodan.com>
2018-04-17 12:08:58 +1000

    support spaces in user name

--- src/client/commands/system/login.rb
+++ src/client/commands/system/login.rb
@@ -1,6 +1,13 @@
 @route += ''
-ps =  {user_name: ARGV[2],
-  password: ARGV[3]
+last_arg = ARGV.count - 1
+if last_arg > 3
+  cn_end = last_arg - 1
+  un = ARGV[2-cn_end]
+else
+  un = ARGV[2]
+end
+ps =  {user_name: un,
+  password: ARGV[last_arg]
   }
 
 perform_post(ps)
