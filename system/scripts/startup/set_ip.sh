5df7f2834851e48cc67deea11183055b6561f643
James L Vodanovich <james@jvodan.com>
2018-04-29 17:59:24 +1000

    -k on curl

--- system/scripts/startup/set_ip.sh
+++ system/scripts/startup/set_ip.sh
@@ -2,4 +2,4 @@
 
 ip=`/opt/engines/bin/system_ip.sh`
 echo -n $ip > /opt/engines/etc/exported/net/ip
-curl ipecho.net/plain > /opt/engines/etc/exported/net/public
+curl -k ipecho.net/plain > /opt/engines/etc/exported/net/public
