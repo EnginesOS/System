c892682e3eca0f2afc30ff30337413eab98852e1
James L Vodanovich <james@jvodan.com>
2018-05-08 12:55:46 +1000

    more debug

--- lib/ruby/engine_builder/builder/base_image.rb
+++ lib/ruby/engine_builder/builder/base_image.rb
@@ -9,7 +9,7 @@ end
 def get_base_image
   base_image_name = read_base_image_from_dockerfile
   raise EngineBuilderException.new(error_hash('Failed to Read Image from Dockerfile')) if base_image_name.nil?
-  log_build_output('Pull base Image')
+  log_build_output('Pull base Image: ' + base_image_name.to_s)
   @core_api.pull_image(base_image_name) #== false
   true
 rescue StandardError => e
