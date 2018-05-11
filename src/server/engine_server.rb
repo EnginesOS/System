040bf286f552adaf89d6f23208121d5e910470c6
James L Vodanovich <james@jvodan.com>
2018-05-11 11:28:51 +1000

    v0 prefix missing

--- src/server/engine_server.rb
+++ src/server/engine_server.rb
@@ -44,9 +44,9 @@ begin
       env['warden'].authenticate!(:user_access_token)
     elsif  request.path == '/v0/containers/engines/status'    
       env['warden'].authenticate!(:admin_user_access_token)
-    elsif request.path.match(/containers\/engine\/[a-zA-Z]\/icon_url/) \
-      ||  request.path.match(/containers\/engine\/[a-zA-Z]\/websites/)  \
-      ||  request.path.match(/containers\/engine\/[a-zA-Z]\/blueprint/)
+    elsif request.path.match(/v0\/containers\/engine\/[a-zA-Z0-9]\/icon_url/) \
+      ||  request.path.match(/v0\/containers\/engine\/[a-zA-Z0-9]]\/websites/)  \
+      ||  request.path.match(/v0\/containers\/engine\/[a-zA-Z0-9]]\/blueprint/)
       env['warden'].authenticate!(:admin_user_access_token)
     else  
       env['warden'].authenticate!(:api_access_token)
