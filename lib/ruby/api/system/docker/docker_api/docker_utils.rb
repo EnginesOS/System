ec3a5e4742ca8b95dba7f84b7937025e99e78aa0
James L Vodanovich <james@jvodan.com>
2018-09-25 12:54:06 +1000

    f

--- lib/ruby/api/system/docker/docker_api/docker_utils.rb
+++ lib/ruby/api/system/docker/docker_api/docker_utils.rb
@@ -121,6 +121,7 @@ module DockerUtils
            STDERR.puts('STDOUT ') # + cl.to_s)
           elsif r.start_with?("\u0002\u0000\u0000\u0000")
             dst = :stderr
+l = r [0..7].unpack('C*')
 cl = l[7] + l[6] * 256 + l[5] * 4096 + l[4] * 65536 + l[3] * 1048576
 STDERR.puts('STDERR ' ) #+ cl.to_s )
             r = r[8..-1]
