1e663163483a28db4902eaa4944e571d0a982628
James L Vodanovich <james@jvodan.com>
2018-09-27 12:37:07 +1000

    use {} around vars

--- system/templates/services/backup/EnginesSystem/ldap_group/backup.sh
+++ system/templates/services/backup/EnginesSystem/ldap_group/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 
-  ldapsearch -D $ldap_dn -w $ldap_password -h $LDAP_HOST -b cn=$group_cn,ou=$CONTAINER_NAME,ou=Applications,ou=Groups,dc=engines,dc=internal 
+  ldapsearch -D ${ldap_dn} -w ${ldap_password} -h ${LDAP_HOST} -b cn=${group_cn},ou=${CONTAINER_NAME},ou=Applications,ou=Groups,dc=engines,dc=internal 
  
