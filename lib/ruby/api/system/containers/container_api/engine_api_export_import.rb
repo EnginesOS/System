59719323bf0a401e56461f84bc52a19e889791c2
James L Vodanovich <james@jvodan.com>
2018-09-28 10:39:09 +1000

    get full service hash to pass variables to resotre /backup

--- lib/ruby/api/system/containers/container_api/engine_api_export_import.rb
+++ lib/ruby/api/system/containers/container_api/engine_api_export_import.rb
@@ -11,7 +11,7 @@ module EngineApiExportImport
 
     SystemDebug.debug(SystemDebug.export_import, :export_service, service_hash)
     cmd_dir = SystemConfig.BackupScriptsRoot + '/' + service_hash[:publisher_namespace] + '/' + service_hash[:type_path] + '/' + service_hash[:service_handle] + '/'
-
+    service_hash = engines_core.retrieve_service_hash(service_hash)
     cmd = cmd_dir + '/backup.sh'
     SystemDebug.debug(SystemDebug.export_import, :export_service, cmd)
 
@@ -44,6 +44,7 @@ result
       stream.close unless stream.nil?
       raise EnginesException.new(warning_hash("Cannot import as single service", service_hash))
     end
+    service_hash = engines_core.retrieve_service_hash(service_hash)
     SystemDebug.debug(SystemDebug.export_import, :import_service, service_params,service_params[:import_method])
     cmd_dir = SystemConfig.BackupScriptsRoot + '/' + service_hash[:publisher_namespace] + '/' + service_hash[:type_path] + '/' + service_hash[:service_handle] + '/'
     if service_params[:import_method] == :replace
