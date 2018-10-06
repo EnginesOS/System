f5bda1e0d24489e03ffb2693fe43fe370f90b755
James L Vodanovich <james@jvodan.com>
2018-10-06 12:22:21 +1000

    f

--- lib/ruby/api/system/docker/docker_api/docker_connection.rb
+++ lib/ruby/api/system/docker/docker_api/docker_connection.rb
@@ -143,14 +143,15 @@ class DockerConnection < ErrorsApi
         read_timeout: 3600,
         query: options,
         path: uri,
-      headers: rheaders}.to_s )
+      headers: rheaders,
+        body: body}.to_s )
       r = sc.request(
       method: :post,
       read_timeout: 3600,
       query: options,
       path: uri,
-      headers: rheaders#,
-      #body: body
+      headers: rheaders,
+      body: body #?
       )
       stream_handler.close
     end
