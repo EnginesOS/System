bbd092da68521c47cd7180f9c90038206a6fe2d1
James L Vodanovich <james@jvodan.com>
2018-10-07 11:26:28 +1100

    fix eh around streams

--- src/server/api/backup/backup.rb
+++ src/server/api/backup/backup.rb
@@ -2,7 +2,12 @@ get '/v0/backup/system_files' do
   begin
     content_type 'application/octet-stream'
     stream do |out|
-      engines_api.backup_system_files(out)
+      begin
+      engines_api.backup_system_files(out)       
+      rescue StandardError => e
+        STDERR.puts('engine_export_persistent_service exception ' + e.to_s)
+        send_encoded_exception(request: request, exception: e)
+      end
     end
   rescue StandardError => e
     send_encoded_exception(request: request, exception: e)
@@ -13,7 +18,12 @@ get '/v0/backup/system_db' do
   begin
     content_type 'application/octet-stream'
     stream do |out|
+      begin
       engines_api.backup_system_db(out)
+      rescue StandardError => e
+        STDERR.puts('engine_export_persistent_service exception ' + e.to_s)
+        send_encoded_exception(request: request, exception: e)
+      end
     end
   rescue StandardError => e
     send_encoded_exception(request: request, exception: e)
@@ -24,7 +34,12 @@ get '/v0/backup/service/registry' do
   begin
     content_type 'application/octet-stream'
     stream do |out|
+      begin
       engines_api.backup_system_registry(out)
+        rescue StandardError => e
+          STDERR.puts('engine_export_persistent_service exception ' + e.to_s)
+          send_encoded_exception(request: request, exception: e)
+        end
     end
   rescue StandardError => e
     send_encoded_exception(request: request, exception: e)
@@ -35,7 +50,12 @@ get '/v0/backup/service/:service_name' do
   begin
     content_type 'application/octet-stream'
     stream do |out|
+      begin
       engines_api.backup_service_data(params[:service_name], out)
+        rescue StandardError => e
+          STDERR.puts('engine_export_persistent_service exception ' + e.to_s)
+          send_encoded_exception(request: request, exception: e)
+        end
     end
   rescue StandardError => e
     send_encoded_exception(request: request, exception: e)
@@ -54,7 +74,12 @@ get '/v0/backup/engine/:engine_name' do
   begin
     content_type 'application/octet-stream'
     stream do |out|
+      begin
       engines_api.backup_engine_config(params[:engine_name], out)
+        rescue StandardError => e
+          STDERR.puts('engine_export_persistent_service exception ' + e.to_s)
+          send_encoded_exception(request: request, exception: e)
+        end
     end
   rescue StandardError => e
     send_encoded_exception(request: request, exception: e)
@@ -67,7 +92,12 @@ get '/v0/backup/engine/:engine_name/service/:publisher_namespace/*' do
     service_hash = engines_api.retrieve_engine_service_hash(hash)
     content_type 'application/octet-stream'
     stream do |out|
+      begin
       engines_api.backup_engine_service(service_hash, out)
+        rescue StandardError => e
+          STDERR.puts('engine_export_persistent_service exception ' + e.to_s)
+          send_encoded_exception(request: request, exception: e)
+        end
     end
   rescue StandardError => e
     send_encoded_exception(request: request, exception: e)
@@ -82,7 +112,12 @@ end
 get '/v0/backup/bundle_engine/:engine_name' do
   begin
     stream do |out|
+      begin
       engines_api.engine_bundle(params[:engine_name], out)
+        rescue StandardError => e
+          STDERR.puts('engine_export_persistent_service exception ' + e.to_s)
+          send_encoded_exception(request: request, exception: e)
+        end
     end
   rescue StandardError => e
     send_encoded_exception(request: request, exception: e)
