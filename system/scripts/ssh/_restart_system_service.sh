fc8ffb527ee57fefecd6f1ad69e0b321339c6ac2
James L Vodanovich <james@jvodan.com>
2018-10-09 16:05:18 +1100

    use  /opt/engines/bin/system_service.rb system restart

--- system/scripts/ssh/_restart_system_service.sh
+++ system/scripts/ssh/_restart_system_service.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
  touch /opt/engines/run/system/flags/engines_restarting
 
- /opt/engines/bin/system_service.rb system stop
- /opt/engines/bin/system_service.rb system wait_for stop 30
- /opt/engines/bin/system_service.rb system start
- /opt/engines/bin/system_service.rb system wait_for_start 25
-res=`/opt/engines/bin/system_service.rb system wait_for_startup 45`
- if test $res = 'false'
+ /opt/engines/bin/system_service.rb system restart
+ #/opt/engines/bin/system_service.rb system wait_for stop 30
+ #/opt/engines/bin/system_service.rb system start
+ #/opt/engines/bin/system_service.rb system wait_for_start 25
+r#es=`/opt/engines/bin/system_service.rb system wait_for_startup 45`
+ if test $? -ne 0
   then
    res=127
   else
