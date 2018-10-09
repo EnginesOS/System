84f2f16dab2984d48d3448a20802f2e1e782d802
James L Vodanovich <james@jvodan.com>
2018-10-09 17:26:26 +1100

    t

--- system/scripts/ssh/restart_system_service.sh
+++ system/scripts/ssh/restart_system_service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo /opt/engines/system/scripts/ssh/_restart_system_service.sh >/tmp/restart_system.log
- /opt/engines/system/scripts/ssh/_restart_system_service.sh >>/tmp/restart_system.log
+
+nohup /opt/engines/system/scripts/ssh/_restart_system_service.sh
