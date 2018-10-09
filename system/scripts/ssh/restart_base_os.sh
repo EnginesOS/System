d2fdf19949d99f2521c1182dcd8744916b6addef
James L Vodanovich <james@jvodan.com>
2018-10-09 15:33:01 +1100

    dont use nohup

--- system/scripts/ssh/restart_base_os.sh
+++ system/scripts/ssh/restart_base_os.sh
@@ -4,10 +4,10 @@ touch /opt/engines/run/system/flags/engines_rebooting
 touch /opt/engines/run/system/flags/engines_rebooted
 if ! test -f /opt/engines/etc/engines_reboot_disabled
  then
-	sudo -n /opt/engines/system/scripts/ssh/sudo/_restart_base_os.sh & 
+	sudo -n /opt/engines/system/scripts/ssh/sudo/_restart_base_os.sh 
 	exit
 else
-	 /opt/engines/system/scripts/ssh/restart_system_service.sh &
+	 /opt/engines/system/scripts/ssh/restart_system_service.sh 
 	rm -f /opt/engines/run/system/flags/engines_rebooting  
 	sleep 2
 fi
--- system/scripts/ssh/restart_system_service.sh
+++ system/scripts/ssh/restart_system_service.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-
-nohup /opt/engines/system/scripts/ssh/_restart_system_service.sh &
+/opt/engines/system/scripts/ssh/_restart_system_service.sh 
