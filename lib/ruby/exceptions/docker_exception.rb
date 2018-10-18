4e2bb11c1ce295d43f0a22b6b91cd90272bd8c80
James L Vodanovich <james@jvodan.com>
2018-10-18 15:22:50 +1100

    404 a warngin

--- lib/ruby/exceptions/docker_exception.rb
+++ lib/ruby/exceptions/docker_exception.rb
@@ -3,6 +3,7 @@ class DockerException < EnginesException
   def initialize(hash)
     SystemDebug.debug(SystemDebug.docker, 'Docker Exception', hash.to_s)
     @status = hash[:status] if hash.is_a?(Hash)   
+    r[:error_type] = :warning if @status == 404
     super(hash)
   end
 end
