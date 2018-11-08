5ff3faf69aa3f48a0e3db646c0b4ee5c7725b26a
James L Vodanovich <james@jvodan.com>
2018-11-08 12:57:08 +1100

    include verb

--- src/client/commands/service_manager/orphan_service.rb
+++ src/client/commands/service_manager/orphan_service.rb
@@ -7,7 +7,7 @@ if ARGV[2] == 'delete'
   @route +=   ARGV[3] + '/' + ARGV[4] + '/' + ARGV[5] + '/' + ARGV[6]
   perform_delete
 elsif ARGV[2] == 'export'
-  @route +=   ARGV[3] + '/' + ARGV[4] + '/' + ARGV[5]
+  @route +=  ARGV[2] + '/' +  ARGV[3] + '/' + ARGV[4] + '/' + ARGV[5]
 else
   @route +=   ARGV[2] + '/' + ARGV[3] + '/' + ARGV[4] 
 end
