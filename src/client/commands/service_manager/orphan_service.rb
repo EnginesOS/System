88edecbb8a20affde877010571dc185b28512f37
James L Vodanovich <james@jvodan.com>
2018-11-08 12:52:50 +1100

    f

--- src/client/commands/service_manager/orphan_service.rb
+++ src/client/commands/service_manager/orphan_service.rb
@@ -1,17 +1,15 @@
 @route += '/orphan_service/'
 STDERR.puts('Arg Count ' + ARGV.count.to_s)
 STDERR.puts('route ' + @route.to_s)
-if ARGV.count == 5
-  @route +=   ARGV[2] + '/' + ARGV[3] + '/' + ARGV[4]
-elsif ARGV.count == 6
-  @route +=   ARGV[2] + '/' + ARGV[3] + '/' + ARGV[4] + '/' + ARGV[5]
-end
+
+
 if ARGV[2] == 'delete'
   @route +=   ARGV[3] + '/' + ARGV[4] + '/' + ARGV[5] + '/' + ARGV[6]
   perform_delete
 elsif ARGV[2] == 'export'
   @route +=   ARGV[3] + '/' + ARGV[4] + '/' + ARGV[5]
-
+else
+  @route +=   ARGV[2] + '/' + ARGV[3] + '/' + ARGV[4] 
 end
 STDERR.puts('route ' + @route.to_s)
 perform_get
