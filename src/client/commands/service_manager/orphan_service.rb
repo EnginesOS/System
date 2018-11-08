4f34c5716dedd308cbf4cfb59ebd0e085078b7ef
James L Vodanovich <james@jvodan.com>
2018-11-08 12:25:08 +1100

    f

--- src/client/commands/service_manager/orphan_service.rb
+++ src/client/commands/service_manager/orphan_service.rb
@@ -3,15 +3,14 @@ STDERR.puts('Arg Count ' + ARGV.count.to_s)
 if ARGV.count == 5
   @route +=   ARGV[2] + '/' + ARGV[3] + '/' + ARGV[4]
 elsif ARGV.count == 6
-@route +=   ARGV[2] + '/' + ARGV[3] + '/' + ARGV[4] + '/' + ARGV[5]
-if ARGV[2] == 'delete'
-@route +=   ARGV[3] + '/' + ARGV[4] + '/' + ARGV[5] + '/' + ARGV[6]
-perform_delete
-elsif ARGV[2] == 'export'
-@route +=   ARGV[3] + '/' + ARGV[4] + '/' + ARGV[5] + '/' + ARGV[6]
-else
-  
+  @route +=   ARGV[2] + '/' + ARGV[3] + '/' + ARGV[4] + '/' + ARGV[5]
 end
-end 
+if ARGV[2] == 'delete'
+  @route +=   ARGV[3] + '/' + ARGV[4] + '/' + ARGV[5] + '/' + ARGV[6]
+  perform_delete
+elsif ARGV[2] == 'export'
+  @route +=   ARGV[3] + '/' + ARGV[4] + '/' + ARGV[5]
+
+end
 
 perform_get
