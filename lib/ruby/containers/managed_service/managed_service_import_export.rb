1bd72221095571e124e13f16fd34c72bb12f3d06
James L Vodanovich <james@jvodan.com>
2018-11-12 12:23:54 +1100

    fmt

--- lib/ruby/containers/managed_service/managed_service_import_export.rb
+++ lib/ruby/containers/managed_service/managed_service_import_export.rb
@@ -8,10 +8,10 @@ module ManagedServiceImportExport
   end
 
   def export_service_data(service_hash, out)
-    @container_api.export_service_data(self,service_hash, out)
+    @container_api.export_service_data(self, service_hash, out)
   end
 
   def import_service_data(service_hash, ins)
-    @container_api.import_service_data(self,service_hash, ins)
+    @container_api.import_service_data(self, service_hash, ins)
   end
 end
