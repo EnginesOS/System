6251fd2fdbecdd2d265ee2cae69396213e9e533b
james <james@engines.org>
2019-02-01 14:45:21 +1100

    resolve blueprint in cmd tool

--- src/client/commands/engine_builder/engine_builder.rb
+++ src/client/commands/engine_builder/engine_builder.rb
@@ -1,9 +1,11 @@
 
 @route += '/' + ARGV[0] + '/' + ARGV[1] 
-if ARGV[1] == 'follow'
+if ARGV[1] == 'resolve_blueprint'
+params = {blueprint_url:  ARGV[2]} 
+elsif ARGV[1] == 'follow'
   @route += '_stream'
   get_stream(@route)
 else
   @route += '/' + ARGV[2] if ARGV.count >= 3
-  perform_get
+  perform_get(params)
 end
