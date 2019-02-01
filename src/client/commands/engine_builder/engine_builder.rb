1294807633e16797986edc715077100fc86317ea
james <james@engines.org>
2019-02-01 14:57:00 +1100

    pass params

--- src/client/commands/engine_builder/engine_builder.rb
+++ src/client/commands/engine_builder/engine_builder.rb
@@ -1,7 +1,7 @@
 
 @route += '/' + ARGV[0] + '/' + ARGV[1] 
 if ARGV[1] == 'resolve_blueprint'
-  perform_get({blueprint_url:  ARGV[2]}) 
+  perform_get(70, {blueprint_url:  ARGV[2]}) 
 elsif ARGV[1] == 'follow'
   @route += '_stream'
   get_stream(@route)
