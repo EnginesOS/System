507e529047ae3743b03a930608157c471e96d4b1
james <james@engines.org>
2019-02-01 14:50:24 +1100

    call it

--- src/client/commands/engine_builder/engine_builder.rb
+++ src/client/commands/engine_builder/engine_builder.rb
@@ -1,11 +1,12 @@
 
 @route += '/' + ARGV[0] + '/' + ARGV[1] 
 if ARGV[1] == 'resolve_blueprint'
-params = {blueprint_url:  ARGV[2]} 
+  params = {blueprint_url:  ARGV[2]}
+  perform_get(params) 
 elsif ARGV[1] == 'follow'
   @route += '_stream'
   get_stream(@route)
 else
   @route += '/' + ARGV[2] if ARGV.count >= 3
-  perform_get(params)
+  perform_get
 end
