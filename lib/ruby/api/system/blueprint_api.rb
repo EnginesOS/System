1a28f90f67177d19916c162dd809361778a854a0
james <james@engines.org>
2019-02-06 13:06:53 +1100

    dbg

--- lib/ruby/api/system/blueprint_api.rb
+++ lib/ruby/api/system/blueprint_api.rb
@@ -84,7 +84,7 @@ class BlueprintApi < ErrorsApi
    # STDERR.puts('Parent BP ' + blueprint.to_s + "\n is a " + blueprint.class.name)
     STDERR.puts('dest BP ' + dest.to_s + "\n is a " + dest.class.name)
     STDERR.puts('key BP ' + key.to_s + " is a " + key.class.name)
-    STDERR.puts('dest software[' + key.to_s + ']' + dest[:software][key].to_s + "\nis a " +  dest[:software][key].class.name)
+    STDERR.puts('dest software[' + key.to_s + ']' + dest[:software].to_s  + "\nis a " +  dest[:software].class.name)
     unless key.is_a?(Array)
       if blueprint[:software].key?(key)
         if blueprint[:software][key].is_a?(Hash)
