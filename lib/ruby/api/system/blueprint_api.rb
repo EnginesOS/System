d3eedfb938aeb5620dfd12aad8db233cf9213af9
james <james@engines.org>
2019-02-11 09:45:46 +1100

    save bp as has

--- lib/ruby/api/system/blueprint_api.rb
+++ lib/ruby/api/system/blueprint_api.rb
@@ -131,7 +131,13 @@ class BlueprintApi < ErrorsApi
   def self.download_and_save_blueprint(basedir, repository_url)
     FileUtils.mkdir_p(basedir)
     d = basedir + '/' + File.basename(repository_url)
-    self.get_http_file(repository_url, d)
+    #self.get_http_file(repository_url, d)
+    download = open(url)
+    bp_str = download.read()
+    bp = symbolize_keys(bp_str)
+    f = File.new(d)
+    f.write (bp)
+    f.close
   end
 
   def self.get_http_file(url, d)
