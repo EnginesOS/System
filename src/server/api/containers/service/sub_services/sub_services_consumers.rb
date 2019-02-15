0f1e25e1fc25d6a55417908e9ba956610b7bd1a1
james <james@engines.org>
2019-02-15 19:16:22 +1100

    dbg

--- src/server/api/containers/service/sub_services/sub_services_consumers.rb
+++ src/server/api/containers/service/sub_services/sub_services_consumers.rb
@@ -9,8 +9,8 @@ get '/v0/containers/service/:service_name/sub_services' do
   begin
     STDERR.puts("\nparams " + params.to_s)
     #  opt_param = [:engine_name, :service_handle]
-    params = assemble_params(params, [:service_name], nil, [:engine_name, :service_handle])
-    return_json_array(engines_api.services_subservices(params))
+    cparams = assemble_params(params, [:service_name], nil, [:engine_name, :service_handle])
+    return_json_array(engines_api.services_subservices(cparams))
   rescue StandardError => e
     send_encoded_exception(request: request, exception: e)
   end
