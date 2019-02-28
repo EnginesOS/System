0fb0ef03b5aed5fdaabb66ce7dadcd680e0a54d9
james <james@rumah>
2019-02-28 15:03:16 +1100

    corrext variable name for python version

--- system/templates/deployment/python/build_scripts/install_python_modules.sh
+++ system/templates/deployment/python/build_scripts/install_python_modules.sh
@@ -3,5 +3,5 @@
      do
      mod=`echo $mod | sed "/[;&]/s///g"`
      python${python_ver} -m pip install --upgrade $mod
-     echo python${python_ver} -m pip install --upgrade $mod
+     echo python${python_version} -m pip install --upgrade $mod
      done
