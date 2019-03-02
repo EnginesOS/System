6d036ca9eb6d6bb0d3b69f14e83fa89bc3b26405
james <james@rumah>
2019-03-02 14:01:20 +1100

    varius arg count

--- src/client/commands/backup/backup.rb
+++ src/client/commands/backup/backup.rb
@@ -1,4 +1,5 @@
  
-@route += '/' + ARGV[0] + '/' + ARGV[1] + '/' + ARGV[2] + '/' + ARGV[3]
+@route += '/' + ARGV[0] + '/' + ARGV[1] + '/' + ARGV[2] 
+@route += '/' + ARGV[3] if ARGV.count > 4
 STDERR.puts("\nRoute " + @route.to_s)
 perform_get
