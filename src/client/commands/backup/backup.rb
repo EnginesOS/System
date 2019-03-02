b285c2f3f74b4a429fb05aadd5d46a9abe62b592
james <james@rumah>
2019-03-02 14:16:40 +1100

    fux

--- src/client/commands/backup/backup.rb
+++ src/client/commands/backup/backup.rb
@@ -1,8 +1,8 @@
  
 @route += '/' + ARGV[0] + '/' + ARGV[1] + '/' + ARGV[2] 
-@route += '/' + ARGV[3] if ARGV.count > 2
-@route += '/' + ARGV[4] if ARGV.count > 3
-@route += '/' + ARGV[5] if ARGV.count > 4
+@route += '/' + ARGV[3] if ARGV.count > 3
+@route += '/' + ARGV[4] if ARGV.count > 4
+@route += '/' + ARGV[5] if ARGV.count > 5
 STDERR.puts("\nRoute " + @route.to_s)
 perform_get
 get_stream(@route)
