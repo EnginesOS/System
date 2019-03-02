3492f2f02bd261256baaa53062d197a43269bf02
james <james@rumah>
2019-03-02 14:46:03 +1100

    support just one arg

--- src/client/commands/backup/backup.rb
+++ src/client/commands/backup/backup.rb
@@ -1,5 +1,6 @@
  
-@route += '/' + ARGV[0] + '/' + ARGV[1] + '/' + ARGV[2] 
+@route += '/' + ARGV[0] + '/' + ARGV[1] 
+@route += '/' + ARGV[2] if ARGV.count > 2
 @route += '/' + ARGV[3] if ARGV.count > 3
 @route += '/' + ARGV[4] if ARGV.count > 4
 @route += '/' + ARGV[5] if ARGV.count > 5
