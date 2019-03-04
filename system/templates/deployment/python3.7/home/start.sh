760a4000442ca5ccac99f7028a739d3d6caa4e37
james <james@rumah>
2019-03-04 13:35:30 +1100

    only create vitural env first time

--- system/templates/deployment/python3.7/home/start.sh
+++ system/templates/deployment/python3.7/home/start.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-virtualenv --system-site-packages /home/app/venv
  if test -f /home/app/venv/bin/activate
   then
    echo Activating virtual env
+   . /home/app/venv/bin/activate
+   else
+    virtualenv --system-site-packages /home/app/venv
+    echo Creating virtualenv
+    echo Activating virtual env
    . /home/app/venv/bin/activate
  fi
  echo Start Calling /home/_start.sh
