8a3fce585aed98fa5a510ddfb8f61854725a8b1c
james <james@rumah>
2019-03-19 09:08:28 +1100

    stderr to dev null

--- system/templates/services/backup/EnginesSystem/ldap_ou/backup.sh
+++ system/templates/services/backup/EnginesSystem/ldap_ou/backup.sh
@@ -10,7 +10,7 @@ SCRIPTPATH=`dirname $SCRIPT`
    exit -1
   fi 
   
-ldapsearch -D ${ldap_dn} -w ${ldap_password} -h ${LDAP_HOST} -b $ou_dn
+ldapsearch -D ${ldap_dn} -w ${ldap_password} -h ${LDAP_HOST} -b $ou_dn 2> /dev/null
 
 
 #ldapsearch -D ${ldap_dn} -w ${ldap_password} -h ${LDAP_HOST} -b ou=${ldap_ou},ou=${parent_engine},ou=Applications,ou=Containers,ou=Engines,dc=engines,dc=internal 
