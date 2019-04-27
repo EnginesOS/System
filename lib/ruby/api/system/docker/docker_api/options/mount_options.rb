503c909be9c4d6111a871afa5c3f5c94aa871c0f
James Vodanovich <james@jvodan.com>
2019-04-27 12:26:31 +1000

    user correct key

--- lib/ruby/api/system/docker/docker_api/options/mount_options.rb
+++ lib/ruby/api/system/docker/docker_api/options/mount_options.rb
@@ -145,9 +145,9 @@ def  mount_string_for_homes(home)
   s =''
     src_cname =  home[:parent_engine]
     src_ctype =  home[:container_type]
-if  home[:home_type] == 'all'
+if  home[:variables][:home_type] == 'all'
  # STDERR.puts('Secrets mount' +  '/var/lib/engines/secrets/' + src_ctype.to_s + 's/' +  src_cname.to_s + '/' + sh.to_s + ':/home/.secrets/'  + sh.to_s + ':ro')
-   s = '/var/lib/engines/home/:/home/users/:'  + home[:access]
+   s = '/var/lib/engines/home/:/home/users/:'  + home[:variables][:access]
 else
   STDERR.puts('serr ' + home.to_s)
 end
