fb9f8ea75e447175061a030e106eb8c3d007888d
James Vodanovich <james@jvodan.com>
2019-05-02 11:36:09 +1000

    dbg o

--- lib/ruby/engine_builder/builder/container_guids.rb
+++ lib/ruby/engine_builder/builder/container_guids.rb
@@ -1,11 +1,13 @@
 module ContainerGuids
   def set_container_guids
-    unless set_guids_from_orphan
+    unless set_guids_from_orphan.is_a?(TrueClass)
       unless @build_params[:permission_as].nil?
+        STDERR.puts('PERMISSION AS')
         @cont_user_id = @core_api.lookup_app_uid(@build_params[:permission_as])
         @data_uid = @core_api.lookup_app_duid(@build_params[:permission_as])
         @data_gid = @core_api.lookup_app_dgid(@build_params[:permission_as])
       else
+        STDERR.puts('NEW CONT ID')
         @cont_user_id = new_container_uid
         @data_uid = new_data_uid
         @data_gid = new_data_gid
@@ -18,8 +20,12 @@ module ContainerGuids
     @build_params[:attached_services].each do |service|
       next unless service[:create_type] == 'orphan'
         r = lookup_ids(service) if service[:create_type] == 'filesystem/local/filesystem'
-        break if r == true
+         if r == true
+           STDERR.puts('Got ids from orphan ' + service.to_s)
+           break
+         end
     end
+    STDERR.puts('Get ids from orphan status' + r.to_s)
     r
   end
   
@@ -28,7 +34,7 @@ module ContainerGuids
     service_hash = @core_api.retrieve_orphan(service)
    if service_hash.is_a?(Hash)
      if service_hash[:varaibles].is_a?(Hash)     
-      @cont_user_id = service_hash[:varaibles][]
+      @cont_user_id = service_hash[:varaibles][:fw_id]
       @data_uid = service_hash[:varaibles][:user]
       @data_gid = service_hash[:varaibles][:group]
       r = true
