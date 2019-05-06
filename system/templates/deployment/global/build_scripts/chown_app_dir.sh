ea627e56624f2065abce97d0de4b0cd24f66e552
James Vodanovich <james@jvodan.com>
2019-05-06 16:41:58 +1000

    create user if not

--- system/templates/deployment/global/build_scripts/chown_app_dir.sh
+++ system/templates/deployment/global/build_scripts/chown_app_dir.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 echo PACKAGE_INSTALLER_RUN $PACKAGE_INSTALLER_RUN
  
@@ -9,6 +9,54 @@ if [ ! -d /home/app ]
   
 mkdir -p /home/fs ; mkdir -p /home/fs/local 
 chown -R $ContUser /home/app /home/fs /home/fs/local   
+
+
+ set_permissions()
+{
+user=`cat /home/engines/etc/user/name`
+
+if test -f /home/engines/etc/user/files_post_install
+ then
+  for file in `cat /home/engines/etc/user/files_post_install`
+   do
+    if ! test -f $file
+     touch $file
+    fi
+    chown $user $file
+   done
+fi
+if test -f /home/engines/etc/user/dirs_post_install
+ then
+  for dir in  `cat /home/engines/etc/user/dirs_post_install`
+   do
+    mkdir -p $dir
+    chown -R $user $dir
+   done
+fi
+
+
+group=`cat /home/engines/etc/group/name`
+
+if test -f /home/engines/etc/group/files_post_install
+ then
+  for file in  `cat /home/engines/etc/group/files_post_install`
+   do
+    if ! test -f $file
+     touch $file
+    fi
+     
+    chown $group $file
+   done
+fi
+if test -f /home/engines/etc/group/dirs_post_install
+ then
+  for dir in  `cat /home/engines/etc/group/dirs_post_install`
+   do
+   mkdir -p $dir
+    chown -R $group $dir
+   done
+fi   
+}
 
 
  
---
+++ system/templates/deployment/node8.9/home/set_cont_user.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+setguids
+set_permissions
+
+set_guids()
+{
+user=`cat /home/engines/etc/user/name`
+getent passwd $user > /dev/null 2&>1
+ if test $? -eq 0
+  then
+	/usr/sbin/usermod -u $cont_uid $user
+ else
+   /usr/sbin/useradd -u $cont_uid -d /home/home_dir/ $user 
+ fi 	
+
+group=`cat /home/engines/etc/group/name`
+
+/usr/bin/getent group $group 2>&1 > /dev/null 
+if test $? -ne 0
+ then
+  /usr/sbin/groupadd -g $cont_uid $group
+else
+  /usr/sbin/groupmod -g $cont_uid $group
+fi
+
+
+/usr/sbin/usermod -u $data_uid data-user
+/usr/sbin/groupmod -g $data_gid data-user
+/usr/sbin/usermod -g data-user data-user 
+/usr/sbin/usermod -G data-user $user
+/usr/sbin/usermod -g $group $user
+/usr/sbin/usermod -G containers $user
+
+
+}
+
+set_permissions()
+{
+
+if test -f /home/engines/etc/user/files
+ then
+  for file in `cat /home/engines/etc/user/files`
+   do
+    if ! test -f $file
+     touch $file
+    fi
+    chown $user $file
+   done
+fi
+if test -f /home/engines/etc/user/dirs
+ then
+  for dir in  `cat /home/engines/etc/user/dirs`
+   do
+    mkdir -p $dir
+    chown -R $user $dir
+   done
+fi
+
+
+
+
+if test -f /home/engines/etc/group/files
+ then
+  for file in  `cat /home/engines/etc/group/files`
+   do
+    if ! test -f $file
+     touch $file
+    fi
+     
+    chown $group $file
+   done
+fi
+if test -f /home/engines/etc/group/dirs
+ then
+  for dir in  `cat /home/engines/etc/group/dirs`
+   do
+   mkdir -p $dir
+    chown -R $group $dir
+   done
+fi   
+}
+
+
