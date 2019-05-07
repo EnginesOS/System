c76f9e12e3ceac00c679e156a225e37318dc24d3
James Vodanovich <james@jvodan.com>
2019-05-07 17:33:21 +1000

    -debug

--- system/templates/deployment/global/home/_start.sh
+++ system/templates/deployment/global/home/_start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PID_FILE=/home/engines/run/engines.pid	
-echo Pids PID_FILE=$PID_FILE>&2
+
 export PID_FILE
 
 if test -f /home/engines/functions/trap.sh 
@@ -10,25 +10,25 @@ if test -f /home/engines/functions/trap.sh
  else
 . /home/trap.sh
 fi
-echo trap Pids PID_FILE=$PID_FILE
+
 . /home/engines/functions/start_functions.sh
-echo start_functionsPids PID_FILE=$PID_FILE>&2
+
 volume_setup
 dynamic_persistence
-echo Pids PID_FILE=$PID_FILE>&2
+
 if test -f /home/_init.sh
  then
    /home/_init.sh
 fi
-echo Pids PID_FILE=$PID_FILE>&2
+
 first_run
-echo Pids PID_FILE=$PID_FILE>&2
+
 restart_required
-echo Pids PID_FILE=$PID_FILE>&2
+
 pre_running
-echo Pids PID_FILE=$PID_FILE>&2
+
 custom_start
-echo Pids PID_FILE=$PID_FILE>&2
+
 
 touch  /home/engines/run/flags/started_once
 
@@ -37,28 +37,17 @@ if ! test -z $exit_start
   exit
 fi   
 
-echo started_oncePids PID_FILE=$PID_FILE >&2
 #for non apache framework (or use custom start)
 if test -f /home/engines/scripts/start/startwebapp.sh 
  then
    launch_app
 elif test -f /usr/sbin/apache2ctl
  then
- echo Pids PID_FILE=$PID_FILE
- ls -la /var/run/engines
- cat /var/run/engines/apache2.pid
-# if test -z $APACHE_PID_FILE
-#  then
-#  APACHE_PID_FILE=$PID_FILE
-# else
-#  PID_FILE=$APACHE_PID_FILE
-# fi
+
  export APACHE_PID_FILE 
- echo start_apachePids PID_FILE=$PID_FILE APACHE_PID_FILE=$APACHE_PID_FILE >&2
    start_apache
 elif test -d /etc/nginx
  then
- echo start_nginxPids PID_FILE=$PID_FILE APACHE_PID_FILE=$APACHE_PID_FILE >&2
    start_nginx	
 elif test -f /home/engines/scripts/blocking.sh 
   then
