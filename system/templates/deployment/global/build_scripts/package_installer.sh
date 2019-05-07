9fa7697c906bf0ffdedc85e9bbb7e580c960e61f
James Vodanovich <james@jvodan.com>
2019-05-07 14:53:27 +1000

    = ! ==

--- system/templates/deployment/global/build_scripts/package_installer.sh
+++ system/templates/deployment/global/build_scripts/package_installer.sh
@@ -58,7 +58,7 @@ echo Install to $destination
   fi
   
  destination=`echo $destination | sed "/\/$/s///"`
- if ! test "/home/app" == $destination  -o "app" == $destination  -o "/app" == $destination
+ if ! test "/home/app" = $destination  -o "app" = $destination  -o "/app" = $destination
   then
   	mkdir -p  "/home/app"
  fi
