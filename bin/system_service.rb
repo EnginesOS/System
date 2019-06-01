4320c5a81fe43f67eb6191fbe626b22892d31bea
James Vodanovich <james@jvodan.com>
2019-06-01 10:17:09 +1000

    f

--- bin/system_service.rb
+++ bin/system_service.rb
@@ -3,7 +3,9 @@
 require '/opt/engines/lib/ruby/api/system/engines_core/engines_core.rb'
 require 'thread'
 require 'yaml'
-
+#FIXME
+#need sinatra for indirecthash in deal_with_jason 
+require 'sinatra'
 core_api = EnginesCore.new
 
 system_api = core_api.system_api
