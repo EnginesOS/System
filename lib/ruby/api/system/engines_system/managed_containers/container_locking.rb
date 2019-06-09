fb0dea3eb13852b610790910b074a4a8005903d4
James Vodanovich <james@jvodan.com>
2019-06-09 13:52:15 +1000

    cut over to in mem conf file locking

--- lib/ruby/api/system/engines_system/managed_containers/container_locking.rb
+++ lib/ruby/api/system/engines_system/managed_containers/container_locking.rb
@@ -1,74 +1,87 @@
 module ContainerLocking
 
   @@lock_timeout  = 2
-  def lock_has_expired(lock_fn)
-    if File.mtime(lock_fn) <  Time.now + @@lock_timeout
-      true
-    else
-      false
-    end
-  rescue StandardError
-    true
-  end
-
+  #  def lock_has_expired(lock_fn)
+  #    if File.mtime(lock_fn) <  Time.now + @@lock_timeout
+  #      true
+  #    else
+  #      false
+  #    end
+  #  rescue StandardError
+  #    true
+  #  end
   def unlock_container_conf_file(state_dir)
+    STDERR.puts('Container ' + state_dir.to_s + 'unlocked' )
     @container_conf_locks.delete(state_dir) if @container_conf_locks.key?(state_dir)
-    File.delete(state_dir + '/lock') if  File.exists?(state_dir + '/lock')
+    #  File.delete(state_dir + '/lock') if  File.exists?(state_dir + '/lock')
   rescue StandardError
     false
   end
 
   def is_container_conf_file_locked?(state_dir)
     r = @container_conf_locks.key?(state_dir)
-    STDERR.puts('Container ' + state_dir.to_s + ' locked =' + r.to_s)
-
-    lock_fn = state_dir + '/lock'
-    if File.exists?(lock_fn)
-      loop = 0
-      while File.exists?(lock_fn)
-        sleep(0.2)
-        STDERR.puts('_container_conf_file_locked ')
-        loop != 1
-        break if loop > 5
-      end
-      File.exists?(lock_fn)
-    else
-      false
-    end
+    STDERR.puts('is Container ' + state_dir.to_s + ' locked =' + r.to_s)
+    r
+    #    lock_fn = state_dir + '/lock'
+    #    if File.exists?(lock_fn)
+    #      loop = 0
+    #      while File.exists?(lock_fn)
+    #        sleep(0.2)
+    #        STDERR.puts('_container_conf_file_locked ')
+    #        loop != 1
+    #        break if loop > 5
+    #      end
+    #      File.exists?(lock_fn)
+    #    else
+    #      false
+    #    end
   end
 
   def lock_container_conf_file(state_dir)
     if @container_conf_locks.key?(state_dir)
-      STDERR.puts('waiting_to_clr_container_conf_file_locked ' +  state_dir.to_s + "\n by:" + @container_conf_locks[state_dir])
+      thr = @container_conf_locks[state_dir]
+      STDERR.puts('waiting_to_clr_container_conf_file_locked ' +  state_dir.to_s + "\n by:" + thr.to_s)
+      begin
+        Timeout.timeout(@@lock_timeout) do
+          thr.join
+          log_error_mesg("lock holder thread returned ",state_dir)
+          true
+        end
+      rescue Timeout::Error
+        unlock_container_conf_file(state_dir)
+        log_error_mesg("Force cleared lock in ",state_dir)
+        false
+      end
     else
       @container_conf_locks[state_dir] = Thread.current
+      true
     end
-    lock_fn = state_dir + '/lock'
-    if  File.exists?(lock_fn) && lock_has_expired(lock_fn) == false
-      loop = 0
-      #FIXME use ioctl
-      while File.exists?(lock_fn)
-        sleep(0.2)
-        loop += 1
-        STDERR.puts('waiting_to_clr_container_conf_file_locked ')
-        if loop > 10
-          pid = File.read(lock_fn)
-          log_error_mesg("cleared lock in ",state_dir,' pid ',pid)
-          File.delete(lock_fn)
-          break
-        end
-      end
-    else
-      lock = File.new(lock_fn, File::CREAT | File::TRUNC | File::RDWR, 0644)
-      begin
-        lock.puts(Process.pid.to_s)
-      ensure
-        lock.close()
-      end
-    end
-    true
+    #    lock_fn = state_dir + '/lock'
+    #    if  File.exists?(lock_fn) && lock_has_expired(lock_fn) == false
+    #      loop = 0
+    #      #FIXME use ioctl
+    #      while File.exists?(lock_fn)
+    #        sleep(0.2)
+    #        loop += 1
+    #        STDERR.puts('waiting_to_clr_container_conf_file_locked ')
+    #        if loop > 10
+    #          pid = File.read(lock_fn)
+    #          log_error_mesg("cleared lock in ",state_dir,' pid ',pid)
+    #          File.delete(lock_fn)
+    #          break
+    #        end
+    #      end
+    #    else
+    #      lock = File.new(lock_fn, File::CREAT | File::TRUNC | File::RDWR, 0644)
+    #      begin
+    #        lock.puts(Process.pid.to_s)
+    #      ensure
+    #        lock.close()
+    #      end
+    #    end
+    #true
   rescue StandardError => e
-    log_error_mesg('locking exception', lock_fn,e)
+    log_error_mesg('locking exception', e)
     true
   end
 end
