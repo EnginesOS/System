0d42c550ff9cc1ae254f558bd8990966f3b63991
James Vodanovich <james@jvodan.com>
2019-06-21 12:32:25 +1000

    f

--- system/templates/deployment/global/engines/scripts/make_persistent.sh
+++ system/templates/deployment/global/engines/scripts/make_persistent.sh
@@ -11,15 +11,16 @@
 			dirname=`dirname "$path" `
 			mkdir -p "$VOLDIR/$dirname"
 			cp -rp "/home/app/$path" "$VOLDIR/$dirname"
-			rm -rf  /home/app/$path"
+			rm -rf  "/home/app/$path"
 			ln -s "$VOLDIR/$path" "/home/app/$path"
-		elif if test -f $path		
+		elif test -f $path	
+		  then	
 			dir=`dirname $path`
     		echo mkdir -p $VOLDIR/$dir
 			mkdir -p $VOLDIR/$dir
 			echo "mv /home/app/$path /$VOLDIR/$dir"
 			cp -rp /home/app/$path /$VOLDIR/$dir
-			rm -rf  /home/app/$path"
+			rm -rf  /home/app/$path
 			echo "ln -s  $VOLDIR/$path /home/app/$path"
 			ln -s  $VOLDIR/$path /home/app/$path
 		fi
