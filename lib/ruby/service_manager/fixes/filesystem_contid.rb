c5db6d160ed9367dae4dded9d425c8da0aafe48b
James Vodanovich <james@jvodan.com>
2019-07-06 10:57:46 +1000

    dbg

--- lib/ruby/service_manager/fixes/filesystem_contid.rb
+++ lib/ruby/service_manager/fixes/filesystem_contid.rb
@@ -15,14 +15,14 @@ module FileSystemContid
     STDERR.puts(' doing ' + engine.container_name.to_s)
     fs_hashs = system_registry_client.find_engine_services_hashes(params)
     unless fs_hashs.is_a?(Hash)
-      STDERR.puts(' got ' + fs_hashs.to_s )
+      STDERR.puts(' got ' + fs_hashs.class.name )
     else
       fs_hashs.each do | service_hash |
-        next if service_hash[:variables].key?(:fw_user)
+        #  next if service_hash[:variables].key?(:fw_user)
         service_hash[:variables][:fw_user] = engine.cont_user_id
         begin
           # system_registry_client.update_attached_service(service_hash)
-          STDERR.puts('H:' + service_hash.to_s )
+          STDERR.puts('H:' + service_hash[:variables][:fw_user].to_s )
         rescue StandardError => e
           STDERR.puts(e.to_s)
         end
