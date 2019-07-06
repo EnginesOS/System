51a92aba296ce9c871c281f5c9fe89e3977c9a8f
James Vodanovich <james@jvodan.com>
2019-07-06 11:35:54 +1000

    d

--- lib/ruby/service_manager/fixes/filesystem_contid.rb
+++ lib/ruby/service_manager/fixes/filesystem_contid.rb
@@ -5,6 +5,7 @@ module FileSystemContid
       STDERR.puts('DOING ' + engine.container_name.to_s)
       fix_fs_cont_id(engine)
     end
+    'DONE'
   end
 
   def fix_fs_cont_id(engine)
@@ -20,16 +21,17 @@ module FileSystemContid
     if fs_hashs.is_a?(Array)
       fs_hashs.each do | service_hash |
         #  next if service_hash[:variables].key?(:fw_user)
-       # service_hash[:variables][:fw_user] = engine.cont_user_id
+        service_hash[:variables][:fw_user] = engine.cont_user_id
         STDERR.puts('H:' + engine.cont_user_id.to_s)
         begin
-          # system_registry_client.update_attached_service(service_hash)
+           system_registry_client.update_attached_service(service_hash)
          # STDERR.puts('H:' + service_hash[:variables][:fw_user].to_s )
         rescue StandardError => e
           STDERR.puts(e.to_s)
         end
       end
       end
+      
   end
 
 end
