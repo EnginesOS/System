4bbc9667f80a32b89974b1d9a5c711ca6898f765
James Vodanovich <james@jvodan.com>
2019-07-06 10:50:54 +1000

    dbg

--- lib/ruby/service_manager/fixes/filesystem_contid.rb
+++ lib/ruby/service_manager/fixes/filesystem_contid.rb
@@ -1,31 +1,33 @@
 module FileSystemContid
-
   def fix_containers_fsid(engines)
     engines.each do |engine|
       fix_fs_cont_id(engine)
     end
   end
-  
+
   def fix_fs_cont_id(engine)
     params = {
-      parent_engine: engine.container_name, 
+      parent_engine: engine.container_name,
       container_type: engine.ctype,
-        publisher_namespace: 'EnginesSystem', 
-        type_path: 'filesystem/local/filesystem' 
+      publisher_namespace: 'EnginesSystem',
+      type_path: 'filesystem/local/filesystem'
     }
+    STDERR.puts(' doing ' + engine.container_name.to_s)
     fs_hashs = system_registry_client.find_engine_services_hashes(params)
-    fs_hashs.each do | service_hash |
-      next if service_hash[:variables].key?(:fw_user)
-      service_hash[:variables][:fw_user] = engine.cont_user_id
-     begin
-     # system_registry_client.update_attached_service(service_hash)
-       STDERR.puts('H:' + service_hash.to_s )
+    unless fs_hashs.is_a?(Hash)
+      STDERR.puts(' got ' + fs_hashs.to_s )
+    else
+      fs_hashs.each do | service_hash |
+        next if service_hash[:variables].key?(:fw_user)
+        service_hash[:variables][:fw_user] = engine.cont_user_id
+        begin
+          # system_registry_client.update_attached_service(service_hash)
+          STDERR.puts('H:' + service_hash.to_s )
         rescue StandardError => e
           STDERR.puts(e.to_s)
         end
+      end
     end
-    
-end
-
+  end
 
 end
