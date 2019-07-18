ad51db6c70054080d544941e443b11c9926090b2
James Vodanovich <james@jvodan.com>
2019-07-18 17:08:44 +1000

    timeous on docker exec

--- lib/ruby/api/system/engines_core/containers/docker_operations.rb
+++ lib/ruby/api/system/engines_core/containers/docker_operations.rb
@@ -1,6 +1,5 @@
 module DockerOperations
- # require '/opt/engines/lib/ruby/api/system/docker/docker_api.rb'
-
+  # require '/opt/engines/lib/ruby/api/system/docker/docker_api.rb'
   # @returns [Boolean]
   # whether pulled or no false if no new image
   def pull_image(image_name)
@@ -12,9 +11,18 @@ module DockerOperations
   end
 
   def exec_in_container(params)
-    params[:background] = false unless params.key?(:background)     
-         STDERR.puts('EXEC IN CONTAINER PARAMS ' + params.keys.to_s)
-    @docker_api.docker_exec(params)
+    params[:background] = false unless params.key?(:background)
+    STDERR.puts('EXEC IN CONTAINER PARAMS ' + params.keys.to_s)
+    STDERR.puts('Time Out ' + params[:timeout])
+    Timeout.timeout(params[:timeout] + 2) do # wait 1 sec longer incase another timeout prior
+      @docker_api.docker_exec(params)
+    end
+  rescue Timeout::Error
+    r = {}
+    r[:result] = -1;
+    r[:stderr] = 'Timeout on Docker exec :' + params[:command_line].to_s + ':' + result[:container].container_name.to_s
+    STDERR.puts(' Timeout ' + r.to_s)
+    raise EnginesException.new(warning_hash('Timeout on Docker exec', r))
   end
 
   def container_name_and_type_from_id(id)
