f74e976ef4b1224c5294d6b24155a3a9af28f0c1
James Vodanovich <james@jvodan.com>
2019-07-18 17:11:43 +1000

    d

--- lib/ruby/api/system/engines_core/containers/docker_operations.rb
+++ lib/ruby/api/system/engines_core/containers/docker_operations.rb
@@ -13,11 +13,12 @@ module DockerOperations
   def exec_in_container(params)
     params[:background] = false unless params.key?(:background)
     STDERR.puts('EXEC IN CONTAINER PARAMS ' + params.keys.to_s)
-    STDERR.puts('Time Out ' + params[:timeout])
+    STDERR.puts('Time Out ' + params[:timeout].to_s)
     Timeout.timeout(params[:timeout] + 2) do # wait 1 sec longer incase another timeout prior
       @docker_api.docker_exec(params)
     end
   rescue Timeout::Error
+    #FIX ME and kill process
     r = {}
     r[:result] = -1;
     r[:stderr] = 'Timeout on Docker exec :' + params[:command_line].to_s + ':' + result[:container].container_name.to_s
