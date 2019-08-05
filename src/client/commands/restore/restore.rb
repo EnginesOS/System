77c286a65b8bc33d9b387f49aa4fe32d0a6a6ffa
James Vodanovich <james@jvodan.com>
2019-08-05 15:28:36 +1000

    suporrt restore engine_bundle

--- src/client/commands/restore/restore.rb
+++ src/client/commands/restore/restore.rb
@@ -1,5 +1,11 @@
 @route += '/' + ARGV[0] + '/' + ARGV[1] + '/' + ARGV[2] + '/' + ARGV[3]
 
+n=0
+while n < ARGV.count
+  @route += '/' + ARGV[n]
+  n = n + 1
+end
+
 content_type='application/octet-stream'
 STDERR.puts  @route
 if ARGV.length == 5
