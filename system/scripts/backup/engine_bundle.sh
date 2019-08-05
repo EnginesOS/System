8dfa0d725ceb98a6d701352266d380edfcf91b92
James Vodanovich <james@jvodan.com>
2019-08-05 11:52:51 +1000

    leave bundir for debug

--- system/scripts/backup/engine_bundle.sh
+++ system/scripts/backup/engine_bundle.sh
@@ -4,11 +4,12 @@ if test -d /opt/engines/run/apps/$1
  then
   if test -d $BUNDLE_DIR
     then
-      tar -cpf - $BUNDLE_DIR /opt/engines/run/apps/$1 
-       if test $? -eq 0
-        then
-         rm -r $BUNDLE_DIR
-       fi 
+      tar -cpf - $BUNDLE_DIR /opt/engines/run/apps/$1
+      #UNCOMMENT FOR PRODUCTION 
+    #   if test $? -eq 0
+    #    then
+    #     rm -r $BUNDLE_DIR
+    #   fi 
   else
    echo '{"result":"error","mesg":"No bundle dir"}'
    exit 2
