89b8100e4d71a47679e39b40d36a3ebf9b914e18
James Vodanovich <james@jvodan.com>
2019-08-05 16:33:00 +1000

    f

--- system/scripts/backup/import_engine_bundle.sh
+++ system/scripts/backup/import_engine_bundle.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir -p /tmp/bundles
-
-cat - | sudo -n /opt/engine/system/scripts/backup/sudo/_import_engine_bundle.sh
+cat - > /tmp/t
+cat /tmp/t | sudo -n /opt/engine/system/scripts/backup/sudo/_import_engine_bundle.sh
 
