119310f18c4b33c1f520760bb535bd678995e7ec
James Vodanovich <james@jvodan.com>
2019-08-05 16:41:41 +1000

    correct path

--- system/scripts/backup/import_engine_bundle.sh
+++ system/scripts/backup/import_engine_bundle.sh
@@ -2,5 +2,5 @@
 
 mkdir -p /tmp/bundles
 cat - > /tmp/t
-cat /tmp/t | sudo -n /opt/engine/system/scripts/backup/sudo/_import_engine_bundle.sh
+cat /tmp/t | sudo -n /opt/engines/system/scripts/backup/sudo/_import_engine_bundle.sh
 
