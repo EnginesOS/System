20a48faa654edeb47217d31ad32b6a8d53b8d320
James Vodanovich <james@jvodan.com>
2019-08-08 15:33:04 +1000

    f

--- src/server/api/containers/service/export_import.rb
+++ src/server/api/containers/service/export_import.rb
@@ -29,7 +29,7 @@ end
 # import the service data gzip optional
 # data is streamed as application/octet-stream
 # @return [true]
-post '/v0/containers/service/:service_name/import' do
+put '/v0/containers/service/:service_name/import' do
   STDERR.puts('SIN IMPORT:' + request.to_s)
   begin
     service = get_service(params[:service_name])
