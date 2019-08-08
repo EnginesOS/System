29fb1b50090d8eb89d1a6ae5124a22c38349f7a2
James Vodanovich <james@jvodan.com>
2019-08-08 14:53:02 +1000

    f

--- src/server/api/containers/service/export_import.rb
+++ src/server/api/containers/service/export_import.rb
@@ -38,6 +38,8 @@ post '/v0/containers/service/:service_name/import' do
      r = request.env['rack.input'].read
      STDERR.puts('SIN IMPORT ' + r.length.to_s)
     STDERR.puts('SIN IMPORT ' + request.body.read)
+    sleep 2
+    STDERR.puts('SIN IMPORT ' + request.body.read)
   #  return_json(service.import_data(request.env['rack.input']))
     # return_json(service.import_data(request.body))
   rescue StandardError => e
