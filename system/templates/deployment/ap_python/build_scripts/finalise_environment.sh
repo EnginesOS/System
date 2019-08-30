7bb6f6ffeb5531a68e07366557e6c1d7af95d510
James Vodanovich <james@jvodan.com>
2019-08-30 12:54:11 +1000

    use tree spec and not manual here

--- system/templates/deployment/ap_python/build_scripts/finalise_environment.sh
+++ system/templates/deployment/ap_python/build_scripts/finalise_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-mkdir  -p /var/log/apache2/ 
-chown python /var/log/apache2/ 
-mkdir  -p /run/apache2/ 
-chown python /run/apache2/
+#mkdir  -p /var/log/apache2/ 
+#chown python /var/log/apache2/ 
+#mkdir  -p /run/apache2/ 
+#chown python /run/apache2/
 if ! test -d /home/app/venv
  then
  mkdir -p /home/app/venv
