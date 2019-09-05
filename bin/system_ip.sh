2fe92c0a14ed39f6c1d33e747889e7e09b70b9d1
James Vodanovich <james@jvodan.com>
2019-09-05 12:56:58 +1000

    match new ifconfig output

--- bin/system_ip.sh
+++ bin/system_ip.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 interface=`netstat -nr | grep ^0.0.0.0 | awk '{print $8}'`
-/sbin/ifconfig  $interface  |grep "inet " |awk '{print $2}'
+/sbin/ifconfig  $interface  |grep "inet " |awk '{print $2}' |cut -f2 -d:
