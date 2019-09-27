6ae17ea288d29a3801296e984e07e4936da66bc6
James Vodanovich <james@jvodan.com>
2019-09-28 09:49:37 +1000

    tdiy save_container and factor out actionators

--- lib/ruby/api/system/engines_system/managed_containers/containers.rb
+++ lib/ruby/api/system/engines_system/managed_containers/containers.rb
@@ -19,7 +19,8 @@ module Containers
 
     # BACKUP Current file with rename
     statefile = state_file(container, true) 
-    log_error_mesg('container locked', container.container_name) unless lock_container_conf_file(state_dir)
+    statedir =   container_state_dir(container)
+    log_error_mesg('container locked', container.container_name) unless lock_container_conf_file(statefile)
     backup_statefile(statefile)
     f = File.new(statefile, File::CREAT | File::TRUNC | File::RDWR, 0600) # was statefile + '_tmp
     begin
@@ -35,78 +36,23 @@ module Containers
     rescue StandardError => e
       ts = Time.now
     end
-    unlock_container_conf_file(state_dir)
+    unlock_container_conf_file(statedir)
     cache_engine(container, ts) unless cache_update_ts(container, ts)
     #STDERR.puts('saved ' + container.container_name + ':' + caller[1].to_s + ':' + caller[2].to_s)
     true
   rescue StandardError => e
-    unlock_container_conf_file(state_dir)
+    unlock_container_conf_file(statedir)
     container.last_error = last_error unless container.nil?
     # FIXME: Need to rename back if failure
     SystemUtils.log_exception(e)
   ensure
-    unlock_container_conf_file(state_dir)
+    unlock_container_conf_file(statedir)
   end
 
   def is_startup_complete?(container)
     File.exist?(container_state_dir(container) + '/run/flags/startup_complete')
   end
-
-  def write_actionators(container, actionators)
-    unless actionators.nil?
-      Dir.mkdir_p(actionator_dir(container)) unless Dir.exist?(actionator_dir(container))
-      serialized_object = YAML.dump(actionators)
-      f = File.new(actionator_dir(container) + '/actionators.yaml', File::CREAT | File::TRUNC | File::RDWR, 0644)
-      begin
-        f.puts(serialized_object)
-        f.flush()
-      ensure
-        f.close
-      end
-    end
-  end
-
-  def get_service_actionator(container, action)
-    actionators = load_service_actionators(container)
-    # STDERR.puts(' ACITONATORS ' + actionators.to_s)
-    # STDERR.puts('LOOKING 4 ' + action.to_s)
-    # STDERR.puts('is it ' + actionators[action.to_sym].to_s)
-    actionators[action.to_sym]
-  end
-
-  def load_service_actionators(container)
-    #    SystemDebug.debug(SystemDebug.actions, container, actionator_dir(container) + '/actionators.yaml')
-    #    if File.exist?(actionator_dir(container) + '/actionators.yaml')
-    #      yaml = File.read(actionator_dir(container) + '/actionators.yaml')
-    #      actionators = YAML::load(yaml)
-    #      SystemDebug.debug(SystemDebug.actions, container, actionators)
-    #      actionators if actionators.is_a?(Hash)
-    #    else
-    #      {}
-    #    end
-    SoftwareServiceDefinition.actionators({
-      type_path: container.type_path, publisher_namespace: container. publisher_namespace })
-  end
-
-  def get_engine_actionator(container, action)
-    actionators = load_engine_actionators(container)
-    #    SystemDebug.debug(SystemDebug.actions, container, actionators[action]) #.to_sym])
-    #  STDERR.puts('ACRTION ' + action.to_s)
-    actionators[action]
-  end
-
-  def load_engine_actionators(container)
-    #   SystemDebug.debug(SystemDebug.actions, container, actionator_dir(container) + '/actionators.yaml')
-    if File.exist?(actionator_dir(container) + '/actionators.yaml')
-      yaml = File.read(actionator_dir(container) + '/actionators.yaml')
-      actionators = YAML::load(yaml)
-      #     SystemDebug.debug(SystemDebug.actions,container ,actionators)
-      actionators if actionators.is_a?(Hash)
-    else
-      {}
-    end
-  end
-
+  
   private
 
   def backup_state_file(statefile)
@@ -134,7 +80,5 @@ module Containers
     state_dir + '/running.yaml'
   end
 
-  def state_dir
-    container_state_dir(container)
-  end
+ 
 end
