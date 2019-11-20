240292d4d42335c004dc7110f970d4086f5a3878
Mark Ratjens <mark@ratjens.com>
2019-11-20 15:40:32 +1100

    delete old shared volumes code

--- lib/ruby/service_manager/managed_services/private/old/shared_volumes.rb
+++
@@ -1,9 +0,0 @@
-def attach_shared_volume(shared_service)
-  #  STDERR.puts(' attach_shared_volume ' + shared_service.to_s)
-  begin
-  engine = core.loadManagedEngine(shared_service[:service_owner_handle])
-  rescue
-
-  end
-  true
-end
