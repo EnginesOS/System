14c894c426a0a64400bde17e0beec898cc49993c
James Vodanovich <james@jvodan.com>
2019-11-25 09:05:28 +1100

    update bootstrap configs for services to memento store

--- system/updates/to_run/engines/87/updater.sh
+++ system/updates/to_run/engines/87/updater.sh
@@ -2,24 +2,24 @@
 
 for file in `find /opt/engines/run/services -name running.yaml`
  do
-   cat $file |  sed "s/ManagedService/Memento/" > /tmp/running.yaml
+   cat $file |  sed "s/ManagedService/Memento/"  | sed "s/setState/set_state/"> /tmp/running.yaml
    cp /tmp/running.yaml $file 
  done 
  
  for file in `find /opt/engines/run/apps -name running.yaml`
  do
-   cat $file |  sed "s/ManagedEngine/Memento/" > /tmp/running.yaml
+   cat $file |  sed "s/ManagedEngine/Memento/" | sed "s/setState/set_state/"> /tmp/running.yaml
    cp /tmp/running.yaml $file 
  done 
  
- for file in `find /opt/engines/run/utility -name config.yaml`
+ for file in `find /opt/engines/run/utilitys -name config.yaml`
  do
-   cat $file |  sed "s/ManagedUtility/Memento/" > /tmp/running.yaml
+   cat $file |  sed "s/ManagedUtility/Memento/" | sed "s/setState/set_state/"> /tmp/running.yaml
    cp /tmp/running.yaml $file 
  done
  
   for file in `find /opt/engines/run/system_services -name running.yaml`
  do
-   cat $file |  sed "s/SystemService/Memento/" > /tmp/running.yaml
+   cat $file |  sed "s/SystemService/Memento/" | sed "s/setState/set_state/"> /tmp/running.yaml
    cp /tmp/running.yaml $file 
  done 
