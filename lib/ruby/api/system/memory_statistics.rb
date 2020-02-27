0efc09ab0916f836f1f781a89e80c7121d639c37
James Vodanovich <james@jvodan.com>
2020-02-27 22:32:12 +1100

    tidy

--- lib/ruby/api/system/memory_statistics.rb
+++ lib/ruby/api/system/memory_statistics.rb
@@ -51,8 +51,8 @@ module MemoryStatistics
     unless container.is_active?
       self.empty_container_result(container)
     else
-      if container && container.container_id.nil? == false && container.container_id != '-1'
-        path = SystemUtils.cgroup_mem_dir(container.container_id.to_s)
+      if container.container_id.nil? == false && container.container_id != '-1'
+        path = SystemUtils.cgroup_mem_dir(container.container_id)
         if Dir.exist?(path)
           ret_val = {
             maximum: File.read(path + '/memory.max_usage_in_bytes').to_i,
@@ -60,7 +60,7 @@ module MemoryStatistics
             limit: File.read(path + '/memory.limit_in_bytes').to_i
           }
         else
-          STDERR.puts('no_cgroup_file for ' + container.container_name + ':' + path.to_s, path)
+          STDERR.puts('no_cgroup_file for ' + container.container_name + ':' + path.to_s)
           SystemUtils.log_error_mesg('no_cgroup_file for ' + container.container_name + ':' + path.to_s, path)
           ret_val = self.empty_container_result(container)
         end
--- lib/ruby/system/system_utils.rb
+++ lib/ruby/system/system_utils.rb
@@ -198,8 +198,7 @@ class SystemUtils
 
   def SystemUtils.cgroup_mem_dir(container_id_str)
     if SystemUtils.get_os_release_data['Major Version'] == '14' ||  SystemUtils.get_os_release_data['Major Version'] == '18'
-      
-      "/sys/fs/cgroup/memory/docker/#{container_id_str}/"
+        "/sys/fs/cgroup/memory/docker/#{container_id_str}"
     elsif Dir.exist?("/sys/fs/cgroup/memory/docker/${container_id_str}/")
     "/sys/fs/cgroup/memory/docker/#{container_id_str}/"
     else
