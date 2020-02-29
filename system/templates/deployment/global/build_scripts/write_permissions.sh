314534ab2f893af14148f7528faa55068a12667c
James Vodanovich <james@jvodan.com>
2020-02-29 19:14:49 +1100

    db

--- system/templates/deployment/global/build_scripts/write_permissions.sh
+++ system/templates/deployment/global/build_scripts/write_permissions.sh
@@ -6,13 +6,14 @@
    path=`echo $path | sed "/\/$/s///"`
    path=`echo $path | sed "/^\/home\/app/s///"`
    echo Path $path
+   whoami
    ls -la /home/app/`dirname $path`
    ls -la /home/
     ls -la /home/app/
      if [ -h  /home/app/$path ] 
       then
   		dest=`ls -la /home/app/$path |cut -f2 -d'>'`
-  		echo dest = $dest
+  		echo  chmod -R gu+rw $dest
   		chmod -R gu+rw $dest
      elif test -d  /home/app/$path 
   	  then
@@ -23,9 +24,13 @@
   		echo mkdir -p  `dirname /home/app/$path`
    		mkdir -p  `dirname /home/app/$path`
    		ls  -la /home/app/$path 
+   		echo touch  /home/app/$path 
    		touch  /home/app/$path 
  	 fi
+ 	 echo chown $ContUser /home/app/$path
+ 	 echo chmod  ug+rw /home/app/$path   
    chown $ContUser /home/app/$path
    chmod  ug+rw /home/app/$path   
 done    
+exit 0
         
