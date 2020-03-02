a8e3b7b10bfa6ee32b48e57d6efab9a910338737
James Vodanovich <james@jvodan.com>
2020-03-02 20:58:27 +1100

    f

--- system/scripts/ssh/sudo/_set_hostname.sh
+++ system/scripts/ssh/sudo/_set_hostname.sh
@@ -12,3 +12,10 @@ if ! test -z $domain_name
 fi
 echo params: $params >/tmp/set_hostname
 echo hostname $hostname.$domain_name >> /tmp/set_hostname
+
+ip=`/opt/engines/bin/system_ip.sh`
+
+cat /etc/hosts | grep -v $ip >/tmp/hosts
+cp /tmp/hosts/ /etc/hosts
+echo $ip $hostname $hostname.$domain_name  >> /etc/hosts
+ 
