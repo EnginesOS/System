b435ad38b3db2a0e1419f6c886e6a49f5ce825dd
James Vodanovich <james@jvodan.com>
2020-06-15 15:15:57 +1000

    dbg

--- system/scripts/ssh/restart_base_os.sh
+++ system/scripts/ssh/restart_base_os.sh
@@ -6,7 +6,7 @@ touch /opt/engines/run/system/flags/engines_rebooted
 if ! test -f /opt/engines/etc/engines_reboot_disabled
  then
 	#sudo -n /opt/engines/system/scripts/ssh/sudo/_restart_base_os.sh &
-	sleep 110
+	sleep 10
 	exit
 else
 	 /opt/engines/system/scripts/ssh/restart_system_service.sh 
