83854a22b8b0a32debb7254f71d961cc6c8c9f66
James Vodanovich <james@jvodan.com>
2020-06-16 16:09:47 +1000

    correct sh syntax

--- system/scripts/ssh/update_base_os.sh
+++ system/scripts/ssh/update_base_os.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 touch /opt/engines/run/system/flags/update_running
 tsdate=`date  +%d_%m_%y_%H:%M`
-sudo  -n /opt/engines/system/scripts/ssh/sudo/_update_base_os.sh &> /var/log/engines/updates/base_os_update.running
+sudo  -n /opt/engines/system/scripts/ssh/sudo/_update_base_os.sh > /var/log/engines/updates/base_os_update.running  2>&1
 
 mv /var/log/engines/updates/base_os_update.running /var/log/engines/updates/base_os_update_$tsdate.log
 rm /opt/engines/run/system/flags/update_running
