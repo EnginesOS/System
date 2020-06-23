7bed2eb2336eb67fe781eff487abefa2daf17481
James Vodanovich <james@jvodan.com>
2020-06-23 17:09:47 +1000

    try

--- src/server/api/containers/service/export_import.rb
+++ src/server/api/containers/service/export_import.rb
@@ -41,6 +41,27 @@ end
 # import the service data gzip optional
 # data is streamed as application/octet-stream
 # @return [true]
+post '/v0/containers/service/:service_name/import' do
+  STDERR.puts('SIN IMPORT post:' + request.to_s)
+  begin
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    service = get_service(params[:service_name])
+    #  STDERR.puts('SIN IMPORT:' + request.to_s)
+    #  STDERR.puts('SIN IMPORT params' + params.to_s)
+    #   STDERR.puts('SIN IMPORT ' + request.body.read)
+
+    r = request.env['rack.input']
+    STDERR.puts('SIN IMPORT ' + r.class.name)
+    return_json(service.import_data(request.env['rack.input']))
+
+  rescue StandardError => e
+    send_encoded_exception(request: request, exception: e)
+  end
+# @method engine_import_persistent_service_data
+# @overload put '/v0/containers/service/:service_name/import'
+# import the service data gzip optional
+# data is streamed as application/octet-stream
+# @return [true]
 put '/v0/containers/service/:service_name/import' do
   STDERR.puts('SIN IMPORT:' + request.to_s)
   begin
