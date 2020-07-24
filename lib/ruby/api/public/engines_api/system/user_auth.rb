e26c2bac4e1084bec770de08b79fc75d73c79114
James Vodanovich <james@jvodan.com>
2020-07-24 14:01:32 +1000

    fdmy

--- lib/ruby/api/public/engines_api/system/user_auth.rb
+++ lib/ruby/api/public/engines_api/system/user_auth.rb
@@ -1,22 +1,21 @@
-class PublicApi 
-
-def set_user_password(params)
-  ldap = bind_as_user(params[:user_name])
+class PublicApi
+  def set_user_password(params)
+    ldap = bind_as_user(params[:user_name])
     #set_user_password returns json for the gui to consume
     set_user_password(ldap, params)
     '{"result":"ok"}'
-end 
- 
+  end
+
   def is_admin_token_valid?(token, ip = nil)
     core.is_admin_token_valid?(token, ip = nil)
   end
-def bind_as_user(params)
-  
-end
 
-def set_user_password(ldap, params)
-  
-end
+  def bind_as_user(params)
 
+  end
+
+  def set_user_password(ldap, params)
+
+  end
 
 end
