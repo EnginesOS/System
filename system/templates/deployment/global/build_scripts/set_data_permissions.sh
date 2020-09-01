31fa1de3f52a94956d52841330fc6ae5abd80cf6
James Vodanovich <james@jvodan.com>
2020-09-02 06:32:08 +1000

    update all service images

--- system/templates/deployment/global/build_scripts/set_data_permissions.sh
+++ system/templates/deployment/global/build_scripts/set_data_permissions.sh
@@ -8,4 +8,13 @@ id data-user | cut -f2 -d: |grep $data_uid >/dev/null
 chown -R $data_uid.$data_gid /home/app 
 chmod -R 774 /home/fs_src
 chmod g+rx ` find /home/fs_src -type d`
-#chmod g+rx ` find /home/app -type d`
+
+chmod -R g+r /home/app
+ 	for dir in  `find /home/app -type d | sed "/ /s/+_+//g"` 
+	  do
+		if test `echo $dir |grep _+_ |wc -l ` -lt 1
+			then 
+				chmod g+x  $dir 
+		fi
+	  done
+  
