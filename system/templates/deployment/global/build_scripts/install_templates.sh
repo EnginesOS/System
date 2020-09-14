354d338d1c6b049d361530261852c8b923fe491a
James Vodanovich <james@jvodan.com>
2020-09-14 16:20:03 +1000

    debug

--- system/templates/deployment/global/build_scripts/install_templates.sh
+++ system/templates/deployment/global/build_scripts/install_templates.sh
@@ -11,6 +11,9 @@ templates=`find /home/engines/templates/ -type f |grep -v keep_me`
      dest_file=`echo $file | sed "/^.*templates\//s///"`
       dest_dir=`dirname $dest_file`
         echo $dest_file in $dest_dir      
+        ls -l  $dest_file
+        echo t
+        ls -l $dest_dir 
       mkdir -p $dest_dir
        # If soft link copy to destination  
         if test -h $dest_file
